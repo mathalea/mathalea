dd12c26e6994e4b8db42d48b1159c8d02b0afd71
remiangot <remi.angot@gmail.com>
2020-02-05 21:51:18 +0100

    Mise à jour 4N20

--- include/mathalea_exercices_4e.js
+++ include/mathalea_exercices_4e.js
@@ -2169,13 +2169,13 @@ function Problemes_Pythagore(){
 /**
 * Puissances d'un relatif (1)
 * * Travailler des résultats automatisés 
-* * Utiliser les propriétés du produit de puissance, du quotient de puissances et des puissances de puissances
+* * mais aussi d'utiliser les propriétés du produit de puissance, du quotient de puissances et des puissances de puissances
 * @auteur Sébastien Lozano
 */
 function Puissances_d_un_relatif_1(){
 	Exercice.call(this); // Héritage de la classe Exercice()
 	this.sup = 1 ; 
-	this.titre = "Puissances : calculs automatisés et règles de calculs"; 
+	this.titre = "Puissances : Calculs automatisés et règles de calculs"; 
 	this.consigne = "Écrire sous la forme $\\mathbf{a^n}$.";
 	this.spacing = 2;
 	this.spacing_corr = 2;
@@ -2186,13 +2186,14 @@ function Puissances_d_un_relatif_1(){
 	function testExp(b,e) {
 		switch (e) {
 			case 1 : 
-				return `$ = ${b}$`;
+				return ` = ${b}$`;
 				break;
 			case 0 : 
-				return `$ = 1$`;
+				return ` = 1$`;
 				break;
 			default : 
-				return ``;						
+				//return `$ = ${b}^{${e}}$`;						
+				return `$`;
 		};
 	}
 
@@ -2200,7 +2201,7 @@ function Puissances_d_un_relatif_1(){
 		this.liste_questions = []; // Liste de questions
 		this.liste_corrections = []; // Liste de questions corrigées
 
-		let type_de_questions_disponibles = [1,2,3,4,5,6,7,8];
+		let type_de_questions_disponibles = [1,2,3,4,5,6,7,8];	
 		let liste_type_de_questions = combinaison_listes(type_de_questions_disponibles,this.nb_questions);
 
 		for (let i = 0, base ,exp , texte, texte_corr, cpt=0; i < this.nb_questions&&cpt<50;) {
@@ -2211,12 +2212,12 @@ function Puissances_d_un_relatif_1(){
 					base = 3; // on travaille sur cette base mais on pourrait rendre la base aléatoire
 					exp = [randint(1,7,[1]),randint(1,7,[1]),randint(1,7,[1])]; // on a besoin de 3 exposants distincts
 					texte = `$\\dfrac{${base}^${exp[0]}\\times ${base*base}}{${base}^${exp[1]} \\times ${base}^${exp[2]}}$`;
-					texte_corr = `$\\dfrac{${base}^${exp[0]}\\times ${base*base}}{${base}^${exp[1]} \\times ${base}^${exp[2]}} $`;
-					texte_corr += `$ = \\dfrac{${base}^${exp[0]}\\times ${base}^{2}}{${base}^${exp[1]} \\times ${base}^${exp[2]}}$`;
-					texte_corr += `$ = \\dfrac{${base}^{${exp[0]}+2}}{${base}^{${exp[1]}+${exp[2]}}}$`;
-					texte_corr += `$ = \\dfrac{${base}^{${exp[0]+2}}}{${base}^{${exp[1]+exp[2]}}}$`;
-					texte_corr += `$ = ${base}^{${exp[0]+2}-${exp[1]+exp[2]}}$`;					
-					texte_corr += `$ = ${base}^{${exp[0]+2-exp[1]-exp[2]}}$`;
+					texte_corr = `$\\dfrac{${base}^${exp[0]}\\times ${base*base}}{${base}^${exp[1]} \\times ${base}^${exp[2]}}`;
+					texte_corr += ` = \\dfrac{${base}^${exp[0]}\\times ${base}^{2}}{${base}^${exp[1]} \\times ${base}^${exp[2]}}`;
+					texte_corr += ` = \\dfrac{${base}^{${exp[0]}+2}}{${base}^{${exp[1]}+${exp[2]}}}`;
+					texte_corr += ` = \\dfrac{${base}^{${exp[0]+2}}}{${base}^{${exp[1]+exp[2]}}}`;
+					texte_corr += ` = ${base}^{${exp[0]+2}-${exp[1]+exp[2]}}`;					
+					texte_corr += ` = ${base}^{${exp[0]+2-exp[1]-exp[2]}}`;
 					// on ne teste l'exposant que pour la sortie puisque l'exposant 1 est évincé
 					texte_corr += testExp(base,exp[0]+2-exp[1]-exp[2]);
 					break;
@@ -2224,12 +2225,12 @@ function Puissances_d_un_relatif_1(){
 					base = 2; // on travaille sur cette base mais on pourrait rendre la base aléatoire
 					exp = [randint(1,7,[1]),randint(1,7,[1])]; // on a besoin de 2 exposants distincts
 					texte = `$\\dfrac{${base}^${exp[0]}\\times ${base**3}}{${base}^${exp[1]}}$`;
-					texte_corr = `$\\dfrac{${base}^${exp[0]}\\times ${base**3}}{${base}^${exp[1]}}$`;
-					texte_corr += `$=\\dfrac{${base}^${exp[0]}\\times ${base}^3}{${base}^${exp[1]}}$`;
-					texte_corr += `$=\\dfrac{${base}^{${exp[0]}+3}}{${base}^${exp[1]}}$`;
-					texte_corr += `$=\\dfrac{${base}^{${exp[0]+3}}}{${base}^${exp[1]}}$`;
-					texte_corr += `$=${base}^{${exp[0]+3}-${exp[1]}}$`;
-					texte_corr += `$=${base}^{${exp[0]+3-exp[1]}}$`;
+					texte_corr = `$\\dfrac{${base}^${exp[0]}\\times ${base**3}}{${base}^${exp[1]}}`;
+					texte_corr += ` = \\dfrac{${base}^${exp[0]}\\times ${base}^3}{${base}^${exp[1]}}`;
+					texte_corr += ` = \\dfrac{${base}^{${exp[0]}+3}}{${base}^${exp[1]}}`;
+					texte_corr += ` = \\dfrac{${base}^{${exp[0]+3}}}{${base}^${exp[1]}}`;
+					texte_corr += ` = ${base}^{${exp[0]+3}-${exp[1]}}`;
+					texte_corr += ` = ${base}^{${exp[0]+3-exp[1]}}`;
 					// on ne teste l'exposant que pour la sortie puisque l'exposant 1 est évincé
 					texte_corr += testExp(base,exp[0]+3-exp[1]);
 					break;
@@ -2239,17 +2240,17 @@ function Puissances_d_un_relatif_1(){
 					// le second exposant ne peut valoir que 1 ou 2 la fonction testExp ne convient pas à l'affichage ici					
 					if (exp[1]==2) {
 						texte = `$\\dfrac{${base}\\times ${base}^${exp[0]}}{${base**2}^${exp[1]}}$`;
-						texte_corr = `$\\dfrac{${base}\\times ${base}^${exp[0]}}{${base**2}^${exp[1]}}$`;
-						texte_corr += `$=\\dfrac{${base}^{1+${exp[0]}}}{(${base}^2)^${exp[1]}}$`;
-						texte_corr += `$=\\dfrac{${base}^{1+${exp[0]}}}{${base}^{2 \\times ${exp[1]}}}$`;
-						texte_corr += `$=\\dfrac{${base}^{${1+exp[0]}}}{${base}^{${2*exp[1]}}}$`;
-					} else {
+						texte_corr = `$\\dfrac{${base}\\times ${base}^${exp[0]}}{${base**2}^${exp[1]}}`;
+						texte_corr += `=\\dfrac{${base}^{1+${exp[0]}}}{(${base}^2)^${exp[1]}}`;
+						texte_corr += `=\\dfrac{${base}^{1+${exp[0]}}}{${base}^{2 \\times ${exp[1]}}}`;
+						texte_corr += `=\\dfrac{${base}^{${1+exp[0]}}}{${base}^{${2*exp[1]}}}`;
+					} else {						
 						texte = `$\\dfrac{${base}\\times ${base}^${exp[0]}}{${base**2}}$`;
-						texte_corr = `$\\dfrac{${base}\\times ${base}^${exp[0]}}{${base**2}}$`;
-						texte_corr += `$=\\dfrac{${base}^{1+${exp[0]}}}{${base}^2}$`;
+						texte_corr = `$\\dfrac{${base}\\times ${base}^${exp[0]}}{${base**2}}`;
+						texte_corr += `=\\dfrac{${base}^{1+${exp[0]}}}{${base}^2}`;
 					};
-					texte_corr += `$=${base}^{${1+exp[0]}-${2*exp[1]}}$`;
-					texte_corr += `$=${base}^{${1+exp[0]-2*exp[1]}}$`;
+					texte_corr += `=${base}^{${1+exp[0]}-${2*exp[1]}}`;
+					texte_corr += `=${base}^{${1+exp[0]-2*exp[1]}}`;
 					// on ne teste l'exposant que pour la sortie puisque l'exposant 1 est évincé
 					texte_corr += testExp(base,1+exp[0]-2*exp[1]);
 					break;
@@ -2257,12 +2258,12 @@ function Puissances_d_un_relatif_1(){
 					base = 2; // on travaille sur cette base mais on pourrait rendre la base aléatoire
 					exp = [randint(1,7,[1])]; // on a besoin de 1 exposant
 					texte = `$\\dfrac{${base}\\times ${base}^${exp[0]}}{${base**2}\\times ${base**2}}$`;
-					texte_corr = `$\\dfrac{${base}\\times ${base}^${exp[0]}}{${base**2}\\times ${base**2}}$`;
-					texte_corr += `$=\\dfrac{${base}^{1+${exp[0]}}}{${base}^2\\times ${base}^2}$`;
-					texte_corr += `$=\\dfrac{${base}^{${1+exp[0]}}}{${base}^{2+2}}$`;
-					texte_corr += `$=\\dfrac{${base}^{${1+exp[0]}}}{${base}^{${2+2}}}$`;
-					texte_corr += `$=${base}^{${1+exp[0]}-${2+2}}$`;					
-					texte_corr += `$=${base}^{${1+exp[0]-2-2}}$`;
+					texte_corr = `$\\dfrac{${base}\\times ${base}^${exp[0]}}{${base**2}\\times ${base**2}}`;
+					texte_corr += `=\\dfrac{${base}^{1+${exp[0]}}}{${base}^2\\times ${base}^2}`;
+					texte_corr += `=\\dfrac{${base}^{${1+exp[0]}}}{${base}^{2+2}}`;
+					texte_corr += `=\\dfrac{${base}^{${1+exp[0]}}}{${base}^{${2+2}}}`;
+					texte_corr += `=${base}^{${1+exp[0]}-${2+2}}`;					
+					texte_corr += `=${base}^{${1+exp[0]-2-2}}`;
 					// on ne teste l'exposant que pour la sortie puisque l'exposant 1 est évincé
 					texte_corr += testExp(base,1+exp[0]-2-2);
 					break;
@@ -2270,12 +2271,12 @@ function Puissances_d_un_relatif_1(){
 					base = 2; // on travaille sur cette base mais on pourrait rendre la base aléatoire
 					exp = [randint(1,7,[1])]; // on a besoin de 1 exposant
 					texte = `$\\dfrac{${base**2}^${exp[0]}}{${base}}$`;
-					texte_corr = `$\\dfrac{${base**2}^${exp[0]}}{${base}}$`;
-					texte_corr += `$=\\dfrac{(${base}^2)^${exp[0]}}{${base}}$`;
-					texte_corr += `$=\\dfrac{${base}^{2\\times ${exp[0]}}}{${base}}$`;
-					texte_corr += `$=\\dfrac{${base}^{${2*exp[0]}}}{${base}}$`;
-					texte_corr += `$=${base}^{${2*exp[0]}-1}$`;					
-					texte_corr += `$=${base}^{${2*exp[0]-1}}$`;
+					texte_corr = `$\\dfrac{${base**2}^${exp[0]}}{${base}}`;
+					texte_corr += `=\\dfrac{(${base}^2)^${exp[0]}}{${base}}`;
+					texte_corr += `=\\dfrac{${base}^{2\\times ${exp[0]}}}{${base}}`;
+					texte_corr += `=\\dfrac{${base}^{${2*exp[0]}}}{${base}}`;
+					texte_corr += `=${base}^{${2*exp[0]}-1}`;					
+					texte_corr += `=${base}^{${2*exp[0]-1}}$`;
 					// Inutile de tester l'exposant final car il vaut au minimum 3
 					// texte_corr += testExp(base,2*exp[0]-1);
 					break;
@@ -2283,12 +2284,12 @@ function Puissances_d_un_relatif_1(){
 					base = 3; // on travaille sur cette base mais on pourrait rendre la base aléatoire
 					exp = [randint(1,3,[1])]; // on a besoin de 1 exposant
 					texte = `$\\dfrac{${base**3}^${exp[0]}}{${base}}$`;
-					texte_corr = `$\\dfrac{${base**3}^${exp[0]}}{${base}}$`;
-					texte_corr += `$=\\dfrac{(${base}^3)^${exp[0]}}{${base}}$`;
-					texte_corr += `$=\\dfrac{${base}^{3\\times ${exp[0]}}}{${base}}$`;
-					texte_corr += `$=\\dfrac{${base}^{${3*exp[0]}}}{${base}}$`;
-					texte_corr += `$=${base}^{${3*exp[0]}-1}$`;
-					texte_corr += `$=${base}^{${3*exp[0]-1}}$`;
+					texte_corr = `$\\dfrac{${base**3}^${exp[0]}}{${base}}`;
+					texte_corr += `=\\dfrac{(${base}^3)^${exp[0]}}{${base}}`;
+					texte_corr += `=\\dfrac{${base}^{3\\times ${exp[0]}}}{${base}}`;
+					texte_corr += `=\\dfrac{${base}^{${3*exp[0]}}}{${base}}`;
+					texte_corr += `=${base}^{${3*exp[0]}-1}`;
+					texte_corr += `=${base}^{${3*exp[0]-1}}$`;
 					// inutile de tester l'exposant final car il vaut au minimum 5 
 					// texte_corr += testExp(base,3*exp[0]-1);
 					break;
@@ -2296,15 +2297,15 @@ function Puissances_d_un_relatif_1(){
 					base = 3; // on travaille sur cette base mais on pourrait rendre la base aléatoire
 					exp = [randint(1,7,[1]),randint(1,7,[1]),randint(1,4,[1])]; // on a besoin de 3 exposants distincts
 					texte = `$\\dfrac{${base}^${exp[0]}\\times ${base}^${exp[1]}}{${base**2}^${exp[2]}}\\times ${base}$`;
-					texte_corr = `$\\dfrac{${base}^${exp[0]}\\times ${base}^${exp[1]}}{${base**2}^${exp[2]}}\\times ${base}$`;
-					texte_corr += `$=\\dfrac{${base}^{${exp[0]}+${exp[1]}}}{(${base}^2)^${exp[2]}}\\times ${base}$`;
-					texte_corr += `$=\\dfrac{${base}^{${exp[0]+exp[1]}}}{${base}^{2\\times ${exp[2]}}}\\times ${base}$`;
-					texte_corr += `$=\\dfrac{${base}^{${exp[0]+exp[1]}}}{${base}^{${2*exp[2]}}}\\times ${base}$`;
-					texte_corr += `$=\\dfrac{${base}^{${exp[0]+exp[1]}}\\times ${base}}{${base}^{${2*exp[2]}}}$`;
-					texte_corr += `$=\\dfrac{${base}^{${exp[0]+exp[1]}+1}}{${base}^{${2*exp[2]}}}$`;
-					texte_corr += `$=\\dfrac{${base}^{${exp[0]+exp[1]+1}}}{${base}^{${2*exp[2]}}}$`;
-					texte_corr += `$=${base}^{${exp[0]+exp[1]+1}-${2*exp[2]}}$`;					
-					texte_corr += `$=${base}^{${exp[0]+exp[1]+1-2*exp[2]}}$`;
+					texte_corr = `$\\dfrac{${base}^${exp[0]}\\times ${base}^${exp[1]}}{${base**2}^${exp[2]}}\\times ${base}`;
+					texte_corr += `=\\dfrac{${base}^{${exp[0]}+${exp[1]}}}{(${base}^2)^${exp[2]}}\\times ${base}`;
+					texte_corr += `=\\dfrac{${base}^{${exp[0]+exp[1]}}}{${base}^{2\\times ${exp[2]}}}\\times ${base}`;
+					texte_corr += `=\\dfrac{${base}^{${exp[0]+exp[1]}}}{${base}^{${2*exp[2]}}}\\times ${base}`;
+					texte_corr += `=\\dfrac{${base}^{${exp[0]+exp[1]}}\\times ${base}}{${base}^{${2*exp[2]}}}`;
+					texte_corr += `=\\dfrac{${base}^{${exp[0]+exp[1]}+1}}{${base}^{${2*exp[2]}}}`;
+					texte_corr += `=\\dfrac{${base}^{${exp[0]+exp[1]+1}}}{${base}^{${2*exp[2]}}}`;
+					texte_corr += `=${base}^{${exp[0]+exp[1]+1}-${2*exp[2]}}`;					
+					texte_corr += `=${base}^{${exp[0]+exp[1]+1-2*exp[2]}}`;
 					// on ne teste l'exposant que pour la sortie puisque l'exposant est évincé
 					texte_corr += testExp(base, exp[0]+exp[1]+1-2*exp[2]);
 					break;
@@ -2312,18 +2313,20 @@ function Puissances_d_un_relatif_1(){
 					base = 2; // on travaille sur cette base mais on pourrait rendre la base aléatoire
 					exp = [randint(1,7,[1])]; // on a besoin de 1 exposant
 					texte = `$\\dfrac{${base**3}\\times ${base}}{${base**2}^${exp[0]}}$`;
-					texte_corr = `$\\dfrac{${base**3}\\times ${base}}{${base**2}^${exp[0]}}$`;
-					texte_corr += `$=\\dfrac{${base}^3\\times ${base}}{(${base}^2)^${exp[0]}}$`;
-					texte_corr += `$=\\dfrac{${base}^{3+1}}{${base}^{2\\times${exp[0]}}}$`;
-					texte_corr += `$=\\dfrac{${base}^{4}}{${base}^{${2*exp[0]}}}$`;
-					texte_corr += `$=${base}^{4-${2*exp[0]}}$`;
-					texte_corr += `$=${base}^{${3+1-2*exp[0]}}$`;
+					texte_corr = `$\\dfrac{${base**3}\\times ${base}}{${base**2}^${exp[0]}}`;
+					texte_corr += `=\\dfrac{${base}^3\\times ${base}}{(${base}^2)^${exp[0]}}`;
+					texte_corr += `=\\dfrac{${base}^{3+1}}{${base}^{2\\times${exp[0]}}}`;
+					texte_corr += `=\\dfrac{${base}^{4}}{${base}^{${2*exp[0]}}}`;
+					texte_corr += `=${base}^{4-${2*exp[0]}}`;
+					texte_corr += `=${base}^{${3+1-2*exp[0]}}`;
 					// on ne teste l'exposant que pour la sortie puisque l'exposant est évincé
 					texte_corr += testExp(base,3+1-2*exp[0]);
 					break;
 																
 			};
-					
+			
+
+		
 			if (this.liste_questions.indexOf(texte)==-1){ // Si la question n'a jamais été posée, on en créé une autre
 				this.liste_questions.push(texte);
 				this.liste_corrections.push(texte_corr);
