73c5ed4a2ba0080ab32c9b265873ddaf2f3522cb
Jean-Claude Lhote <lhotejc@gmail.com>
2020-02-11 21:29:03 +0100

    Ajout 6N21 beta : 6N21 est devenu 6N30 et 6N22 est devenu 6N30-2

--- include/mathalea_exercices.js
+++ include/mathalea_exercices.js
@@ -51,12 +51,13 @@ var liste_des_exercices_disponibles = {
 		'6N13' : Exercice_6N13,
 		'6N20' : Exercice_fractions_decomposer,
 		'6N20-2':Exercice_fractions_differentes_ecritures,
-		'6N21' : Lire_abscisse_decimale,
-		'6N22' : Placer_points_sur_axe,
+		'6N21' : Lire_abscisse_fractionnaire,
 		'6N23' : Exercice_ecriture_decimale_a_partir_de_fraction_decimale,
 		'6N23_1' : Exercice_differentes_ecritures_nombres_decimaux,
 		'6N24' : Exercice_6N24,
 		'6N24_1' : Exercice_multiplier_ou_diviser_un_nombre_entier_par_10_100_1000,
+		'6N30' : Lire_abscisse_decimale,
+		'6N30-2' : Placer_points_sur_axe,
 		'6N31' : Comparer_decimaux,
 		'6N33' : Fraction_d_un_nombre,
 		'6N33-1' : Pourcentage_d_un_nombre,
@@ -182,7 +183,7 @@ function Exercice() {
 }
 
 /**
-* Lire l'abscisse fractionnaire d'un point
+* Lire l'abscisse décimale d'un point
 * @Auteur Jean-Claude Lhote et Rémi Angot
 */
 function Lire_abscisse_decimale(){
@@ -257,9 +258,85 @@ function Lire_abscisse_decimale(){
 	}
 	this.besoin_formulaire_numerique = ['Niveau de difficulté',4,"1 : Un chiffre après la virgule\n2 : Deux chiffres après la virgule \n3 : Trois chiffres après la virgule\n4 : Mélange"];
 }
-
 /**
 * Lire l'abscisse fractionnaire d'un point
+* @Auteur Jean-Claude Lhote et Rémi Angot
+*/
+function Lire_abscisse_fractionnaire(){
+	'use strict';
+	Exercice.call(this); // Héritage de la classe Exercice()
+	this.titre = "Lire l'abscisse fractionnaire d'un point";
+	this.consigne = "Lire l'abscisse de chacun des points suivants.";
+	this.nb_questions = 3;
+	this.nb_questions_modifiable = true;
+	this.nb_cols = 1;
+	this.nb_cols_corr = 1;
+	this.spacing = 1;
+    this.spacing_corr = 1;
+	this.sup=4;
+
+	this.nouvelle_version = function(numero_de_l_exercice){ // numero_de_l_exercice est 0 pour l'exercice 1
+		let type_de_questions;
+		this.liste_questions=[];
+		this.liste_corrections=[];
+		this.contenu = ''; // Liste de questions
+		this.contenu_correction = ''; // Liste de questions corrigées
+		if (this.sup==4) 	type_de_questions=combinaison_listes([1,2,3],this.nb_questions);
+		else 				type_de_questions=combinaison_listes([parseInt(this.sup)],this.nb_questions);
+		
+
+		this.contenu = html_consigne(this.consigne)
+		for (let i = 0,abs0,l1,l2,l3,x1,x2,x3,x11,x22,x33, pas1,pas2, id_unique, texte, texte_corr; i < this.nb_questions;i++) {
+			l1=lettre_depuis_chiffre(i*3+1)
+			l2=lettre_depuis_chiffre(i*3+2)
+			l3=lettre_depuis_chiffre(i*3+3)
+			switch (type_de_questions[i]) {
+				case 1: // Placer des demis ou des tiers sur un axe 
+					abs0 = randint(0,9);
+					pas1 = 1;
+					pas2 = randint(2,3);
+					x1 = randint(0, 2); x2 = randint(3, 4); x3 = randint(5, 6);
+					x11 = randint(1,pas2); x22 = randint(1,pas2); x33 = randint(1,pas2)
+					break;
+
+				case 2: // Placer des quarts,des cinquièmes sur un axe
+					abs0 = randint(0, 9);
+					pas1 = 1;
+					pas2 = randint(4,5);
+					x1 = randint(0, 2); x2 = randint(3, 4); x3 = randint(5, 6);
+					x11 = randint(1, pas2); x22 = randint(1, pas2); x33 = randint(1, pas2)
+					break;
+
+				case 3: // Placer des sixièmes des septièmes et des huitièmes sur un axe 
+					abs0 = randint(0,9);
+					pas1 = 1;
+					pas2 = randint(6,7);
+					x1 = randint(0, 2); x2 = randint(3, 4); x3 = randint(5, 6);
+					x11 = randint(1, pas2); x22 = randint(1, pas2); x33 = randint(1, pas2)
+					break;
+			}
+			if (sortie_html) {
+				id_unique = `${i}_${Date.now()}`
+				this.contenu += `<div id="div_svg${numero_de_l_exercice}${id_unique}" style="width: 90%; height: 200px;  "></div>`
+				SVG_reperage_sur_un_axe(`div_svg${numero_de_l_exercice}${id_unique}`, abs0, 6, pas1, pas2, [[l1, x1, x11], [l2, x2, x22], [l3, x3, x33]], [[abs0 + 1 / pas1, 1, 0], [abs0 + 2 / pas1, 2, 0], [abs0 + 3 / pas1, 3, 0], [abs0 + 4 / pas1, 4, 0], [abs0 + 5 / pas1, 5, 0], [abs0 + 6 / pas1, 6, 0]])
+				this.contenu_correction += `<div id="div_svg_corr${numero_de_l_exercice}${id_unique}" style="width: 90%; height: 200px;  "></div>`
+				SVG_reperage_sur_un_axe(`div_svg_corr${numero_de_l_exercice}${id_unique}`, abs0, 6, pas1, pas2, [[l1, x1, x11, true], [l2, x2, x22, true], [l3, x3, x33, true]], [[abs0 + 1 / pas1, 1, 0], [abs0 + 2 / pas1, 2, 0], [abs0 + 3 / pas1, 3, 0], [abs0 + 4 / pas1, 4, 0], [abs0 + 5 / pas1, 5, 0], [abs0 + 6 / pas1, 6, 0]])
+			}
+			else { //sortie Latex 
+				texte=Latex_reperage_sur_un_axe(2.4, abs0, 6, pas1, pas2, [[l1, x1, x11], [l2, x2, x22], [l3, x3, x33]], [[abs0 + 1 / pas1, 1, 0], [abs0 + 2 / pas1, 2, 0], [abs0 + 3 / pas1, 3, 0], [abs0 + 4 / pas1, 4, 0], [abs0 + 5 / pas1, 5, 0], [abs0 + 6 / pas1, 6, 0]]);
+				texte_corr=Latex_reperage_sur_un_axe(2.4, abs0, 6, pas1, pas2, [[l1, x1, x11,true], [l2, x2, x22,true], [l3, x3, x33,true]], [[abs0 + 1 / pas1, 1, 0], [abs0 + 2 / pas1, 2, 0], [abs0 + 3 / pas1, 3, 0], [abs0 + 4 / pas1, 4, 0], [abs0 + 5 / pas1, 5, 0], [abs0 + 6 / pas1, 6, 0]]);
+				this.liste_questions.push(texte)
+				this.liste_corrections.push(texte_corr);
+			}
+		
+		}
+		if (!sortie_html) liste_de_question_to_contenu(this); 
+	}
+	this.besoin_formulaire_numerique = ['Niveau de difficulté',4,"1 : Un chiffre après la virgule\n2 : Deux chiffres après la virgule \n3 : Trois chiffres après la virgule\n4 : Mélange"];
+}
+
+/**
+* Placer Un point d'abscisse décimale
 * @Auteur Jean-Claude Lhote et Rémi Angot
 */
 function Placer_points_sur_axe(){
