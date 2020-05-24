6437913912c46c202f4e9db54322aa066de6b79d
Sébastien LOZANO <sebastien.lozano@ac-nancy-metz.fr>
2020-05-24 17:30:02 +0200

    Exos tests commentés

--- include/mathalea_exercices_3e.js
+++ include/mathalea_exercices_3e.js
@@ -2917,7 +2917,7 @@ function Premier_ou_pas_critere_par7_par11(){
 };
 /**
  * 3A12 - Fractions irreductibles
- * @Auteur Sébastien Lozano
+ * @author Sébastien Lozano
  */
  
 function FractionsIrreductibles(){
@@ -2998,7 +2998,7 @@ function FractionsIrreductibles(){
 
 /**
  * 3A13 - PGCD_PPCM_Engrenages
- * @Auteur Sébastien Lozano
+ * @author Sébastien Lozano
  */
  
 function PGCD_PPCM_Engrenages(){
@@ -3070,3 +3070,80 @@ function PGCD_PPCM_Engrenages(){
 	}
 	//this.besoin_formulaire_numerique = ['Règle à travailler',5,"1 : Produit de deux puissances de même base\n2 : Quotient de deux puissances de même base\n3 : Puissance de puissance\n4 : Produit de puissances de même exposant\n5 : Mélange"]; 
 };
+
+
+ /**
+  * test de fonctions
+  */
+
+
+	function tester_des_fonctions(){
+		'use strict';
+		Exercice.call(this); // Héritage de la classe Exercice()
+		this.sup = 1 ; 
+		this.titre = "Tester des fonctions"; 
+		// pas de différence entre la version html et la version latex pour la consigne
+		this.consigne =`Tester des fonctions.`;
+		this.consigne += `<br>`;
+		sortie_html ? this.spacing = 3 : this.spacing = 2;
+		sortie_html ? this.spacing_corr = 2: this.spacing_corr = 1;
+		this.nb_questions = 4;
+		//this.correction_detaillee_disponible = true;
+		this.nb_cols = 1;
+		this.nb_cols_corr = 1;
+		this.sup = 1;
+	
+		this.nouvelle_version = function(numero_de_l_exercice){
+			let type_de_questions;
+			if (sortie_html) { // les boutons d'aide uniquement pour la version html
+				//this.bouton_aide = '';
+				//this.bouton_aide = modal_pdf(numero_de_l_exercice,"pdf/FicheArithmetique-3A13.pdf","Aide mémoire sur les fonctions (Sébastien Lozano)","Aide mémoire")		
+				//this.bouton_aide += modal_video('conteMathsNombresPremiers','videos/LesNombresPremiers.mp4','Petit conte mathématique','Intro Vidéo');
+			} else { // sortie LaTeX
+			};
+	
+			this.liste_questions = []; // Liste de questions
+			this.liste_corrections = []; // Liste de questions corrigées
+			this.contenu = ''; // Liste de questions
+			this.contenu_correction = ''; // Liste de questions corrigées
+	
+			let type_de_questions_disponibles = [1,2,3,4];
+			//let type_de_questions_disponibles = [1];
+			let liste_type_de_questions = combinaison_listes_sans_changer_ordre(type_de_questions_disponibles,this.nb_questions);
+	
+				for (let i = 0, texte, texte_corr, cpt=0; i < this.nb_questions&&cpt<50;) {
+					type_de_questions = liste_type_de_questions[i];
+					
+		
+					switch (type_de_questions) {
+						case 1 : // périmètre d'un carré de côté x			
+							texte = 'Test fonction resol 3x3 de JC sur $(x-1)(x-2)(x-3) = x^3-6x^2+11x-6$<br>';
+							texte += resol_sys_lineaire_3x3(1,2,3,0,0,0,-6);
+							texte_corr = 'corr type 1';
+							break;		
+						case 2 : // périmètre d'un carré de côté x			
+							texte = 'type 2';
+							texte_corr = 'corr type 2';
+							break;	
+						case 3 : // périmètre d'un carré de côté x			
+							texte = 'type 3';
+							texte_corr = 'corr type 3';
+							break;	
+						case 4 : // périmètre d'un carré de côté x			
+							texte = 'type 4';
+							texte_corr = 'corr type 4';
+							break;		
+					};
+				
+					if (this.liste_questions.indexOf(texte)==-1){ // Si la question n'a jamais été posée, on en créé une autre
+						this.liste_questions.push(texte);
+						this.liste_corrections.push(texte_corr);
+						i++;
+					}
+					cpt++
+				}	
+		
+			liste_de_question_to_contenu(this);
+		}
+		//this.besoin_formulaire_numerique = ['Règle à travailler',5,"1 : Produit de deux puissances de même base\n2 : Quotient de deux puissances de même base\n3 : Puissance de puissance\n4 : Produit de puissances de même exposant\n5 : Mélange"]; 
+	};
