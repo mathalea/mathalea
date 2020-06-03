bdb780e9bad1a67acf209a30d21fa883ca8167fc
Sébastien LOZANO <sebastien.lozano@ac-nancy-metz.fr>
2020-06-03 18:36:30 +0200

    modif FicheArithmetique-3A10.pdf + FicheArithmetique-3A12.pdf pour ajout dans 3A12

--- include/mathalea_exercices_3e.js
+++ include/mathalea_exercices_3e.js
@@ -3465,7 +3465,7 @@ function Fractions_irreductibles(){
 		let type_de_questions;
 		if (sortie_html) { // les boutons d'aide uniquement pour la version html
 			//this.bouton_aide = '';
-			//this.bouton_aide = modal_pdf(numero_de_l_exercice,"pdf/FicheArithmetique-3A12.pdf","Aide mémoire sur les fonctions (Sébastien Lozano)","Aide mémoire")					
+			this.bouton_aide = modal_pdf(numero_de_l_exercice,"pdf/FicheArithmetique-3A12.pdf","Aide mémoire sur les fonctions (Sébastien Lozano)","Aide mémoire")					
 			//this.bouton_aide += modal_video('conteMathsNombresPremiers','videos/LesNombresPremiers.mp4','Petit conte mathématique','Intro Vidéo');
 		} else { // sortie LaTeX
 		};
