53bdb3211a4b0679bd7f53ced5bc109732783a7e
lhotejc <lhotejc@gmail.com>
2020-08-06 16:44:05 +0200

    pointIntersectionLC(d,C,'M',1) bug encore sur le cas général.
    Fonctionne avec droite horizontale et verticale

--- include/mathalea2d.js
+++ include/mathalea2d.js
@@ -1012,6 +1012,77 @@ function Cercle(O,r,color){
 function cercle(...args){ 
 	return new Cercle(...args)
 }
+function pointIntersectionLC(d,C,nom='',n=1){
+	let O = C.centre
+	let r = C.rayon
+	let a = d.a
+	let b=d.b
+	let c=d.c 
+	let xO = O.x
+	let yO = O.y
+	let Delta,delta,xi,yi,xi_prime,yi_prime
+	console.log(a,b,c,r,xO,yO)
+	if (b==0) { // la droite est verticale
+		xi=calcul(-c/a)	
+		xi_prime=xi
+		Delta=calcul(4*(-xO*xO-c*c/(a*a)-2*xO*c/a+r*r))
+		if (Delta<0) return false
+		else if (egal(Delta,0)) { //un seul point d'intersection
+			yi=calcul(yO+Math.sqrt(Delta)/2)
+			yi_prime=yi
+		}
+		else {//deux points d'intersection
+		yi=calcul(yO-Math.sqrt(Delta)/2)
+		yi_prime=calcul(yO+Math.sqrt(Delta)/2)
+		}
+	}
+	else if (a==0) { // la droite est horizontale
+		yi=calcul(-c/b)
+		yi_prime=yi
+		Delta=calcul(4*(-yO*yO-c*c/(b*b)-2*yO*c/b+r*r))
+		if (Delta<0) return false
+		else if (egal(Delta,0)) { //un seul point d'intersection
+			xi=calcul(xO+Math.sqrt(Delta)/2)
+			xi_prime=xi
+		}
+		else {//deux points d'intersection
+		xi=calcul(xO-Math.sqrt(Delta)/2)
+		xi_prime=calcul(xO+Math.sqrt(Delta)/2)
+		}
+	}
+	else { //cas général
+		Delta=calcul((2*(a*c/(b*b)+yO*a/b-xO))**2-4*(1+(a/b)**2)*(xO*xO+yO*yO+(c/b)**2+2*yO*c/b-r*r))
+		console.log(Delta)
+		if (Delta<0) return false
+		else if (egal(Delta,0)) { //un seul point d'intersection
+			delta=Calcul(Math.sqrt(Delta))
+			xi=calcul((-(a*c/b+yO*a/b-xO)-delta)/(2*(1+(a/b)**2)))
+			xi_prime=xi
+			yi=calcul((-a*xi-c)/b)
+			yi_prime=yi
+		}
+		else  {//deux points d'intersection
+		xi=calcul((-(a*c/b+yO*a/b-xO)-delta)/(2*(1+(a/b)**2)))
+		xi_prime=calcul((-(a*c/b+yO*a/b-xO)+delta)/(2*(1+(a/b)**2)))
+		yi=calcul((-a*xi-c)/b)
+		yi_prime=calcul((-a*xi_prime-c)/b)
+		}
+	}
+	if (n==1) {
+		if (yi_prime>yi) {
+			return point(xi_prime,yi_prime,nom)
+		} else {
+			return point(xi,yi,nom)
+		}
+	} else {
+		if (yi_prime>yi) {
+			return point(xi,yi,nom)
+		} else {
+			return point(xi_prime,yi_prime,nom)
+		}
+	}
+
+}
 
 /**
 * M = pointIntersectionCC(c1,c2,'M') // M est le point d'intersection le plus haut des cercles c1 et c2
