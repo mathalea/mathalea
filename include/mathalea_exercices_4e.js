e5b23f71b8c9a33b61d386aa57a49dec1c04e371
Sébastien LOZANO <sebastien.lozano@ac-nancy-metz.fr>
2020-08-09 19:17:26 +0200

    4C30-1 --> début + cas des entiers positifs

--- include/mathalea_exercices_4e.js
+++ include/mathalea_exercices_4e.js
@@ -4019,14 +4019,14 @@ function Egalite_Pythagore(){
  * @author Sébastien Lozano
  */
 
-function Signe_produit_quotient_relatifs(){
+function Signe_produit_quotient_relatifs() {
 	'use strict';
 	Exercice.call(this); // Héritage de la classe Exercice()	
 	this.sup=1;
 	if (this.exo == this.beta+'4C10-1') { 
 		this.sup = 4;
 		this.titre = `Signe d'un produit de nombres relatifs`;			
-	} else if (this.exo == this.beta+'4C10-2') {//via angles
+	} else if (this.exo == this.beta+'4C10-2') {
 		this.sup = 5;
 		this.titre = `Signe d'un quotient de nombres relatifs`;
 	} else {			
@@ -4039,16 +4039,9 @@ function Signe_produit_quotient_relatifs(){
 	this.nb_cols_corr = 1;
 	this.nb_questions_modifiable = false;
 
-	//this.liste_packages = `bclogo`;
-	
 	let type_de_questions_disponibles;
 	
 	this.nouvelle_version = function(numero_de_l_exercice){
-		// this.introduction=info_message({
-		// 	titre : "Exercice BETA",
-		// 	texte: "En cours de réalisation "
-
-		// });
 		
 		this.sup=Number(this.sup); // attention le formulaire renvoie un string, on a besoin d'un number pour le switch !
 		//console.log(typeof this.sup)
@@ -4116,8 +4109,6 @@ function Signe_produit_quotient_relatifs(){
 		for (let i = 0, texte, texte_corr, cpt=0; i < this.nb_questions && cpt<50; ) {			
 			// on ne choisit que des nombres compris entre 1 et 20
 			let nb_max = 20;
-			// on choisit un signe au hasard
-			let signe = randint(-1,1,[0]);
 			// Le tableau des relatifs necessaires, il m'en faut max 4 !
 			let num = new Relatif(randint(-1,1,[0])*randint(1,nb_max),randint(-1,1,[0])*randint(1,nb_max),randint(-1,1,[0])*randint(1,nb_max),randint(-1,1,[0])*randint(1,nb_max));
 			
@@ -4182,14 +4173,14 @@ function Signe_produit_quotient_relatifs(){
 		} else {
 			//this.besoin_formulaire_numerique = ['Niveau de difficulté',2,"1 : sans conversions de longueurs\n2 : avec conversions de longueurs"];
 		};
-}
+};
 
 /**
  * Signe du produit de relatifs
  * 4C10-1
  * @author Sébastien Lozano
  */
-function Signe_produit_relatifs(){
+function Signe_produit_relatifs() {
 	this.beta = ``;
 	this.exo = this.beta+`4C10-1`;
 	Signe_produit_quotient_relatifs.call(this);
@@ -4200,8 +4191,147 @@ function Signe_produit_relatifs(){
  * 4C10-2
  * @author Sébastien Lozano
  */
-function Signe_quotient_relatifs(){
+function Signe_quotient_relatifs() {
 	this.beta = ``;
 	this.exo = this.beta+`4C10-2`;
 	Signe_produit_quotient_relatifs.call(this);
+};
+
+/**
+ * Encadrer par des puissances de 10
+ * 4C30-1
+ * @author Sébastien Lozano
+ */
+function Puissances_encadrement() {
+	'use strict';
+	Exercice.call(this); // Héritage de la classe Exercice()	
+	this.sup=1;
+	this.nb_questions = 6;
+	this.titre = `Encadrer avec des puissances de 10`;	
+
+	this.consigne = `Encadrer les nombres suivants par deux puisances de 10 d'exposants consécutifs.`;
+	
+	this.nb_cols = 1;
+	this.nb_cols_corr = 1;
+	//this.nb_questions_modifiable = false;
+
+
+	let type_de_questions_disponibles;
+	
+	this.nouvelle_version = function(numero_de_l_exercice){
+		
+		this.sup=Number(this.sup); // attention le formulaire renvoie un string, on a besoin d'un number pour le switch !
+
+			switch(this.sup) {
+				case 1 : // nombre enier positif
+					type_de_questions_disponibles = [1,2,3,4,5,6];
+					//this.nb_questions = type_de_questions_disponibles.length;	
+					//this.nb_questions = 3;
+					break;
+				case 2 : // nombre décimal positif
+					type_de_questions_disponibles = [2];
+					//this.nb_questions = type_de_questions_disponibles.length;	
+					//this.nb_questions = 3;
+					break;
+				case 3 : // nombre décimal positif inférieur à 1
+					type_de_questions_disponibles = [3];
+					//this.nb_questions = type_de_questions_disponibles.length;	
+					//this.nb_questions = 3;
+					break;
+				case 4 : // Mélange
+					type_de_questions_disponibles = [1,1,2,2,3,3];
+					//this.nb_questions = type_de_questions_disponibles.length;	
+					break;
+			};
+
+		//let liste_type_de_questions = combinaison_listes(type_de_questions_disponibles,this.nb_questions) // Tous les types de questions sont posées mais l'ordre diffère à chaque "cycle"
+		let liste_type_de_questions = combinaison_listes_sans_changer_ordre(type_de_questions_disponibles,this.nb_questions) // Tous les types de questions sont posées --> à remettre comme ci dessus
+
+		this.liste_questions = []; // Liste de questions
+		this.liste_corrections = []; // Liste de questions corrigées
+		
+		for (let i = 0, texte, texte_corr, cpt=0; i < this.nb_questions && cpt<50; ) {
+			// nombre entier positif, un entre 1 et 10, puis 10 et 100 puis ....100 000 et 1 000 000
+			// 
+			//let entiers_positifs = [randint(1,10),randint(11,100),randint(101,1000),randint(1001,10000),randint(10001,100000),randint(100001,1000000)];
+			let ent_pos = [];
+			ent_pos.push({
+				val:`$${randint(1,10)}$`,
+				puissance_inf:`$10^{0}$`,
+				puissance_sup:`$10^{1}$`
+			});
+			ent_pos.push({
+				val:`$${randint(11,100)}$`,
+				puissance_inf:`$10^{1}$`,
+				puissance_sup:`$10^{2}$`
+			});
+			ent_pos.push({
+				val:`$${randint(101,1000)}$`,
+				puissance_inf:`$10^{2}$`,
+				puissance_sup:`$10^{3}$`
+			});
+			ent_pos.push({
+				val:`$${randint(1001,10000)}$`,
+				puissance_inf:`$10^{3}$`,
+				puissance_sup:`$10^{4}$`
+			});
+			ent_pos.push({
+				val:`$${randint(10001,100000)}$`,
+				puissance_inf:`$10^{4}$`,
+				puissance_sup:`$10^{5}$`
+			});
+			ent_pos.push({
+				val:`$${randint(100001,100000)}$`,
+				puissance_inf:`$10^{5}$`,
+				puissance_sup:`$10^{6}$`
+			});
+
+			let nb_entier_positif = randint(50,100000000);
+			let nb_decimal_positif =randint(50,100)/10;
+			let nb_decimal_positif_inf_un = randint(1,100)/100;
+			
+			switch (liste_type_de_questions[i]) {
+				case 1 : // nombre enier positif
+					texte = `${ent_pos[0].val}`;
+					texte_corr = `${ent_pos[0].puissance_inf} $\\leqslant$ ${ent_pos[0].val} $\\leqslant$ ${ent_pos[0].puissance_sup}`;
+					break;
+				case 2 : // nombre enier positif
+					texte = `${ent_pos[1].val}`;
+					texte_corr = `${ent_pos[1].puissance_inf} $\\leqslant$ ${ent_pos[1].val} $\\leqslant$ ${ent_pos[1].puissance_sup}`;
+					break;
+				case 3 : // nombre enier positif
+					texte = `${ent_pos[2].val}`;
+					texte_corr = `${ent_pos[2].puissance_inf} $\\leqslant$ ${ent_pos[2].val} $\\leqslant$ ${ent_pos[2].puissance_sup}`;
+					break;
+				case 4 : // nombre enier positif
+					texte = `${ent_pos[3].val}`;
+					texte_corr = `${ent_pos[3].puissance_inf} $\\leqslant$ ${ent_pos[3].val} $\\leqslant$ ${ent_pos[3].puissance_sup}`;
+					break;
+				case 5 : // nombre enier positif
+					texte = `${ent_pos[4].val}`;
+					texte_corr = `${ent_pos[4].puissance_inf} $\\leqslant$ ${ent_pos[4].val} $\\leqslant$ ${ent_pos[4].puissance_sup}`;
+					break;
+				case 6 : // nombre enier positif
+					texte = `${ent_pos[5].val}`;
+					texte_corr = `${ent_pos[5].puissance_inf} $\\leqslant$ ${ent_pos[5].val} $\\leqslant$ ${ent_pos[5].puissance_sup}`;
+					break;																				
+				case 7 : // nombre décimal positif
+					texte = `b ${nb_decimal_positif}`;
+					texte_corr = `${nb_decimal_positif}`;
+					break;
+				case 8 : // nombre décimal positif inferieur à 1
+					texte = `${nb_decimal_positif_inf_un}`;
+					texte_corr = `${nb_decimal_positif_inf_un}`;
+					break;		
+			};
+			if (this.liste_questions.indexOf(texte)==-1){ // Si la question n'a jamais été posée, on en créé une autre
+				this.liste_questions.push(texte);
+				this.liste_corrections.push(texte_corr);
+				i++;
+			}	
+			cpt++;	
+		}
+		liste_de_question_to_contenu(this);
+	};
+	this.besoin_formulaire_numerique = ['Niveau de difficulté',4,"1 : nombre enier positif\n2 : nombre décimal positif\n3 : nombre enier positif inférieur à un\n4 : Mélange"];
 };
