2426d8131811e8a22e2467d6d779f3b96144b2f3
lhotejc <lhotejc@gmail.com>
2020-08-13 20:43:01 +0200

    Methode representation ajoutée (un seul type)

--- include/mathalea_fractions.js
+++ include/mathalea_fractions.js
@@ -60,6 +60,46 @@ function Fraction(num,den) {
     this.differenceFraction = function(f2) {
         return this.sommeFraction(f2.oppose())
     }
+    this.representation = function (depart = 0, type = 'gateau') {
+
+        if (type == 'gateau') {
+            let n = quotientier(this.numIrred, this.denIrred)
+            let num = this.numIrred
+            let k,dep
+            for (k = 0; k < n; k++) {
+                let O = point(2 + k * 5, 2)
+                let C = cercle(O, 2)
+                let s, a
+                for (let i = 0; i < this.denIrred; i++) {
+                    s = segment(O, rotation(point(4+k*5, 2), O, i * 360 / this.denIrred))
+                }
+                dep = rotation(point(4 + k * 5, 2), O, depart * 360 / this.denIrred)
+                for (let j = 0; j < Math.min(this.denIrred, num); j++) {
+                    a = arc(dep, O, 360 / this.denIrred, true, fill = 'gray', color = 'black')
+                    dep = rotation(dep, O, 360 / this.denIrred)
+                }
+                num -= this.denIrred
+            }
+            console.log(k,n)
+            let O = point(2 + k * 5, 2)
+            let C = cercle(O, 2)
+            let s, a
+            for (let i = 0; i < this.denIrred; i++) {
+                s = segment(O, rotation(point(4+k*5, 2), O, i * 360 / this.denIrred))
+            }
+            dep = rotation(point(4 + k * 5, 2), O, depart * 360 / this.denIrred)
+            for (let j = 0; j < Math.min(this.denIrred, num); j++) {
+                a = arc(dep, O, 360 / this.denIrred, true, fill = 'gray', color = 'black')
+                dep = rotation(dep, O, 360 / this.denIrred)
+            }
+        }
+        else if (type == 'segment') {
+
+        }
+        else {
+
+        }
+    }
 }
 
 function fraction (a,b) {
