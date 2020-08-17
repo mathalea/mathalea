b2ff5daf729228f53c8c9de5017f5aad1ef4e767
Sébastien LOZANO <sebastien.lozano@ac-nancy-metz.fr>
2020-08-17 07:05:33 +0200

    beta5L13-4 début. fork of JC 5L13-1

--- include/mathalea_exercices_5e.js
+++ include/mathalea_exercices_5e.js
@@ -3944,6 +3944,90 @@ function Calculer_une_expression_litterale() {
 }
 
 /**
+ * 5L13-4
+ * Déterminer la dernière opération à effectuer dans une expression littéral
+ * @author Sébastien Lozano fork Jean-Claude Lhote
+ */
+function Calculer_une_expression_litteraleBis() {
+	'use strict'
+	Exercice.call(this); // Héritage de la classe Exercice()
+	this.consigne = "";
+	this.nb_questions = 4;
+	this.nb_cols = 1;
+	this.nb_cols_corr = 1;
+	this.sup2=false; // si false alors utilisation de nombres entiers, si true alors utilisation de nombres à un chiffre après la virgule.
+	this.sup=false
+	this.version=5 // 1 pour ecrire une expression, 2 pour écrire la phrase, 3 pour écrire l'expression et la calculer, 4 pour calculer une expression numérique
+	this.litteral = true;
+	this.titre = `Déterminer la dernière opération à effectuer dans une expression littérale`;
+
+	this.nouvelle_version = function(numero_de_l_exercice){
+		let type_de_questions_disponibles
+		this.liste_questions = []; // Liste de questions
+		this.liste_corrections = []; // Liste de questions corrigées
+		if (!this.sup) { // Si aucune liste n'est saisie
+		//type_de_questions_disponibles = [1,2,3,4,5]
+		type_de_questions_disponibles = [2,3,4,5]
+		}
+		else {
+			if (typeof(this.sup)=='number'){ // Si c'est un nombre c'est qu'il y a qu'une expression
+			type_de_questions_disponibles[0] = this.sup
+				this.nb_questions=1
+			} else {
+				type_de_questions_disponibles = this.sup.split("-");// Sinon on créé un tableau à partir des valeurs séparées par des -
+				this.nb_questions=type_de_questions_disponibles.length
+			}	
+		}
+		let expf,expn,expc,decimal=1,nbval,nb_operations,resultats
+		let liste_type_de_questions = combinaison_listes(type_de_questions_disponibles,this.nb_questions) 
+		if (this.sup2) decimal=10;
+		for (let i = 0, texte, texte_corr,val1,val2, cpt=0; i < this.nb_questions && cpt<50; ) {
+			nb_operations=parseInt(liste_type_de_questions[i])
+			val1=randint(2,5)
+			val2=randint(6,9)
+			if (this.version>2&&nb_operations==1&&!this.litteral) nb_operations++
+			if (!this.litteral)
+				resultats=Choisir_expression_numerique(nb_operations,decimal)
+			else 
+				resultats=Choisir_expression_litterale(nb_operations,decimal,val1,val2)
+			expf=resultats[0]
+			expn=resultats[1]
+			expc=resultats[2]
+			nbval=resultats[3]
+			switch (this.version) {
+				case 5:
+					if (expn.indexOf('ou')>0) expn=expn.substring(0,expn.indexOf('ou')) // on supprime la deuxième expression fractionnaire
+					this.consigne=`Déterminer la dernière opération à effectuer s'il fallait faire le calcul pour des valeurs données de $x$ et de $y$.`
+					//if (!this.litteral) texte=`${expn}.`
+					//else if (nbval==2) texte=`Pour $x=${val1}$ et $y=${val2}$, calculer ${expn}.`
+					//else texte =`Pour $x=${val1}$, calculer ${expn}.`
+					texte =`${expn}.`
+					//if (!this.litteral) texte_corr=`${expc}.`
+					//else if (nbval==2) texte_corr=`Pour $x=${val1}$ et $y=${val2}$ :<br>${expc}.`
+					//else texte_corr=`Pour $x=${val1}$ :<br>${expc}.`
+					texte_corr = `Pour fixer les idées, choissions des valeurs pour $x$ et $y$, par exemple $x=${val1}$ et $y=${val2}$<br>`
+					texte_corr += `Le calcul serait le suivant : ${expc}.<br>`;
+					texte_corr += `Pour n'importe quelles valeurs de $x$ et de $y$ choisies, les étapes sont les mêmes, elles respectent les règles de priorité opératoire.<br>`
+					texte_corr += `La dernière opération dans ${expn} est donc ...`;
+					break
+ 		
+			}
+			if (this.liste_questions.indexOf(texte)==-1){ // Si la question n'a jamais été posée, on en créé une autre
+				this.liste_questions.push(texte);
+				this.liste_corrections.push(texte_corr);
+				i++;
+			}
+			cpt++;	
+		}
+		liste_de_question_to_contenu(this);
+	}
+	this.besoin_formulaire_texte = ['Choix des expressions','Nombres séparés par des tirets\n 1 : Expressions de base à une opération\n2 : Expressions à deux opérations\n3 : Expressions à 3 opérations\n4 : Expressions à 4 opérations\n5 : Expressions complexes'] // Texte, tooltip
+	this.besoin_formulaire2_case_a_cocher = ["Avec décimaux.",false]
+
+}
+
+
+/**
  * Fonction noyau pour les 7 fonctions précédentes qui utilisent les mêmes variables et la fonction Choisir_expression_numerique
  * @Auteur Jean-Claude Lhote
  */
