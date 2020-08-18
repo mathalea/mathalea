81ff51598418a82a2cc4666730ba6e2c4117c9af
Sébastien LOZANO <sebastien.lozano@ac-nancy-metz.fr>
2020-08-18 16:19:20 +0200

    beta6P11-1 ajout de sauts de ligne entre les réponses des sous questions

--- include/mathalea_exercices.js
+++ include/mathalea_exercices.js
@@ -11940,21 +11940,21 @@ function Proportionnalite_par_linearite_bis(){
         correction:`
         C'est une situation de proportionnalité. Nous pouvons donc utiliser les propriétés de linéarité de la proportionnalité.
         <br>C'est ce que nous allons faire pour les deux premères questions.
+        <br>
         <br>${num_alpha(k_corr++)} Pour $${situation.n1}$ ${pluriel(situation.n1,situation)}, on paie $${tex_prix(situation.pu*situation.n1)}€$.
         <br> Pour $${situation.n2}$ ${pluriel(situation.n2,situation)}, on paie $${tex_prix(situation.pu*situation.n2)}€$.
         <br> Donc pour $${situation.n1}+${situation.n2}$ ${pluriel(situation.n3,situation)}, on paie $${tex_prix(situation.pu*situation.n1)} € + ${tex_prix(situation.pu*situation.n2)}€$.
         <br> ${texte_en_couleur(`${situation.prenom3} paiera donc $${tex_prix(situation.pu*situation.n3)}€$ pour $${situation.n3}$ ${pluriel(situation.n3,situation)}.`)}
-
+        <br>
         <br>${num_alpha(k_corr++)} Pour $${situation.n1}$ ${pluriel(situation.n1,situation)}, on paie $${tex_prix(situation.pu*situation.n1)}€$.
         <br> Pour $${situation.n2}$ ${pluriel(situation.n2,situation)}, on paie $${tex_prix(situation.pu*situation.n2)}€$.
         <br> Donc pour $${situation.n1}-${situation.n2}$ ${pluriel(situation.n4,situation)}, on paie $${tex_prix(situation.pu*situation.n1)}€-${tex_prix(situation.pu*situation.n2)}€$.
         <br> ${texte_en_couleur(`${situation.prenom4} paiera donc $${tex_prix(situation.pu*situation.n4)}€$ pour $${situation.n4}$ ${pluriel(situation.n4,situation)}.`)}
-
+        <br>
         <br>${num_alpha(k_corr++)} On peut utiliser l'une ou l'autre des informations de l'énoncé pour répondre en revenant à l'unité.
         <br> Par exemple pour $${situation.n1}$ ${pluriel(situation.n1,situation)}, on paie $${tex_prix(situation.pu*situation.n1)}€$.
         <br> Donc $1$ ${situation.achat_sing} coûte $${tex_prix(situation.pu*situation.n1)}\\div ${situation.n1} = ${tex_prix(situation.pu)}€$.
         <br> Pour $${tex_prix(situation.pu*situation.n_max)}€$ nous aurons donc $${tex_prix(situation.pu*situation.n_max)}\\div ${tex_prix(situation.pu)}€ = ${situation.n_max}$ ${pluriel(situation.n_max,situation)}.
-
         <br> ${texte_en_couleur(`Avec $${tex_prix(situation.pu*situation.n_max)}€$, ${situation.prenom_max} pourra donc acheter $${situation.n_max}$ ${pluriel(situation.n_max,situation)}.`)}
         `
 			})
