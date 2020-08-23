98a53b0cf1eff0d048e3a884be9a7ee2d44494c8
Gaelle Morvan <gaelle@morvans.net>
2020-08-23 15:29:35 +0200

    Suite, quelques corrections

--- include/mathalea_exercices_1re.js
+++ include/mathalea_exercices_1re.js
@@ -20,14 +20,14 @@ function Terme_d_une_suite_definie_explicitement(){
       
       a = randint(1, 7);
       b = randint(1, 10);
-      k = randint(0, 20)
+      k = randint(0, 20);
 			
-			texte = "Soit $(u_n)$ une suite définie pour tout entier $n\\in\\mathbb{N}$ par $u_n = ${a}n+${b}$."
-      texte += "Calculer $u_${k}$."
+			texte = 'Soit $(u_n)$ une suite définie pour tout entier $n\\in\\mathbb{N}$ par $u_n = ${a}n+${b}$.'
+      texte += 'Calculer $u_${k}$.'
 			// énoncé 
 			// Rappel ${a} permet de récupérer la valeur de a dans un littéral de gabarit définit entre accents graves
       
-      "Dans l'expression de $u_n$, on remplasse $n$ par ${k}, on obtient : $u_${k} = ${a*k+b}$"
+      texte_corr = "Dans l'expression de $u_n$, on remplasse $n$ par ${k}, on obtient : $u_${k} = ${a*k+b}$"
 			// la correction de la question
 			
 			if (this.liste_questions.indexOf(texte)==-1){ // Si la question n'a jamais été posée, on en créé une autre
