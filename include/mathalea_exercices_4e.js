82dda199db57c8499c557b7c8c9d5567c14c627d
Sébastien LOZANO <sebastien.lozano@ac-nancy-metz.fr>
2020-08-25 10:49:23 +0200

    4C25-0 correctif this.spacing version LaTeX

--- include/mathalea_exercices_4e.js
+++ include/mathalea_exercices_4e.js
@@ -7569,8 +7569,8 @@ function Problemes_additifs_fractions() {
   this.nb_cols = 1;
   this.nb_cols_corr = 1;
   //this.nb_questions_modifiable = false;
-  sortie_html ? (this.spacing = 3) : (this.spacing = 2);
-  sortie_html ? (this.spacing_corr = 3) : (this.spacing_corr = 2);
+  sortie_html ? (this.spacing = 2) : (this.spacing = 1.5);
+  sortie_html ? (this.spacing_corr = 3) : (this.spacing_corr = 1.15);
 
   let type_de_questions_disponibles;
 
