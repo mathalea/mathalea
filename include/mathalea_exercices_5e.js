94b547393f01608e8adb4787f05eb96b03fa1a77
Sébastien LOZANO <sebastien.lozano@ac-nancy-metz.fr>
2020-08-26 16:09:59 +0200

    beta5N20-0 avec le diagramme mais sans correction !

--- include/mathalea_exercices_5e.js
+++ include/mathalea_exercices_5e.js
@@ -6220,13 +6220,13 @@ function Problemes_additifs_fractions_5e(){
 		let liste_type_de_questions = combinaison_listes_sans_changer_ordre(type_de_questions_disponibles,this.nb_questions) // Tous les types de questions sont posées --> à remettre comme ci dessus		
 		
 		for (let i = 0, texte, texte_corr, cpt=0; i < this.nb_questions && cpt<50; ) {
-			// on définit les fractions pour les vols
+			// on définit les fractions pour les vols et les arguments pour le graphique
 			let frac_vols = [
-				[1,12],
-				[1,12],
-				[1,12],
-				[1,4],
-				[1,2]
+				[1,12,[1.8,' ','black',2,1,'red',0.4]],
+				[1,12,[1.8,' ','black',2,1,'red',0.4]],
+				[1,12,[1.8,' ','black',2,1,'red',0.4]],
+				[1,4,[1.8,' ','black',2,1,'blue',0.4]],
+				[1,2,[1.8,' ','black',2,1,'green',0.4]]
 			];
 			// on mélange pour l'aléatoire
 			frac_vols = shuffle(frac_vols);
@@ -6252,35 +6252,40 @@ function Problemes_additifs_fractions_5e(){
 						article:destinations_vols[0][0],
 						nom: destinations_vols[0][1],
 						frac: frac_vols[0],
-						angle: calcul(360/frac_vols[0][1])
+						angle: calcul(360/frac_vols[0][1]),
+						arg_graph: frac_vols[0][2],
 					},
 					cat2:{
 						destination:destinations_vols[1][0]+destinations_vols[1][1],
 						article:destinations_vols[1][0],
 						nom: destinations_vols[1][1],
 						frac: frac_vols[1],
-						angle: calcul(360/frac_vols[1][1])
+						angle: calcul(360/frac_vols[1][1]),
+						arg_graph: frac_vols[1][2],
 					},
 					cat3:{
 						destination:destinations_vols[2][0]+destinations_vols[2][1],
 						article:destinations_vols[2][0],
 						nom: destinations_vols[2][1],
 						frac: frac_vols[2],
-						angle: calcul(360/frac_vols[2][1])
+						angle: calcul(360/frac_vols[2][1]),
+						arg_graph: frac_vols[2][2],
 					},
 					cat4:{
 						destination:destinations_vols[3][0]+destinations_vols[3][1],
 						article:destinations_vols[3][0],
 						nom: destinations_vols[3][1],
 						frac: frac_vols[3],
-						angle: calcul(360/frac_vols[3][1])
+						angle: calcul(360/frac_vols[3][1]),
+						arg_graph: frac_vols[3][2],
 					},
 					cat5:{
 						destination:destinations_vols[4][0]+destinations_vols[4][1],
 						article:destinations_vols[4][0],
 						nom: destinations_vols[4][1],
 						frac: frac_vols[4],
-						angle: calcul(360/frac_vols[4][1])
+						angle: calcul(360/frac_vols[4][1]),
+						arg_graph: frac_vols[4][2],
 					},
 					q1a:q1a,
 					q1b:q1b,
@@ -6296,6 +6301,131 @@ function Problemes_additifs_fractions_5e(){
 				{//case 4 -->
 				},		
 			];
+			// une fonction pour gérer le codage des angles
+			function myCodageAngle(A,O,B,angle,[...args]) {
+				if (angle == 90) {
+					return codageAngleDroit(A,O,B);
+				} else {
+					return codeAngle(A,O,angle,...args)
+				};
+			};
+
+			// une fonction pour gérer l'affichage correct de la légende
+			//param est l'ordonnée du point qui sert à la mediatrice !
+			function myLegendeOK(param) {
+				if (param<0) {
+					return 2;
+				} else {
+					return 1;
+				};
+			};
+
+			//une fonction pour positionner le label
+			// y est l'ordonnée du point
+			function myLabelPosition(y) {
+				if (y<0) {
+					return 'below';
+				} else {
+					return 'above';
+				};
+			};
+
+			// on prépare la fenetre mathalea2d
+			let fenetreMathalea2D = {xmin:-10,ymin:-8,xmax:10,ymax:8,pixelsParCm:20,scale:0.5}
+			let O_vols = point(0,0);
+			let A_vols = point(fenetreMathalea2D.xmin+6,0);
+			let c_vols = cercleCentrePoint(O_vols,A_vols,'blue');
+			c_vols.epaisseur = 2;
+			// on trace les quartiers
+			//cat1
+			let B_vols = rotation(A_vols,O_vols,situations[0].cat1.angle);
+			let s_OA_vols = segment(O_vols,A_vols);
+			let s_OB_vols = segment(O_vols,B_vols);			
+			let codage_AOB = myCodageAngle(A_vols,O_vols,B_vols,situations[0].cat1.angle,situations[0].cat1.arg_graph)
+			//cat2
+			let C_vols = rotation(B_vols,O_vols,situations[0].cat2.angle);			
+			let s_OC_vols = segment(O_vols,C_vols);			
+			let codage_BOC = myCodageAngle(B_vols,O_vols,C_vols,situations[0].cat2.angle,situations[0].cat2.arg_graph)
+			//cat3
+			let D_vols = rotation(C_vols,O_vols,situations[0].cat3.angle);			
+			let s_OD_vols = segment(O_vols,D_vols);			
+			let codage_COD = myCodageAngle(C_vols,O_vols,D_vols,situations[0].cat3.angle,situations[0].cat3.arg_graph)
+			//cat4
+			let E_vols = rotation(D_vols,O_vols,situations[0].cat4.angle);			
+			let s_OE_vols = segment(O_vols,E_vols);			
+			let codage_DOE = myCodageAngle(D_vols,O_vols,E_vols,situations[0].cat4.angle,situations[0].cat4.arg_graph)
+			//cat5
+			let F_vols = rotation(E_vols,O_vols,situations[0].cat5.angle);			
+			let s_OF_vols = segment(O_vols,F_vols);			
+			let codage_EOF = myCodageAngle(E_vols,O_vols,F_vols,situations[0].cat5.angle,situations[0].cat5.arg_graph)
+
+			// légende 
+			let A_legende = point(fenetreMathalea2D.xmin+4,0);
+			let L_vols_cat1 = rotation(A_legende,O_vols,situations[0].cat1.angle/2,situations[0].cat1.nom);
+			L_vols_cat1.positionLabel = myLabelPosition(L_vols_cat1.y);
+			let LL_vols_cat1 = rotation(A_vols,O_vols,situations[0].cat1.angle/2,situations[0].cat1.nom);
+			let s_legende_cat1 = segment (L_vols_cat1,LL_vols_cat1);
+			s_legende_cat1.styleExtremites = '->';
+			s_legende_cat1.pointilles = true;
+
+			let L_vols_cat2 = rotation(L_vols_cat1,O_vols,situations[0].cat1.angle/2+situations[0].cat2.angle/2,situations[0].cat2.nom);
+			L_vols_cat2.positionLabel = myLabelPosition(L_vols_cat2.y);
+			let LL_vols_cat2 = rotation(B_vols,O_vols,situations[0].cat2.angle/2,situations[0].cat2.nom);
+			let s_legende_cat2 = segment (L_vols_cat2,LL_vols_cat2);
+			s_legende_cat2.styleExtremites = '->';
+			s_legende_cat2.pointilles = true;
+
+			let L_vols_cat3 = rotation(L_vols_cat2,O_vols,situations[0].cat2.angle/2+situations[0].cat3.angle/2,situations[0].cat3.nom);
+			L_vols_cat3.positionLabel = myLabelPosition(L_vols_cat3.y);
+			let LL_vols_cat3 = rotation(C_vols,O_vols,situations[0].cat3.angle/2,situations[0].cat3.nom);
+			let s_legende_cat3 = segment (L_vols_cat3,LL_vols_cat3);
+			s_legende_cat3.styleExtremites = '->';
+			s_legende_cat3.pointilles = true;
+
+			let L_vols_cat4 = rotation(L_vols_cat3,O_vols,situations[0].cat3.angle/2+situations[0].cat4.angle/2,situations[0].cat4.nom);
+			L_vols_cat4.positionLabel = myLabelPosition(L_vols_cat4.y);
+			let LL_vols_cat4 = rotation(D_vols,O_vols,situations[0].cat4.angle/2,situations[0].cat4.nom);
+			let s_legende_cat4 = segment (L_vols_cat4,LL_vols_cat4);
+			s_legende_cat4.styleExtremites = '->';
+			s_legende_cat4.pointilles = true;
+
+			let L_vols_cat5 = rotation(L_vols_cat4,O_vols,situations[0].cat4.angle/2+situations[0].cat5.angle/2,situations[0].cat5.nom);
+			L_vols_cat5.positionLabel = myLabelPosition(L_vols_cat5.y);
+			let LL_vols_cat5 = rotation(E_vols,O_vols,situations[0].cat5.angle/2,situations[0].cat5.nom);
+			let s_legende_cat5 = segment (L_vols_cat5,LL_vols_cat5);
+			s_legende_cat5.styleExtremites = '->';
+			s_legende_cat5.pointilles = true;
+
+			
+			let mesAppels = [
+				c_vols,
+				s_OA_vols,
+				s_OB_vols,
+				s_OC_vols,
+				s_OD_vols,
+				s_OE_vols,
+				s_OF_vols,
+				codage_AOB,
+				codage_BOC,				
+				codage_COD,				
+				codage_DOE,				
+				codage_EOF,				
+				labelPoint(L_vols_cat1),
+				labelPoint(L_vols_cat2),
+				labelPoint(L_vols_cat3),
+				labelPoint(L_vols_cat4),
+				labelPoint(L_vols_cat5),
+				s_legende_cat1,
+				s_legende_cat2,
+				s_legende_cat3,
+				s_legende_cat4,
+				s_legende_cat5,
+			];
+			let fig_vols = mathalea2d(
+				fenetreMathalea2D,
+				mesAppels
+			);
+			situations[0].fig = fig_vols;
 
 			let enonces = [];			
 			let i_sous_question=0;
@@ -6304,6 +6434,9 @@ function Problemes_additifs_fractions_5e(){
 				enonces.push({
 					enonce:`
 					On a représenté sur le diagramme circulaire ci-contre la répartition des ${situations[k].fin_enonce_situation}.
+					<br>${texte_gras(`Les angles de même couleur ont la même mesure.`)}
+					<br>${texte_gras(`L'angle vert est un angle plat.`)}
+					<br> ${situations[k].fig}
 					<br>${num_alpha(i_sous_question++)} Quelle fraction représente les ${situations[k].nom_enonce} vers ${situations[k].cat1.destination} ?
 					<br>${num_alpha(i_sous_question++)} Quelle fraction représente les ${situations[k].nom_enonce} vers ${situations[k].cat2.destination} ?
 					<br>${num_alpha(i_sous_question++)} Sachant que ${situations[k].last_question[0]} ${situations[k].nb_total} ${situations[k].last_question[1]}
