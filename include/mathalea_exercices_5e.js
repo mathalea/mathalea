36f9e5bf795f3b5ad874ff2165edcdcd1f11a99e
Sébastien LOZANO <sebastien.lozano@ac-nancy-metz.fr>
2020-08-27 14:47:54 +0200

    beta5R20-4 coquille de la doublette a a

--- include/mathalea_exercices_5e.js
+++ include/mathalea_exercices_5e.js
@@ -6557,7 +6557,7 @@ function Problemes_additifs_relatifs_5e(){
 					question:``,
 					correction:`
 					${situations[k].prenom} ${situations[k].enonce_5} $${situations[k].nb_tot_lancers}$ fois ${situations[k].enonce_6},
-					sur les 15 lancers, on sait combien de fois il a a perdu de l'argent et combien de fois il a gagné $${tex_prix(situations[k].gain_unitaire)}$€, les autres lancers correspondent donc au nombre de fois où ${situations[k].pronomMin} a ${situations[k].correction_1} et qu'${situations[k].pronomMin} a gagné $${tex_prix(situations[k].gain_multiple)}$€ 
+					sur les 15 lancers, on sait combien de fois il a perdu de l'argent et combien de fois il a gagné $${tex_prix(situations[k].gain_unitaire)}$€, les autres lancers correspondent donc au nombre de fois où ${situations[k].pronomMin} a ${situations[k].correction_1} et qu'${situations[k].pronomMin} a gagné $${tex_prix(situations[k].gain_multiple)}$€ 
 					<br> $${situations[k].nb_tot_lancers}-${situations[k].nb_pertes}-${situations[k].nb_gains_unitaires} = ${situations[k].nb_tot_lancers-situations[k].nb_pertes-situations[k].nb_gains_unitaires}$,
 					${situations[k].pronomMin} a donc ${situations[k].correction_1} ${situations[k].nb_gains} fois.
 
