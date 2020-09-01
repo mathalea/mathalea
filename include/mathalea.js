ad13c147be6f9f20be47dad7e16a8cc367d64130
remiangot <remi.angot@gmail.com>
2020-09-01 22:22:40 +0200

    Bouton QRCode

--- include/mathalea.js
+++ include/mathalea.js
@@ -783,6 +783,21 @@ window.onload = function()  {
 			}, 1000);
 		});
 		
+		if (document.getElementById('btnQRcode')){
+			document.getElementById('btnQRcode').addEventListener('click',function () {
+				$('#ModalQRcode').html('');
+				let qrcode = new QRCode(document.getElementById("ModalQRcode"), {
+					text: window.location.href,
+					width: 800,
+					height: 600,
+					colorDark : "#000000",
+					colorLight : "#ffffff",
+					correctLevel : QRCode.CorrectLevel.H
+				});
+				qrcode.makeCode(window.location.href)
+				$('#ModalQRcode').modal('show')
+			})
+		}
 	}
 		
 
