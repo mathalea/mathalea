215fc8f9256360ce7ce2c18f4094aeb5244cd63c
remiangot <remi.angot@gmail.com>
2020-09-03 15:51:06 +0200

    2D Ajout du lien vers le SVG + ?script= ou ?url=

--- include/mathalea2dsvg.js
+++ include/mathalea2dsvg.js
@@ -1,24 +1,32 @@
-let divSvg = '';
+let divSvg = "";
 let script = "";
+let url = new URL(window.location.href);
 window.onload = function () {
-	divSvg = document.getElementById("svg");
-  let parametresURL = window.location.href.split("?")[1];
-  if (parametresURL) {
-    // Si dans l'URL il y a quelque-chose après le ?
-    fetch(`/m2d/${parametresURL}.m2d`)
+  divSvg = document.getElementById("svg");
+
+  if (url.searchParams.get("url")) { // Si on spécifie une url
+	  fetch(`/m2d/${url.searchParams.get("url")}.m2d`)
       .then(function (response) {
-        if (response.ok) {
-          return response.text();
-        } else {
-          return `//Fichier /m2d/${parametresURL}.m2d non trouvé`;
-        }
-      })
-      .then((text) => afficheSVG(text));
-  }
+		  if (response.ok) {
+			  return response.text();
+			} else {
+				return `//Fichier /m2d/${url.searchParams.get("url")}.m2d non trouvé`;
+			}
+		})
+		.then((text) => afficheSVG(text));
+	} else if (url.searchParams.get("script")){ // Si un script est présent dans l'URL
+    script = decodeURIComponent(url.searchParams.get("script"));
+    afficheSVG(script)
+	}
+
 };
 
 function executeCode(txt) {
   return Function(txt)();
+}
+
+function editScript(txt) {
+    window.open(window.location.href.replace('mathalea2dsvg.html','mathalea2d.html'))
 }
 
 function afficheSVG(text) {
