0efd0abb895e200b863e04a46aff62239c1fe802
Sébastien LOZANO <sebastien.lozano@ac-nancy-metz.fr>
2020-09-13 09:44:18 +0200

    beta6N11-4 trame

--- include/mathalea_exercices.js
+++ include/mathalea_exercices.js
@@ -14804,9 +14804,9 @@ function Ranger_ordre_croissant_decroissant(){
  this.beta = true;	
  this.sup=1;
  if (this.beta) {
-   this.nb_questions = 5;
+   this.nb_questions = 2;
  } else {
-   this.nb_questions = 3;
+   this.nb_questions = 2;
  };	
 
  this.titre = "Ranger une liste de nombres entiers dans l'ordre croissant ou décroissant";	
@@ -14822,9 +14822,10 @@ function Ranger_ordre_croissant_decroissant(){
 
  this.nouvelle_version = function(numero_de_l_exercice){
    if (this.beta) {
-     type_de_questions_disponibles = [0,1,2,3,4];			
+     type_de_questions_disponibles = [0,1];			
    } else {
-         type_de_questions_disponibles = shuffle([choice([1,3]),choice([2,4]),0]);      			
+         //type_de_questions_disponibles = shuffle([choice([1,3]),choice([2,4]),0]);      			
+         type_de_questions_disponibles = [0,1];			
    };
 
    this.liste_questions = []; // Liste de questions
@@ -14834,27 +14835,27 @@ function Ranger_ordre_croissant_decroissant(){
    let liste_type_de_questions = combinaison_listes_sans_changer_ordre(type_de_questions_disponibles,this.nb_questions) // Tous les types de questions sont posées --> à remettre comme ci dessus		
    
    for (let i = 0, texte, texte_corr, cpt=0; i < this.nb_questions && cpt<50; ) {
+     // les chiffres
+     let c1 = randint(1,9);
+     let c2 = randint(1,9,[c1]);
+     let c3 = randint(1,9,[c1,c2]);
+     let c4 = randint(1,9,[c1,c2,c3]);
+     let c5 = randint(1,9,[c1,c2,c3,c4]);
 
      // pour les situations, autant de situations que de cas dans le switch !
      let situations = [
        {//case 0 -->
+        ordre:'croissant',
        },
        {//case 1 -->
-       },
-       {//case 2 -->
-       },
-       {//case 3 -->
-       },
-       {//case 4 -->
-       },
-   
+        ordre:'décroissant',
+       },   
      ];
 
      let enonces = [];
      for (let k=0;k<situations.length;k++) {
        enonces.push({
-         enonce:`
-         Type ${k}				
+         enonce:`Dans l'ordre ${situations[k].ordre}<br>         
          `,
          question:``,
          correction:`
@@ -14865,57 +14866,27 @@ function Ranger_ordre_croissant_decroissant(){
            
            // autant de case que d'elements dans le tableau des situations
      switch (liste_type_de_questions[i]){
-       case 0 : 
-         texte = `${enonces[0].enonce}`;
-         if (this.beta) {
-           texte += `<br>`;
-           texte += `<br> =====CORRECTION======<br>${enonces[0].correction}`;
-           texte += `             `
-           texte_corr = ``;	
-         } else {
-           texte_corr = `${enonces[0].correction}`;
-         };
-               break;	
-           case 1 : 
-         texte = `${enonces[1].enonce}`;
-         if (this.beta) {
-           texte += `<br>`;
-           texte += `<br> =====CORRECTION======<br>${enonces[1].correction}`;
-           texte_corr = ``;	
-         } else {
-           texte_corr = `${enonces[1].correction}`;
-         };
-               break;
-           case 2 : 
-         texte = `${enonces[2].enonce}`;
-         if (this.beta) {
-           texte += `<br>`;
-           texte += `<br> =====CORRECTION======<br>${enonces[2].correction}`;
-           texte_corr = ``;	
-         } else {
-           texte_corr = `${enonces[2].correction}`;
-         };
-               break;				
-           case 3 : 
-         texte = `${enonces[3].enonce}`;
-         if (this.beta) {
-           texte += `<br>`;
-           texte += `<br> =====CORRECTION======<br>${enonces[3].correction}`;
-           texte_corr = ``;	
-         } else {
-           texte_corr = `${enonces[3].correction}`;
-         };
-         break;				
-            case 4 : 
-         texte = `${enonces[4].enonce}`;
-         if (this.beta) {
-           texte += `<br>`;
-           texte += `<br> =====CORRECTION======<br>${enonces[4].correction}`;
-           texte_corr = ``;	
-         } else {
-           texte_corr = `${enonces[4].correction}`;
-         };
-         break;				
+      case 0 : 
+        texte = `${enonces[0].enonce}`;
+        if (this.beta) {
+          texte += `<br>`;
+          texte += `<br> =====CORRECTION======<br>${enonces[0].correction}`;
+          texte += `             `
+          texte_corr = ``;	
+        } else {
+          texte_corr = `${enonces[0].correction}`;
+        };
+        break;	
+      case 1 : 
+        texte = `${enonces[1].enonce}`;
+        if (this.beta) {
+          texte += `<br>`;
+          texte += `<br> =====CORRECTION======<br>${enonces[1].correction}`;
+          texte_corr = ``;	
+        } else {
+          texte_corr = `${enonces[1].correction}`;
+        };
+        break;			
      };			
      
      if (this.liste_questions.indexOf(texte)==-1){ // Si la question n'a jamais été posée, on en créé une autre
@@ -14926,7 +14897,6 @@ function Ranger_ordre_croissant_decroissant(){
      cpt++;	
    }
    liste_de_question_to_contenu(this);
-
  }
  //this.besoin_formulaire_numerique = ['Niveau de difficulté',2,"1 : Entiers naturels\n2 : Entiers relatifs"];
  //this.besoin_formulaire2_case_a_cocher = ["Avec des équations du second degré"];	
