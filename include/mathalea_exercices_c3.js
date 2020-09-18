24d0e8dca3b3a9239ad64282dcdcfc332c27a3fe
Lhote Jean-Claude <lhotejc@gmail.com>
2020-09-18 22:46:13 +0200

    Ajout Cours Moyen. Fonctionnel ?

---
+++ include/mathalea_exercices_c3.js
@@ -0,0 +1,10 @@
+// Exercices de 6ème déclinés en cycle3
+function Ecrire_entiers_cycle3(){
+    Ecrire_nombres_entiers.call(this)
+    this.sup=1
+    this.sup2=1
+}
+function Division_cycle3(){
+    Divisions_euclidiennes.call(this)
+    this.sup=1
+}
