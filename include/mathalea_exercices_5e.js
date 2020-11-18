cac2f224e1d216e5b4251c5807341609b29e6dec
Sébastien LOZANO <sebastien.lozano@ac-nancy-metz.fr>
2020-11-18 17:11:03 +0100

    :wrench:5N11-1 correctif tooltip du formulaire

--- include/mathalea_exercices_5e.js
+++ include/mathalea_exercices_5e.js
@@ -7421,7 +7421,7 @@ function Tableaux_et_pourcentages(){
 		liste_de_question_to_contenu(this);
 
 	}
-	this.besoin_formulaire_numerique = ['Niveau de difficulté',2,"1 : coefficient enttier\n2 : coefficient décimal"];
+	this.besoin_formulaire_numerique = ['Niveau de difficulté',2,"1 : coefficient entier\n2 : coefficient décimal"];
 	//this.besoin_formulaire2_case_a_cocher = ["Avec des équations du second degré"];	
 };
 
