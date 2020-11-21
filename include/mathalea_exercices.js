df0b7384e4e72c635d21225c464023f1addf4abe
Gain <mireillegain64@gmail.com>
2020-11-21 18:38:48 +0100

    bug beta6N31-3

--- include/mathalea_exercices.js
+++ include/mathalea_exercices.js
@@ -16552,72 +16552,109 @@ function Ordre_de_grandeur_operations_decimaux(){
  * @author Mireille Gain, s'inspirant de 6N31-1 de Sébastien Lozano
  */
 
-function Arrondir_un_decimal(){
+function Arrondir_une_valeur(){
 	'use strict';
   Exercice.call(this); // Héritage de la classe Exercice()
 	this.titre = "Arrondir une valeur";	
   this.consigne = "Encadrer chaque nombre à l'unité, puis au dixième, puis au centième; dans chaque cas, mettre ensuite en évidence son arrondi.";
-  this.nb_questions = 4;
+  this.nb_questions = 3;
   this.nb_cols = 3;
   this.nb_cols_corr = 1;
-  this.sup = 1;
+  this.sup = 2;	
 
 	sortie_html? this.spacing_corr = 0.5 : this.spacing_corr = 1.5;
 
 	this.nouvelle_version = function(){
     this.liste_questions = [];
     this.liste_corrections = [];
-  let texte = "", texte_corr = "";	
-  
+  let m,c,d,u,di,ci,mi,me,ce,de,n,den,num,nb,rac;
   
 	for (let i = 0, texte, texte_corr, cpt=0; i < this.nb_questions && cpt<50;) {
-if (this.sup==1){
-      let m=randint(0,9),
-          c=randint(0,9),
-          d=randint(0,9),
-          u=randint(0,9),
-          di=randint(0,9),
-          ci=randint(0,9),
-          mi=randint(1,9),
-          me=randint(0,1),
-          ce=randint(0,1),
-          de=randint(0,1),
-          n=me*m*1000 + ce*c*100 + de*d*10 + u*1 + calcul(di*0.1 + ci*0.01 + mi*0.001);}
-else if (this.supp==2){n=4/7}
-else {n=Math.sqrt(7)}
-      texte=`$${tex_nombre(n)}$`
-      texte_corr="Encadrement et arrondi à l'unité : &ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;"
-      if (di<5) {
-      texte_corr+=`$${mise_en_evidence(tex_nombre(troncature(n,0)))} &ensp;< ${tex_nombre(n)} < ${tex_nombre(troncature(n+1,0))}$					`
+    
+    if (this.sup==1){
+      m=randint(0,9);
+      c=randint(0,9);
+      d=randint(0,9);
+      u=randint(0,9);
+      di=randint(1,9);
+      ci=randint(1,9);
+      mi=randint(1,9,5);
+      me=randint(0,1);
+      ce=randint(0,1);
+      de=randint(0,1);
+      n=calcul(me*m*1000 + ce*c*100 + de*d*10 + u*1 + di*0.1 + ci*0.01 + mi*0.001);
+      nb=n;
       }
-        else {
-        texte_corr+=`$${tex_nombre(troncature(n,0))} &ensp;< ${tex_nombre(n)} < ${mise_en_evidence(tex_nombre(troncature(n+1,0)))}$					`
+  
+    else if (this.supp==2){
+      den=choice([7,9,11,13]);
+      num=randint(1,50,[7,9,11,13,14,18,21,22,26,27,28,33,35,36,39,42,44,45,49]);
+      n=calcul(num/den);
+      nb=fraction(num,den);
+}
+    else if (this.supp==3){
+      rac=randint(3,99,[4,9,16,25,36,49,64,81]);
+      n=Math.sqrt(rac);
+      nb=tex_racine_carree(Math.sqrt(rac));
+      texte=`$${tex_racine_carree(Math.sqrt(rac))}$`;
+      texte_corr = '';
+    }
+
+    if (this.sup==1){
+      texte=`$${tex_nombre(nb)}$`;
+      texte_corr+="Encadrement et arrondi à l'unité : &ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;"
+      if (di<5) {
+      texte_corr+=`$${mise_en_evidence(tex_nombre(troncature(n,0)))} &ensp;< ${nb} < ${tex_nombre(troncature(n+1,0))}$`
+      }
+        else {  
+        texte_corr+=`$${tex_nombre(troncature(n,0))} &ensp;< ${nb} < ${mise_en_evidence(tex_nombre(troncature(n+1,0)))}$`
         }
+
       texte_corr+="<br>Encadrement et arrondi au dixième : &ensp;&ensp;&ensp; "
       if (ci<5) {
-        texte_corr+=`$${mise_en_evidence(tex_nombre(troncature(n,1)))} < ${tex_nombre(n)} < ${tex_nombre(troncature(n+0.1,1))}$					`
+        texte_corr+=`$${mise_en_evidence(tex_nombre(troncature(n,1)))} < ${nb} < ${tex_nombre(troncature(n+0.1,1))}$`
         }
        else {
-          texte_corr+=`$${tex_nombre(troncature(n,1))} &ensp;< ${tex_nombre(n)} < ${mise_en_evidence(tex_nombre(troncature(n+0.1,1)))}$					`
+          texte_corr+=`$${tex_nombre(troncature(n,1))} &ensp;< ${nb} < ${mise_en_evidence(tex_nombre(troncature(n+0.1,1)))}$`
           }
 
       texte_corr+="<br>Encadrement et arrondi au centième : &ensp;"
       if (mi<5) {
-        texte_corr+=`$${mise_en_evidence(tex_nombre(troncature(n,2)))} < ${tex_nombre(n)} < ${tex_nombre(troncature(n+0.01,2))}$					`
+        texte_corr+=`$${mise_en_evidence(tex_nombre(troncature(n,2)))} < ${nb} < ${tex_nombre(troncature(n+0.01,2))}$`
         }
         else {
-          texte_corr+=`$${tex_nombre(troncature(n,2))} &ensp;< ${tex_nombre(n)} < ${mise_en_evidence(tex_nombre(troncature(n+0.01,2)))}$					`
+          texte_corr+=`$${tex_nombre(troncature(n,2))} &ensp;< ${nb} < ${mise_en_evidence(tex_nombre(troncature(n+0.01,2)))}$`
           }
 
-      if (this.liste_questions.indexOf(texte)==-1){ // Si la question n'a jamais été posée, on en créé une autre
-				this.liste_questions.push(texte); // Sinon on enregistre la question dans liste_questions
-				this.liste_corrections.push(texte_corr); // On fait pareil pour la correction
-				i++; // On passe à la question suivante
-      }
-      cpt++;
-			}				
-		liste_de_question_to_contenu(this);
-  };
+  }
+
+
+  if (this.sup==2){    
+    texte=`$${tex_fraction(num,den)}$`;
+    texte_corr+="Encadrement et arrondi à l'unité : &ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;"
+    texte_corr+=`$${mise_en_evidence(tex_nombre(troncature(n,0)))} &ensp;< ${nb} < ${tex_nombre(troncature(n+1,0))}$`
+    texte_corr+=`$${tex_nombre(troncature(n,0))} &ensp;< ${nb} < ${mise_en_evidence(tex_nombre(troncature(n+1,0)))}$`
+    
+   
+    texte_corr+="<br>Encadrement et arrondi au dixième : &ensp;&ensp;&ensp; "
+    texte_corr+=`$${mise_en_evidence(tex_nombre(troncature(n,1)))} < ${nb} < ${tex_nombre(troncature(n+0.1,1))}$`
+    texte_corr+=`$${tex_nombre(troncature(n,1))} &ensp;< ${nb} < ${mise_en_evidence(tex_nombre(troncature(n+0.1,1)))}$`
+    
+    texte_corr+="<br>Encadrement et arrondi au centième : &ensp;"
+    texte_corr+=`$${mise_en_evidence(tex_nombre(troncature(n,2)))} < ${nb} < ${tex_nombre(troncature(n+0.01,2))}$`
+    texte_corr+=`$${tex_nombre(troncature(n,2))} &ensp;< ${nb} < ${mise_en_evidence(tex_nombre(troncature(n+0.01,2)))}$`
+        }
+
+    if (this.liste_questions.indexOf(texte)==-1){ // Si la question n'a jamais été posée, on en créé une autre
+      this.liste_questions.push(texte); // Sinon on enregistre la question dans liste_questions
+      this.liste_corrections.push(texte_corr); // On fait pareil pour la correction
+      i++; // On passe à la question suivante
+    }
+    cpt++;
+    }				
+  liste_de_question_to_contenu(this);
+}
+
     this.besoin_formulaire_numerique = ['Type de nombre', 3, `1 : Nombre décimal\n 2 : Fraction\n 3 : Racine carrée`]
 }
 
