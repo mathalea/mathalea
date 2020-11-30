7f11867cecb9a2502ca99a8ca9eabfe67a303dbc
Unknown <lhotejc@gmail.com>
2020-11-30 20:00:02 +0100

    beta3G11 : 6 minutes de codage !

--- include/mathalea_exercices_3e.js
+++ include/mathalea_exercices_3e.js
@@ -7647,8 +7647,8 @@ function Instruction_conditionelle(){
 };
 
 /**
- * Construction de translaté avec dispositif d'auto-correction aléatoire
- * Ref beta3G11 
+ * Construction d'images par rotation avec dispositif d'auto-correction aléatoire
+ * Ref beta3G10-3 
  * @Auteur Jean-Claude Lhote
  * Publié le 30/11/2020
  */
@@ -7719,7 +7719,102 @@ function Construire_rotation_point_3e(){
 		objets_enonce.push(tracePoint(M[i]),labelPoint(M[i]),cibles[i])
 		objets_correction.push(tracePoint(M[i],N[i]),labelPoint(M[i],N[i]),cibles[i])
 		objets_correction.push(arcPointPointAngle(M[i],N[i],angle,true,arcenciel(i),'gray',0.2)) 
-		texte_corr+=`$${noms[i]}\'$, le symétrique du point $${noms[i]}$ est dans la case ${cellules[i]} de la grille ${i+1}.<br>`
+		texte_corr+=`$${noms[i]}\'$, l\'image du point $${noms[i]}$ est dans la case ${cellules[i]} de la grille ${i+1}.<br>`
+	  }
+  
+	  for (let i=0;i<nbpoints;i++){
+		xMin=Math.min(xMin,N[i].x-3,M[i].x-3)
+		yMin=Math.min(yMin,N[i].y-3,M[i].y-3)
+		xMax=Math.max(xMax,N[i].x+3,M[i].x+3)
+		yMax=Math.max(yMax,N[i].y+3,M[i].y+3)
+	  }
+	  
+	  fenetreMathalea2d=[xMin,yMin,xMax,yMax]
+  
+	  this.liste_questions.push(mathalea2d({xmin:xMin,ymin:yMin,xmax:xMax,ymax:yMax,pixelsParCm:20,scale:0.7},objets_enonce))
+	  this.liste_corrections.push(texte_corr+mathalea2d({xmin:xMin,ymin:yMin,xmax:xMax,ymax:yMax,pixelsParCm:20,scale:0.7},objets_correction))
+	  liste_de_question_to_contenu(this)
+  
+	//  let nonchoisi,coords=[],x,y,objets_enonce=[],objets_correction=[],nomd,label_pos
+  
+	}
+   this.besoin_formulaire_numerique = ['Nombre de points (1 à 5)',5,"1\n2\n3\n4\n5"];
+   // this.besoin_formulaire2_case_a_cocher = ["Avec des points de part et d'autre"];	
+  }
+  /**
+ * Construction d'images par homothétie avec dispositif d'auto-correction aléatoire
+ * Ref beta3G11-3 
+ * @Auteur Jean-Claude Lhote
+ * Publié le 30/11/2020
+ */
+function Construire_homothetie_point_3e(){
+	Exercice.call(this); // Héritage de la classe Exercice()
+	this.titre = "Construire l\'image d'un point par une homothetie";
+	this.consigne = "Construire l\'image des points par l\'homothétie de centre O";
+	this.nb_questions = 1;
+	this.nb_questions_modifiable=false
+	this.nb_cols = 1;
+	this.nb_cols_corr = 1;
+	this.sup = 3;
+	this.nouvelle_version = function () {
+		let k = randint (-4,4,0)/2
+		this.consigne = "Construire l\'image des points par l\'homothétie de centre O";
+		this.consigne+=` et de rapport ${k}`
+	  this.liste_questions = []; // Liste de questions
+	  this.liste_corrections = []; // Liste de questions corrigées
+	  let result=[0,0],texte_corr="",nbpoints=parseInt(this.sup)
+  
+	  // On prépare la figure...
+	  let O=point(0,0,'O')
+	  let marks=['/','//','///','x','o','S','V']
+	  let noms=choisit_lettres_differentes(nbpoints,'QO',majuscule=true)
+	  let cibles=[],M=[],N=[],objets_enonce=[],objets_correction=[]  //cibles, M point marqués, N symétrique de M
+	  let cellules=[]
+	  let xMin,yMin,xMax,yMax
+	  [xMin,yMin,xMax,yMax]=[0,0,0,0]
+	  for (let i=0;i<nbpoints;i++) { //On place les cibles.
+		  N.push(point(calcul(randint(-80,80,0)/10),calcul(randint(-80,80,0)/10),noms[i]+"\'"))
+		  nontrouve=true
+		  while (longueur(N[i],O)<3||nontrouve) {
+			  nontrouve=true
+			  if (longueur(N[i],O)<3) {
+				N[i].x=calcul(randint(-80,80,0)/10)
+				N[i].y=calcul(randint(-80,80,0)/10)
+			  }
+			  else {
+				assezloin=true
+				for (let j=0;j<i;j++){
+				   if (longueur(N[i],N[j])<4.5) assezloin=false
+				}
+				if (assezloin==false) {
+				 N[i].x=calcul(randint(-80,80,0)/10)
+				 N[i].y=calcul(randint(-80,80,0)/10)
+				}
+				else nontrouve=false
+			  }
+		  }
+	  }
+  
+	  objets_enonce.push(tracePoint(O),labelPoint(O))
+	  objets_correction.push(tracePoint(O),labelPoint(O))
+  
+	  for (let i=0;i<nbpoints;i++){
+		cellules.push(choice(["A1","A2","A3","A4","B1","B2","B3","B4","C1","C2","C3","C4","D1","D2","D3","D4"]))
+		result=dansLaCibleCarree(N[i].x,N[i].y,4,0.6,cellules[i])
+		cible=cibleCarree({x:result[0],y:result[1],rang:4,num:i+1,taille:0.6})
+		cible.taille=0.6
+		cible.color='orange'
+		cible.opacite=0.7
+		cibles.push(cible)
+	  }
+	  for (let i=0;i<nbpoints;i++) {
+		M.push(homothetie(N[i],O,1/k,noms[i]))
+		objets_enonce.push(tracePoint(M[i]),labelPoint(M[i]),cibles[i])
+		objets_correction.push(tracePoint(M[i],N[i]),labelPoint(M[i],N[i]),cibles[i])
+		s=segment(M[i],N[i])
+		s.color=arcenciel(i)
+		objets_correction.push(s) 
+		texte_corr+=`$${noms[i]}\'$, l\'image du point $${noms[i]}$ est dans la case ${cellules[i]} de la grille ${i+1}.<br>`
 	  }
   
 	  for (let i=0;i<nbpoints;i++){
