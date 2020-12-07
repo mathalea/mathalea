bcc77c341785918add2f3fa6c00a841b57e501e0
lhotejc <lhotejc@gmail.com>
2020-12-07 19:39:06 +0100

    Sauvegarde pour transfert de PC

--- include/mathalea_exercices.js
+++ include/mathalea_exercices.js
@@ -14838,7 +14838,9 @@ function Exercice_labyrinthe_multiples() {
 
 /**
  * @Auteur Jean-Claude Lhote
- * 
+ * Publié le 7/12/2020
+ * Ref beta6C12
+ * Sortir du labyrinthe en utilisant les critères de divisibilité.
  */
 function Exercice_labyrinthe_divisibilite() {
   "use strict"
@@ -14916,6 +14918,143 @@ function Exercice_labyrinthe_divisibilite() {
       listeMultiples.push(table * i)
     }
     listeMultiples = combinaison_listes(listeMultiples, 12)
+    for (let a = 1; a < 7; a++) {
+      for (let b = 0; b < 3; b++) {
+        trouve = false
+        for (let k = 0; k < monchemin.length; k++){
+          if (monchemin[k][0] == a && monchemin[k][1] == b) trouve = true
+        }
+        if (!trouve) {
+          laby.nombres[a - 1][b] = randint(200,5000) * table + randint(1, table - 1)
+        }
+        else {
+          laby.nombres[a - 1][b] = listeMultiples[index]
+          index++
+        }
+      }
+    } // Le tableau de nombre étant fait, on place les objets nombres.
+    laby.nombres2d = laby.placeNombres(laby.nombres,1)
+    params = { xmin: -4, ymin: 0, xmax: 22, ymax: 11, pixelsParCm: 20, scale: 0.7 }
+    texte += mathalea2d(params, laby.murs2d, laby.nombres2d)
+    texte_corr += mathalea2d(params, laby.murs2d, laby.nombres2d, laby.chemin2d)
+    this.contenu = texte
+    this.contenu_correction = texte_corr;
+  }
+  this.besoin_formulaire_numerique = ["Critère de divisibilité ",5,'1 : Par 2\n2 : Par 3\n3 : Par 4\n4 : Par 5\n5 : Par 9']
+  this.besoin_formulaire2_numerique = ["Critère de divisibilité supplémentaire ",6,'1 : Aucun\n2 : Par 2\n3 : Par 3\n4 : Par 4\n5 : Par 5\n6 : Par 9'];
+  this.besoin_formulaire3_numerique = ['Niveau de rapidité', 6, '1 : Escargot\n 2 : Tortue\n 3 : Lièvre\n 4 : Antilope\n 5 : Guépard\n 6 : Au hasard']
+} // Fin de l'exercice.
+function Exercice_labyrinthe_numeration() {
+  "use strict"
+  Exercice.call(this)
+  this.titre = "Labyrinthe de numération décimale";
+  this.consigne=""
+  this.niveau = '6e'
+  this.nb_questions = 1;
+  this.nb_questions_modifiable = false
+  this.nb_cols = 1;
+  this.nb_cols_corr = 1;
+  this.pas_de_version_LaTeX = false
+  this.pas_de_version_HMTL = false
+  this.sup3 = 3
+  this.sup = 9;
+
+  //this.consigne=`Trouve la sortie en ne passant que par les cases contenant un nombre divisible par $${parseInt(this.sup)}$.`
+
+  this.nouvelle_version = function (numero_de_l_exercice) {
+    let params, texte, texte_corr, trouve
+    let laby = labyrinthe()
+    laby.niveau = parseInt(this.sup3) // Le niveau (de 1 à 6=mélange) définit le nombre d'étapes
+    laby.chemin = laby.choisitChemin(laby.niveau) // On choisi un chemin
+    laby.murs2d = laby.construitMurs(laby.chemin) // On construit le labyrinthe
+    laby.chemin2d = laby.traceChemin(laby.chemin) // On trace le chemin solution
+    let monchemin = laby.chemin
+    let positions=[['unités',1],['dixièmes',0.1],['centièmes',0.01],['millièmes',0.001],['dizaines',10],['centaines',100],['unités de mille',1000],['dix-millièmes',0.0001],['dizaines de mille',10000],['centaines de mille',100000],['unités de millions',1000000]]
+    let chiffre
+    let listeNombresOK = [], index = 0,rangMax,rang
+    if (this.niveau = 'CM') {
+      rangMax=6
+    }
+    else {
+      rangMax=10
+    }
+    rang=randint(0,rangMax)
+    chiffre=randint(0,9)
+    texte = `${texte_en_couleur_et_gras(`Trouve la sortie en ne passant que par les cases contenant un nombre dont le chiffre des ${positions[rang][0]} est un `,'black')}$${chiffre}$.<br>`
+    texte_corr = `${texte_en_couleur_et_gras(`Voici le chemin en marron et la sortie était la numéro $${2 - monchemin[monchemin.length - 1][1] + 1}$.`, 'black')}<br>`
+    // Zone de construction du tableau de nombres : Si ils sont sur monchemin et seulement si, ils doivent vérifier la consigne
+    let uM,Cm,Dm,Um,C,D,U,d,c,m,dm,nombretemp
+    for (let i = 0; i <= 20; i++) {
+      if (positions[rang][0]!='unités de millions') {
+        uM=choice ([0,0,0,1,2],chiffre)
+      }
+      else {
+        uM=chiffre
+      }
+      if (positions[rang][0]!='centaines de mille') {
+        Cm=randint (0,9,chiffre)
+      }
+      else {
+        Cm=chiffre
+      }
+      if (positions[rang][0]!='dizaines de mille') {
+        Dm=randint (0,9,chiffre)
+      }
+      else {
+        Dm=chiffre
+      }
+      if (positions[rang][0]!='unités de mille') {
+        Um=randint (0,9,chiffre)
+      }
+      else {
+        Um=chiffre
+      }
+      if (positions[rang][0]!='centaines') {
+        C=randint (0,9,chiffre)
+      }
+      else {
+        C=chiffre
+      }
+      if (positions[rang][0]!='dizaines') {
+        D=randint (0,9,chiffre)
+      }
+      else {
+        D=chiffre
+      }
+      if (positions[rang][0]!='unités') {
+        U=randint (0,9,chiffre)
+      }
+      else {
+        U=chiffre
+      }
+      if (positions[rang][0]!='dixièmes') {
+        d=randint (0,9,chiffre)
+      }
+      else {
+        d=chiffre
+      }
+      if (positions[rang][0]!='centièmes') {
+        c=randint (0,9,chiffre)
+      }
+      else {
+        c=chiffre
+      }
+      if (positions[rang][0]!='millièmes') {
+        m=randint (0,9,chiffre)
+      }
+      else {
+        m=chiffre
+      }
+      if (positions[rang][0]!='dix-millièmes') {
+        dm=randint (0,9,chiffre)
+      }
+      else {
+        dm=chiffre
+      }
+      nombretemp=calcul2(uM*1000000+Cm*100000+Dm*10000+Um*1000+C*100+D*10+U+d*0.1+c*0.01+m*0.001+dm*0.0001)
+      listeNombresOK.push(nombretemp)
+    }
+    listeNombresOK = combinaison_listes(listeNombresOK, 12)
     for (let a = 1; a < 7; a++) {
       for (let b = 0; b < 3; b++) {
         trouve = false
