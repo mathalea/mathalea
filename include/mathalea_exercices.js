d22b698158653b117f175afecc6e6778c68b629b
Rémi Angot <remi.angot@gmail.com>
2020-12-11 08:01:19 +0100

    6C31-2 suppression des 5 au dénominateur pour que le nombre ne soit pas décimal + troncature + meilleur mélange des questions

--- include/mathalea_exercices.js
+++ include/mathalea_exercices.js
@@ -11114,18 +11114,20 @@ function Valeur_approchee_division_decimale() {
      {
       // Une fraction irréductible avec un dénominateur qui comporte un facteur différent de 2 ou de 5
       // aura une écriture décimale périodique infinie
-      let k1 = choice([3,5,7,11,13]);
-      let k2 = choice([3,5,7,11,13],k1);
+      let k1 = choice([3,7,11,13]);
+      let k2 = choice([3,7,11,13],k1);
       let a = choice([3,5,7,11,13],[k1,k2])*choice([3,5,7,11,13],[k1,k2])
       let b = k1*k2
       let q = math.round(a/b,6)
       texte = `On sait que $${a}\\div${b}\\approx${tex_nombre(q)}$.`;
       let liste_de_questions1 = [
-        [`La valeur approchée par défaut de $${a}\\div${b}$ au dixième près est : `, math.floor(a/b,1)],
-        [`La valeur approchée par excès de $${a}\\div${b}$ au dixième près est : `, math.ceil(a/b,1)],
+        [`La valeur approchée par défaut de $${a}\\div${b}$ au dixième près est : `, math.floor(a/b,1)], 
+        [`La valeur approchée par excès de $${a}\\div${b}$ au dixième près est : `, math.ceil(a/b,1)], 
         [`La valeur approchée par défaut de $${a}\\div${b}$ au centième près est : `, math.floor(a/b,2)],
+        [`La troncature de $${a}\\div${b}$ au centième près est : `, math.floor(a/b,2)],
         [`La valeur approchée par excès de $${a}\\div${b}$ au centième près est : `, math.ceil(a/b,2)],
         [`La valeur approchée par défaut de $${a}\\div${b}$ au millième près est : `, math.floor(a/b,3)],
+        [`La troncature de $${a}\\div${b}$ au millième près est : `, math.floor(a/b,3)],
         [`La valeur approchée par excès de $${a}\\div${b}$ au millième près est : `, math.ceil(a/b,3)],
       ]
       let liste_de_questions2 = [
@@ -11134,16 +11136,41 @@ function Valeur_approchee_division_decimale() {
         [`La valeur approchée de $${a}\\div${b}$ au millième près est : `, math.round(a/b,3)],
       ]
 
-      shuffle(liste_de_questions1)
+      texte_corr = `On sait que $${a}\\div${b}\\approx${tex_nombre(q)}$.`;
+      // Questions peuvent être défaut, excès ou excès, défaut ou troncature, excès ou excès, troncature
+      let choix = randint(1,4);
+      switch (choix) {
+        case 1:
+          texte+= `<br><br> ${num_alpha(0)} ${liste_de_questions1[0][0]}\\ldots`
+          texte+= `<br><br> ${num_alpha(1)} ${liste_de_questions1[4][0]}\\ldots`
+          texte_corr+= `<br><br> ${num_alpha(0)} ${liste_de_questions1[0][0]} $ ${tex_nombre(liste_de_questions1[0][1])}$`
+          texte_corr+= `<br><br> ${num_alpha(1)} ${liste_de_questions1[4][0]} $ ${tex_nombre(liste_de_questions1[4][1])}$`
+          break;
+        case 2:
+          texte+= `<br><br> ${num_alpha(0)} ${liste_de_questions1[1][0]}\\ldots`
+          texte+= `<br><br> ${num_alpha(1)} ${liste_de_questions1[5][0]}\\ldots`
+          texte_corr+= `<br><br> ${num_alpha(0)} ${liste_de_questions1[1][0]} $ ${tex_nombre(liste_de_questions1[1][1])}$`
+          texte_corr+= `<br><br> ${num_alpha(1)} ${liste_de_questions1[5][0]} $ ${tex_nombre(liste_de_questions1[5][1])}$`
+          break;
+        case 3:
+          texte+= `<br><br> ${num_alpha(0)} ${liste_de_questions1[3][0]}\\ldots`
+          texte+= `<br><br> ${num_alpha(1)} ${liste_de_questions1[7][0]}\\ldots`
+          texte_corr+= `<br><br> ${num_alpha(0)} ${liste_de_questions1[3][0]} $ ${tex_nombre(liste_de_questions1[3][1])}$`
+          texte_corr+= `<br><br> ${num_alpha(1)} ${liste_de_questions1[7][0]} $ ${tex_nombre(liste_de_questions1[7][1])}$`
+          break;
+        case 4:
+          texte+= `<br><br> ${num_alpha(0)} ${liste_de_questions1[4][0]}\\ldots`
+          texte+= `<br><br> ${num_alpha(1)} ${liste_de_questions1[7][0]}\\ldots`
+          texte_corr+= `<br><br> ${num_alpha(0)} ${liste_de_questions1[4][0]} $ ${tex_nombre(liste_de_questions1[4][1])}$`
+          texte_corr+= `<br><br> ${num_alpha(1)} ${liste_de_questions1[7][0]} $ ${tex_nombre(liste_de_questions1[7][1])}$`
+          break;
+      }
       shuffle(liste_de_questions2)
-      texte+= `<br><br> ${num_alpha(0)} ${liste_de_questions1[0][0]}\\ldots`
-      texte+= `<br><br> ${num_alpha(1)} ${liste_de_questions1[1][0]}\\ldots`
+      
       texte+= `<br><br> ${num_alpha(2)} ${liste_de_questions2[0][0]}\\ldots`
       texte+= `<br><br> ${num_alpha(3)} ${liste_de_questions2[1][0]}\\ldots`
       
-      texte_corr = `On sait que $${a}\\div${b}\\approx${tex_nombre(q)}$.`;
-      texte_corr+= `<br><br> ${num_alpha(0)} ${liste_de_questions1[0][0]} $ ${tex_nombre(liste_de_questions1[0][1])}$`
-      texte_corr+= `<br><br> ${num_alpha(1)} ${liste_de_questions1[1][0]} $ ${tex_nombre(liste_de_questions1[1][1])}$`
+      
       texte_corr+= `<br><br> ${num_alpha(2)} ${liste_de_questions2[0][0]} $ ${tex_nombre(liste_de_questions2[0][1])}$`
       texte_corr+= `<br><br> ${num_alpha(3)} ${liste_de_questions2[1][0]} $ ${tex_nombre(liste_de_questions2[1][1])}$`
 
