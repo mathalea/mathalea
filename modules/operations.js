989b6b9b0518b48229c2dea0e5a870fe1dbbef59
lhotejc <lhotejc@gmail.com>
2021-01-03 17:48:02 +0100

    Division2d semble fonctionner... +ordreDeGrandeur

--- modules/operations.js
+++ modules/operations.js
@@ -1,33 +1,10 @@
+import { ordreDeGrandeur } from "/modules/outils.js"
 import { mathalea2d,texteParPosition,segment} from "/modules/2d.js"
 
 export default function Operation({ operande1 = 1, operande2 = 2, type = 'addition' }) {
 
-    let Code=``
-    let cacherleszeros = function(chaine){ //tilisée pour créer une chaine de retenues, un zéro sera mis en blanc sur blanc.
-        let resultat=""
-        for (let i=0;i<chaine.length;i++){
-            if (chaine[i]!='0'){
-                resultat+=chaine[i]
-            }
-            else {
-                resultat+=``
-            }
-        }
-        return resultat
-    }
-    let cacherleszerosbarrelesautres = function(chaine){ //tilisée pour créer une chaine de retenues, un zéro sera mis en blanc sur blanc.
-        let resultat=""
-        for (let i=0;i<chaine.length;i++){
-            if (chaine[i]!='0'){
-                resultat+=`$\\cancel{${chaine[i]}}$`
-            }
-            else {
-                resultat+=`<font color=#FFFFFF>$0$</font>`
-            }
-        }
-        return resultat
-    }
-
+    let Code
+  
     let cacherleszerosdevant = function(chaine){
         let blancs=""
         while (chaine[0]=='0') {
@@ -39,66 +16,71 @@ export default function Operation({ operande1 = 1, operande2 = 2, type = 'additi
         }
         return blancs
     }
-    let cacherboutdenombre = function (nombre, boutocculte,souligne) { //fonction qui ne laisse visible qu'une partie d'un nombre et le souligne si besoin
-        let chaine; // retourne une chaine avec un nombre en mode maths.
-        let soulignedebut,soulignefin
-        if (souligne) {
-            soulignedebut=`\\underline{`
-            soulignefin=`}`
-        }
-        else {
-            soulignedebut=``
-            soulignefin=``
-        }
-        chaine = Number(nombre).toString();
-        if ((boutocculte > 0) && (boutocculte < chaine.length)) {
-            chaine = `$${soulignedebut}${chaine.substr(0, chaine.length - boutocculte)}${soulignefin}$<font color=#FFFFFF>$${chaine.substr(chaine.length - boutocculte)}$</font>`;
-        }
-        else {
-            chaine=`$${soulignedebut}${chaine}${soulignefin}$`
-        }
-        return chaine;
-    }
-
-    let DivisionPoseeHtml = function (dividende, diviseur) { // retourne le code de la division posée en html
-        let longueurquotiententier, code = "", quotiententier, stringquotiententier
-        let chiffrequotient, dividendetemp, lignes, index, longtronc, restes = [], quotients = [], troncatures = [];
-        quotiententier = Math.floor(dividende / diviseur);
-        dividende = Number(dividende).toString();
-        stringquotiententier = Number(quotiententier).toString();
-        longueurquotiententier = stringquotiententier.length;
-        lignes = '';
-        for (let i = 0; i < longueurquotiententier - 1; i++) {
-            lignes += '0';
-        }
-        dividendetemp = parseInt(dividende);
-        index = 0;
-        longtronc = stringquotiententier.length - 1;
-        for (let i = 0; i < longueurquotiententier; i++) {
-            chiffrequotient = stringquotiententier.charAt(i) + lignes;
-            dividendetemp = dividendetemp - parseInt(chiffrequotient) * diviseur;
-            if (stringquotiententier.charAt(i) != '0') {
-                quotients[index] = parseInt(chiffrequotient) * diviseur;
-                restes[index] = dividendetemp;
-                troncatures[index] = longtronc;
-                index++;
+  
+    let DivisionPosee2d = function (divid,divis){
+        let objets=[]
+        let ecriresoustraction =function(i,j,P){
+            objets.push(texteParPosition('-',i-1,10-i*2,'milieu','black',1.2,'middle',true))
+            for (let k=i;k<=j;k++){
+                objets.push(texteParPosition(P[k-i],k,10-i*2,'milieu','black',1.2,'middle',true))
             }
-            lignes = lignes.substr(1);
-            longtronc--;
         }
-        troncatures[index] = 0;
-        longueurquotiententier = quotients.length;
-        lignes = "";
-        index = dividende.length;
-        for (let i = 0; i < longueurquotiententier; i++) {
-            lignes += `$-$<font color=#FFFFFF>_</font>${cacherboutdenombre(quotients[i], troncatures[i],true)}<br>${cacherboutdenombre(restes[i], troncatures[i + 1])}<br>`;
-            index = (restes[i] + '').length;
+        let ecrirereste = function(i,j,R){
+            objets.push(segment(i-1,9.6-i*2,i+R.length,9.6-i*2))
+            for (let k=R.length-1;k>=0;k--){
+                objets.push(texteParPosition(R[k],i+1+k,9-i*2,'milieu','black',1.2,'middle',true))
+            }
         }
-        stringquotiententier = quotiententier + '';
-        code = `<table border=0 cellspacing=0 cellpadding=2 class="res" style="line-height:1.2em"><tr><td align=right>$${dividende}$</td><td align=left style=border-bottom-style:solid;border-bottom-width:2px;border-left-style:solid;border-left-width:2px;>$${diviseur}$</td></tr>`;
-        code += `<tr><td align=right >${lignes}</td><td align=left valign=top style=border-left-style:solid;border-left-width:2px;border-left-color: black;>$${stringquotiententier}$</td></tr></table>`;
+        let ecrirequotient=function(i,Q){
+            objets.push(texteParPosition(Q,n+0.5+i,10,'milieu','black',1.2,'middle',true))
+        }
+
+        let divd=[],Q=[],R=[],P=[]
+        let dividende=Number(divid).toString()
+        let diviseur=Number(divis).toString()
+        let n=Math.log10(ordreDeGrandeur(divid,1)) //nombre de chiffres du dividende
+        let m=Math.log10(ordreDeGrandeur(divis,1))  //nombre de chiffre du diviseur
+        for (let i=0;i<n;i++){ //on écrit le dividende
+            objets.push(texteParPosition(dividende[i],i,11,'milieu','black',1.2,'middle',true))
+        }
+        for (let i=0;i<m;i++){ //on écrit le dividende
+            objets.push(texteParPosition(diviseur[i],i+n+0.5,11,'milieu','black',1.2,'middle',true))
+        }
+        
+
+        objets.push(segment(n,11.5,n,11.5-n*2)) //on trace le trait vertical
+        objets.push(segment(n,10.5,n+m,10.5)) //on trace le trait horizontal
+
+        let i=0,j=m-1
+        divd.push(dividende.substr(0,m))
+        if (parseInt(divd[0])<divis){
+            divd[0]+=dividende.substr(m,1)
+            j++
+        }
+        while (j<n){
+        Q.push(Number(Math.floor(parseInt(divd[i])/divis)).toString())
+        R.push(Number(parseInt(divd[i])%divis).toString())
+        P.push(Number(parseInt(Q[i])*divis).toString())
+        if (Q[i]=='0'){
+            for (let z=0;z<m;z++){
+                P[i]+='0'
+            }
+        }
+        console.log(Q[i],R[i],P[i],'i=',i,'j=',j)
+        ecriresoustraction(i,j,P[i])
+        j++
+        if (j<n) {
+            R[i]+=dividende.substr(j,1)
+        }
+        divd.push(R[i])
+        ecrirereste(i,j,R[i])
+        ecrirequotient(i,Q[i])
+        i++
+        }
+        let code =mathalea2d({xmin:-1.5,ymin:10-2*n,xmax:n+m+1,ymax:11.5,pixelsParCm:20,scale:0.8},objets)
         return code
     }
+
     let AdditionPosee2d=function(operande1,operande2){
         let code = "",objets=[]
         let sop1 = Number(operande1).toString()
@@ -147,55 +129,6 @@ export default function Operation({ operande1 = 1, operande2 = 2, type = 'additi
         return code
         }
 
-   /* let AdditionPoseeHtml = function(operande1,operande2){
-        let code = ""
-        let sop1 = Number(operande1).toString()
-        let sop2 = Number(operande2).toString()
-        let sresultat,resultat,lresultat
-        let lop1 = sop1.length
-        let lop2 = sop2.length
-        let longueuroperandes=Math.max(lop1,lop2)
-        let retenues=""
-        if (lop1>lop2){ // si op1 a plus de chiffres qu'op2 on complète op2 avec des zéros.
-            for(let j=0;j<lop1-lop2;j++){
-                sop2=`0`+sop2
-            }
-        }
-        else if (lop2>lop1) { //on fait pareil pour op1 si c'est op2 le plus 'grand'
-            for(let j=0;j<lop2-lop1;j++){
-            sop1=`0`+sop1
-            }
-        }
-        // les deux operande ont le même nomre de chiffres
-        for (let i=longueuroperandes-1;i>0;i--){ // on construit la chaine des retenues.
-            if (parseInt(sop1[i])+parseInt(sop2[i])>9) {
-                retenues=`1${retenues}`
-            }
-            else {
-                retenues=`0${retenues}`
-            }
-        }
-        retenues=`0`+retenues
-        sop1=`0${sop1}`
-        retenues=cacherleszeros(retenues)
-        resultat = operande1 + operande2
-        sresultat = Number(resultat).toString()
-        lresultat=sresultat.length
-        for (let i =0;i<longueuroperandes+1-lresultat;i++){
-            sresultat=`0${sresultat}`
-        }
-        code += `<div id="addition" class="additionBox" style="position: static;">`
-        code +=`<div id="retenues" class="retenuesBox" style="position: relative; left:1.2em; top:-0.5em; color:red">${retenues}</div>`
-        code +=`<div id="terme1" style="position: relative; top:-0.5em" line-height: 0.8em>${cacherleszerosdevant(sop1)}</div>`
-        code +=`<div id="terme2" style="position: relative; top:-0.8em" line-height: 0.8em>+${cacherleszerosdevant(sop2)}</div>`
-        code+=`<div id="barrea" style="position:relative; top:-1.4em;"><hr align=left width=${(longueuroperandes+2)*10}></div>`
-        code +=`<div id="somme" style="position: relative;top:-2em;" line-height: 0.8em>${cacherleszerosdevant(sresultat)}</div>`
-       
-
-        code+=`</div>`;
-        return code
-    }
-*/
     let SoustractionPosee2d = function(operande1,operande2){
         let code = "",sop1,sop2,objets=[]
         if (operande1<operande2) {
@@ -246,57 +179,8 @@ export default function Operation({ operande1 = 1, operande2 = 2, type = 'additi
         code +=mathalea2d({xmin:-0.5,ymin:0,xmax:longueuroperandes,ymax:5,pixelsParCm:20,scale:0.8},objets)
         return code
     }
- /*   let SoustractionPoseeHtml = function(operande1,operande2){
-        let code = "",sop1,sop2
-        if (operande1<operande2) {
-            sop2 = Number(operande1).toString()
-            sop1 = Number(operande2).toString()
-        }
-        else {
-            sop1 = Number(operande1).toString()
-            sop2 = Number(operande2).toString()
-        }
-        let sresultat,resultat,lresultat
-        let lop1 = sop1.length
-        let lop2 = sop2.length
-        let longueuroperandes=Math.max(lop1,lop2)
-        let retenues="0"
-        if (lop1>lop2){ // si op1 a plus de chiffres qu'op2 on complète op2 avec des zéros.
-            for(let j=0;j<lop1-lop2;j++){
-                sop2=`0`+sop2
-            }
-        }
-        // les deux operande ont le même nomre de chiffres
-        for (let i=longueuroperandes-1;i>0;i--){ // on construit la chaine des retenues.
-            if (parseInt(sop1[i])<(parseInt(sop2[i])+parseInt(retenues.charAt(0)))) {
-                retenues=`1${retenues}`
-            }
-            else {
-                retenues=`0${retenues}`
-            }
-        }
-        retenues=`0${retenues}`
-        sop1=`0${sop1}`
-        retenues=cacherleszeros(retenues)
-        resultat = operande1 - operande2
-        sresultat = Number(resultat).toString()
-        lresultat=sresultat.length
-        for (let i =0;i<longueuroperandes+1-lresultat;i++){
-            sresultat=`0${sresultat}`
-        }
-        code += `<div id="soustraction" class="additionBox" style="position: relative">`
-        code +=`<div id="retenuesh" class="retenuesBox" style="position: relative; left:1.2em; top:-0.5em; color:red">${retenues}</div>`
-        code +=`<div id="terme1" style="position: relative; top:-0.5em" line-height: 0.8em>${cacherleszerosdevant(sop1)}</div>`
-        code +=`<div id="terme2" style="position: relative; left:-0.3em; top:-0.8em" line-height: 0.8em>$-$${cacherleszerosdevant(sop2)}</div>`
-        code+=`<div id="retenuesb" class="retenuesBox" style="position: relative; left:0.2em; top:-2.5em; color: blue" line-height: 0.8em>${retenues}</div>`
-        code+=`<div id="barres" style="position:relative; top:-1.8em;"><hr align=left width=${(longueuroperandes+2)*10}></div>`
-        code +=`<div id="difference" style="position: relative;top:-2.5em;" line-height: 0.8em>${cacherleszerosdevant(sresultat)}</div>`
-        code+=`</div>`;
-        return code
-    }
-    */
     let MultiplicationPosee2d = function(operande1,operande2){
-        let code = "",sop1,sop2,objets=[]
+        let code,sop1,sop2,objets=[]
         let produits=[],strprod,sommes=[]
         if (operande1<operande2) {
             sop2 = Number(operande1).toString()
@@ -396,7 +280,7 @@ export default function Operation({ operande1 = 1, operande2 = 2, type = 'additi
                 if (sresultat[i]!=' ') objets.push(texteParPosition(sresultat[i],i*0.6,4.5-lop2,'milieu','black',1.3,'middle',true))
             }
                 
-        code +=mathalea2d({xmin:-0.5,ymin:4-lop2,xmax:longueurtotale+2,ymax:8,pixelsParCm:20,scale:0.8},objets)
+        code =mathalea2d({xmin:-0.5,ymin:4-lop2,xmax:longueurtotale+2,ymax:8,pixelsParCm:20,scale:0.8},objets)
 
 
         return code
@@ -405,37 +289,16 @@ export default function Operation({ operande1 = 1, operande2 = 2, type = 'additi
 
     switch (type) {
         case 'addition':
-            if (sortie_html) {
-               Code+=AdditionPosee2d(operande1,operande2)
-            }
-            else {
-                Code += `$\\opadd{${operande1}}{${operande2}}$`
-            }
+               Code=AdditionPosee2d(operande1,operande2)
             break
         case 'soustraction':
-            if (sortie_html) {
-                Code+=SoustractionPosee2d(operande1,operande2)
-             }
-             else {
-                 Code += `$\\opsub{${operande1}}{${operande2}}$`
-             }           
+                Code=SoustractionPosee2d(operande1,operande2)
             break
         case 'multiplication':
-            if (sortie_html) {
-                Code+=MultiplicationPosee2d(operande1,operande2)
-             }
-             else {
-                 Code += `$\\opmul{${operande1}}{${operande2}}$`
-             }           
-  
+                Code=MultiplicationPosee2d(operande1,operande2)
             break
         case 'division':
-            if (sortie_html) {
-                Code +=DivisionPoseeHtml(operande1, operande2);
-            }
-            else {
-                Code +=`$\\opidiv[voperation=top]{${operande1}}{${operande2}}$`
-            }
+                Code =DivisionPosee2d(operande1, operande2);
             break
     }
     return Code
