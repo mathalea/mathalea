6bfa50a1af4ea994da074163bac1ee11efa35fc6
lhotejc <lhotejc@gmail.com>
2021-01-25 19:11:20 +0100

    Modifications 3G40

--- exercices/3e/3G40.js
+++ exercices/3e/3G40.js
@@ -1,7 +1,8 @@
 import Exercice from '../ClasseExercice.js';
-import {liste_de_question_to_contenu,randint,choisit_lettres_differentes} from "/modules/outils.js"
+import {num_alpha,combinaison_listes,randint,choisit_lettres_differentes} from "/modules/outils.js"
 import {mathalea2d,tracePoint,labelPoint} from "/modules/2d.js"
 import {point3d,droite3d,vecteur3d,arete3d,sphere3d,rotation3d,rotationV3d,demicercle3d} from "/modules/3d.js"
+import { sens_de_rotation3d } from '/modules/3d.js';
 
 export default function ReperageSurLaSphere() {
     "use strict"
@@ -22,12 +23,24 @@ export default function ReperageSurLaSphere() {
     this.nouvelle_version = function (){
     this.liste_questions = [] // tableau contenant la liste des questions 
     this.liste_corrections = []
+    let liste_type_de_questions=[]
+    if (this.sup==1) liste_type_de_questions=combinaison_listes([1],this.nb_questions)
+    else if (this.sup==2) liste_type_de_questions=combinaison_listes([2],this.nb_questions)
+    else liste_type_de_questions=combinaison_listes([1,2],this.nb_questions)
       this.contenu=''
       this.contenu_correction=''
       let O=point3d(0,0,0,false,'O')
       let M=point3d(10,0,0,true,'M')
-      let PoleNord=point3d(0,0,10,false,'N')
-      let PoleSud=point3d(0,0,-10,false,'S')
+      let PoleNord=point3d(0,0,11,false,'N')
+      let PoleSud=point3d(0,0,-11,false,'S')
+      PoleNord.p2d.positionLabel='above'
+      let Pn=labelPoint(PoleNord.p2d)
+      PoleSud.p2d.positionLabel='below'
+      let Ps=labelPoint(PoleSud.p2d)
+      Pn.taille=3
+      Ps.taille=3
+      Pn.color='brown'
+      Ps.color='brown'
 
       let Axe=arete3d(PoleSud,PoleNord)
       Axe.p2d.epaisseur=2
@@ -41,15 +54,15 @@ export default function ReperageSurLaSphere() {
       let equateur1=demicercle3d(O,normalV,R,'visible','red',0)
       let equateur2=demicercle3d(O,normalV,R,'caché','red',0)
       let greenwitch=demicercle3d(O,normalH,vecteur3d(0,0,-10),'visible','green',0)
-      greenwitch.epaisseur=3
-      greenwitch.opacite=0.3
+      let rotationTerre=sens_de_rotation3d(droite3d(PoleSud,normalV),vecteur3d(-4,-8,4),120,3,'purple')
+      greenwitch.epaisseur=4
+      greenwitch.opacite=1
       equateur1.epaisseur=3
       equateur2.epaisseur=3
-      let objets_enonce = [],params_enonce // on initialise le tableau des objets Mathalea2d de l'enoncé
-    //  let objets_correction = [] // Idem pour la correction
+      let objets_enonce = [],params_enonce,objets_correction=[]// on initialise les tableaux des objets Mathalea2d
       let latitudes=[],longitudes=[],P=[],EstouOuest=[],NordouSud=[],nom=[]
-      objets_enonce.push(Sph,Axe.p2d,equateur1,equateur2,greenwitch)
- //     objets_correction.push(Sph,Axe.p2d,equateur1,equateur2,greenwitch)
+      objets_enonce.push(Sph,Axe.p2d,equateur1,equateur2,greenwitch,Pn,Ps,rotationTerre)
+      objets_correction.push(Sph,Axe.p2d,equateur1,equateur2,greenwitch,Pn,Ps)
       for (let i=0;i<this.nb_questions;i++){
         latitudes.push(0)
         longitudes.push(0)
@@ -58,10 +71,8 @@ export default function ReperageSurLaSphere() {
         NordouSud.push('N')
       }
       nom=choisit_lettres_differentes(this.nb_questions,'Q')
-      this.contenu=`Quelles sont les coordonnées des points `
-      for (let i = 0, latitude,longitude,M,OP,lab,croix; i < this.nb_questions;) {
-
-       
+      this.contenu=``
+      for (let i = 0, latitude,longitude,M,lab,croix; i < this.nb_questions;) {
         latitude=randint(-3,6,0)*10
         longitude=randint(-6,4)*10
         while (latitudes.indexOf(latitude)!=-1&&longitudes.indexOf(longitude)!=-1) {
@@ -79,7 +90,6 @@ export default function ReperageSurLaSphere() {
         P[i].visible=true
         P[i].p2d.nom=`${nom[i]}`
         P[i].p2d.positionLabel='above left'
-        OP=arete3d(O,P[i])
         lab=labelPoint(P[i].p2d)
         lab.color='blue'
         lab.taille=2
@@ -88,24 +98,32 @@ export default function ReperageSurLaSphere() {
         croix.epaisseur=2
         croix.color='blue'
         croix.style='o'
-
-        if (i<this.nb_questions-1) this.contenu += `$${nom[i]}$, `
-        else this.contenu+= `$${nom[i]}$.`
-       this.contenu_correction+=`Les coordonnées de $${nom[i]}$ sont $${Math.abs(longitudes[i])}\\degree ${EstouOuest[i]}$ ; $${Math.abs(latitudes[i])}\\degree ${NordouSud[i]}$.<br>` 
-         objets_enonce.push(croix,lab)
-         i++
-       //   objets_correction.push(OP.p2d,tracePoint(P[i].p2d),labelPoint(P[i].p2d))
+        switch (liste_type_de_questions[i]) {
+          case 1:
+            this.contenu +=`${num_alpha(i)} Donner les coordonnées GPS du point $${nom[i]}$.<br>`
+            this.contenu_correction+=`${num_alpha(i)} Les coordonnées de $${nom[i]}$ sont $(${Math.abs(longitudes[i])}\\degree$${EstouOuest[i]} ; $${Math.abs(latitudes[i])}\\degree$${NordouSud[i]}) ou $(${longitudes[i]}\\degree$ ; $${latitudes[i]}\\degree )$.<br>` 
+            objets_enonce.push(croix,lab)
+            objets_correction.push(croix,lab)
+            break
+          case 2:
+            this.contenu +=`${num_alpha(i)} Placer le point $${nom[i]}$ de  coordonnées GPS $(${Math.abs(longitudes[i])}\\degree$${EstouOuest[i]} ; $${Math.abs(latitudes[i])}\\degree$${NordouSud[i]}) ou $(${longitudes[i]}\\degree$ ; $${latitudes[i]}\\degree )$.<br>`
+            this.contenu_correction+=`${num_alpha(i)} Le point $${nom[i]}$ de coordonnées GPS $(${Math.abs(longitudes[i])}\\degree$${EstouOuest[i]} ; $${Math.abs(latitudes[i])}\\degree$${NordouSud[i]}) ou $(${longitudes[i]}\\degree$ ; $${latitudes[i]}\\degree )$ est placé sur cette sphère.<br>` 
+            objets_correction.push(croix,lab)
+          break
+        }
+        i++
       }
         // paramètres pour la perspective
   mathalea.anglePerspective=30
   mathalea.coeffPerspective=0.5
-      params_enonce = { xmin:-12, ymin: -12, xmax: 12, ymax: 12, pixelsParCm: 20, scale: 1, mainlevee: false}
+      params_enonce = { xmin:-12, ymin: -13, xmax: 12, ymax: 13, pixelsParCm: 20, scale: 1, mainlevee: false}
 
      // texte_corr += mathalea2d(params_correction, objets_correction)
       this.contenu +='<br>'+ mathalea2d(params_enonce, objets_enonce)
+      this.contenu_correction +='<br>'+ mathalea2d(params_enonce, objets_correction)
     };
   
-  //	this.besoin_formulaire_numerique = ['Type de questions', 3, `1 : Perpendiculaires\n 2 : Parallèles\n 3 : Mélange`]
+  this.besoin_formulaire_numerique = ['Type de questions', 3, `1 : Lire des coordonnées\n 2 : Placer des points\n 3 : Mélange`]
   //  this.besoin_formulaire2_numerique = ["Type de cahier",3,`1 : Cahier à petits careaux\n 2 : Cahier à gros carreaux (Seyes)\n 3 : Feuille blanche`];
   // this.besoin_formulaire3_case_a_cocher =['figure à main levée',true]
   
--- modules/3d.js
+++ modules/3d.js
@@ -667,22 +667,36 @@ export function pave3d(A,B,C,E,color='black'){
       }
   }
   
-  function Sens_de_rotation3d(axe,rayon,angle){
+  function Sens_de_rotation3d(axe,rayon,angle,epaisseur=1,color='black'){
     ObjetMathalea2D.call(this)
+    this.epaisseur=epaisseur
+    this.color=color
     let M,N,s,objets=[],d,A,B
     M=translation3d(axe.origine,rayon)
     for (let i=0;i<angle;i+=5){
       N=rotation3d(M,axe,5)
       s=segment(M.p2d,N.p2d)
+      s.color=this.color
+      s.epaisseur=this.epaisseur
       objets.push(s)
       M=N
     }
     N=rotation3d(M,axe,5)
-    objets.push(segment(M.p2d,N.p2d))
+    s=segment(M.p2d,N.p2d)
+    s.color=this.color
+    s.epaisseur=this.epaisseur
+    objets.push(s)
     d=droite3d(N,axe.directeur)
     A=rotation3d(M,d,30)
     B=rotation3d(M,d,-30)
-    objets.push(segment(N.p2d,A.p2d),segment(N.p2d,B.p2d))
+    s=segment(N.p2d,A.p2d)
+    s.color=this.color
+    s.epaisseur=this.epaisseur
+    objets.push(s)
+    s=segment(N.p2d,B.p2d)
+    s.color=this.color
+    s.epaisseur=this.epaisseur
+    objets.push(s)   
     this.svg =function (coeff) {
       let code = "";
       for (let objet of objets) {
