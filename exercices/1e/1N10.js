57d0418baab8e78bfa90f7159b3986f12a35f88f
lhotejc <lhotejc@gmail.com>
2021-01-26 22:32:55 +0100

    Exos impactés par les modifications de la classe Fraction

--- exercices/1e/1N10.js
+++ exercices/1e/1N10.js
@@ -119,9 +119,9 @@ export default function Terme_d_une_suite_definie_explicitement() {
           texte += `<br>Calculer $u_{${k}}$.`;
           frac = fraction(a * k + b, c * k + d);
           texte_corr = `Dans l'expression de $u_n$ on remplace $n$ par $${k}$, on obtient : $u_{${k}} = \\dfrac{${a}\\times ${k} ${ecriture_algebrique(b)}}{${c}\\times ${k}
-          ${ecriture_algebrique(d)}} = ` + frac.texFraction();
+          ${ecriture_algebrique(d)}} = ` + frac.texFraction;
           if (pgcd(a * k + b, c * k + d) != 1)
-            texte_corr += `=` + frac.texFractionSimplifiee();
+            texte_corr += `=` + frac.texFractionSimplifiee;
           texte_corr += `$.`;
           break;
       }
