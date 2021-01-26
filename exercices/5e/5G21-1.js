3d63eae6c74606bbb43a936a37a34a5c44adb3d7
Sébastien LOZANO <sebastien.lozano@ac-nancy-metz.fr>
2021-01-26 21:25:54 +0100

    :bulb: oubli date maj 5G21-1

--- exercices/5e/5G21-1.js
+++ exercices/5e/5G21-1.js
@@ -3,6 +3,7 @@ import Constructibilite_des_triangles from './_Constructibilite_des_triangles.js
 /**
  * Vocabulaire des triangles 
  * 5G21-1
+ * Mise à jour le 2021-01-25
  * @author Sébastien Lozano
  */
 export default function Constructibilite_des_triangles_longueurs() {
