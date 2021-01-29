0f23a04b0c2f8d06950ebbd3918366c092830590
lhotejc <lhotejc@gmail.com>
2021-01-29 23:33:57 +0100

    beta5G40 finale ?

--- exercices/beta/beta5G40.js
+++ exercices/beta/beta5G40.js
@@ -1,6 +1,6 @@
 import Exercice from '../ClasseExercice.js';
 import {liste_de_question_to_contenu,randint,calcul,choisit_lettres_differentes,lettre_depuis_chiffre,choice} from "/modules/outils.js"
-import {point,labelPoint,similitude,polygoneAvecNom,tracePoint,demiDroite,segment,traceCompas,dansLaCibleCarree,cibleCarree,rotation,longueur,mathalea2d} from "/modules/2d.js"
+import {point,labelPoint,similitude,polygoneAvecNom,tracePoint,texteParPoint,homothetie,demiDroite,segment,traceCompas,dansLaCibleCarree,cibleCarree,rotation,longueur,mathalea2d} from "/modules/2d.js"
 import { codeSegments, pointAdistance } from './../../modules/2d.js';
 
 
@@ -31,18 +31,18 @@ export default function Constructions_parallelogrammes() {
 			return lettre + chiffre;
 		};
         // On prépare la figure...
-		let noms = choisit_lettres_differentes(5, 'QO', true); // on choisit 5 lettres, les 4 premières sont les sommets, la 5e est le centre
+		let noms = ['A','B','C','D','O']//choisit_lettres_differentes(5, 'QO', true); // on choisit 5 lettres, les 4 premières sont les sommets, la 5e est le centre
         let type_de_question
         if (this.sup<5) type_de_question=parseInt(this.sup)
         else type_de_question=randint(1,4)
         let nom=`$${noms[0]+noms[1]+noms[2]+noms[3]}$`
-        let A,B,C,D,O,p,d1,d2,d3,d4,c1,c2,c3,c4,dd1,dd2
+        let A,B,C,D,O,p,d1,d2,d3,d4,c1,c2,c3,c4,dd1,dd2,dd3,dd4
         let objets_enonce=[],objets_correction=[],result2,result,cible,cible2,cellule,cellule2
         // Préparation de la figure aléatoire et des objets 2d utiles
         O=point(0,0,noms[4])
-        A=pointAdistance(O,calcul(randint(30,50)/10),randint(0,179)*choice([-1,1]),noms[0])
+        A=rotation(pointAdistance(O,calcul(randint(50,70)/10)),O,randint(0,179)*choice([-1,1]),noms[0])
         C=rotation(A,O,180,noms[2])
-        B=similitude(A,O,randint(30,60)*choice([-1,1]),randint(3,8,5)*choice([-1,1])/5,noms[1])
+        B=similitude(A,O,randint(40,80)*choice([-1,1]),randint(4,7,5)*choice([-1,1])/5,noms[1])
         D=rotation(B,O,180,noms[3])
         p=polygoneAvecNom(A,B,C,D)
         d1=segment(O,A)
@@ -53,8 +53,10 @@ export default function Constructions_parallelogrammes() {
         c2=segment(B,C)
         c3=segment(C,D)
         c4=segment(D,A)
-        dd1=demiDroite(B,A)
-        dd2=demiDroite(B,C)
+        dd1=demiDroite(A,B)
+        dd2=demiDroite(A,D)
+        dd3=demiDroite(C,D)
+        dd4=demiDroite(C,B)
         cellule=celluleAlea(5)
         cellule2=celluleAlea(5)
         result = dansLaCibleCarree(C.x, C.y, 5, 0.6, cellule);
@@ -67,11 +69,15 @@ export default function Constructions_parallelogrammes() {
         cible2.taille = 0.6;
         cible2.color = 'grey';
         cible2.opacite = 0.7;
-
+		let xMin, yMin, xMax, yMax
+		xMin = Math.min(A.x, B.x,C.x,D.x)-3;
+		yMin = Math.min(A.y, B.y,C.y,D.y)-3;
+		xMax = Math.max(A.x, B.x,C.x,D.x)+3;
+		yMax = Math.max(A.y, B.y,C.y,D.y)+3;
 
         switch (type_de_question) {
             case 1: // deux côtés consécutifs
-                this.consigne = `Construire le parallélogramme ${nom}.`;
+                this.consigne = `Construire le parallélogramme $${nom}$.`;
                 texte_corr=`Plusieurs constructions sont possibles :<br>`
                 if (this.correction_detaillee){
                 texte_corr+=`- En utilisant l'égalité des longueurs : $${noms[0]+noms[1]}=${noms[3]+noms[2]}$ et $${noms[2]+noms[1]}=${noms[3]+noms[0]}$.<br>`
@@ -90,7 +96,7 @@ export default function Constructions_parallelogrammes() {
                 objets_correction.push(p[0],p[1],cible,traceCompas(D,C,30),traceCompas(B,C,30),codeSegments("||",'red',A,B,D,C),codeSegments("///",'blue',A,D,B,C))
             break
             case 2: // trois sommets consécutifs
-                this.consigne = `Construire le parallélogramme ${nom}.`;
+                this.consigne = `Construire le parallélogramme $${nom}$.`;
                 texte_corr=`Plusieurs constructions sont possibles :<br>`
                 if (this.correction_detaillee){
                 texte_corr+=`- En utilisant l'égalité des longueurs : $${noms[0]+noms[1]}=${noms[3]+noms[2]}$ et $${noms[2]+noms[1]}=${noms[3]+noms[0]}$.<br>`
@@ -109,7 +115,7 @@ export default function Constructions_parallelogrammes() {
  
             break
             case 3: // deux sommmets consécutifs plus le centre
-             this.consigne = `Construire le parallélogramme ${nom} de centre $${noms[4]}$.`;
+             this.consigne = `Construire le parallélogramme $${nom}$ de centre $${noms[4]}$.`;
              if (this.correction_detaillee){
                 texte_corr+=`Le point $${noms[3]}$ est le symétrique du point $${noms[1]}$ par rapport à $${noms[4]}$.<br>`
                 texte_corr+=`Le point $${noms[2]}$ est le symétrique du point $${noms[0]}$ par rapport à $${noms[4]}$.<br>`
@@ -119,22 +125,26 @@ export default function Constructions_parallelogrammes() {
                 c1.styleExtremites='|-|'
                 objets_enonce.push(tracePoint(A,B,O),labelPoint(O,A,B),cible,cible2)
                 objets_correction.push(p[0],p[1],cible,cible2,d1,d2,d3,d4,codeSegments("||","red",A,O,O,C),codeSegments("|||","blue",B,O,O,D))
-            
-
-           
         
             break
             case 4: // Un angle formé par deux demi-droites et le centre
-              this.consigne = `Construire le parallélogramme $${nom} de centre ${noms[4]}.`;
+              this.consigne = `Construire le parallélogramme $${nom}$ de centre ${noms[4]}.`;
+              if (this.correction_detaillee){
+                texte_corr+=`Le point $${noms[2]}$ est le symétrique du point $${noms[0]}$ par rapport à $${noms[4]}$.<br>`
+                texte_corr+=`La symétrique de la demi-droite $[${noms[0]+noms[1]})$ par rapport à $${noms[4]}$ est la demi-droite d'origine $${noms[2]}$ parallèle à $[${noms[0]+noms[1]})$.<br>`
+                texte_corr+=`La symétrique de la demi-droite $[${noms[0]+noms[3]})$ par rapport à $${noms[4]}$ est la demi-droite d'origine $${noms[2]}$ parallèle à $[${noms[0]+noms[3]})$.<br>`
+                }
+                texte_corr+=`Le point $${noms[2]}$ se trouve dans la case ${cellule} de la cible 1.<br>`
+                texte_corr+=`Le point $${noms[3]}$ se trouve dans la case ${cellule2} de la cible 2.<br>`
+                c1.styleExtremites='|-|'
+                objets_enonce.push(dd1,dd2,tracePoint(O),labelPoint(O,A),texteParPoint('x',similitude(D,A,4,1.3)),texteParPoint('y',similitude(B,A,4,1.3)),cible,cible2)
+                objets_correction.push(dd1,dd2,dd3,dd4,p[0],p[1],tracePoint(O),labelPoint(O),cible,cible2,d1,d3,codeSegments("||","red",A,O,O,C))
+            
         
             break
             
         }
-		let xMin, yMin, xMax, yMax
-		xMin = Math.min(A.x, B.x,C.x,D.x)-3;
-		yMin = Math.min(A.y, B.y,C.y,D.y)-3;
-		xMax = Math.max(A.x, B.x,C.x,D.x)+3;
-		yMax = Math.max(A.y, B.y,C.y,D.y)+3;
+
 
 		this.liste_questions.push(texte+mathalea2d({ xmin: xMin, ymin: yMin, xmax: xMax, ymax: yMax, pixelsParCm: 20, scale: 0.7 }, objets_enonce));
 		this.liste_corrections.push(texte_corr + mathalea2d({ xmin: xMin, ymin: yMin, xmax: xMax, ymax: yMax, pixelsParCm: 20, scale: 0.7 }, objets_correction));
