9560bdbfd057501c91217fa673862fff50e5d939
lhotejc <lhotejc@gmail.com>
2021-02-07 14:24:47 +0100

    beta3G32-1 1 cas OK

--- exercices/beta/beta3G32-1.js
+++ exercices/beta/beta3G32-1.js
@@ -1,5 +1,7 @@
 import Exercice from '../ClasseExercice.js';
-import {liste_de_question_to_contenu,combinaison_listes,randint,arrondi,calcul,tex_nombre} from "/modules/outils.js"
+import {liste_de_question_to_contenu,combinaison_listes,randint,arrondi,calcul,tex_nombre,arrondi_virgule} from "/modules/outils.js"
+import { pointAdistance, polygoneAvecNom, afficheMesureAngle,codageAngleDroit,point,segment,mathalea2d} from './../../modules/2d.js';
+import { texteSurSegment } from '/modules/2d.js';
 /**
  * Description didactique de l'exercice
  * @Auteur 
@@ -13,6 +15,7 @@ export default function Calculs_trigonometriques() {
   this.nb_cols = 2; // Uniquement pour la sortie LaTeX
   this.nb_cols_corr = 2; // Uniquement pour la sortie LaTeX
   this.sup = 1; // Niveau de difficulté à ne définir que si on peut le modifier avec un formulaire en paramètre
+  this.sup2 = false;
   this.tailleDiaporama = 100; // Pour les exercices chronométrés. 50 par défaut pour les exercices avec du texte
   this.video = "" // Id YouTube ou url
 this.nb_questions=1
@@ -21,7 +24,7 @@ this.nb_questions=1
     this.liste_questions = []; // Liste de questions
     this.liste_corrections = []; // Liste de questions corrigées
     let objet=[['arbre','un',''],['immeuble','un',''],['éolienne','une','te'],['montagne','une','te']]
-    let distance,hauteur,beta,alpha,teta,taille,index
+    let distance,hauteur,beta,alpha,teta,taille,index,A,B,O,H,S,echelle,objets=[],p
     let type_de_questions_disponibles = ['type1']; // On créé 3 types de questions
     let liste_type_de_questions = combinaison_listes(type_de_questions_disponibles,this.nb_questions); // Tous les types de questions sont posés mais l'ordre diffère à chaque "cycle"
     for (let i = 0, texte, texte_corr, cpt = 0; i < this.nb_questions && cpt < 50;) {
@@ -29,6 +32,7 @@ this.nb_questions=1
         
       switch (liste_type_de_questions[i]) { // Suivant le type de question, le contenu sera différent
         case 'type1': 
+              
         distance=randint(5,300)
         hauteur=calcul(randint(150,190)/100)
         beta=Math.atan(hauteur/distance)
@@ -39,12 +43,35 @@ this.nb_questions=1
         else if (taille<50) index=1
         else if (taille<100) index=2
         else index=3
-
+        A=point(0,0,'A')
+        B=pointAdistance(A,12,0,'B')
+        O=pointAdistance(A,3,90,'O')
+        H=pointAdistance(B,3,90,'H')
+        S=pointAdistance(B,9,90,'S')
+        p=polygoneAvecNom(A,B,H,S,O)
+        objets.push(p[1],p[0],segment(O,B),segment(O,H),codageAngleDroit(O,A,B),codageAngleDroit(A,B,H),codageAngleDroit(O,H,S))
+        objets.push(afficheMesureAngle(B,O,S,'black',3,'α'),afficheMesureAngle(A,B,O,'black',2,'β'),afficheMesureAngle(B,O,H,'black',2,'β'))
+        objets.push(texteSurSegment(`${tex_nombre(hauteur)} m`,O,A,'black',-0.5),texteSurSegment(`${tex_nombre(distance)} m`,O,H))
           texte = `On observateur regarde ${objet[index][1]} ${objet[index][0]} sous un angle de $${alpha}\\degree$.<br>`;
           texte+=`Cet${objet[index][2]} ${objet[index][0]} est situé à une distance de $${tex_nombre(distance)}$ m de l'observateur.<br>`
           texte+=`l'oeil de l'observateur est situé à $${tex_nombre(hauteur)}$ m du sol.<br>`
-          texte+=`Calculer la hauteur de cet${objet[index][2]} ${objet[index][0]} arrondie au mètre près`
-          texte_corr = `Cet${objet[index][2]} ${objet[index][0]} mesure $${tex_nombre(Math.round(taille))}$ m de hauteur.`;
+          if (this.sup2) {
+
+          texte+=`$O$ représente l'oeil de l'observateur, $[BS]$ représente cet${objet[index][2]} ${objet[index][0]}.<br>`
+
+            texte+=`Le schéma ci-dessous n'est pas en vraie grandeur.<br>`+mathalea2d({xmin:-1,ymin:-1,xmax:14,ymax:11,pixelsParCm:20,scale:1},objets);
+            texte+=`<br>Calculer d'abord l'angle $β$.<br>`
+            texte+=`En déduire la mesure de l'angle $\\widehat{HOS}$.<br>`
+            texte+=`Calculer alors la longueur $HS$.<br>`
+          }
+          texte+=`Calculer la hauteur de cet${objet[index][2]} ${objet[index][0]} arrondie au mètre près.<br>`
+
+          texte_corr=mathalea2d({xmin:-1,ymin:-1,xmax:14,ymax:11,pixelsParCm:20,scale:1},objets)
+          texte_corr+=`<br>Dans le triangle $OHB$ rectangle en $H$, $tan(β)=\\dfrac{HB}{OH}$.<br>D'où $β=Atan(\\dfrac{${tex_nombre(hauteur)}}{${tex_nombre(distance)}})\\approx ${arrondi_virgule(beta)}\\degree$.<br>`
+          texte_corr+=`$\\widehat{HOS}=α-β\\approx ${arrondi_virgule(alpha-beta)}$.<br>`
+          texte_corr+=`$HS=OH\\times tan(\\widehat{HOS})\\approx ${distance}\\times tan(${arrondi_virgule(alpha-beta)})\\approx ${tex_nombre(taille-hauteur)}$ m.<br>`
+          texte_corr+=`$BS=BH+HS=${tex_nombre(hauteur)}+${tex_nombre(taille-hauteur)}=${tex_nombre(taille)}$ m.<br>`
+          texte_corr += `Cet${objet[index][2]} ${objet[index][0]} mesure $${tex_nombre(Math.round(taille))}$ m de hauteur.`;
           break;
         case 'type2': 
             texte = `Question ${i+1} de type 2`;
@@ -68,6 +95,7 @@ this.nb_questions=1
     liste_de_question_to_contenu(this);
   };
   this.besoin_formulaire_numerique = ['Niveau de difficulté',3];
+  this.besoin_formulaire2_case_a_cocher =['Afficher un schéma et des questions intermédiaires'];
 }
 
 // python3 list-to-js.py pour faire apparaitre l'exercice dans le menu
