f61416e63f32cc2ff98c1f985cdc8d5696b98502
Rémi Angot <remi.angot@gmail.com>
2021-02-14 07:52:57 +0100

    latexParPoint et latexParCoordonnees ont maintenant un fond blanc par défaut (l'argument de la couleur de fond est le dernier argument pour rester compatible avec ce qui a déjà été fait).

--- modules/2d.js
+++ modules/2d.js
@@ -7008,14 +7008,14 @@ export function texteParPosition(texte, x, y, orientation = "milieu", color,scal
  *
  * @Auteur Rémi Angot
  */
-function LatexParPoint(texte, A, color, size = 200, hauteurLigne = 12) {
+function LatexParPoint(texte, A, color = 'black', size = 200, hauteurLigne = 12, colorBackground = 'white') {
   let demiSize = calcul(size/2)
   ObjetMathalea2D.call(this);
   this.color = color;
   this.svg = function (coeff) {
     return `<foreignObject style="overflow: visible;" y="${A.ySVG(coeff)-hauteurLigne}" x="${A.xSVG(coeff)-demiSize}" width="${size*2}" height="50" id="${this.id}" ><div style="margin-left: auto;
     margin-right: auto;width:${size}px;position:fixed!important; text-align:center">
-    $\\color{${color}}${texte}$</div></foreignObject>`;
+    $\\colorbox{${colorBackground}}{$\\color{${color}}{${texte}}$}$</div></foreignObject>`;
   };
   this.tikz = function () {
     let code = `\\draw (${A.x},${A.y}) node[anchor = center] {$${texte}$};`;
@@ -7026,9 +7026,9 @@ export function latexParPoint(...args) {
   return new LatexParPoint(...args);
 }
 
-export function latexParCoordonnees(texte, x, y) {
+export function latexParCoordonnees(texte, x, y, color = 'black', size = 200, hauteurLigne = 12, colorBackground = 'white') {
   let A = point(x, y);
-  return latexParPoint(texte, A);
+  return latexParPoint(texte, A, color, size, hauteurLigne, colorBackground);
 }
 
 /**
