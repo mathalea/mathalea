6c9fadeb96d0e0fecf58a2b45a311efa5b30a8f6
Unknown <lhotejc@gmail.com>
2021-02-15 22:16:52 +0100

    betaP010 presque bon pour le service ...
    reste à voir pour le second degré et le 1er degré !

--- exercices/beta/betaP010.js
+++ exercices/beta/betaP010.js
@@ -1,5 +1,5 @@
 import Exercice from '../ClasseExercice.js';
-import {liste_de_question_to_contenu,ecriture_parenthese_si_negatif,printlatex,ecriture_algebrique,arrondi_virgule,ecriture_algebrique_sauf1,ecriture_nombre_relatif} from "/modules/outils.js"
+import {unSiPositifMoinsUnSinon,signe,liste_de_question_to_contenu,ecriture_parenthese_si_negatif,printlatex,ecriture_algebrique,arrondi_virgule,ecriture_algebrique_sauf1,ecriture_nombre_relatif} from "/modules/outils.js"
 import { tableau_de_variation,mathalea2d } from '/modules/2d.js';
 import {fraction} from "/modules/Fractions.js"
 /**
@@ -38,14 +38,14 @@ export default function variation_polynome_degre3() {
     let texte='',texte_corr=''
        let coef_f=this.sup.split('/')
        console.log(coef_f)
-    let a,b,c,d,a1,b1,c1,a2,b2,xx,rac=[],t
+    let a,b,c,d,a1,b1,c1,a2,b2,xx,xxs,rac=[],t
     [a,b,c,d]=coef_f
     a1=3*a
     b1=2*b
     c1=1*c    
     a2=6*a
     b2=2*b
-    let x2=fraction(-b,3*a)
+   
     let trouver_les_racines=function(a0,b0,c0){
 console.log(a0,b0,c0)
     let delta=b0*b0-4*a0*c0 // on calcule les racines de f'
@@ -57,15 +57,30 @@ console.log(a0,b0,c0)
       console.log('delta=0',-b0/2/a0)
       return [-b0/2/a0]
     }
+    let x1s,x3s,s,sig1,sig2
     let x1=(-b0+Math.sqrt(delta))/2/a0
     let x3=(-b0-Math.sqrt(delta))/2/a0
+    s=unSiPositifMoinsUnSinon(a0)
+    sig1=signe(s)
+    sig2=signe(-s)
+    if (b0%2==0){
+    x1s=math.parse(`${-b0*s/2}/${Math.abs(a0)}${sig1}sqrt(${b0*b0/4-a0*c0})/${Math.abs(a0)}`).toTex()
+    x3s=math.parse(`${-b0*s/2}/${Math.abs(a0)}${sig2}sqrt(${b0*b0/4-a0*c0})/${Math.abs(a0)}`).toTex()
+    }
+    else{
+      x1s=math.parse(`(${-b0}+sqrt(${b0*b0}+${-4*a0*c0}))/${2*a0}`).toTex()
+      x3s=math.parse(`(${-b0}-sqrt(${b0*b0}+${-4*a0*c0}))/${2*a0}`).toTex()
+    }
     if (x3<x1) { // on ordonne les racines de f'
       xx=x3
+      xxs=x3s
       x3=x1
+      x3s=x1s
       x1=xx
+      x1s=xxs
     }
-    console.log(x1,x3)
-      return [x1,x3]    
+    console.log(x1,x3,x1s,x3s)
+      return [x1,x3,x1s,x3s]    
 
   }
     let f=function(x){
@@ -82,7 +97,7 @@ console.log(a0,b0,c0)
           rac=trouver_les_racines(a1,b1,c1)
           t=tableau_de_variation({colorBackground:'red',escpl:5,delatcl:0.8,lgt:3.5, 
             tabInit:[[['$x$',1,20],["$f'(x)$",1,60],["$f(x)$",2,60]],
-           ['$-\\infty$',30,`$${arrondi_virgule(rac[0])}$`,50,`$${arrondi_virgule(rac[1])}$`,60,'$+\\infty$',30]],
+           ['$-\\infty$',30,`$${rac[2]}$`,50,`$${rac[3]}$`,60,'$+\\infty$',30]],
             tabLines:
             [['Line',30,'',0,'-',20,'z',20,'+',20,'z',20,'-',20],
             ['Var',10,'+/$+\\infty$',30,`-/$${arrondi_virgule(f(rac[0]))}$`,50,`+/$${arrondi_virgule(f(rac[1]))}$`,50,'-/$-\\infty$',30]
