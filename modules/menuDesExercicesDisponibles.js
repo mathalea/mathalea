8a777e8c088ffd45b2d0011c82a2b43992cdf978
Unknown <lhotejc@gmail.com>
2021-03-14 18:45:29 +0100

    Test dictionnaireC3

--- modules/menuDesExercicesDisponibles.js
+++ modules/menuDesExercicesDisponibles.js
@@ -457,12 +457,13 @@ export function listeTheme(theme) {
 }
 
 let dictionnaireC3 =  {
-        "c3N30": {
-            "titre": "Conversion d\'unités de k,h,da vers unité",
-            "url": "/exercices/6e/6N34.js",
-            "sup":2,
-            "nb_questions":6
-        }
+    "c3N30": {
+        "titre": "Conversion d\'unités de k,h,da vers unité",
+        "url": "/exercices/6e/6N34.js",
+        "sup":1,
+        "sup2":false,
+        "nb_questions":5
+    }
 }
 
 let dictionnaireDNB = {
