18b22bc16599e6a2c810c71b55a895f2061eb6cd
Unknown <lhotejc@gmail.com>
2021-03-17 20:45:54 +0100

    Carré unité à l'envers dans P005

--- exercices/Profs/P005.js
+++ exercices/Profs/P005.js
@@ -1,5 +1,5 @@
 import Exercice from '../ClasseExercice.js';
-import {point,segment,polygone,carreIndirect,grille,grilleHorizontale,texteParPosition,mathalea2d,grilleVerticale} from "/modules/2d.js"
+import {point,segment,polygone,carre,grille,grilleHorizontale,texteParPosition,mathalea2d,grilleVerticale} from "/modules/2d.js"
 
 /**
  * Fonction permettant aux enseignants de proposer des grilles décimale à colorier
@@ -19,7 +19,7 @@ export default function Feuille_de_grilles() {
       for (let i = 0; i < 5; i++) {
         objets.length = 0;
         //pixelsParCm=50
-        objets.push(carreIndirect(point(1, 1), point(2, 1)));
+        objets.push(carre(point(1, 1), point(2, 1)));
         objets.push(texteParPosition("= 1 unité", 3.5, 1.5));
         objets.push(grille(15, -2, 19, 2, 'black', 1, 4));
         for (let j = 0; j < 11; j++) {
@@ -50,7 +50,7 @@ export default function Feuille_de_grilles() {
       for (let i = 0; i < 4; i++) {
         objets.length = 0;
         //pixelsParCm=50
-        objets.push(carreIndirect(point(1, 1.5), point(2, 1.5)));
+        objets.push(carre(point(1, 1.5), point(2, 1.5)));
         objets.push(texteParPosition("= 1 unité", 3.5, 2));
         objets.push(grille(15, -2.5, 20, 2.5, 'black', 1, 0.5));
         objets.push(grille(11, -0.5, 12, 0.5, 'black', 0.3, 0.1));
@@ -85,7 +85,7 @@ export default function Feuille_de_grilles() {
           D = point(0 + j * 1.1, 0.5);
           objets.push(polygone(A, B, C, D));
         }
-        objets.push(carreIndirect(point(1, 1.5), point(2, 1.5)));
+        objets.push(carre(point(1, 1.5), point(2, 1.5)));
         objets.push(texteParPosition("= 1 unité", 3.5, 2));
         objets.push(segment(point(11.5, 0.5), point(15, 5)));
         objets.push(segment(point(11.5, -0.5), point(15, -5)));
