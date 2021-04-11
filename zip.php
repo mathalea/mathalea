9d1386b8b0045d193fd59f8f98a045e486e97cba
Sébastien LOZANO <sebastien.lozano@ac-nancy-metz.fr>
2021-04-11 20:16:37 +0200

    :alien: zip.php trim() de la chaine issue du formulaire mais on ne peut pas échapper les caractères spéciaux !

--- zip.php
+++ zip.php
@@ -8,9 +8,7 @@
                     $filename = "test".time().".zip";
 
                     // On supprime tout ce qui a plus de 1 minute
-                    $sec = 60; // Nombre de secondes  
-
-                    //$path = ""; // chemin du répertoire
+                    $sec = 60; // Nombre de secondes                      
                     
                     if (is_dir($thisdir) )
                         {
@@ -29,14 +27,20 @@
                         exit("Impossible d'ouvrir le fichier <$filename>\n");
                     }
 
-                    $zip->addFromString("fichier" . time().".tex", $_POST['encoded_snip']);                   
+                    // On nettoie la chaine du formulaire pour éviter les injections
+                    $encoded_snip = $_POST['encoded_snip'];
+                    $encoded_snip = trim($encoded_snip);
+                    //$encoded_snip = stripslashes($encoded_snip);
+                    //$encoded_snip = htmlspecialchars($encoded_snip);
+
+                    $zip->addFromString("fichier" . time().".tex", $encoded_snip);                   
                     $zip->addFile($thisdir."/automultiplechoice.sty","/automultiplechoice.sty");
                     echo "Nombre de fichiers : " . $zip->numFiles . "\n";
                     echo "Statut :" . $zip->status . "\n";
                     $zip->close();
                     if(file_exists($thisdir.'/'.$filename)){
                         echo "file exist";
-                    //     // push to download the zip
+                    // On lance la routine de téléchargement
                     header('Content-type: application/zip'); // on indique que c'est une archive
                     header('Content-Transfer-Encoding: fichier'); // transfert en binaire (fichier)
                     header('Content-Disposition: attachment; filename="'.$thisdir.'/'.$filename.'"'); // nom de l'archive
@@ -44,6 +48,8 @@
                     header('Pragma: no-cache'); 
                     header('Expires: 0');
                     header("location:$thisdir/$filename"); // redirection vers le téléchargement de l'archive pouyr le moment                    
+                    //header("location:https://www.overleaf.com/docs?snip_uri=https://coopmaths.fr/$thisdir/$filename"); // pour passer à overleaf
+                    //header("location:https://www.overleaf.com/docs?snip_uri=https://coopmaths.fr/fichiers/images.zip"); // pour tester avec l'existant
                     };
 
                   } else {
