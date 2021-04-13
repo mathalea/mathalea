0f0ab1a1b401836893361502e46045d94eb2f9fc
Gain <mireillegain.prof@gmail.com>
2021-04-13 18:56:46 +0200

    Correctif bug 6N31-3.js et complément racine carrée

---
+++ exercices/beta/beta4G20-5.js
@@ -0,0 +1,12 @@
+import Arrondir_une_valeur from '../6e/6N31-3.js'
+/**
+ * Transformations : trouvers un point numéroté par une des transformations du plan. Fonction générale utilisée sur tous les niveaux
+ * @Auteur Mireille Gain, 13 avril 2021
+ * référence 4G20-5
+ */
+
+export default function Arrondir_une_valeur_4e() {
+  Arrondir_une_valeur_4e.call(this);
+  this.sup = 3;
+  this.titre = "Trouver la valeur arrondie d'une racine carrée";
+}
