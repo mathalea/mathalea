56362d4ea117757fa1bf5a72cc8c5a836dda5e05
Rémi Angot <remi.angot@gmail.com>
2021-04-16 22:21:50 +0200

    Revert "eslint --fix + correction chemins imports"
    This reverts commit fb3f158f766be9b94a5535493753f90d5236a127.

--- exercices/6e/6N14.js
+++ exercices/6e/6N14.js
@@ -1,65 +1,67 @@
-import Exercice from '../ClasseExercice.js'
-import { combinaison_listes, liste_de_question_to_contenu, randint } from '/modules/outils.js'
-import { mathalea2d } from '../../modules/2d.js'
-import { fraction } from '../../modules/Fractions.js'
+import Exercice from '../ClasseExercice.js';
+import { combinaison_listes, liste_de_question_to_contenu, randint } from "/modules/outils.js";
+import {mathalea2d} from "/modules/2d.js"
+import {fraction} from "/modules/fractions.js"
 /**
  * 6N14
  * Représenter des fractions simples avec des disques partagés de façon adéquate.
  * @Auteur Jean-Claude Lhote
  */
-export default function Representer_une_fraction () {
-  Exercice.call(this) // Héritage de la classe Exercice()
-  this.titre = 'Représenter des fractions'
-  this.consigne = ''
-  this.nb_questions = 4
-  this.nb_cols = 2
-  this.nb_cols_corr = 2
-  this.sup = 3
+export default function Representer_une_fraction() {
+  Exercice.call(this); // Héritage de la classe Exercice()
+  this.titre = "Représenter des fractions";
+  this.consigne = "";
+  this.nb_questions = 4;
+  this.nb_cols = 2;
+  this.nb_cols_corr = 2;
+  this.sup = 3;
 
   this.nouvelle_version = function (numero_de_l_exercice) {
-    this.liste_questions = [] // Liste de questions
-    this.liste_corrections = [] // Liste de questions corrigées
-    let Xmin, Xmax, Ymin, Ymax, ppc, sc, g, k, carreaux, objets
-    ppc = 20
+    this.liste_questions = []; // Liste de questions
+    this.liste_corrections = []; // Liste de questions corrigées
+    let Xmin, Xmax, Ymin, Ymax, ppc, sc, g, k, carreaux, objets;
+    ppc = 20;
     if (sortie_html) {
-      sc = 0.5
+      sc = 0.5;
     } else {
-      sc = 0.4
+      sc = 0.4;
     }
 
-    const params = {
+    let params = {
       xmin: -2.2,
       ymin: -2.2,
       xmax: 18,
       ymax: 3,
       pixelsParCm: ppc,
-      scale: sc
-    }; let den; let num; let f
+      scale: sc,
+    }, den, num, f;
 
-    const liste = combinaison_listes([2, 3, 4, 5, 6], this.nb_questions)
+    let liste = combinaison_listes([2, 3, 4, 5, 6], this.nb_questions);
 
     for (let i = 0, texte, texte_corr, cpt = 0; i < this.nb_questions && cpt < 50;) {
-      objets = []
-      den = liste[i]
-      num = randint(1, den * 3)
-      f = fraction(num, den)
-      texte = `Sachant qu'un disque représente une unité, représenter la fraction $${f.texFraction}$ en coloriant la part correspondante.<br>`
-      texte += mathalea2d(params, fraction(den * 3, den).representation(0, 0, 2, 0, 'gateau', 'white'))
-      texte_corr = `Voici sur ces dessins, colorié en bleu, la part correspondante à la fraction $${f.texFraction}$ :<br>`
-      texte_corr += mathalea2d(params, f.representation(0, 0, 2, randint(0, den - 1), 'gateau', 'blue'))
+      objets = [];
+      den = liste[i];
+      num = randint(1, den * 3);
+      f = fraction(num, den);
+      texte = `Sachant qu'un disque représente une unité, représenter la fraction $${f.texFraction}$ en coloriant la part correspondante.<br>`;
+      texte += mathalea2d(params, fraction(den * 3, den).representation(0, 0, 2, 0, 'gateau', 'white'));
+      texte_corr = `Voici sur ces dessins, colorié en bleu, la part correspondante à la fraction $${f.texFraction}$ :<br>`;
+      texte_corr += mathalea2d(params, f.representation(0, 0, 2, randint(0, den - 1), 'gateau', 'blue'));
       if (this.liste_questions.indexOf(texte) == -1) {
         // Si la question n'a jamais été posée, on en crée une autre
-        this.liste_questions.push(texte)
-        this.liste_corrections.push(texte_corr)
-        i++
+        this.liste_questions.push(texte);
+        this.liste_corrections.push(texte_corr);
+        i++;
       }
-      cpt++
+      cpt++;
     }
-    liste_de_question_to_contenu(this)
-  }
+    liste_de_question_to_contenu(this);
+  };
   this.besoin_formulaire_numerique = [
-    'Type de cahier',
+    "Type de cahier",
     3,
-    '1 : Cahier à petits careaux\n 2 : Cahier à gros carreaux (Seyes)\n 3 : Feuille blanche'
-  ]
+    `1 : Cahier à petits careaux\n 2 : Cahier à gros carreaux (Seyes)\n 3 : Feuille blanche`,
+  ];
+
+
 }
