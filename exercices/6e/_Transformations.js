3b7caf13438a2b43b0655fbae1011c06c2c34864
lhotejc <lhotejc@gmail.com>
2021-04-20 09:39:30 +0200

    pas de version latex =false

--- exercices/6e/_Transformations.js
+++ exercices/6e/_Transformations.js
@@ -17,7 +17,6 @@ export default function Transformations() {
   this.nb_questions_modifiable = false;
   this.nb_cols = 1;
   this.nb_cols_corr = 1;
-  this.pas_de_version_LaTeX = true;
   // this.sup = 1; // 1 pour les 6ème, 2 pour les 5èmes, 3 pour les 4èmes, et 4 pour les 3èmes.
   sortie_html ? (this.spacing_corr = 2.5) : (this.spacing_corr = 1.5);
   this.nouvelle_version = function (numero_de_l_exercice) {
