3109ca261fc3889718edfb021751c53bdba60dd9
Rémi Angot <remi.angot@gmail.com>
2021-04-23 14:25:58 +0200

    Update inputNum.js

--- testsBrowser/scenarios/inputNum.js
+++ testsBrowser/scenarios/inputNum.js
@@ -31,6 +31,11 @@ async function test (page) {
             - si input form_sup30 est de type number, tester toutes les valeur jusqu'à max
             - si input form_sup30 est de type checkbox, tester true et false
     */
+    async function nouvellesDonnees (n = 5) {
+      for (let i = 0; i <= 5; i++) {
+        await page.click('text=Nouvelles données')
+      }
+    }
     await page.click('text=Paramètres')
     if (await page.$('#form_sup0') && await page.$('#form_sup20')) {
       const type = await page.getAttribute('input#form_sup0', 'type')
@@ -44,11 +49,7 @@ async function test (page) {
           await page.fill('#form_sup0', i.toString())
           for (let j = min2; j <= max2; j++) {
             await page.fill('#form_sup20', j.toString())
-            await page.click('text=Nouvelles données')
-            await page.click('text=Nouvelles données')
-            await page.click('text=Nouvelles données')
-            await page.click('text=Nouvelles données')
-            await page.click('text=Nouvelles données')
+            await nouvellesDonnees()
           }
         }
       }
@@ -59,11 +60,7 @@ async function test (page) {
         const min = await page.getAttribute('input#form_sup0', 'min')
         for (let i = min; i <= max; i++) {
           await page.fill('#form_sup0', i.toString())
-          await page.click('text=Nouvelles données')
-          await page.click('text=Nouvelles données')
-          await page.click('text=Nouvelles données')
-          await page.click('text=Nouvelles données')
-          await page.click('text=Nouvelles données')
+          await nouvellesDonnees()
         }
       }
     }
