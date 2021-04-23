a7e18abf2982ea5b0e4b07e60eb2df1b22473bf9
Rémi Angot <remi.angot@gmail.com>
2021-04-23 10:11:47 +0200

    Scénario de tests pour un ou deux formulaires numériques

--- testsBrowser/scenarios/6C31.js
+++ testsBrowser/scenarios/6C31.js
@@ -6,7 +6,7 @@ const prefs = require('prefs')
  * @return {Promise<boolean>}
  */
 async function test (page) {
-  await page.goto(`${prefs.baseUrl}exercice.html?ex=6C31`)
+  await page.goto(`${prefs.baseUrl}exercice.html?ex=4C32-1`)
   // // Conserve toutes les erreurs
   // page.on('console', msg => {
   //   if (msg.type() === 'error') { console.log(`Error text: "${msg.text()}"`) }
@@ -23,20 +23,39 @@ async function test (page) {
           - si input form_sup30 est de type checkbox, tester true et false
   */
   await page.click('text=Paramètres')
-  // c'est un https://playwright.dev/docs/api/class-elementhandle
-  const formSup0 = await page.$('#form_sup0')
-  // si on est là c'est qu'il existe (sinon ça plante avec un timeout), on cherche un input dedans
-  const type = await page.getAttribute('input#form_sup0', 'type')
-  if (type === 'number') {
-    const max = await page.getAttribute('input#form_sup0', 'max')
-    const min = await page.getAttribute('input#form_sup0', 'min')
-    for (let i = min; i <= max; i++) {
-      await page.fill('#form_sup0', i.toString())
-      await page.click('text=Nouvelles données')
-      await page.click('text=Nouvelles données')
-      await page.click('text=Nouvelles données')
-      await page.click('text=Nouvelles données')
-      await page.click('text=Nouvelles données')
+  if (await page.$('#form_sup0') && await page.$('#form_sup20')) {
+    const type = await page.getAttribute('input#form_sup0', 'type')
+    const type2 = await page.getAttribute('input#form_sup20', 'type')
+    if (type === 'number' && type2 === 'number') {
+      const max = await page.getAttribute('input#form_sup0', 'max')
+      const min = await page.getAttribute('input#form_sup0', 'min')
+      const max2 = await page.getAttribute('input#form_sup20', 'max')
+      const min2 = await page.getAttribute('input#form_sup20', 'min')
+      for (let i = min; i <= max; i++) {
+        await page.fill('#form_sup0', i.toString())
+        for (let j = min2; j <= max2; j++) {
+          await page.fill('#form_sup20', j.toString())
+          await page.click('text=Nouvelles données')
+          await page.click('text=Nouvelles données')
+          await page.click('text=Nouvelles données')
+          await page.click('text=Nouvelles données')
+          await page.click('text=Nouvelles données')
+        }
+      }
+    }
+  } else if (await page.$('#form_sup0')) {
+    const type = await page.getAttribute('input#form_sup0', 'type')
+    if (type === 'number') {
+      const max = await page.getAttribute('input#form_sup0', 'max')
+      const min = await page.getAttribute('input#form_sup0', 'min')
+      for (let i = min; i <= max; i++) {
+        await page.fill('#form_sup0', i.toString())
+        await page.click('text=Nouvelles données')
+        await page.click('text=Nouvelles données')
+        await page.click('text=Nouvelles données')
+        await page.click('text=Nouvelles données')
+        await page.click('text=Nouvelles données')
+      }
     }
   }
   return true
