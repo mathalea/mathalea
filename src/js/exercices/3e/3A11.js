f5380e1059240db4e77fd13f816df01bf2962910
Rémi Angot <remi.angot@gmail.com>
2021-05-06 09:57:30 +0200

    warn_message => warnMessage

--- src/js/exercices/3e/3A11.js
+++ src/js/exercices/3e/3A11.js
@@ -1,5 +1,5 @@
 import Exercice from '../ClasseExercice.js';
-import {listeQuestionsToContenu,randint,shuffle,combinaisonListesSansChangerOrdre,nombre_avec_espace,texte_en_couleur_et_gras,modalPdf,modalVideo,cribleEratostheneN,warn_message} from '../../modules/outils.js'
+import {listeQuestionsToContenu,randint,shuffle,combinaisonListesSansChangerOrdre,nombre_avec_espace,texte_en_couleur_et_gras,modalPdf,modalVideo,cribleEratostheneN,warnMessage} from '../../modules/outils.js'
 export const titre = 'Primalité ou pas'
 
 /**
