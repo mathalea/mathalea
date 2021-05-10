64f912f43bf616ea4d4bc1e846ccd5512956c13e
lhotejc <lhotejc@gmail.com>
2021-05-10 21:49:53 +0200

    calcule(x) et calculeS(x) ajout dans fonctionsMaths.js à comparer avec calcul()

--- src/js/modules/fonctionsMaths.js
+++ src/js/modules/fonctionsMaths.js
@@ -54,3 +54,20 @@ export function asin (x) {
 export function atan (x) {
   return arrondi(degres(Math.atan(x)), 1)
 }
+export function calcule (a) {
+  if (typeof a === 'string') {
+    return parseFloat(Number(a).toFixed(6))
+  } else {
+    return parseFloat(a.toFixed(6))
+  }
+}
+export function calculeS (a) {
+  let result
+  if (typeof a === 'string') {
+    result = Number(a).toFixed(6).split('.')
+  } else {
+    result = a.toFixed(6).split('.')
+  }
+  result[1] = result[1].replace(/[0]*$/, '')
+  if (result[1].length !== 0) { return result[0] + ',' + result[1] } else return result[0]
+}
