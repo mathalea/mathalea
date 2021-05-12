177ad559ad23d3b61dfeff543dd44b09b0731239
Rémi Angot <remi.angot@gmail.com>
2021-05-12 09:34:35 +0200

    correction erreur de distributivité dans 3L15

--- src/js/exercices/3e/3L15.js
+++ src/js/exercices/3e/3L15.js
@@ -46,18 +46,18 @@ export default function ExerciceEquations () {
     for (let i = 0, a, b, c, d, texte, texteCorr, cpt = 0; i < this.nbQuestions && cpt < 50;) {
       switch (listeTypeQuestions[i]) { // Suivant le type de question, le contenu sera différent
         case 'ax2+bx':
-          a = randint(-9, 9)
-          b = randint(-9, 9)
+          a = randint(-9, 9, 0)
+          b = randint(-9, 9, 0)
           texte = ax2plusbx(a, b)[0]
           texteCorr = ax2plusbx(a, b)[1]
           break
         case 'ax2+bxAvec1':
           if (randint(1, 2) === 1) {
             a = choice([-1, 1])
-            b = randint(-9, 9, [-1, 1])
+            b = randint(-9, 9, [-1, 0, 1])
           } else {
             b = choice([-1, 1])
-            a = randint(-9, 9, [-1, 1])
+            a = randint(-9, 9, [-1, 0, 1])
           }
           texte = ax2plusbx(a, b)[0]
           texteCorr = ax2plusbx(a, b)[1]
@@ -109,27 +109,27 @@ export default function ExerciceEquations () {
             texte = `$ ${rienSi1(b * c)}x^2 ${ecritureAlgebrique(a)} = ${rienSi1(b)}x(${rienSi1(c)}x ${ecritureAlgebrique(d)}) $`
             texteCorr = `$ ${rienSi1(b * c)}x^2 ${ecritureAlgebrique(a)} = ${rienSi1(b)}x(${rienSi1(c)}x ${ecritureAlgebrique(d)}) $`
             texteCorr += '<br>'
-            texteCorr += `$ ${rienSi1(b * c)}x^2 ${ecritureAlgebrique(a)} = ${rienSi1(b * c)}x^2 ${ecritureAlgebriqueSauf1(d)}x $`
+            texteCorr += `$ ${rienSi1(b * c)}x^2 ${ecritureAlgebrique(a)} = ${rienSi1(b * c)}x^2 ${ecritureAlgebriqueSauf1(d * b)}x $`
             texteCorr += '<br>'
-            texteCorr += `$ ${a} = ${rienSi1(d)}x $`
+            texteCorr += `$ ${a} = ${rienSi1(d * b)}x $`
             texteCorr += '<br>'
-            texteCorr += `$ ${texFraction(a, d)} = x $`
-            if ((a < 0 && d < 0) || pgcd(a, d) !== 1) {
+            texteCorr += `$ ${texFraction(a, d * b)} = x $`
+            if ((a < 0 && d * b < 0) || pgcd(a, d * b) !== 1) {
               texteCorr += '<br>'
-              texteCorr += ` $ x = ${texFractionReduite(a, d)} $`
+              texteCorr += ` $ x = ${texFractionReduite(a, d * b)} $`
             }
           } else {
             texte = `$ ${rienSi1(b)}x(${rienSi1(c)}x ${ecritureAlgebrique(d)}) = ${rienSi1(b * c)}x^2 ${ecritureAlgebrique(a)} $`
             texteCorr = `$  ${rienSi1(b)}x(${rienSi1(c)}x ${ecritureAlgebrique(d)}) = ${rienSi1(b * c)}x^2 ${ecritureAlgebrique(a)} $`
             texteCorr += '<br>'
-            texteCorr += `$ ${rienSi1(b * c)}x^2 ${ecritureAlgebriqueSauf1(d)}x = ${rienSi1(b * c)}x^2 ${ecritureAlgebrique(a)}$`
+            texteCorr += `$ ${rienSi1(b * c)}x^2 ${ecritureAlgebriqueSauf1(b * d)}x = ${rienSi1(b * c)}x^2 ${ecritureAlgebrique(a)}$`
             texteCorr += '<br>'
-            texteCorr += `$ ${rienSi1(d)}x = ${a} $`
+            texteCorr += `$ ${rienSi1(b * d)}x = ${a} $`
             texteCorr += '<br>'
-            texteCorr += `$ x = ${texFraction(a, d)}$`
-            if ((a < 0 && d < 0) || pgcd(a, d) !== 1) {
+            texteCorr += `$ x = ${texFraction(a, b * d)}$`
+            if ((a < 0 && b * d < 0) || pgcd(a, b * d) !== 1) {
               texteCorr += '<br>'
-              texteCorr += ` $ x = ${texFractionReduite(a, d)} $`
+              texteCorr += ` $ x = ${texFractionReduite(a, b * d)} $`
             }
           }
 
