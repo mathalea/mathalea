ffddf2fb5146f6a72eba246372a9d743d1616d6a
Rémi Angot <remi.angot@gmail.com>
2021-05-14 19:31:47 +0200

    6C10-3 interactif QCM

--- src/js/exercices/6e/6C10-3.js
+++ src/js/exercices/6e/6C10-3.js
@@ -1,10 +1,9 @@
-/* global mathalea */
 import Exercice from '../Exercice.js'
-import { context } from '../../modules/context.js'
-import { listeQuestionsToContenu, creerCouples, choice, texNombre, texNombre2, calcul, shuffle2tableaux } from '../../modules/outils.js'
-import { propositionsQcm, elimineDoublons } from '../../modules/gestionQcm.js'
+import { listeQuestionsToContenu, creerCouples, choice, texNombre, texNombre2, calcul } from '../../modules/outils.js'
+import { propositionsQcm } from '../../modules/gestionQcm.js'
 
 export const amcReady = true
+export const interactifReady = true
 export const amcType = 1 // type de question AMC
 
 export const titre = 'Tables de multiplications et nombres décimaux'
@@ -26,6 +25,9 @@ export default function ExerciceTablesMultiplicationsEtDecimaux (
   this.tailleDiaporama = 100
   this.qcmDisponible = true
   this.modeQcm = false
+  this.interactifReady = true
+  this.interactif = true
+  this.amcType = amcType
 
   this.nouvelleVersion = function () {
     this.qcm = ['6C10-3', [], 'tables de multiplications et nombres décimaux', 1]
@@ -35,7 +37,7 @@ export default function ExerciceTablesMultiplicationsEtDecimaux (
       // Si aucune table n'est saisie
       this.sup = '2-3-4-5-6-7-8-9'
     }
-    let tables = []; let tabrep; let tabicone
+    let tables = []
     if (typeof this.sup === 'number') {
       // Si c'est un nombre c'est qu'il y a qu'une seule table
       tables[0] = this.sup
@@ -47,22 +49,11 @@ export default function ExerciceTablesMultiplicationsEtDecimaux (
       [2, 3, 4, 5, 6, 7, 8, 9, 10],
       this.nbQuestions
     ) // Liste tous les couples possibles (2,3)≠(3,2)
-    for (
-      let i = 0, a, b, k1, k2, couple, texte, texteCorr;
-      i < this.nbQuestions;
-      i++
-    ) {
+    for (let i = 0, a, b, k1, k2, couple, texte, texteCorr; i < this.nbQuestions; i++) {
+      this.autoCorrection[i] = {}
       a = couples[i][0]
       b = couples[i][1]
-      couple = choice([
-        [1, 10],
-        [1, 100],
-        [1, 1000],
-        [10, 100],
-        [10, 1000],
-        [100, 100],
-        [100, 1000]
-      ])
+      couple = choice([[1, 10], [1, 100], [1, 1000], [10, 100], [10, 1000], [100, 100], [100, 1000]])
       k1 = couple[0]
       k2 = couple[1]
       a = calcul(a / k1)
@@ -86,26 +77,47 @@ export default function ExerciceTablesMultiplicationsEtDecimaux (
       /**********************************/
       // QCM
       /**********************************/
-      tabrep = [`$${texNombre2(a * b)}$`, `$${texNombre2(calcul(a * b / 10))}$`, `$${texNombre2(calcul(a * b * 10))}$`, `$${texNombre2(calcul(a * b / 100))}$`, `$${texNombre2(calcul(a * b * 100))}$`]
-      tabicone = [1, 0, 0, 0, 0];
-      [tabrep, tabicone] = elimineDoublons(tabrep, tabicone)
-      this.qcm[1].push([`${texte}\n`,
-        tabrep,
-        tabicone])
-
-      shuffle2tableaux(tabrep, tabicone)
-      if (this.modeQcm && !context.isAmc) {
-        this.tableauSolutionsDuQcm[i] = tabicone
-        texte += propositionsQcm(this.numeroExercice, i, tabrep, tabicone).texte
-        texteCorr += propositionsQcm(this.numeroExercice, i, tabrep, tabicone).texteCorr
+      this.autoCorrection[i].enonce = `${texte}\n`
+      this.autoCorrection[i].propositions = [
+        {
+          texte: `$${texNombre2(a * b)}$`,
+          statut: true,
+          feedback: 'Correct !'
+        },
+        {
+          texte: `$${texNombre2(calcul(a * b / 10))}$`,
+          statut: false,
+          feedback: 'Compte le nombre de zéros dans chaque facteur'
+        },
+        {
+          texte: `$${texNombre2(calcul(a * b * 10))}$`,
+          statut: false,
+          feedback: 'Compte le nombre de zéros dans chaque facteur'
+        },
+        {
+          texte: `$${texNombre2(calcul(a * b / 100))}$`,
+          statut: false,
+          feedback: 'Compte le nombre de zéros dans chaque facteur'
+        },
+        {
+          texte: `$${texNombre2(calcul(a * b * 100))}$`,
+          statut: false,
+          feedback: 'Compte le nombre de zéros dans chaque facteur'
+        }
+      ]
+      this.autoCorrection[i].options = {
+        ordered: false,
+        lastChoice: 5
       }
-
+      if (this.interactif) {
+        texte += propositionsQcm(this, i).texte
+        // texteCorr += propositionsQcm(this.numeroExercice, i, tabrep, tabicone).texteCorr
+      }
       this.listeQuestions.push(texte)
       this.listeCorrections.push(texteCorr)
     }
     listeQuestionsToContenu(this)
   }
-  
   this.besoinFormulaireTexte = [
     'Choix des tables',
     'Nombres séparés par des tirets'
