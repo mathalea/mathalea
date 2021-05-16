b84620e799fe28e6188687bec3d0088e3774ccf9
Rémi Angot <remi.angot@gmail.com>
2021-05-16 18:55:07 +0200

    interactif

--- src/js/exercices/CM/CM017.js
+++ src/js/exercices/CM/CM017.js
@@ -1,25 +1,30 @@
 import Exercice from '../Exercice.js'
-import { context } from '../../modules/context.js'
-import {listeQuestionsToContenu,randint,choice,calcul,texNombrec,texNombre} from '../../modules/outils.js'
+import { ajouteChampTexte, setReponse } from '../../modules/gestionInteractif.js'
+import { listeQuestionsToContenu, randint, choice, calcul, texNombrec, texNombre } from '../../modules/outils.js'
 export const titre = 'Diviser un nombre décimal par 10, 100 ou 1000'
-
+export const amcReady = false
+export const interactifReady = true
+export const amcType = 4
 /**
  * Un entier à un 1 ou 2 chiffres, un nombre décimal avec une partie décimale à un ou 2 chiffres à diviser par 10, 100 ou 1000
  * @Auteur Rémi Angot
  * Référence CM017
 */
-export default function Diviser_decimal_par_10_100_1000() {
-  Exercice.call(this); // Héritage de la classe Exercice()
-  this.titre = titre;
-  this.consigne = "Calculer";
-  this.nbQuestions = 10;
-  this.nbCols = 2;
-  this.nbColsCorr = 2;
-  this.tailleDiaporama = 100;
+export default function DiviserDecimalPar101001000 () {
+  Exercice.call(this) // Héritage de la classe Exercice()
+  this.titre = titre
+  this.amcReady = amcReady
+  this.amcType = amcType
+  this.interactifReady = interactifReady
+  this.consigne = 'Calculer'
+  this.nbQuestions = 10
+  this.nbCols = 2
+  this.nbColsCorr = 2
+  this.tailleDiaporama = 100
 
   this.nouvelleVersion = function () {
-    this.listeQuestions = []; // Liste de questions
-    this.listeCorrections = []; // Liste de questions corrigées
+    this.listeQuestions = [] // Liste de questions
+    this.listeCorrections = [] // Liste de questions corrigées
 
     for (
       let i = 0, texte, texteCorr, a, b, cpt = 0;
@@ -31,24 +36,25 @@ export default function Diviser_decimal_par_10_100_1000() {
         randint(11, 99),
         calcul(randint(11, 99) / 10),
         calcul(randint(101, 999) / 100),
-        calcul(randint(1, 9) / 10),
-      ]);
-      b = choice([10, 100, 1000]);
-      texte = `$${texNombre(a)}\\div${texNombre(b)}$`;
+        calcul(randint(1, 9) / 10)
+      ])
+      b = choice([10, 100, 1000])
+      texte = `$${texNombre(a)}\\div${texNombre(b)}=$`
       texteCorr = `$${texNombre(a)}\\div${texNombre(b)}=${texNombrec(
         a / b
-      )}$`;
+      )}$`
+      setReponse(this, i, calcul(a / b))
+      if (this.interactif) texte += ajouteChampTexte(this, i)
 
       if (this.listeQuestions.indexOf(texte) === -1) {
         // Si la question n'a jamais été posée, on en crée une autre
-        this.listeQuestions.push(texte);
-        this.listeCorrections.push(texteCorr);
-        i++;
+        this.listeQuestions.push(texte)
+        this.listeCorrections.push(texteCorr)
+        i++
       }
-      cpt++;
+      cpt++
     }
-    listeQuestionsToContenu(this);
-  };
-  //this.besoinFormulaireNumerique = ['Niveau de difficulté',3];
+    listeQuestionsToContenu(this)
+  }
+  // this.besoinFormulaireNumerique = ['Niveau de difficulté',3];
 }
-
