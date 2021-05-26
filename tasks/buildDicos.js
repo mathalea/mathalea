1b73cad5e9b03c9dcc0bdd6485a8e7053892fd07
Sébastien LOZANO <sebastien.lozano@ac-nancy-metz.fr>
2021-05-26 22:02:14 +0200

    symbole ✖︎ en gras

--- tasks/buildDicos.js
+++ tasks/buildDicos.js
@@ -73,8 +73,6 @@ for (const file of exercicesList) {
       continue
     }
     titre = module.titre
-    // On teste à l'ancienne la présence de this.qcm dans le code car dans ce cas le booléen amcReady doit être true
-    // On affiche une erreur dans le terminal pour signaler qu'il faut l'ajouter    
     amcReady = Boolean(module.amcReady)
     interactifReady = Boolean(module.interactifReady)
     description = module.description ? module.description : 'pas de description'
@@ -154,8 +152,8 @@ for (const file of exercicesList) {
     // On ajoute amcType que si amcReady est à true
     if (amcReady) {
       // On ajuste la propriété text de amcType différemment si c'est un tableau ou non
-      //let typeText = ["qcmMono","qcmMult","AMCOpen","AMCNum","AMCOpenNum","AMCOpenNum✖︎2","AMCOpenNum✖︎3"]      
-      let typeText = ["qcmMono","qcmMult","AMCOpen","AMCNum","AMCOpenNum","AMCOpenNum×2","AMCOpenNum×3"]      
+      let typeText = ["qcmMono","qcmMult","AMCOpen","AMCNum","AMCOpenNum","AMCOpenNum✖︎2","AMCOpenNum✖︎3"]      
+      //let typeText = ["qcmMono","qcmMult","AMCOpen","AMCNum","AMCOpenNum","AMCOpenNum×2","AMCOpenNum×3"]      
       if (typeof amcType.num === 'number')  {        
         switch (amcType.num) {
           case 1:
@@ -247,20 +245,6 @@ fs.writeFileSync(dictFile, `export default ${JSON.stringify(dicoAlea, null, 2)}`
 endWarnText()
 console.log(`${dictFile} généré ${sumWarnings()}`)
 // ligne supprimée avant il y avait un dico spécifique pour AMC cf commit 7dac24e
-// const csvDir = path.resolve(__dirname, '..', 'src', 'csv')
-// let csvFile = path.resolve(csvDir,'.','listingParTypes.csv')
-// fs.writeFileSync(csvFile,`id,titre,amcReady,amcType,interactifReady,\r\n`)
-// Object.entries(dicoAlea).forEach(([id,props]) => {
-//   if (props.amcReady && props.interactifReady) {
-//     fs.appendFileSync(csvFile,`${id},${props.titre.replace(/[,;]/g, '')},OK,${props.amcType.text},OK,${props.interactifType}\r\n`)
-//   } else if (props.amcReady && !props.interactifReady) {
-//     fs.appendFileSync(csvFile,`${id},${props.titre.replace(/[,;]/g, '')},OK,${props.amcType.text},KO,KO\r\n`)
-//   } else if (!props.amcReady && props.interactifReady) {
-//     fs.appendFileSync(csvFile,`${id},${props.titre.replace(/[,;]/g, '')},KO,KO,OK,${props.interactifType}\r\n`)
-//   }
-// })
-// console.log(`${csvFile} généré`)
-
 const mdDir = path.resolve(__dirname, '..', 'src', '.')
 let mdFile  = path.resolve(mdDir,'.','exosAmcInteractifs.md')
 fs.writeFileSync(mdFile,`|id|titre|description|amcReady|amcType|interactifReady|interactifType|\r\n`)
