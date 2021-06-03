4a047bc4f49e58fd4ae7c6087e10aafa96af2130
Rémi Angot <remi.angot@gmail.com>
2021-06-03 22:36:35 +0200

    Ajout Interactif

--- src/js/exercices/3e/3S12.js
+++ src/js/exercices/3e/3S12.js
@@ -1,9 +1,11 @@
 import Exercice from '../Exercice.js'
-import { context } from '../../modules/context.js'
-import {randint,listeQuestionsToContenu,choice,premiereLettreEnMajuscule,numAlpha,texFractionSigne,calcul,arrondi,arrondiVirgule, texteGras} from '../../modules/outils.js';
-import {repere2,traceBarre,mathalea2d} from '../../modules/2d.js';
+import { randint, listeQuestionsToContenu, choice, premiereLettreEnMajuscule, numAlpha, texFractionSigne, calcul, arrondi, arrondiVirgule, texteGras } from '../../modules/outils.js'
+import { repere2, traceBarre, mathalea2d } from '../../modules/2d.js'
+import { ajouteChampTexteMathLive, setReponse } from '../../modules/gestionInteractif.js'
 
 export const titre = 'Calculer des effectifs et des fréquences'
+export const interactifReady = true
+export const interactifType = 'mathLive'
 
 /**
 * Calculer des effectifs et des fréquences.
@@ -12,82 +14,86 @@ export const titre = 'Calculer des effectifs et des fréquences'
 * 2021-02-07
 */
 
-export default function calcul_Effectif_Frequence() {
-    "use strict"
-    Exercice.call(this)
-    this.titre = titre;
-    this.consigne = `Répondre aux questions à l'aide du graphique et de calculs.`;
-    this.nbQuestions = 4; // Ici le nombre de questions
-    this.nbQuestionsModifiable=false; // Active le formulaire nombre de questions
-    this.nbCols = 2; // Le nombre de colonnes dans l'énoncé LaTeX
-    this.nbColsCorr = 2;// Le nombre de colonne pour la correction LaTeX
-    this.tailleDiaporama = 50;
-    this.pasDeVersionLatex=false; // mettre à true si on ne veut pas de l'exercice dans le générateur LaTeX
-    this.pas_de_version_HMTL=false; // mettre à true si on ne veut pas de l'exercice en ligne
-    this.video = "https://youtu.be/GWDDay-mdVA"; // Id YouTube ou url
-    this.correctionDetailleeDisponible=false;
+export default function calculEffectifFrequence () {
+  Exercice.call(this)
+  this.titre = titre
+  this.interactifReady = interactifReady
+  this.interactifType = interactifType
+  this.consigne = 'Répondre aux questions à l\'aide du graphique et de calculs.'
+  this.nbQuestions = 4 // Ici le nombre de questions
+  this.nbQuestionsModifiable = false // Active le formulaire nombre de questions
+  this.nbCols = 1 // Le nombre de colonnes dans l'énoncé LaTeX
+  this.nbColsCorr = 1// Le nombre de colonne pour la correction LaTeX
+  this.tailleDiaporama = 50
+  this.pasDeVersionLatex = false // mettre à true si on ne veut pas de l'exercice dans le générateur LaTeX
+  this.pas_de_version_HMTL = false // mettre à true si on ne veut pas de l'exercice en ligne
+  this.video = 'https://youtu.be/GWDDay-mdVA' // Id YouTube ou url
+  this.correctionDetailleeDisponible = false
   // Voir la Classe Exercice pour une liste exhaustive des propriétés disponibles.
-  
-   this.sup = 1; // A décommenter : valeur par défaut d'un premier paramètre
+
+  this.sup = 1 // A décommenter : valeur par défaut d'un premier paramètre
   //  this.sup2 = false; // A décommenter : valeur par défaut d'un deuxième paramètre
   //  this.sup3 = false; // A décommenter : valeur par défaut d'un troisième paramètre
-  
+
   // c'est ici que commence le code de l'exercice cette fonction crée une copie de l'exercice
-    this.nouvelleVersion = function () {
+  this.nouvelleVersion = function () {
     // la variable numeroExercice peut être récupérée pour permettre de différentier deux copies d'un même exo
     // Par exemple, pour être certain de ne pas avoir les mêmes noms de points en appelant 2 fois cet exo dans la même page
-  
-    this.listeQuestions = []; // tableau contenant la liste des questions 
-    this.listeCorrections = [];
-    //let listeTypeDeQuestions_disponibles= [];
-    let lstQuadri = ['girafes', 'zèbres', 'gnous', 'buffles', 'gazelles', 'crocodiles', 'rhinocéros', 'léopards', 'guépards', 'hyènes', 'lycaons', 'servals', 'phacochères'];
-    let lstOiseaux = ['hérons', 'marabouts', 'flamants roses', 'cigognes', 'grues', 'vautours'];
-    let nbAnimaux = 4 + parseInt(this.sup); // nombre d'animaux différents dans l'énoncé (entre 5 et 7)
-    let nbQuadri = 3 ;
-    let lstAnimauxExo = []; //liste des animaux uniquement cités dans l'exercice
-    let lstNombresAnimaux = []; // liste des effectifs de chaque animal
-    let lstVal = []; // liste des valeurs à éviter pour les effectifs
-    let N = 0, nom=``, texte=``, texteCorr=``;
-  
+
+    this.listeQuestions = [] // tableau contenant la liste des questions
+    this.listeCorrections = []
+    // let listeTypeDeQuestions_disponibles= [];
+    const lstQuadri = ['girafes', 'zèbres', 'gnous', 'buffles', 'gazelles', 'crocodiles', 'rhinocéros', 'léopards', 'guépards', 'hyènes', 'lycaons', 'servals', 'phacochères']
+    const lstOiseaux = ['hérons', 'marabouts', 'flamants roses', 'cigognes', 'grues', 'vautours']
+    const nbAnimaux = 4 + parseInt(this.sup) // nombre d'animaux différents dans l'énoncé (entre 5 et 7)
+    const nbQuadri = 3
+    const lstAnimauxExo = [] // liste des animaux uniquement cités dans l'exercice
+    const lstNombresAnimaux = [] // liste des effectifs de chaque animal
+    let lstVal = [] // liste des valeurs à éviter pour les effectifs
+    let N = 0; let nom = ''; let texte = ''; let texteCorr = ''
+
     for (let i = 0; i < nbAnimaux; i++) {
-      N = randint(2, 10, lstVal); // choisit un nombre entre 2 et 10 sauf dans les valeurs à éviter
-      lstNombresAnimaux.push(N);
-      lstVal = lstVal.concat([N]); // valeurs à supprimer pour éviter des valeurs égales
-      if (i<nbQuadri){
-        nom = choice(lstQuadri, lstAnimauxExo); // choisit un animal au hasard sauf parmi ceux déjà utilisés
-      } else
-      {
-        nom = choice(lstOiseaux, lstAnimauxExo); // choisit un animal au hasard sauf parmi ceux déjà utilisés
+      N = randint(2, 10, lstVal) // choisit un nombre entre 2 et 10 sauf dans les valeurs à éviter
+      lstNombresAnimaux.push(N)
+      lstVal = lstVal.concat([N]) // valeurs à supprimer pour éviter des valeurs égales
+      if (i < nbQuadri) {
+        nom = choice(lstQuadri, lstAnimauxExo) // choisit un animal au hasard sauf parmi ceux déjà utilisés
+      } else {
+        nom = choice(lstOiseaux, lstAnimauxExo) // choisit un animal au hasard sauf parmi ceux déjà utilisés
       }
-      lstAnimauxExo.push(nom);
+      lstAnimauxExo.push(nom)
     }
 
-    let lstNomParc = ['Dramve', 'Fatenmin', 'Batderfa', 'Vihi', 'Genser', 'Barbetdou', 'Dramrendu', 'Secai', 'Cipeudram', 'Cigel', 'Lisino', 'Fohenlan', 'Farnfoss', 'Kinecardine', 'Zeffari', 'Barmwich', 'Swadlincote', 'Swordbreak', 'Loshull', 'Ruyron', 'Fluasall', 'Blueross', 'Vlane'];
+    const lstNomParc = ['Dramve', 'Fatenmin', 'Batderfa', 'Vihi', 'Genser', 'Barbetdou', 'Dramrendu', 'Secai', 'Cipeudram', 'Cigel', 'Lisino', 'Fohenlan', 'Farnfoss', 'Kinecardine', 'Zeffari', 'Barmwich', 'Swadlincote', 'Swordbreak', 'Loshull', 'Ruyron', 'Fluasall', 'Blueross', 'Vlane']
 
-    texte += `Dans le parc naturel de ` + choice(lstNomParc) + `, il y a des animaux. ` ;
-    texte += `Certains sont des quadripèdes (`;
+    texte += 'Dans le parc naturel de ' + choice(lstNomParc) + ', il y a des animaux. '
+    texte += 'Certains sont des quadripèdes ('
     for (let i = 0; i < nbQuadri; i++) {
-      texte += lstAnimauxExo[i] + ', ';
-    } 
-    texte = texte.substring(0, texte.length-2);
-    texte += `), et d'autres sont des oiseaux (`;
-    for (let i = nbQuadri; i < nbAnimaux; i++) {
-      texte += lstAnimauxExo[i] + ', ';
+      texte += lstAnimauxExo[i] + ', '
     }
-    texte = texte.substring(0, texte.length-2);
-    texte += `). `;
-    
-    texte += 'Voici un diagramme en bâtons qui donne le nombre d’individus pour chaque espèce.<br>';
-    texte += numAlpha(0) + ` Quel est l'effectif des ` + lstAnimauxExo[0] + ` ?<br>`;
-    texte += numAlpha(1) + ` Calculer la fréquence des ` + lstAnimauxExo[1] + ` ? Donner le résultat sous la forme d'un pourcentage.<br>`;
-    texte += numAlpha(2) + ` Calculer l'effectif des quadripèdes ? <br>`;
-    texte += numAlpha(3) + ` Calculer la fréquence des oiseaux ? Donner le résultat sous la forme d'un pourcentage.<br>`;
+    texte = texte.substring(0, texte.length - 2)
+    texte += '), et d\'autres sont des oiseaux ('
+    for (let i = nbQuadri; i < nbAnimaux; i++) {
+      texte += lstAnimauxExo[i] + ', '
+    }
+    texte = texte.substring(0, texte.length - 2)
+    texte += '). '
 
-    texte += `Les pourcentages seront éventuellement arrondis à 0,1% près. <br>`;
+    texte += 'Voici un diagramme en bâtons qui donne le nombre d’individus pour chaque espèce.<br>'
+    texte += numAlpha(0) + ' Quel est l\'effectif des ' + lstAnimauxExo[0] + ' ?<br>'
+    texte += ajouteChampTexteMathLive(this, 1)
+    texte += numAlpha(1) + ' Calculer la fréquence des ' + lstAnimauxExo[1] + ' ? Donner le résultat sous la forme d\'un pourcentage.<br>'
+    texte += ajouteChampTexteMathLive(this, 2)
+    texte += numAlpha(2) + ' Calculer l\'effectif des quadripèdes ? <br>'
+    texte += ajouteChampTexteMathLive(this, 3)
+    texte += numAlpha(3) + ' Calculer la fréquence des oiseaux ? Donner le résultat sous la forme d\'un pourcentage.<br>'
+    texte += ajouteChampTexteMathLive(this, 4)
 
-    let coef = 1;
+    texte += 'Les pourcentages seront éventuellement arrondis à 0,1% près. <br>'
 
-    let r = repere2({
+    const coef = 1
+
+    const r = repere2({
       grilleX: false,
       grilleY: 'pointilles',
       xThickListe: [],
@@ -100,76 +106,72 @@ export default function calcul_Effectif_Frequence() {
       yMin: 0,
       axeXStyle: '',
       yLegende: "Nombre d'individus"
-    });
+    })
 
-    let lstElementGraph = [];
+    const lstElementGraph = []
     for (let i = 0; i < nbAnimaux; i++) {
-      lstElementGraph.push(traceBarre((((r.xMax - r.xMin) / (nbAnimaux + 1)) * (i + 1)), lstNombresAnimaux[i], premiereLettreEnMajuscule(lstAnimauxExo[i]), { unite: 1 / coef }));
+      lstElementGraph.push(traceBarre((((r.xMax - r.xMin) / (nbAnimaux + 1)) * (i + 1)), lstNombresAnimaux[i], premiereLettreEnMajuscule(lstAnimauxExo[i]), { unite: 1 / coef }))
     }
-    texte += mathalea2d({ xmin: -5, xmax: 11, ymin: -4, ymax: 11, pixelsParCm: 30, scale: 1 }, r, lstElementGraph);
+    texte += '<br>' + mathalea2d({ xmin: -5, xmax: 11, ymin: -4, ymax: 11, pixelsParCm: 30, scale: 1 }, r, lstElementGraph)
 
-//début de la correction
-    //question 1    
-    texteCorr += numAlpha(0) + texteGras(` D'après le graphique, il y a ` + lstNombresAnimaux[0] + ` ` + lstAnimauxExo[0] + `. <br>`);
-    //question 2
-    let Ntotal = lstNombresAnimaux[0];
-     texteCorr += numAlpha(1) + ` L'effectif total des animaux est : ` + lstNombresAnimaux[0] ;
+    // début de la correction
+    // question 1
+    texteCorr += numAlpha(0) + texteGras(' D\'après le graphique, il y a ' + lstNombresAnimaux[0] + ' ' + lstAnimauxExo[0] + '. <br>')
+    setReponse(this, 1, lstNombresAnimaux[0])
+    // question 2
+    let Ntotal = lstNombresAnimaux[0]
+    texteCorr += numAlpha(1) + ' L\'effectif total des animaux est : ' + lstNombresAnimaux[0]
     for (let i = 1; i < nbAnimaux; i++) {
-      texteCorr += ` + ` + lstNombresAnimaux[i];
-      Ntotal += lstNombresAnimaux[i];
-    } 
-    
-    texteCorr += ` = `+ Ntotal + `. `
-    texteCorr += ` D'après le graphique, il y a ` + lstNombresAnimaux[1] + ` ` + lstAnimauxExo[1] + `. <br>`;
-    texteCorr += ` La fréquence (ou la proportion) de  ` + lstAnimauxExo[1] + ` est : $ ` + texFractionSigne(lstNombresAnimaux[1], Ntotal) + `$ `;
-    // test de l'arrondi
-    if (calcul(lstNombresAnimaux[1]/Ntotal) == arrondi(lstNombresAnimaux[1]/Ntotal,3)) {
-      texteCorr += `= `;
-    } else 
-    {
-      texteCorr += `$\\approx $ `;
+      texteCorr += ' + ' + lstNombresAnimaux[i]
+      Ntotal += lstNombresAnimaux[i]
     }
-    texteCorr += arrondiVirgule(lstNombresAnimaux[1]/Ntotal,3) + `. <br>`;
-    texteCorr += texteGras(`La fréquence des ` + lstAnimauxExo[1] + ` est donc : ` + arrondiVirgule(100*lstNombresAnimaux[1]/Ntotal,1) + `%. <br>`);
 
-    //question 3
-    texteCorr += numAlpha(2) + ` On fait la somme des effectifs de chaque espèce de quadripèdes : `
-    let N_totalQuadri = lstNombresAnimaux[0];
-     texteCorr += lstNombresAnimaux[0] ;
+    texteCorr += ' = ' + Ntotal + '. '
+    texteCorr += ' D\'après le graphique, il y a ' + lstNombresAnimaux[1] + ' ' + lstAnimauxExo[1] + '. <br>'
+    texteCorr += ' La fréquence (ou la proportion) de  ' + lstAnimauxExo[1] + ' est : $ ' + texFractionSigne(lstNombresAnimaux[1], Ntotal) + '$ '
+    // test de l'arrondi
+    if (calcul(lstNombresAnimaux[1] / Ntotal) === arrondi(lstNombresAnimaux[1] / Ntotal, 3)) {
+      texteCorr += '= '
+    } else {
+      texteCorr += '$\\approx $ '
+    }
+    texteCorr += arrondiVirgule(lstNombresAnimaux[1] / Ntotal, 3) + '. <br>'
+    texteCorr += texteGras('La fréquence des ' + lstAnimauxExo[1] + ' est donc : ' + arrondiVirgule(100 * lstNombresAnimaux[1] / Ntotal, 1) + '%. <br>')
+    setReponse(this, 2, [calcul(100 * lstNombresAnimaux[1] / Ntotal, 1), `${calcul(100 * lstNombresAnimaux[1] / Ntotal, 1)}\\%`])
+    // question 3
+    texteCorr += numAlpha(2) + ' On fait la somme des effectifs de chaque espèce de quadripèdes : '
+    let NTotalQuadri = lstNombresAnimaux[0]
+    texteCorr += lstNombresAnimaux[0]
     for (let i = 1; i < nbQuadri; i++) {
-      texteCorr += ` + ` + lstNombresAnimaux[i];
-      N_totalQuadri += lstNombresAnimaux[i];
-    } 
-    texteCorr += `. <br>`
-    texteCorr += texteGras(`L'effectif des quadripèdes est donc : ` + N_totalQuadri + `.<br>`);
-
-
-
-    //question 4
-    let Ntotal_oiseaux = lstNombresAnimaux[3];
-     texteCorr += numAlpha(3) + ` L'effectif total des oiseaux est : ` + lstNombresAnimaux[3] ;
-    for (let i = 4; i < nbAnimaux; i++) {
-      texteCorr += ` + ` + lstNombresAnimaux[i];
-      Ntotal_oiseaux += lstNombresAnimaux[i];
-    } 
-    texteCorr += ` = `+ Ntotal_oiseaux + `. `
-    texteCorr += ` L'effectif total des animaux est : ` + Ntotal + `. <br>`;
-    texteCorr += ` La fréquence (ou la proportion) d'oiseaux est : $ ` + texFractionSigne(Ntotal_oiseaux, Ntotal) + `$ `;
-    // test de l'arrondi
-    if (calcul(Ntotal_oiseaux/Ntotal) == arrondi(Ntotal_oiseaux/Ntotal,3)) {
-      texteCorr += `= `;
-    } else 
-    {
-      texteCorr += `$\\approx $ `;
+      texteCorr += ' + ' + lstNombresAnimaux[i]
+      NTotalQuadri += lstNombresAnimaux[i]
     }
-    texteCorr += arrondiVirgule(Ntotal_oiseaux/Ntotal,3) + `. <br>`;
-    texteCorr += texteGras(`La fréquence des oiseaux est donc : ` + arrondiVirgule(100*Ntotal_oiseaux/Ntotal,1) + `%. <br>`);
+    texteCorr += '. <br>'
+    texteCorr += texteGras('L\'effectif des quadripèdes est donc : ' + NTotalQuadri + '.<br>')
+    setReponse(this, 3, NTotalQuadri)
+    // question 4
+    let NTotalOiseaux = lstNombresAnimaux[3]
+    texteCorr += numAlpha(3) + ' L\'effectif total des oiseaux est : ' + lstNombresAnimaux[3]
+    for (let i = 4; i < nbAnimaux; i++) {
+      texteCorr += ' + ' + lstNombresAnimaux[i]
+      NTotalOiseaux += lstNombresAnimaux[i]
+    }
+    texteCorr += ' = ' + NTotalOiseaux + '. '
+    texteCorr += ' L\'effectif total des animaux est : ' + Ntotal + '. <br>'
+    texteCorr += ' La fréquence (ou la proportion) d\'oiseaux est : $ ' + texFractionSigne(NTotalOiseaux, Ntotal) + '$ '
+    // test de l'arrondi
+    if (calcul(NTotalOiseaux / Ntotal) === arrondi(NTotalOiseaux / Ntotal, 3)) {
+      texteCorr += '= '
+    } else {
+      texteCorr += '$\\approx $ '
+    }
+    texteCorr += arrondiVirgule(NTotalOiseaux / Ntotal, 3) + '. <br>'
+    texteCorr += texteGras('La fréquence des oiseaux est donc : ' + arrondiVirgule(100 * NTotalOiseaux / Ntotal, 1) + '%. <br>')
+    setReponse(this, 4, [calcul(100 * NTotalOiseaux / Ntotal, 1), calcul(100 * NTotalOiseaux / Ntotal, 1) + '\\%'])
+    this.listeQuestions.push(texte)
+    this.listeCorrections.push(texteCorr)
 
-    this.listeQuestions.push(texte);
-    this.listeCorrections.push(texteCorr);
-
-    listeQuestionsToContenu(this); // On envoie l'exercice à la fonction de mise en page
-  };
-  this.besoinFormulaireNumerique = [`Nombre d'espèces différentes`, 3, ` choix 1 : 5 espèces\n choix 2 : 6 espèces\n choix 3 : 7 espèces`];
+    listeQuestionsToContenu(this) // On envoie l'exercice à la fonction de mise en page
+  }
+  this.besoinFormulaireNumerique = ['Nombre d\'espèces différentes', 3, ' choix 1 : 5 espèces\n choix 2 : 6 espèces\n choix 3 : 7 espèces']
 } // Fin de l'exercice.
-  
--- src/js/exercices/3e/3S15.js
+++ src/js/exercices/3e/3S15.js
@@ -1,105 +1,103 @@
 import Exercice from '../Exercice.js'
-import { context } from '../../modules/context.js'
-import {listeQuestionsToContenu,randint,choice,ecritureParentheseSiNegatif,prenom,listeDeNotes,joursParMois,unMoisDeTemperature,nomDuMois,texNombre} from '../../modules/outils.js'
-
+import { listeQuestionsToContenu, randint, choice, ecritureParentheseSiNegatif, prenom, listeDeNotes, joursParMois, unMoisDeTemperature, nomDuMois, texNombre } from '../../modules/outils.js'
+import { ajouteChampTexteMathLive, setReponse } from '../../modules/gestionInteractif.js'
 
 export const titre = 'Calculer des étendues'
+export const interactifReady = true
+export const interactifType = 'mathLive'
 
 /**
  * Calculer des étendues de séries statistiques
 * @author Jean-Claude Lhote
 * Référence 3S15
 */
-export default function Calculer_des_etendues() {
-	Exercice.call(this); // Héritage de la classe Exercice()
-	this.titre = titre;
-	this.consigne = "";
-	this.nbQuestions = 1;
-	this.spacing = 1;
-	this.spacingCorr = 1;
-	this.nbColsCorr = 1;
-	this.nbCols = 1;
-	this.sup = 1;
+export default function CalculerEtendues () {
+  Exercice.call(this) // Héritage de la classe Exercice()
+  this.titre = titre
+  this.interactifReady = interactifReady
+  this.interactifType = interactifType
+  this.consigne = ''
+  this.nbQuestions = 1
+  this.spacing = 1
+  this.spacingCorr = 1
+  this.nbColsCorr = 1
+  this.nbCols = 1
+  this.sup = 1
 
-	this.nouvelleVersion = function () {
-		this.listeQuestions = []; // Liste de questions
-		this.listeCorrections = []; // Liste de questions corrigées
+  this.nouvelleVersion = function () {
+    this.sup = parseInt(this.sup)
+    this.listeQuestions = [] // Liste de questions
+    this.listeCorrections = [] // Liste de questions corrigées
 
-		for (let i = 0, nombreNotes, notes, min, max, temperatures, nombre_temperatures, texte, texteCorr, cpt = 0; i < this.nbQuestions && cpt < 50;) {
-			if (this.sup == 1) { // ici on trie des notes
-				nombreNotes = randint(8, 12);
-				notes = listeDeNotes(nombreNotes, randint(0, 7), randint(13, 20)); // on récupère une série de notes (série brute)
-				min = 20, max = 0;
-				for (let  j = 0; j < nombreNotes; j++) { // On cherche la note minimum et la note maximum
-					min = Math.min(notes[j], min);
-					max = Math.max(notes[j], max);
-				}
-				texte = `${prenom()} a obtenu ces notes ce trimestre-ci en mathématiques :<br>`;
-				texte += `$${notes[0]}$`;
-				for (let j = 1; j < nombreNotes - 1; j++)
-					texte += `; $${notes[j]}$ `; // On liste les notes
-				texte += `et $${notes[nombreNotes - 1]}$.<br>`;
-				texte += `Calculer l\'étendue de cette série de notes.`;
-				texteCorr = `La note la plus basse est : $${min}$.<br>La note la plus haute est $${max}$<br>`;
-				texteCorr += 'Donc l\'étendue de cette série est : ' + `$${texNombre(max)}-${texNombre(min)}=${texNombre(max - min)}$`;
-			}
-			else { // ici on relève des températures
-				let mois = randint(1, 12);
-				let annee = randint(1980, 2019);
-				let temperatures_de_base = [3, 5, 9, 13, 19, 24, 26, 25, 23, 18, 10, 5];
-				nombre_temperatures = joursParMois(mois);
-				temperatures = unMoisDeTemperature(temperatures_de_base[mois - 1], mois, annee); // série brute de un mois de température
-				max = 0;
-				min = 20;
-				texte = `En ${nomDuMois(mois)} ${annee}, à ${choice(['Moscou', 'Berlin', 'Paris', 'Bruxelles', 'Rome', 'Belgrade'])}, on a relevé les températures suivantes<br>`;
+    for (let i = 0, nombreNotes, notes, min, max, temperatures, nombreTemperatures, texte, texteCorr, cpt = 0; i < this.nbQuestions && cpt < 50;) {
+      if (this.sup === 1) { // ici on trie des notes
+        nombreNotes = randint(8, 12)
+        notes = listeDeNotes(nombreNotes, randint(0, 7), randint(13, 20)) // on récupère une série de notes (série brute)
+        min = 20
+        max = 0
+        for (let j = 0; j < nombreNotes; j++) { // On cherche la note minimum et la note maximum
+          min = Math.min(notes[j], min)
+          max = Math.max(notes[j], max)
+        }
+        texte = `${prenom()} a obtenu ces notes ce trimestre-ci en mathématiques :<br>`
+        texte += `$${notes[0]}$`
+        for (let j = 1; j < nombreNotes - 1; j++) { texte += `; $${notes[j]}$ ` } // On liste les notes
+        texte += `et $${notes[nombreNotes - 1]}$.<br>`
+        texte += 'Calculer l\'étendue de cette série de notes.'
+        texteCorr = `La note la plus basse est : $${min}$.<br>La note la plus haute est $${max}$<br>`
+        texteCorr += 'Donc l\'étendue de cette série est : ' + `$${texNombre(max)}-${texNombre(min)}=${texNombre(max - min)}$`
+      } else { // ici on relève des températures
+        const mois = randint(1, 12)
+        const annee = randint(1980, 2019)
+        const temperaturesDeBase = [3, 5, 9, 13, 19, 24, 26, 25, 23, 18, 10, 5]
+        nombreTemperatures = joursParMois(mois)
+        temperatures = unMoisDeTemperature(temperaturesDeBase[mois - 1], mois, annee) // série brute de un mois de température
+        max = 0
+        min = 20
+        texte = `En ${nomDuMois(mois)} ${annee}, à ${choice(['Moscou', 'Berlin', 'Paris', 'Bruxelles', 'Rome', 'Belgrade'])}, on a relevé les températures suivantes<br>`
 
-				texte += '$\\def\\arraystretch{1.5}\\begin{array}{|c'; // tableau des températures 1/2
-				texte += '|c';
-				for (let j = 0; j < Math.round(temperatures.length / 2); j++)
-					texte += '|c';
-				texte += '}\\hline  \\text{Jour}';
-				for (let j = 0; j < Math.round(temperatures.length / 2); j++)
-					texte += '&' + texNombre(j + 1);
-				texte += '\\\\\\hline \\text{Température\\thickspace en} \\thickspace ^\\circ\\text{C}';
-				for (let j = 0; j < Math.round(temperatures.length / 2); j++) { // on cherche le minimum et le maximum
-					texte += '&' + temperatures[j];
-					min = Math.min(temperatures[j], min);
-					max = Math.max(temperatures[j], max);
-				}
-				texte += '\\\\\\hline\\end{array}$<br><br>';
+        texte += '$\\def\\arraystretch{1.5}\\begin{array}{|c' // tableau des températures 1/2
+        texte += '|c'
+        for (let j = 0; j < Math.round(temperatures.length / 2); j++) { texte += '|c' }
+        texte += '}\\hline  \\text{Jour}'
+        for (let j = 0; j < Math.round(temperatures.length / 2); j++) { texte += '&' + texNombre(j + 1) }
+        texte += '\\\\\\hline \\text{Température\\thickspace en} \\thickspace ^\\circ\\text{C}'
+        for (let j = 0; j < Math.round(temperatures.length / 2); j++) { // on cherche le minimum et le maximum
+          texte += '&' + temperatures[j]
+          min = Math.min(temperatures[j], min)
+          max = Math.max(temperatures[j], max)
+        }
+        texte += '\\\\\\hline\\end{array}$<br><br>'
 
-				texte += '$\\def\\arraystretch{1.5}\\begin{array}{|c'; // tableau des températures 2/2
-				texte += '|c';
-				for (let j = Math.round(temperatures.length / 2); j < temperatures.length; j++)
-					texte += '|c';
-				texte += '}\\hline  \\text{Jour}';
-				for (let j = Math.round(temperatures.length / 2); j < temperatures.length; j++)
-					texte += '&' + texNombre(j + 1);
-				texte += '\\\\\\hline \\text{Température\\thickspace en} \\thickspace ^\\circ\\text{C}';
-				for (let j = Math.round(temperatures.length / 2); j < temperatures.length; j++) { // on cherche le minimum et le maximum
-					texte += '&' + temperatures[j];
-					min = Math.min(temperatures[j], min);
-					max = Math.max(temperatures[j], max);
-				}
-				texte += '\\\\\\hline\\end{array}$<br><br>';
+        texte += '$\\def\\arraystretch{1.5}\\begin{array}{|c' // tableau des températures 2/2
+        texte += '|c'
+        for (let j = Math.round(temperatures.length / 2); j < temperatures.length; j++) { texte += '|c' }
+        texte += '}\\hline  \\text{Jour}'
+        for (let j = Math.round(temperatures.length / 2); j < temperatures.length; j++) { texte += '&' + texNombre(j + 1) }
+        texte += '\\\\\\hline \\text{Température\\thickspace en} \\thickspace ^\\circ\\text{C}'
+        for (let j = Math.round(temperatures.length / 2); j < temperatures.length; j++) { // on cherche le minimum et le maximum
+          texte += '&' + temperatures[j]
+          min = Math.min(temperatures[j], min)
+          max = Math.max(temperatures[j], max)
+        }
+        texte += '\\\\\\hline\\end{array}$<br><br>'
 
-				texte += 'Calculer l\'amplitude thermique de ce mois (l\'étendue de la série).';
-				texteCorr = `En ${nomDuMois(mois)} ${annee}, la température minimale est ` + `$${min}^\\circ\\text{C}$.<br>La température maximale est $${max}^\\circ\\text{C}$.<br> L\'amplitude thermique est :<br>`;
-				texteCorr += `$${texNombre(max)}-${ecritureParentheseSiNegatif(min)}$`;
-				if (min < 0)
-					texteCorr += `$\\thickspace~=${texNombre(max)}+${texNombre(-min)}$`;
-				texteCorr += `$\\thickspace=${texNombre(max - min)}^\\circ\\text{C}$`;
-
-			}
-
-			if (this.listeQuestions.indexOf(texte) === -1) { // Si la question n'a jamais été posée, on en créé une autre
-				this.listeQuestions.push(texte);
-				this.listeCorrections.push(texteCorr);
-				i++;
-			}
-			cpt++;
-		}
-		listeQuestionsToContenu(this);
-	};
-	this.besoinFormulaireNumerique = ['Type de séries', 2, "1 : Série de notes\n 2 : Série de températures"];
+        texte += 'Calculer l\'amplitude thermique de ce mois (l\'étendue de la série).'
+        texteCorr = `En ${nomDuMois(mois)} ${annee}, la température minimale est ` + `$${min}^\\circ\\text{C}$.<br>La température maximale est $${max}^\\circ\\text{C}$.<br> L'amplitude thermique est :<br>`
+        texteCorr += `$${texNombre(max)}-${ecritureParentheseSiNegatif(min)}$`
+        if (min < 0) { texteCorr += `$\\thickspace~=${texNombre(max)}+${texNombre(-min)}$` }
+        texteCorr += `$\\thickspace=${texNombre(max - min)}^\\circ\\text{C}$`
+      }
+      setReponse(this, i, max - min)
+      texte += ajouteChampTexteMathLive(this, i)
+      if (this.listeQuestions.indexOf(texte) === -1) { // Si la question n'a jamais été posée, on en créé une autre
+        this.listeQuestions.push(texte)
+        this.listeCorrections.push(texteCorr)
+        i++
+      }
+      cpt++
+    }
+    listeQuestionsToContenu(this)
+  }
+  this.besoinFormulaireNumerique = ['Type de séries', 2, '1 : Série de notes\n 2 : Série de températures']
 }
