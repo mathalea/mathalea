6cda93e98631e86d16052945ef1a610d63654583
Jean-Claude Lhote <lhotejc@gmail.com>
2021-06-05 09:55:54 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ src/js/exercices/6e/6M11.js
@@ -0,0 +1,24 @@
+/* eslint-disable camelcase */
+import { PerimetreOuAireDeCarresRectanglesTriangles, amcReady, interactifReady, amcType, interactifType } from './6M11-1.js'
+export const titre = 'Aires carrés, rectangles et triangles rectangles'
+
+/**
+ * Un carré, un rectangle et un triangle rectangle sont tracés.
+ *
+ * Il faut calculer les aires
+ *
+ * Pas de version LaTeX
+ * @author Rémi Angot
+ * Référence 6M11-1
+ */
+export default function AireCarresRectanglesTriangles () {
+  PerimetreOuAireDeCarresRectanglesTriangles.call(this) // Héritage de la classe Exercice()
+  this.titre = titre
+  this.ref = '6M11'
+  this.amcReady = amcReady
+  this.amcType = amcType
+  this.interactif = false
+  this.interactifReady = interactifReady
+  this.interactifType = interactifType
+  this.consigne = "Calculer l'aire des 3 figures suivantes"
+}
