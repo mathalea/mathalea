7fa2d36bf8e52a1a562327f17e48aac320f95cb7
lhotejc <lhotejc@gmail.com>
2021-06-11 09:05:40 +0200

    Ajout Interactifs et AMC (issus de 6M30)

--- src/js/exercices/4e/4G53.js
+++ src/js/exercices/4e/4G53.js
@@ -3,6 +3,8 @@ import CalculDeVolumes from '../6e/6M30.js'
 export const titre = 'Calculs de volumes'
 export const interactifReady = true
 export const interactifType = ['qcm', 'mathLive']
+export const amcReady = true
+export const amcType = 4
 
 /**
  * @author Jean-claude Lhote
@@ -10,7 +12,11 @@ export const interactifType = ['qcm', 'mathLive']
 export default function CalculDeVolumes_4e () {
   CalculDeVolumes.call(this)
   this.titre = titre
+  this.amcReady = amcReady
+  this.amcType = amcType
   this.interactifReady = interactifReady
+  this.interactifType=interactifType
+  
   this.sup = 1
   this.classe = 4
 }
