07fc53e923913075c69f87fdc3422c443dd72f29
Sébastien LOZANO <sebastien.lozano@ac-nancy-metz.fr>
2021-07-01 15:09:03 +0200

    scores - suppression scoresVerifResult.php + nettoyage code

--- src/php/scoresKey.php
+++ src/php/scoresKey.php
@@ -1,12 +1,12 @@
 <?php
 // TODO
 // =>OK => Vérifier/Nettoyer les variables qui arrivent du client, il n'y en a plus 
-// => Gestion d'un feedback d'erreur sur le format du userId
-// =>OK=> Gestion d'un feedback d'erreur si le userId n'existe pas et qu'on demande d'enregistrer avec
-// =>OK=> Placement de la variable $keypass cf post de Rémi
+// =>OK => Gestion d'un feedback d'erreur sur le format du userId
+// =>OK => Gestion d'un feedback d'erreur si le userId n'existe pas et qu'on demande d'enregistrer avec PLUS D'ACTUALITE
+// =>OK => Placement de la variable $keypass cf post de Rémi
 // => Suppression des espaces userId trop vieux, On garde 15 jours un autre délai qu'on pourra adapter,
 // la routine de nettoyage serait lancée à chaque requete 
-// => Problème url avec des % une fois qu'on affecte un userId
+// =>OK => Problème url avec des % une fois qu'on affecte un userId
 
 $contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : '';
 
@@ -32,176 +32,153 @@ if ($contentType === "application/json") {
   // On récupère le booléen pour savoir si on enregistre des scores ou pas
   $isVerifResult = $decoded->isVerifResult;
 
-  // if ($isVerifResult) {
-  //   // Il faudra factoriser pour vérifier les variables suiavntes
-  //   $prof1 = $decoded->userId[0];
-  //   $prof2 = $decoded->userId[1];
-  //   $prof3 = $decoded->userId[2];
-  //   $classe1 = $decoded->userId[3];
-  //   $classe2 = $decoded->userId[4];
-  //   $eleve1 = $decoded->userId[5];
-  //   $eleve2 = $decoded->userId[6];
-  //   $url = "pas d'url pour les verifsResult";
-  //   $errors .= "errors verifResult";
-
-  // } else {    
-    // Il faut créer le répertoire sur le serveur s'il n'existe pas
-    // On teste une création côté serveur
-    // Si c'est la création d'un nouvel espace on génère le userId ici
-    // Sinon on le récupère via un fetch 
-    if (!$isSubmitUserId && !$isVerifResult) {
-      $prof1 = chr(rand(65,90));
-      $prof2 = chr(rand(65,90));
-      $prof3 = chr(rand(65,90));
-      // On met des chaine vide pour la classe et eleve par défaut
-      $classe1 = "";
-      $classe2 = "";
-      $eleve1 = "";
-      $eleve2 = "";        
-    } elseif ($isSubmitUserId || $isVerifResult) {
-      $prof1 = $decoded->prof1;
-      $prof2 = $decoded->prof2;
-      $prof3 = $decoded->prof3;
-      $classe1 = $decoded->classe1;
-      $classe2 = $decoded->classe2;
-      $eleve1 = $decoded->eleve1;
-      $eleve2 = $decoded->eleve2;  
-      // On ne crée l'espace que si les variables on on le format voulu
-      // => les variables prof1,prof2 et prof3 sont des majuscules
-      // => les variables classe1 et classe2 sont des caractères alphanumériques
-      // => les variables eleve1 et eleve2 sont des caractères alphanumériques
-      $prof = str_split($prof1.$prof2.$prof3);
-      $classe = str_split($classe1.$classe2);
-      $eleve = str_split($eleve1.$eleve2);
-      // Un booléen pour tester si le code prof est en majuscules
-      $isOkProf = true;
-      $isOkClasse = true;
-      $isOkEleve = true;
-      $userIdLen = strlen($decoded->userId);    
-      if ($userIdLen > 7) {
-        $errors .= "le code doit avoir 7 caractères maximum.<br>";
-      }
-      if (count($prof)!=3) {
-        $errors .= "Le code prof doit avoir 3 caractères.<br>"; 
-      };
-      if (count($classe)!=2) {
-        $errors .= "Le code classe doit avoir 2 caractères.<br>"; 
-      };
-      if (count($eleve)!=2) {
-        $errors .= "Le code eleve doit avoir 2 caractères.<br>"; 
-      };
-
-      foreach ($prof as $car) {
-        if (!(ctype_upper($car))) {
-          $isOkProf = false;
-        } 
-      }
-      if (!$isOkProf) {
-        $errors .= "Le code prof ne doit comporter que des lettres majuscules !<br>";
-      }  
-      foreach ($classe as $car) {
-        if (!(ctype_alnum($car))) {
-          $isOkClasse = false;      
-        };
-        if (ctype_alpha($car)) {
-          if (!ctype_upper($car)) {
-            $isOkClasse = false;
-          }
-        } 
-
-      }
-      if (!$isOkClasse) {
-        $errors .= "Le code classe doit être alphanumerique !<br>";
-        $errors .= "Les caractères alphabétiques du code classe doivent être des majuscules !<br>";
-      }
-      foreach ($eleve as $car) {
-        if (!(ctype_alnum($car))) {
-          $isOkEleve = false;      
-        };
-        if (ctype_alpha($car)) {
-          if (!ctype_upper($car)) {
-            $isOkEleve = false;
-          }
-        } 
-      }
-      if (!$isOkEleve) {
-        $errors .= "Le code eleve doit être alphanumerique !<br>";
-        $errors .= "Les caractères alphabétiques du code eleve doivent être des majuscules !<br>";
-      }  
+  // Il faut créer le répertoire sur le serveur s'il n'existe pas
+  // On teste une création côté serveur
+  // Si c'est la création d'un nouvel espace on génère le userId ici
+  // Sinon on le récupère via un fetch 
+  if (!$isSubmitUserId && !$isVerifResult) {
+    $prof1 = chr(rand(65,90));
+    $prof2 = chr(rand(65,90));
+    $prof3 = chr(rand(65,90));
+    // On met des chaine vide pour la classe et eleve par défaut
+    $classe1 = "";
+    $classe2 = "";
+    $eleve1 = "";
+    $eleve2 = "";        
+  } elseif ($isSubmitUserId || $isVerifResult) {
+    $prof1 = $decoded->prof1;
+    $prof2 = $decoded->prof2;
+    $prof3 = $decoded->prof3;
+    $classe1 = $decoded->classe1;
+    $classe2 = $decoded->classe2;
+    $eleve1 = $decoded->eleve1;
+    $eleve2 = $decoded->eleve2;  
+    // On ne crée l'espace que si les variables on on le format voulu
+    // => les variables prof1,prof2 et prof3 sont des majuscules
+    // => les variables classe1 et classe2 sont des caractères alphanumériques
+    // => les variables eleve1 et eleve2 sont des caractères alphanumériques
+    $prof = str_split($prof1.$prof2.$prof3);
+    $classe = str_split($classe1.$classe2);
+    $eleve = str_split($eleve1.$eleve2);
+    // Un booléen pour tester si le code prof est en majuscules
+    $isOkProf = true;
+    $isOkClasse = true;
+    $isOkEleve = true;
+    $userIdLen = strlen($decoded->userId);    
+    if ($userIdLen > 7) {
+      $errors .= "le code doit avoir 7 caractères maximum.<br>";
     }
-    
+    if (count($prof)!=3) {
+      $errors .= "Le code prof doit avoir 3 caractères.<br>"; 
+    };
+    if (count($classe)!=2) {
+      $errors .= "Le code classe doit avoir 2 caractères.<br>"; 
+    };
+    if (count($eleve)!=2) {
+      $errors .= "Le code eleve doit avoir 2 caractères.<br>"; 
+    };
 
-    if ($errors=="") {
-      $path = './resultats/'.$prof1.'/'.$prof2.'/'.$prof3;
-
-      // On génère une nouvelle clef uniquement si l'arborescence n'existe pas
-      // Sinon on récupère la clef dans le nom du fichier on verra plus tard s'il y a plusieurs fichiers
-      if (!file_exists($path)) {
-        if ($isSubmitUserId) {
-          $errors .= "Le code prof n'existe pas - scoresKey.php <br>";
-          foreach ($prof as $car) {
-            if (!(ctype_upper($car))) {
-              $errors .= "Le code prof ne doit comporter que des majuscules !<br>";
-            } 
-          }
-        } else {
-          mkdir($path, 0775, true);
-          // On génère une clef  
-          $keypass = md5(uniqid(rand(), true));
-          // On crée le sous-repertoire
-          mkdir($path.'/'.$keypass, 0775, true); 
+    foreach ($prof as $car) {
+      if (!(ctype_upper($car))) {
+        $isOkProf = false;
+      } 
+    }
+    if (!$isOkProf) {
+      $errors .= "Le code prof ne doit comporter que des lettres majuscules !<br>";
+    }  
+    foreach ($classe as $car) {
+      if (!(ctype_alnum($car))) {
+        $isOkClasse = false;      
+      };
+      if (ctype_alpha($car)) {
+        if (!ctype_upper($car)) {
+          $isOkClasse = false;
         }
-        // Il faut créer le dossier de stockage s'il n'existe pas à partir de la clef  
-        $pathToFile = $path.'/'.$keypass;
-        $url = $pathToFile;
-      } else {
-        if (sizeof(scandir($path))>2) {// S'il y a déjà un sous-dossier son nom est le keypass à recuperer pour les enregistrements      
-          $keypass = scandir($path)[2];
-        } else {
-          $keypass = md5(uniqid(rand(), true));
-          // On crée le sous-repertoire
-          mkdir($path.'/'.$keypass, 0775, true);
-        };
-        // Il faut créer le dossier de stockage s'il n'existe pas à partir de la clef  
-        $pathToFile = $path.'/'.$keypass.'/'.$classe1.$classe2;
-        mkdir($path.'/'.$keypass.'/'.$classe1.$classe2, 0775, true);
-        if ($isSubmitUserId) {
-          $url = "pas d'url à suivre pour les élèves !";
-        };
-        if ($isVerifResult) {
-          $url = "pas d'url pour les verifsResult";
-          $fileNameToSaveDatas = $pathToFile.'/semaine'.$currentWeek.'.csv';
-          // On ouvre le fichier
-          $fp = fopen($fileNameToSaveDatas, 'a+');      
-          // S'il n'existe pas on crée l'entete et on ajoute les données
-          if (strlen(file_get_contents($fileNameToSaveDatas))==0) {
-            fputs($fp, "idUser,idExo,sup,sup2,sup3,nbBonnesReponse,nbQuestions,score en pourcentage,date,heure \r\n");  
-          };
-          fputs($fp, $decoded->userId.','.$decoded->exId.','.$decoded->sup.','.$decoded->sup2.','.$decoded->sup3.','.$decoded->nbBonnesReponses.','.$decoded->nbQuestions.','.$decoded->score.'%,'.$currentDate.','.$currentTime."\r\n");  
-          fclose($fp);
-        };
-        
-      };  
+      } 
 
-      // Si un utilisateur transmet un userId, on enregistre dans un fichier mais c'est inutile de gérer ce fichier ici
-      // // On ouvre le fichier
-      // $fp = fopen($pathToFile, 'a+');      
-      // // S'il n'existe pas on crée l'entete
-      // if (strlen(file_get_contents($pathToFile))==0) {
-      //   fputs($fp, "idUser,idExo,nbBonnesReponse,nbQuestions,date,heure \r\n");  
-      // };
-      // fclose($fp);
     }
+    if (!$isOkClasse) {
+      $errors .= "Le code classe doit être alphanumerique !<br>";
+      $errors .= "Les caractères alphabétiques du code classe doivent être des majuscules !<br>";
+    }
+    foreach ($eleve as $car) {
+      if (!(ctype_alnum($car))) {
+        $isOkEleve = false;      
+      };
+      if (ctype_alpha($car)) {
+        if (!ctype_upper($car)) {
+          $isOkEleve = false;
+        }
+      } 
+    }
+    if (!$isOkEleve) {
+      $errors .= "Le code eleve doit être alphanumerique !<br>";
+      $errors .= "Les caractères alphabétiques du code eleve doivent être des majuscules !<br>";
+    }  
+  }
+  
 
-    echo json_encode(array(
-      //"url" => $path.'/'.$keypass.'scores.csv',
-      //"url" => $pathToFile,
-      "url" => $url,
-      //"userId" => $lettre1.$lettre2.$lettre3.$chiffre1.$chiffre2,
-      "userId" => $prof1.$prof2.$prof3.$classe1.$classe2.$eleve1.$eleve2,
-      "errors" => $errors
-    ));  
-  //}
+  if ($errors=="") {
+    $path = './resultats/'.$prof1.'/'.$prof2.'/'.$prof3;
+
+    // On génère une nouvelle clef uniquement si l'arborescence n'existe pas
+    // Sinon on récupère la clef dans le nom du fichier on verra plus tard s'il y a plusieurs fichiers
+    if (!file_exists($path)) {
+      if ($isSubmitUserId) {
+        $errors .= "Le code prof n'existe pas - scoresKey.php <br>";
+        foreach ($prof as $car) {
+          if (!(ctype_upper($car))) {
+            $errors .= "Le code prof ne doit comporter que des majuscules !<br>";
+          } 
+        }
+      } else {
+        mkdir($path, 0775, true);
+        // On génère une clef  
+        $keypass = md5(uniqid(rand(), true));
+        // On crée le sous-repertoire
+        mkdir($path.'/'.$keypass, 0775, true); 
+      }
+      // Il faut créer le dossier de stockage s'il n'existe pas à partir de la clef  
+      $pathToFile = $path.'/'.$keypass;
+      $url = $pathToFile;
+    } else {
+      if (sizeof(scandir($path))>2) {// S'il y a déjà un sous-dossier son nom est le keypass à recuperer pour les enregistrements      
+        $keypass = scandir($path)[2];
+      } else {
+        $keypass = md5(uniqid(rand(), true));
+        // On crée le sous-repertoire
+        mkdir($path.'/'.$keypass, 0775, true);
+      };
+      // Il faut créer le dossier de stockage s'il n'existe pas à partir de la clef  
+      $pathToFile = $path.'/'.$keypass.'/'.$classe1.$classe2;
+      mkdir($path.'/'.$keypass.'/'.$classe1.$classe2, 0775, true);
+      if ($isSubmitUserId) {
+        $url = "pas d'url à suivre pour les élèves !";
+      };
+      if ($isVerifResult) {
+        $url = "pas d'url pour les verifsResult";
+        $fileNameToSaveDatas = $pathToFile.'/semaine'.$currentWeek.'.csv';
+        // On ouvre le fichier
+        $fp = fopen($fileNameToSaveDatas, 'a+');      
+        // S'il n'existe pas on crée l'entete et on ajoute les données
+        if (strlen(file_get_contents($fileNameToSaveDatas))==0) {
+          fputs($fp, "idUser,idExo,sup,sup2,sup3,nbBonnesReponse,nbQuestions,score en pourcentage,date,heure \r\n");  
+        };
+        fputs($fp, $decoded->userId.','.$decoded->exId.','.$decoded->sup.','.$decoded->sup2.','.$decoded->sup3.','.$decoded->nbBonnesReponses.','.$decoded->nbQuestions.','.$decoded->score.'%,'.$currentDate.','.$currentTime."\r\n");  
+        fclose($fp);
+      };
+      
+    };  
+  }
+
+  echo json_encode(array(
+    //"url" => $path.'/'.$keypass.'scores.csv',
+    //"url" => $pathToFile,
+    "url" => $url,
+    //"userId" => $lettre1.$lettre2.$lettre3.$chiffre1.$chiffre2,
+    "userId" => $prof1.$prof2.$prof3.$classe1.$classe2.$eleve1.$eleve2,
+    "errors" => $errors
+  ));  
 
   // Si json_decode échoue, le JSON est invalide.
   if(! is_array($decoded)) {
