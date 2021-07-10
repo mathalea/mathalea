6a65fcdd9aa0fd134441ae677bd2d4a5a85f3934
Sébastien LOZANO <sebastien.lozano@ac-nancy-metz.fr>
2021-07-10 22:35:04 +0200

    scores-test de CRON via script PHP et fetch API

---
+++ src/php/scoresCleanSpaces.php
@@ -0,0 +1,40 @@
+<?php
+$msg = "Suppression KO !";
+// // Procédure de suppression des fichiers avec exploration recursive
+function recursiveDelete($pathToClean,$timeBeforeDelete) {
+    $repertoire = opendir($pathToClean); // On définit le répertoire dans lequel on souhaite travailler.
+    
+    while (false !== ($fichier = readdir($repertoire))) // On lit chaque fichier du répertoire dans la boucle.
+    {
+    $chemin = $pathToClean."/".$fichier; // On définit le chemin du fichier à effacer.
+    
+    // Si le fichier n'est pas un répertoire…
+    if ($fichier != ".." && $fichier != "." && !is_dir($fichier))
+          {
+          $Diff = (time() - filectime($chemin));
+          if ($Diff > $timeBeforeDelete) unlink($chemin); // On efface si c'est trop vieux depuis la dernière modification        
+          }
+    elseif (is_dir($fichier)) 
+          {
+            recursiveDelete($fichier,$timeBeforeDelete);
+          }
+    }
+    closedir($repertoire);
+  }
+  
+    // // On supprime tout ce qui a plus de 1 minute
+    $thisdir = "./resultats";
+    if (!is_dir($thisdir)) {
+        mkdir($thisdir);
+        mkdir('./test');
+    }
+    
+    if (rmdir($thisdir)) {
+        $msg = "Suppression OK !";
+    };
+
+echo json_encode(array(
+    "msg" => $msg,
+  ));  
+
+?>
