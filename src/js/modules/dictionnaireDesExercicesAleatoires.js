a47f3b1a477f5cf1291991a1bffeb5aabd83c82d
Laurence Candille <87067866+LaurCan@users.noreply.github.com>
2021-07-13 12:43:56 +0200

    Rajout plusieurs questions dans 1 fichier

--- src/js/modules/dictionnaireDesExercicesAleatoires.js
+++ src/js/modules/dictionnaireDesExercicesAleatoires.js
@@ -2289,13 +2289,6 @@ export default {
     "interactifReady": false,
     "name": "6C35"
   },
-  "6C36": {
-    "titre": "Problème - de plus de moins",
-    "url": "/exercices/6e/6C36.js",
-    "amcReady": false,
-    "interactifReady": false,
-    "name": "6C36"
-  },
   "6C3603-niv1": {
     "titre": "Problème - de plus de moins",
     "url": "/exercices/6e/6C3603-niv1.js",
@@ -3242,6 +3235,27 @@ export default {
     "amcReady": false,
     "interactifReady": false,
     "name": "beta6C33-1"
+  },
+  "beta6C36-V2": {
+    "titre": "Problème - de plus de moins",
+    "url": "/exercices/beta/beta6C36-V2.js",
+    "amcReady": false,
+    "interactifReady": false,
+    "name": "beta6C36-V2"
+  },
+  "beta6C36-V3": {
+    "titre": "Addition toute simple",
+    "url": "/exercices/beta/beta6C36-V3.js",
+    "amcReady": false,
+    "interactifReady": false,
+    "name": "beta6C36-V3"
+  },
+  "beta6C36": {
+    "titre": "Problème - de plus de moins",
+    "url": "/exercices/beta/beta6C36.js",
+    "amcReady": false,
+    "interactifReady": false,
+    "name": "beta6C36"
   },
   "beta6G21": {
     "titre": "Construire un triangle aux instruments",
