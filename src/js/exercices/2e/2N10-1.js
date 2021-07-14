7d138a9107b2772a146627c8eb95e8c1fb45b79a
Stéphane <mathsguyon@gmail.com>
2021-07-14 13:00:36 +0200

    correction bug détecté par Gilles

--- src/js/exercices/2e/2N10-1.js
+++ src/js/exercices/2e/2N10-1.js
@@ -1,7 +1,6 @@
 import Exercice from '../Exercice.js'
 import { context } from '../../modules/context.js'
-import {listeQuestionsToContenu,randint,choice,combinaisonListes,ecritureAlgebrique,ecritureParentheseSiNegatif} from '../../modules/outils.js'
-
+import { listeQuestionsToContenu, randint, choice, combinaisonListes, ecritureAlgebrique, ecritureParentheseSiNegatif } from '../../modules/outils.js'
 
 export const titre = 'Connaître les propriétés calculatoires des racines carrées'
 
@@ -9,133 +8,120 @@ export const titre = 'Connaître les propriétés calculatoires des racines carr
  * 2N10-1
  * @author Stéphane Guyon
  */
-export default function proprietes_racine_carree() {
+export default function proprietes_racine_carree () {
+  Exercice.call(this) // Héritage de la classe Exercice()
+  this.titre = titre
+  this.consigne = 'Effectuer, si possible, les calculs suivants :'
+  this.nbQuestions = 5
+  this.nbCols = 2
+  this.nbColsCorr = 2
+  this.sup = 1 //
+  this.nouvelleVersion = function () {
+    this.listeQuestions = [] // Liste de questions
+    this.listeCorrections = [] // Liste de questions corrigées
+    const typesDeQuestionsDisponibles = [1, 2, 3, 4, 5, 6, 7]; let typesDeQuestions
+    const listeTypeDeQuestions = combinaisonListes(typesDeQuestionsDisponibles, this.nbQuestions)
+    for (let i = 0, a, b, c, d, e, texte, texteCorr, cpt = 0; i < this.nbQuestions && cpt < 50;) {
+      typesDeQuestions = listeTypeDeQuestions[i]
+      switch (typesDeQuestions) {
+        // Cas par cas, on définit le type de nombres que l'on souhaite
+        // Combien de chiffres ? Quelles valeurs ?
+        case 1:// calcul de (a sqrt b)²
 
-    Exercice.call(this); // Héritage de la classe Exercice()
-    this.titre = titre;
-    this.consigne = "Effectuer, si possible, les calculs suivants :";
-    this.nbQuestions = 5;
-    this.nbCols = 2;
-    this.nbColsCorr = 2;
-    this.sup = 1; //
-    this.nouvelleVersion = function () {
-        this.listeQuestions = []; // Liste de questions
-        this.listeCorrections = []; // Liste de questions corrigées
-        let typesDeQuestionsDisponibles = [1, 2, 3, 4, 5, 6, 7],typesDeQuestions
-        let listeTypeDeQuestions = combinaisonListes(typesDeQuestionsDisponibles, this.nbQuestions);
-        for (let i = 0, a, b, c, d, e, texte, texteCorr, cpt = 0; i < this.nbQuestions && cpt < 50;) {
-            typesDeQuestions = listeTypeDeQuestions[i];
-            switch (typesDeQuestions) {
-                // Cas par cas, on définit le type de nombres que l'on souhaite
-                // Combien de chiffres ? Quelles valeurs ?
-                case 1:
-
-                    a = randint(2, 9) * choice([-1, 1]);
-                    b = randint(2, 11, [4, 8, 9]);
-                    c = a * a * b;
-                    d = a * a;
-                    texte = `$\\left(${a} \\sqrt{${b}}\\right)^{2}$`;
-                    texteCorr = `$\\left(${a} \\sqrt{${b}}\\right)^{2}=${a}^{2}\\times \\left(\\sqrt{${b}}\\right)^{2}$
+          a = randint(2, 9) * choice([-1, 1])
+          b = randint(2, 11, [4, 8, 9])
+          c = a * a * b
+          d = a * a
+          texte = `$\\left(${a} \\sqrt{${b}}\\right)^{2}$`
+          texteCorr = `$\\left(${a} \\sqrt{${b}}\\right)^{2}=${a}^{2}\\times \\left(\\sqrt{${b}}\\right)^{2}$
                         $\\phantom{\\left(${a} \\sqrt{${b}}\\right)^{2}}$<br>
                         $\\phantom{\\left(${a} \\sqrt{${b}}\\right)^{2}}=${d}\\times ${b}$<br>
-                        $\\phantom{\\left(${a} \\sqrt{${b}}\\right)^{2}}=${c}$`;
+                        $\\phantom{\\left(${a} \\sqrt{${b}}\\right)^{2}}=${c}$`
 
-                    break;
-                case 2:
+          break
+        case 2:// calcul de (a sqrt b)*c sqrt b
 
-                    a = randint(2, 9) * choice([-1, 1]);
-                    c = randint(2, 9) * choice([-1, 1]);
-                    d = randint(2, 9) * choice([-1, 1]);
-                    b = randint(2, 11, [4, 8, 9]);
-                    e = c * d;
+          a = randint(2, 9) * choice([-1, 1])
+          c = randint(2, 9) * choice([-1, 1])
+          d = randint(2, 9) * choice([-1, 1])
+          b = randint(2, 11, [4, 8, 9])
+          e = c * d
 
-
-                    texte = `$ ${c} \\sqrt{${b}}\\times ${ecritureParentheseSiNegatif(d)} \\sqrt{${b}}$`;
-                    texteCorr = `$ ${c} \\sqrt{${b}}\\times ${ecritureParentheseSiNegatif(d)} \\sqrt{${b}}=${c}\\times ${ecritureParentheseSiNegatif(d)} \\sqrt{${b}} \\times \\sqrt{${b}}$<br>
+          texte = `$ ${c} \\sqrt{${b}}\\times ${ecritureParentheseSiNegatif(d)} \\sqrt{${b}}$`
+          texteCorr = `$ ${c} \\sqrt{${b}}\\times ${ecritureParentheseSiNegatif(d)} \\sqrt{${b}}=${c}\\times ${ecritureParentheseSiNegatif(d)} \\sqrt{${b}} \\times \\sqrt{${b}}$<br>
                         $\\phantom{${c} \\sqrt{${b}}\\times ${ecritureParentheseSiNegatif(d)} \\sqrt{${b}}}=${e}\\times ${b}$<br>
-                        $\\phantom{${c} \\sqrt{${b}}\\times ${ecritureParentheseSiNegatif(d)} \\sqrt{${b}}}=${e * b}$`;
+                        $\\phantom{${c} \\sqrt{${b}}\\times ${ecritureParentheseSiNegatif(d)} \\sqrt{${b}}}=${e * b}$`
 
-                    break;
-                case 3:
+          break
+        case 3://  calcul de (a sqrt b)*(c + d sqrt b)
+          a = randint(2, 9) * choice([-1, 1])
+          c = randint(2, 9) * choice([-1, 1])
+          d = randint(2, 9) * choice([-1, 1])
+          b = randint(2, 11, [4, 8, 9])
+          e = c * d
 
-                    a = randint(2, 9) * choice([-1, 1]);
-                    c = randint(2, 9) * choice([-1, 1]);
-                    d = randint(2, 9) * choice([-1, 1]);
-                    b = randint(2, 11, [4, 8, 9]);
-                    e = c * d;
-
-
-                    texte = `$ ${a} \\sqrt{${b}}\\left( ${c}  ${ecritureAlgebrique(d)}\\sqrt{${b}}\\right)$`;
-                    texteCorr = `$${a} \\sqrt{${b}}\\left( ${c}  ${ecritureAlgebrique(d)}\\sqrt{${b}}\\right)=
+          texte = `$ ${a} \\sqrt{${b}}\\left( ${c}  ${ecritureAlgebrique(d)}\\sqrt{${b}}\\right)$`
+          texteCorr = `$${a} \\sqrt{${b}}\\left( ${c}  ${ecritureAlgebrique(d)}\\sqrt{${b}}\\right)=
                         ${a} \\sqrt{${b}}\\times ${ecritureParentheseSiNegatif(c)}${ecritureAlgebrique(a)} \\sqrt{${b}}\\times ${ecritureParentheseSiNegatif(d)}\\sqrt{${b}}$<br>
-                        $\\phantom{${a} \\sqrt{${b}}\\left( ${c}  ${ecritureAlgebrique(d)}\\sqrt{${b}}\\right)}=${a * c}\\sqrt{${b}}${ecritureAlgebrique(a)}\\times ${ecritureParentheseSiNegatif(d)}*${b}$<br>
-                        $\\phantom{${a} \\sqrt{${b}}\\left( ${c}  ${ecritureAlgebrique(d)}\\sqrt{${b}}\\right)}=${a * c}\\sqrt{${b}}${ecritureAlgebrique(a * d * b)}$`;
+                        $\\phantom{${a} \\sqrt{${b}}\\left( ${c}  ${ecritureAlgebrique(d)}\\sqrt{${b}}\\right)}=${a * c}\\sqrt{${b}}${ecritureAlgebrique(a)}\\times ${ecritureParentheseSiNegatif(d)}\\times ${b}$<br>
+                        $\\phantom{${a} \\sqrt{${b}}\\left( ${c}  ${ecritureAlgebrique(d)}\\sqrt{${b}}\\right)}=${a * c}\\sqrt{${b}}${ecritureAlgebrique(a * d * b)}$`
 
-                    break;
-                case 4:
+          break
+        case 4://  calcul de sqrt b + sqrt c
 
-                    a = randint(2, 9);
+          a = randint(2, 9)
 
-                    d = randint(2, 9);
-                    b = randint(2, 11, [4, 8, 9]);
-                    c = randint(2, 19, [4, 8, 9, 12, 16, 18, b]);
-                    e = c * d;
+          d = randint(2, 9)
+          b = randint(2, 11, [4, 8, 9])
+          c = randint(2, 19, [4, 8, 9, 12, 16, 18, b])
+          e = c * d
 
+          texte = `$  \\sqrt{${b}}+\\sqrt{${c}}$`
+          texteCorr = `$  \\sqrt{${b}}+\\sqrt{${c}}$ n'est pas simplifiable`
 
-                    texte = `$  \\sqrt{${b}}+\\sqrt{${c}}$`;
-                    texteCorr = `$  \\sqrt{${b}}+\\sqrt{${c}}$ n'est pas simplifiable`;
+          break
+        case 5://  calcul de  sqrt b² + sqrt c²
 
-                    break;
-                case 5:
+          b = randint(2, 11)
+          c = randint(2, 11, [b])
+          e = c * d
 
+          texte = `$  \\sqrt{${b * b}}+\\sqrt{${c * c}}$`
+          texteCorr = `$  \\sqrt{${b * b}}+\\sqrt{${c * c}}=${b}+${c}=${b + c}$ `
 
-                    b = randint(2, 11);
-                    c = randint(2, 11, [b]);
-                    e = c * d;
+          break
+        case 6://  calcul de  sqrt (b²c/c)
 
+          b = randint(2, 11)
+          c = randint(2, 7, [b])
+          d = b * b * c
 
-                    texte = `$  \\sqrt{${b * b}}+\\sqrt{${c * c}}$`;
-                    texteCorr = `$  \\sqrt{${b * b}}+\\sqrt{${c * c}}=${b}+${c}=${b + c}$ `;
-
-                    break;
-                case 6:
-
-
-                    b = randint(2, 11);
-                    c = randint(2, 7, [b]);
-                    d = b * b * c;
-
-
-                    texte = `$ \\sqrt{\\dfrac{${d}}{${c}}}$`;
-                    texteCorr = `$ \\sqrt{\\dfrac{${d}}{${c}}}= \\sqrt{\\dfrac{${b}^{2}\\times${c}}{${c}}}$<br>
+          texte = `$ \\sqrt{\\dfrac{${d}}{${c}}}$`
+          texteCorr = `$ \\sqrt{\\dfrac{${d}}{${c}}}= \\sqrt{\\dfrac{${b}^{2}\\times${c}}{${c}}}$<br>
                         $\\phantom{\\sqrt{\\dfrac{${d}}{${c}}}}=\\sqrt{${b}^{2}}$<br>
-                        $\\phantom{\\sqrt{\\dfrac{${d}}{${c}}}}=${b}$ `;
+                        $\\phantom{\\sqrt{\\dfrac{${d}}{${c}}}}=${b}$ `
 
-                    break;
-                case 7:
+          break
+        case 7://  calcul de sqrt b * sqrt c
 
+          b = randint(2, 11, [4, 9])
+          c = randint(2, 7, [b])
+          d = b * c
 
-                    b = randint(2, 11, [4, 9]);
-                    c = randint(2, 7, [b]);
-                    d = b * c;
-
-
-                    texte = `$ \\sqrt{${d}}\\times \\sqrt{${c}}$`;
-                    texteCorr = `$ \\sqrt{${d}}\\times \\sqrt{${c}}=\\sqrt{${d}\\times${c}}$<br>
+          texte = `$ \\sqrt{${d}}\\times \\sqrt{${c}}$`
+          texteCorr = `$ \\sqrt{${d}}\\times \\sqrt{${c}}=\\sqrt{${d}\\times${c}}$<br>
                         $\\phantom{\\sqrt{${d}}\\times \\sqrt{${c}}}=\\sqrt{${b}\\times${c}\\times${c}}$<br>
-                        $\\phantom{\\sqrt{${d}}\\times \\sqrt{${c}}}=${c}\\sqrt{${b}}$ `;
+                        $\\phantom{\\sqrt{${d}}\\times \\sqrt{${c}}}=${c}\\sqrt{${b}}$ `
 
-                    break;
-
-            }
-            if (this.listeQuestions.indexOf(texte) === -1) { // Si la question n'a jamais été posée, on en créé une autre
-                this.listeQuestions.push(texte);
-                this.listeCorrections.push(texteCorr);
-                i++;
-            }
-            cpt++;
-        }
-        listeQuestionsToContenu(this);
-    };
-
+          break
+      }
+      if (this.listeQuestions.indexOf(texte) === -1) { // Si la question n'a jamais été posée, on en créé une autre
+        this.listeQuestions.push(texte)
+        this.listeCorrections.push(texteCorr)
+        i++
+      }
+      cpt++
+    }
+    listeQuestionsToContenu(this)
+  }
 }
