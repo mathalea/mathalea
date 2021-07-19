70675e6c079000d2245196ea21a74998af8d5f1f
Laurence Candille <87067866+LaurCan@users.noreply.github.com>
2021-07-19 13:48:55 +0200

    dernière version

--- src/js/exercices/beta/beta6C38.js
+++ src/js/exercices/beta/beta6C38.js
@@ -28,7 +28,7 @@ export default function AugmenterEtReduireDunPourcentage () {
     this.listeCorrections = [] // Liste de questions corrigées
     this.introduction = lampeMessage({
       titre: 'Calculatrice autorisée.',
-      texte: 'Résoudre les problèmes suivants au brouillon et écrire les réponses dans les cases, ne pas préciser "€" ni "euros" ...',
+      texte: 'Ecrire les réponses dans les cases sans arrondir, ne pas préciser "€" ni "euros" ...',
       couleur: 'nombres'
     })
     const typeQuestionsDisponibles = ['augmentation', 'réduction'] // On créé 2 types de questions
@@ -39,13 +39,7 @@ export default function AugmenterEtReduireDunPourcentage () {
     let mr, ma // montant réduction, montant augmentation
     let final1, final2 // prix final 1 , prix final 2
     let prenom1, prenom2 // choix aleatoire des prenoms
-
-    for (let i = 0, texte, texteCorr, cpt = 0; i < this.nbQuestions && cpt < 50;) {
-      // Boucle principale où i+1 correspond au numéro de la question
-      prenom1 = prenomM()
-      prenom2 = prenomF()
-      billet = randint(100, 200)
-      loyer = randint(501, 899, [600, 700, 800])
+    function nombreDecimales (n) {
       if (n === 0) {
         pr = randint(1, 6) * 10
         pa = randint(1, 3) * 10
@@ -58,16 +52,26 @@ export default function AugmenterEtReduireDunPourcentage () {
         pr = calcul((randint(40, 60) * 100 + randint(1, 9) * 10) / 100)
         pa = calcul((randint(1, 9) * 10 + randint(1, 9)) / 10)
       }
+    }
+
+    for (let i = 0, texte, texteCorr, cpt = 0; i < this.nbQuestions && cpt < 50;) {
+      // Boucle principale où i+1 correspond au numéro de la question
+      prenom1 = prenomM()
+      prenom2 = prenomF()
+      billet = randint(100, 200)
+      loyer = randint(501, 899, [600, 700, 800])
+
       switch (listeTypeQuestions[i]) { // Suivant le type de question, le contenu sera différent
         case 'réduction':
+          nombreDecimales(n)
           mr = calcul(pr * billet / 100)
           final1 = calcul(billet - mr)
           texte = `<br> Un billet d'avion coûte ${billet}€. ${prenom1} bénéficie d'une réduction de ${pr} %."<br>`
           texte += 'a) Le montant de la réduction est :'
           texte += ajouteChampTexteMathLive(this, i) + '  €<br><br>'
-          setReponse(this, i, mr)
+          setReponse(this, i, [mr, mr * 10], { formatInteractif: 'calcul' })
           texte += `b) Finalement, ${prenom1} paiera son billet :`
-          texte += ajouteChampTexteMathLive(this, i + this.nbQuestions) + '  €<br>'
+          texte += ajouteChampTexteMathLive(this, i + this.nbQuestions + 1) + '  €<br>'
           texteCorr = `<br>a) Le montant de la réduction est :     $${billet}\\times ${pr} : 100 = $`
           texteCorr += texteEnCouleurEtGras(`$${texPrix(mr)}€$<br>`)
           texteCorr += `b) Finalement, ${prenom1} paiera son billet : $${billet} - ${mr} = $`
@@ -75,15 +79,16 @@ export default function AugmenterEtReduireDunPourcentage () {
           setReponse(this, i + this.nbQuestions + 1, final1)
           break
         case 'augmentation':
+          nombreDecimales(n)
           calcul(ma = pa * loyer / 100)
           calcul(final2 = loyer + ma)
 
           texte = `<br> Le loyer de l'appartement de ${prenom2} coûte ${loyer}€. Au 1er janvier, il augmente de ${pa} %."<br>`
-          texte += 'a) Le montant de l augmentation est :'
+          texte += 'a) Le montant de l\'augmentation est :'
           texte += ajouteChampTexteMathLive(this, i) + '  €<br><br>'
           texte += `b) Finalement, ${prenom2} paiera son loyer :`
           setReponse(this, i, ma)
-          texte += ajouteChampTexteMathLive(this, i + this.nbQuestions) + '  €'
+          texte += ajouteChampTexteMathLive(this, i + this.nbQuestions + 1) + '  €'
           setReponse(this, i + this.nbQuestions + 1, final2)
           texteCorr = `<br>a) Le montant de l'augmentation est :     $${loyer}\\times ${pa} : 100 = $`
           texteCorr += texteEnCouleurEtGras(`$${texPrix(ma)}€$<br>`)
