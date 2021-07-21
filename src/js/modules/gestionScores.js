e0f8429fb62358ad81a571c83566394b6a04f58a
Sébastien LOZANO <sebastien.lozano@ac-nancy-metz.fr>
2021-07-21 22:43:54 +0200

    :ambulance: :alien: Scores - La reponse des script php est du json.

--- src/js/modules/gestionScores.js
+++ src/js/modules/gestionScores.js
@@ -33,7 +33,7 @@ export default function gestionScores () {
       'Content-Type': 'application/json'
     }
   })
-    .then(response => response.text())
+    .then(response => response.json())// on a besoin de récupérer la réponse du serveur avant de l'utiliser
     .then(response => {
       if (document.getElementById('scoresInfosTimeLeft')) {
         // console.log(response.timeLeft)
@@ -227,7 +227,7 @@ export default function gestionScores () {
           eleve2: userId[6]
         })
       })
-        .then(response => response.text())// on a besoin de récupérer la réponse du serveur avant de l'utiliser
+        .then(response => response.json())// on a besoin de récupérer la réponse du serveur avant de l'utiliser
         .then(response => {
           if (response.errors !== '') { // S'il y a des erreurs on ne fait rien
             if (document.getElementById('scoresInputUserIdError')) {
@@ -325,7 +325,7 @@ export default function gestionScores () {
           isVerifResult: false
         })
       })
-        .then(response => response.text())// on a besoin de récupérer la réponse du serveur avant de l'utiliser
+        .then(response => response.json())// on a besoin de récupérer la réponse du serveur avant de l'utiliser
         .then(response => {
           if (document.getElementById('scoresFeedback')) {
             document.getElementById('scoresFeedbackHeader').innerHTML = 'Espace scores - Création validée'
