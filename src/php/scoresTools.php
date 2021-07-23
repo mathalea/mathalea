19ee650da40d86bc69adbee92301400c48b1f919
Sébastien LOZANO <sebastien.lozano@ac-nancy-metz.fr>
2021-07-23 07:41:21 +0200

    scores-l'index.php des espaces de scores liste les fichiers et les classes

--- src/php/scoresTools.php
+++ src/php/scoresTools.php
@@ -19,6 +19,28 @@ function createIndexScores($path,$codeProf) {
   // On ouvre le fichier
   $fp = fopen($indexProfSpace, 'a+');
   // On écrit dedans un template de base à modifier plus tard
+  $string = '<?php 
+  $classes = array();
+  $dir_iterator = new RecursiveDirectoryIterator(dirname(__FILE__));
+  $iterator = new RecursiveIteratorIterator($dir_iterator);
+  foreach ($iterator as $file){
+      if(!is_dir($file) && !in_array($file->getFilename(), array(".","..","index.php")) && !in_array(substr($file->getPath(),-2),$classes)) {    
+          array_push($classes,substr($file->getPath(),-2));
+      }
+  }
+
+  foreach ($classes as $classe) {
+  echo "Classe de ".$classe." : <br>\r\n";
+  echo "<ul>\r\n";
+  foreach ($iterator as $file) {
+      if (substr($file->getPath(),-2) == $classe && !in_array($file->getFilename(), array(".","..")) ) {
+         echo "<li><a href=\"".substr($file->getPath(),-2)."/".$file->getFilename()."\">".$file->getFilename()."</a></li>\r\n";
+      };
+  }
+  echo "</ul>\r\n";    
+  }
+
+?>';
   fputs($fp,"
   <!DOCTYPE html>
   <html>
@@ -30,12 +52,11 @@ function createIndexScores($path,$codeProf) {
       <body>
           <h1>Espace des scores <b>".$codeProf[0].$codeProf[1].$codeProf[2]."</b></h1>
           <h2>Liste des fichiers par classe et par semaine</h2>                
-          <?php
-              echo 'Hello World !';
-          ?>              
+            $string      
       </body>
   </html>
   ");
   fclose($fp); 
 };
+
 ?>
