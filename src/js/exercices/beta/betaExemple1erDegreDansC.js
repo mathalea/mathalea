51ac5297b185a584ae48c793e23424bf70623172
CireCunis <schraf@aliceadsl.fr>
2021-07-29 13:46:33 +0200

    Enlevement de XCas
    Pour un chargement plus rapide

--- src/js/exercices/beta/betaExemple1erDegreDansC.js
+++ src/js/exercices/beta/betaExemple1erDegreDansC.js
@@ -19,7 +19,6 @@ export default function NomQuelconqueDeLaFonctionQuiCreeExercice () {
   this.sup = 1 // Niveau de difficulté
   this.tailleDiaporama = 100 // Pour les exercices chronométrés. 50 par défaut pour les exercices avec du texte
   this.video = '' // Id YouTube ou url
-  this.typeExercice = 'XCas'
   this.spacingCorr = 2
   this.nouvelleVersion = function () {
     this.listeQuestions = [] // Liste de questions
