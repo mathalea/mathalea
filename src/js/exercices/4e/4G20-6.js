70e9e533a4b30bc1594720d9b07f4d4739afabd1
J-C Lhote <lhotejc@gmail.com>
2021-08-08 15:02:32 +0200

    4G20-6 interactif + AMC

--- src/js/exercices/4e/4G20-6.js
+++ src/js/exercices/4e/4G20-6.js
@@ -1,6 +1,12 @@
 import Exercice from '../Exercice.js'
-import { listeQuestionsToContenu, combinaisonListes, arrondiVirgule, randint } from '../../modules/outils.js'
+import { listeQuestionsToContenu, combinaisonListes, arrondiVirgule, randint, arrondi, nombreDeChiffresDe, nombreDeChiffresDansLaPartieDecimale, nombreDeChiffresDansLaPartieEntiere } from '../../modules/outils.js'
+import { ajouteChampTexteMathLive, setReponse } from '../../modules/gestionInteractif.js'
+import { context } from '../../modules/context.js'
 export const titre = 'Encadrer une racine carrée et en donner un arrondi'
+export const interactifReady = true // pour définir qu'exercice peut s'afficher en mode interactif.
+export const interactifType = 'mathLive'
+export const amcReady = true // pour définir que l'exercice peut servir à AMC
+export const amcType = 'AMCHybride'
 
 /**
  * Encadrer une racine carrée et en donner une valeur approchée
@@ -20,6 +26,7 @@ export default function CalculValeurApprocheeRacineCarree () {
   this.sup = 3
 
   this.nouvelleVersion = function (numeroExercice) {
+    this.autoCorrection = []
     this.listeQuestions = []
     this.listeCorrections = []
     this.sup = parseInt(this.sup)
@@ -35,7 +42,7 @@ export default function CalculValeurApprocheeRacineCarree () {
     const typeQuestionsDisponibles = ['unite', 'dixieme', 'centieme']
 
     const listeTypeQuestions = combinaisonListes(typeQuestionsDisponibles, this.nbQuestions)
-    for (let i = 0, texte, texteCorr, type, a, reponse, pasReponse, reponseG, reponseD, cpt = 0; i < this.nbQuestions && cpt < 50;) {
+    for (let i = 0, texte, texteCorr, indexRep = 0, type, a, nbDec, reponse, pasReponse, reponseG, reponseD, cpt = 0; i < this.nbQuestions && cpt < 50;) {
       if (listeAvecOuSansCalculatrice[i] === 'avec') {
         a = randint(2, 300, [4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169, 196, 225, 256, 289])
         switch (listeTypeQuestions[i]) {
@@ -44,40 +51,144 @@ export default function CalculValeurApprocheeRacineCarree () {
             reponse = arrondiVirgule(Math.sqrt(a), 0)
             reponseG = arrondiVirgule(Math.floor(Math.sqrt(a)), 0)
             reponseD = arrondiVirgule(Math.ceil(Math.sqrt(a)), 0)
+            setReponse(this, indexRep, arrondi(Math.sqrt(a), 0))
+            nbDec = 0
             break
           case 'dixieme':
             type = 'au dixième'
             reponse = arrondiVirgule(Math.sqrt(a), 1)
             reponseG = arrondiVirgule(Math.floor(Math.sqrt(a) * 10) / 10, 1)
             reponseD = arrondiVirgule(Math.ceil(Math.sqrt(a) * 10) / 10, 1)
+            setReponse(this, indexRep, arrondi(Math.sqrt(a), 1))
+            nbDec = 1
             break
           case 'centieme':
             type = 'au centième'
             reponse = arrondiVirgule(Math.sqrt(a), 2)
             reponseG = arrondiVirgule(Math.floor(Math.sqrt(a) * 100) / 100, 2)
             reponseD = arrondiVirgule(Math.ceil(Math.sqrt(a) * 100) / 100, 2)
+            setReponse(this, indexRep, arrondi(Math.sqrt(a), 2))
+            nbDec = 2
             break
         }
         if (reponse === reponseG) pasReponse = reponseD
         else pasReponse = reponseG
-        texte = `Encadrer $\\sqrt{${a}}$ ${type} près et en donner un arrondi ${type} près.`
+        if (!this.interactif) {
+          texte = `Encadrer $\\sqrt{${a}}$ ${type} près et en donner un arrondi ${type} près.`
+        } else {
+          texte = `Donner la valeur arrondie de $\\sqrt{${a}}$ ${type} près : `
+          texte += ajouteChampTexteMathLive(this, indexRep, 'largeur10 inline')
+        }
         texteCorr = `$\\sqrt{${a}} \\simeq ${arrondiVirgule(Math.sqrt(a), 6)}$.<br>`
         texteCorr += `Or $${reponseG} < ${arrondiVirgule(Math.sqrt(a), 6)} < ${reponseD}$,<br>`
         texteCorr += `et $${arrondiVirgule(Math.sqrt(a), 6)}$ est plus proche de $${reponse}$ que de $${pasReponse}$.<br>`
         texteCorr += `Donc l'arrondi ${type} près de $\\sqrt{${a}}$ est $${reponse}$.`
       } else if (listeAvecOuSansCalculatrice[i] === 'sans') {
         a = randint(2, 300, [4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169, 196, 225, 256, 289])
-        texte = `Sans utiliser de calculatrice, encadrer $\\sqrt{${a}}$ entre deux nombres entiers.`
         reponseG = Math.floor(Math.sqrt(a))
         reponseD = Math.ceil(Math.sqrt(a))
+        texte = `Sans utiliser de calculatrice, encadrer $\\sqrt{${a}}$ entre deux nombres entiers.<br>`
+        if (this.interactif) {
+          texte += ajouteChampTexteMathLive(this, indexRep, 'largeur10 inline')
+          texte += ` $< \\sqrt{${a}} <$ `
+          texte += ajouteChampTexteMathLive(this, indexRep + 1, 'largeur10 inline')
+          setReponse(this, indexRep, reponseG)
+          setReponse(this, indexRep + 1, reponseD)
+        }
         texteCorr = `$${reponseG}^2 = ${reponseG ** 2}$ et $${reponseD}^2 = ${reponseD ** 2}$.<br>`
         texteCorr += `Or $${reponseG ** 2} < ${a} < ${reponseD ** 2}$,<br>`
         texteCorr += `donc $\\sqrt{${reponseG ** 2}} < \\sqrt{${a}} < \\sqrt{${reponseD ** 2}}$,<br>`
         texteCorr += `enfin $${reponseG} < \\sqrt{${a}} < ${reponseD}$.`
       }
       if (this.listeQuestions.indexOf(texte) === -1) {
+        if (context.isAmc) {
+          if (listeAvecOuSansCalculatrice[i] === 'avec') {
+            this.autoCorrection[i] = {
+              enonce: texte,
+              propositions: [
+                {
+                  type: 'AMCOpen',
+                  propositions: [{
+                    texte: texteCorr,
+                    statut: 3
+                  }]
+                },
+                {
+                  type: 'AMCNum',
+                  propositions: [{
+                    texte: '',
+                    statut: '',
+                    reponse: {
+                      texte: `arrondi de $\\sqrt (${a})$ ${type} près : ${reponse}.`,
+                      valeur: parseFloat(reponse.replaceAll(',', '.')),
+                      param: {
+                        digits: nombreDeChiffresDansLaPartieEntiere(parseFloat(reponse.replaceAll(',', '.'))),
+                        decimals: nbDec,
+                        signe: false,
+                        approx: 0
+                      }
+                    }
+                  }]
+                }
+              ]
+            }
+          } else {
+            this.autoCorrection[i] = {
+              enonce: texte,
+              propositions: [
+                {
+                  type: 'AMCOpen',
+                  propositions: [{
+                    texte: texteCorr,
+                    statut: 3
+                  }]
+                },
+                {
+                  type: 'AMCNum',
+                  propositions: [{
+                    texte: '',
+                    statut: '',
+                    reponse: {
+                      texte: '',
+                      valeur: reponseG,
+                      param: {
+                        digits: nombreDeChiffresDansLaPartieEntiere(reponseG),
+                        decimals: 0,
+                        signe: false,
+                        approx: 0
+                      }
+                    }
+                  }]
+                },
+                {
+                  type: 'AMCNum',
+                  propositions: [{
+                    texte: '',
+                    statut: '',
+                    reponse: {
+                      texte: '',
+                      valeur: reponseD,
+                      param: {
+                        digits: nombreDeChiffresDansLaPartieEntiere(reponseD),
+                        decimals: 0,
+                        signe: false,
+                        approx: 0
+                      }
+                    }
+                  }]
+                }
+              ]
+            }
+          }
+        }
         this.listeQuestions.push(texte)
         this.listeCorrections.push(texteCorr)
+        if (listeAvecOuSansCalculatrice[i] === 'avec') {
+          indexRep++
+        } else {
+          indexRep += 2
+        }
+
         i++
       }
       cpt++
