f0fde58fac4e4a5ba44c5df14a035ebb4d1f14e9
J-C Lhote <lhotejc@gmail.com>
2021-08-10 16:02:06 +0200

    Modification des fréquences de transfo selon les niveaux

--- src/js/exercices/6e/_Transformations.js
+++ src/js/exercices/6e/_Transformations.js
@@ -77,9 +77,9 @@ export default function Transformations () {
     const transformation = parseInt(this.sup) - 1
     const listeTypeDeQuestions = [
       [1, 2, 3, 4],
-      [1, 2, 3, 4, 7],
-      [1, 2, 3, 4, 7, 8],
-      [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+      [1, 2, 7, 7, 7, 7],
+      [1, 2, 7, 7, 8, 8, 8, 8, 8],
+      [1, 2, 5, 5, 5, 6, 6, 6, 7, 7, 8, 8, 8, 9, 9, 9, 10, 10, 10]
     ]
     const choixTransformation = combinaisonListes(
       listeTypeDeQuestions[transformation],
