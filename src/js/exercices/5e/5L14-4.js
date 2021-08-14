3522caaa5d0bd5bc8a595fd19362b744d354f493
Guillaume Valmont <guillaume.valmont@pm.me>
2021-08-14 15:51:34 +0400

    Ajout du paramétrage et de la structure de l'expression littérale dans la correction

--- src/js/exercices/5e/5L14-4.js
+++ src/js/exercices/5e/5L14-4.js
@@ -7,22 +7,33 @@ export const titre = 'Déterminer la dernière opération à effectuer dans une 
  * Référence 5L14-4
  * Déterminer la dernière opération à effectuer dans une expression littérale
  * @author Sébastien Lozano fork Jean-Claude Lhote
+ * Rendu paramétrable et ajout de la structure d'une expression le 14/08/2021 : Guillaume Valmont
  */
 export default function DeterminerDerniereOperationExpressionLitterale () {
   'use strict'
   Exercice.call(this) // Héritage de la classe Exercice()
   this.debug = false
-  this.consigne = ''
   this.nbQuestions = 4
   this.nbCols = 1
   this.nbColsCorr = 1
   this.sup2 = false // si false alors utilisation de nombres entiers, si true alors utilisation de nombres à un chiffre après la virgule.
+  this.consigne = 'Déterminer la dernière opération à effectuer s\'il fallait faire le calcul pour des valeurs données de $x$ et de $y$.'
 
   this.nouvelleVersion = function () {
     this.listeQuestions = [] // Liste de questions
     this.listeCorrections = [] // Liste de questions corrigées
-    const typesDeQuestionsDisponibles = [5] // expressions complexes
-    let expn; let expc; let decimal = 1; let nbOperations; let resultats; let lastOp
+
+    let typesDeQuestionsDisponibles = []
+    if (!this.sup3 || this.sup3 === 'NaN') { // Si aucune liste n'est saisie
+      typesDeQuestionsDisponibles = ['5']
+    } else {
+      if (typeof (this.sup3) === 'number') { // Si c'est un nombre c'est qu'il y a qu'une expression
+        typesDeQuestionsDisponibles[0] = this.sup3
+      } else {
+        typesDeQuestionsDisponibles = this.sup3.split('-')// Sinon on créé un tableau à partir des valeurs séparées par des -
+      }
+    }
+    let expn; let expc; let decimal = 1; let nbOperations; let resultats; let lastOp; let structureExpression
     const listeTypeDeQuestions = combinaisonListes(typesDeQuestionsDisponibles, this.nbQuestions)
     if (this.sup2) decimal = 10
     for (let i = 0, texte, texteCorr, val1, val2, cpt = 0; i < this.nbQuestions && cpt < 50;) {
@@ -36,28 +47,27 @@ export default function DeterminerDerniereOperationExpressionLitterale () {
       expc = resultats[2]
       // nbval = resultats[3]
       lastOp = resultats[4]
+      structureExpression = resultats[6]
 
-      switch (listeTypeDeQuestions[i]) {
-        case 5:
-          if (expn.indexOf('ou') > 0) expn = expn.substring(0, expn.indexOf('ou')) // on supprime la deuxième expression fractionnaire
-          this.consigne = 'Déterminer la dernière opération à effectuer s\'il fallait faire le calcul pour des valeurs données de $x$ et de $y$.'
-          texte = `${expn}.`
-          if (this.debug) {
-            texte += '<br><br>=====CORRECTION======<br>'
-            texte += `Pour fixer les idées, choissions des valeurs pour $x$ et $y$, par exemple $x=${val1}$ et $y=${val2}$.`
-            texte += `<br>Le calcul serait le suivant :<br> ${expc}.`
-            texte += '<br>Pour n\'importe quelles valeurs de $x$ et de $y$ choisies, les étapes sont les mêmes, elles respectent les priorités opératoires.'
-            texte += texteEnCouleur(`<br>La dernière opération dans ${expn} est donc une ${lastOp}.`)
-            texteCorr = ''
-          } else {
-            texteCorr = `Pour fixer les idées, choissions des valeurs pour $x$ et $y$, par exemple $x=${val1}$ et $y=${val2}$.`
-            texteCorr += `<br>Le calcul serait le suivant : ${expc}.`
-            texteCorr += '<br>Pour n\'importe quelles valeurs de $x$ et de $y$ choisies, les étapes sont les mêmes, elles respectent les priorités opératoires.'
-            texteCorr += texteEnCouleur(`<br>La dernière opération dans ${expn} est donc une ${lastOp}.`)
-          };
+      if (expn.indexOf('ou') > 0) expn = expn.substring(0, expn.indexOf('ou')) // on supprime la deuxième expression fractionnaire
+      texte = `${expn}.`
+      if (this.debug) {
+        texte += '<br><br>=====CORRECTION======<br>'
+        texte += `Pour fixer les idées, choissions des valeurs pour $x$ et $y$, par exemple $x=${val1}$ et $y=${val2}$.`
+        texte += `<br>Le calcul serait le suivant :<br> ${expc}.`
+        texte += '<br>Pour n\'importe quelles valeurs de $x$ et de $y$ choisies, les étapes sont les mêmes, elles respectent les priorités opératoires.'
+        texte += texteEnCouleur(`<br>La dernière opération dans ${expn} est donc une ${lastOp}.`)
+        texteCorr = ''
+      } else {
+        texteCorr = `Pour fixer les idées, choissions des valeurs pour $x$ et $y$, par exemple $x=${val1}$ et $y=${val2}$.`
+        texteCorr += `<br>Le calcul serait le suivant : ${expc}.`
+        texteCorr += '<br>Pour n\'importe quelles valeurs de $x$ et de $y$ choisies, les étapes sont les mêmes, elles respectent les priorités opératoires.'
+        texteCorr += texteEnCouleur(`<br>La dernière opération dans ${expn} est donc une ${lastOp}.`)
+        if (this.consigne === 'Déterminer la structure de l\'expression littérale') {
+          texteCorr += texteEnCouleur(`<br>Cette expression est donc ${structureExpression}.`)
+        }
+      };
 
-          break
-      }
       if (this.listeQuestions.indexOf(texte) === -1) { // Si la question n'a jamais été posée, on en créé une autre
         this.listeQuestions.push(texte)
         this.listeCorrections.push(texteCorr)
@@ -69,4 +79,5 @@ export default function DeterminerDerniereOperationExpressionLitterale () {
   }
   this.besoinFormulaireCaseACocher = ['Avec signes × devant les parenthèses', true]
   this.besoinFormulaire2CaseACocher = ['Avec décimaux.', false]
+  this.besoinFormulaire3Texte = ['Nombre d\'opérations', 'Nombres séparés par des tirets\n1 : 1 opération\n2 : 2 opérations\n3 : 3 opérations\n4 : 4 opérations\n5 : Entre 2 et 5 opérations']
 }
