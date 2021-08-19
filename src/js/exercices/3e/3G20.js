9c259f0dc17516cc26c39e52ad57584e5f2386fa
J-C Lhote <lhotejc@gmail.com>
2021-08-19 21:11:00 +0200

    3G20 et 4G30 interactifs

--- src/js/exercices/3e/3G20.js
+++ src/js/exercices/3e/3G20.js
@@ -1,5 +1,5 @@
 import Thales2D from '../4e/_Thales2D.js'
-export const titre = 'Calculer des longueurs avec la propriété de Thalès'
+export { interactifReady, interactifType, amcReady, amcType, titre } from '../4e/_Thales2D.js'
 
 /**
  * Calcul de longueurs avec le théorème de Thalès
@@ -9,5 +9,4 @@ export const titre = 'Calculer des longueurs avec la propriété de Thalès'
 export default function Thales2D3e () {
   Thales2D.call(this)
   this.sup = 2
-  this.titre = titre
 }
