781491db675916fff758cf9467deef6c2b972a3e
Angot <remi.angot@gmail.com>
2021-08-22 11:57:15 +0200

    Bouton eval se désactive quand un exercice n'est pas interactif.

--- src/js/mathalea.js
+++ src/js/mathalea.js
@@ -376,13 +376,15 @@ function miseAJourDuCode () {
   for (const exercice of listeObjetsExercice) {
     if (!exercice.interactifReady) {
       tousLesExercicesSontInteractifs = false
-      if (document.getElementById('btnCan')) {
+      if (document.getElementById('btnCan') && document.getElementById('btnEval') ) {
         document.getElementById('btnCan').classList.add('disabled')
+        document.getElementById('btnEval').classList.add('disabled')
       }
     }
   }
-  if (document.getElementById('btnCan') !== null) {
+  if (document.getElementById('btnCan') !== null && document.getElementById('btnEval')!== null) {
     tousLesExercicesSontInteractifs ? document.getElementById('btnCan').classList.remove('disabled') : document.getElementById('btnCan').classList.add('disabled')
+    tousLesExercicesSontInteractifs ? document.getElementById('btnEval').classList.remove('disabled') : document.getElementById('btnEval').classList.add('disabled')
   }
 
   const btn1Question = document.getElementById('btn1Question')
