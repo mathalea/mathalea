674e19b7cde0a3390c7b0babf4b0b3e6cc88d1c4
Laurence Candille <87067866+LaurCan@users.noreply.github.com>
2021-08-23 09:49:54 +0200

    3L13-1-i renommé en 3L13-1 et fin

--- src/js/exercices/3e/3L13-1.js
+++ src/js/exercices/3e/3L13-1.js
@@ -1,7 +1,13 @@
 import Exercice from '../Exercice.js'
 import { context } from '../../modules/context.js'
-import {listeQuestionsToContenu,randint,combinaisonListes,rienSi1,ecritureAlgebrique,ecritureParentheseSiNegatif,signe,abs,pgcd,texFractionReduite,miseEnEvidence,texFraction} from '../../modules/outils.js'
+import { listeQuestionsToContenu, randint, combinaisonListes, rienSi1, ecritureAlgebrique, ecritureParentheseSiNegatif, signe, abs, pgcd, texFractionReduite, miseEnEvidence, texFraction, lampeMessage } from '../../modules/outils.js'
+import { ajouteChampTexteMathLive, setReponse } from '../../modules/gestionInteractif.js'
+import Fraction from '../../modules/Fraction.js'
 export const titre = 'Équation du premier degré (utilisant la distributivité)'
+export const interactifReady = true
+export const interactifType = 'mathLive'
+export const amcReady = true
+export const amcType = 'AMCHybride'
 
 /**
 * Équation du premier degré
@@ -10,152 +16,166 @@ export const titre = 'Équation du premier degré (utilisant la distributivité)
 * * Type 3 : k-(ax+b)=cx+d
 * * Tous les types
 * @author Rémi Angot
+* Rendre interactif Laurence Candille
 * 3L13-1
 */
-export default function Exercice_equation1_2() {
-	Exercice.call(this); // Héritage de la classe Exercice()
-	this.titre = titre;
-	this.consigne = 'Résoudre les équations suivantes';
-	this.spacing = 2;
-	context.isHtml ? this.spacingCorr = 3 : this.spacingCorr = 2;
-	this.correctionDetailleeDisponible = true;
-	if (!context.isHtml) {
-		this.correctionDetaillee = false;
-	}
-	this.nbQuestions = 3;
+export default function ExerciceEquation1Tiret2 () {
+  Exercice.call(this) // Héritage de la classe Exercice()
+  this.titre = titre
+  this.consigne = 'Résoudre les équations suivantes :'
+  this.spacing = 2
+  this.interactif = true
+  this.interactifType = 'mathLive'
+  context.isHtml ? this.spacingCorr = 3 : this.spacingCorr = 2
+  this.correctionDetailleeDisponible = true
+  if (!context.isHtml) {
+    this.correctionDetaillee = false
+  }
+  this.nbQuestions = 3
 
-	this.nouvelleVersion = function () {
-		this.listeQuestions = []; // Liste de questions
-		this.listeCorrections = []; // Liste de questions corrigées
-		let listeTypeDeQuestions = ['ax+b=cx+d', 'k(ax+b)=cx+d', 'k-(ax+b)=cx+d'];
-		listeTypeDeQuestions = combinaisonListes(listeTypeDeQuestions, this.nbQuestions);
-		for (let i = 0, a, b, c, d, k,texte, texteCorr, cpt = 0; i < this.nbQuestions && cpt < 50;) { // On limite le nombre d'essais pour chercher des valeurs nouvelles
-			a = randint(-9, 9, 0);
-			b = randint(-9, 9, 0);
-			c = randint(-9, 9, 0);
-			d = randint(-9, 9, 0);
-			k = randint(2, 9);
-			if (listeTypeDeQuestions[i] == 'ax+b=cx+d') {
-				if (c == a) { c = randint(1, 9, [a]); } // sinon on arrive à une division par 0
-				if (!this.sup && a < c) {
-					c = randint(1, 9);
-					a = randint(c + 1, 15); // a sera plus grand que c pour que a-c>0
-				}
-				texte = `$${rienSi1(a)}x${ecritureAlgebrique(b)}=${rienSi1(c)}x${ecritureAlgebrique(d)}$`;
-				texteCorr = texte + '<br>';
-				if (this.correctionDetaillee) {
-					if (c > 0) {
-						texteCorr += `On soustrait $${rienSi1(c)}x$ aux deux membres.<br>`;
-					} else {
-						texteCorr += `On ajoute $${rienSi1(-1 * c)}x$ aux deux membres.<br>`;
-					}
-				}
-				texteCorr += `$${rienSi1(a)}x${ecritureAlgebrique(b)}${miseEnEvidence(signe(-1 * c) + rienSi1(abs(c)) + 'x')}=${c}x+${d}${miseEnEvidence(signe(-1 * c) + rienSi1(abs(c)) + 'x')}$<br>`;
-				texteCorr += `$${rienSi1(a - c)}x${ecritureAlgebrique(b)}=${d}$<br>`;
-				if (this.correctionDetaillee) {
-					if (b > 0) {
-						texteCorr += `On soustrait $${b}$ aux deux membres.<br>`;
-					} else {
-						texteCorr += `On ajoute $${-1 * b}$ aux deux membres.<br>`;
-					}
-				}
-				texteCorr += `$${rienSi1(a - c)}x${ecritureAlgebrique(b)}${miseEnEvidence(ecritureAlgebrique(-1 * b))}=${d}${miseEnEvidence(ecritureAlgebrique(-1 * b))}$<br>`;
-				texteCorr += `$${rienSi1(a - c)}x=${d - b}$<br>`;
+  this.nouvelleVersion = function () {
+    this.listeQuestions = [] // Liste de questions
+    this.listeCorrections = [] // Liste de questions corrigées
+    this.introduction = lampeMessage({
+      titre: 'Calculatrice autorisée.',
+      texte: `Résoudre les équations au brouillon et écrire les solutions dans les cases.<br> Pour une solution comme 0,333... seule une fraction (par ex : $${texFraction(1, 3)})$ est une réponse correcte.`,
+      couleur: 'nombres'
+    })
+    let listeTypeDeQuestions = ['ax+b=cx+d', 'k(ax+b)=cx+d', 'k-(ax+b)=cx+d']
+    listeTypeDeQuestions = combinaisonListes(listeTypeDeQuestions, this.nbQuestions)
+    for (let i = 0, a, b, c, d, k, texte, texteCorr, cpt = 0; i < this.nbQuestions && cpt < 50;) { // On limite le nombre d'essais pour chercher des valeurs nouvelles
+      a = randint(-9, 9, 0)
+      b = randint(-9, 9, 0)
+      c = randint(-9, 9, 0)
+      d = randint(-9, 9, 0)
+      k = randint(2, 9)
+      if (listeTypeDeQuestions[i] === 'ax+b=cx+d') {
+        if (c === a) { c = randint(1, 9, [a]) } // sinon on arrive à une division par 0
+        if (!this.sup && a < c) {
+          c = randint(1, 9)
+          a = randint(c + 1, 15) // a sera plus grand que c pour que a-c>0
+        }
+        texte = `$${rienSi1(a)}x${ecritureAlgebrique(b)}=${rienSi1(c)}x${ecritureAlgebrique(d)}$<br>`
+        texteCorr = texte
+        texte += '$x =$' + ajouteChampTexteMathLive(this, i, 'inline largeur25') + '<br><br>'
+        setReponse(this, i, new Fraction(d - b, a - c), { formatInteractif: 'fractionEgale' })
+        if (this.correctionDetaillee) {
+          if (c > 0) {
+            texteCorr += `On soustrait $${rienSi1(c)}x$ aux deux membres.<br>`
+          } else {
+            texteCorr += `On ajoute $${rienSi1(-1 * c)}x$ aux deux membres.<br>`
+          }
+        }
+        texteCorr += `$${rienSi1(a)}x${ecritureAlgebrique(b)}${miseEnEvidence(signe(-1 * c) + rienSi1(abs(c)) + 'x')}=${c}x+${d}${miseEnEvidence(signe(-1 * c) + rienSi1(abs(c)) + 'x')}$<br>`
+        texteCorr += `$${rienSi1(a - c)}x${ecritureAlgebrique(b)}=${d}$<br>`
+        if (this.correctionDetaillee) {
+          if (b > 0) {
+            texteCorr += `On soustrait $${b}$ aux deux membres.<br>`
+          } else {
+            texteCorr += `On ajoute $${-1 * b}$ aux deux membres.<br>`
+          }
+        }
+        texteCorr += `$${rienSi1(a - c)}x${ecritureAlgebrique(b)}${miseEnEvidence(ecritureAlgebrique(-1 * b))}=${d}${miseEnEvidence(ecritureAlgebrique(-1 * b))}$<br>`
+        texteCorr += `$${rienSi1(a - c)}x=${d - b}$<br>`
 
-				if (this.correctionDetaillee) { texteCorr += `On divise les deux membres par $${a - c}$.<br>`; }
-				texteCorr += `$${rienSi1(a - c)}x${miseEnEvidence('\\div' + ecritureParentheseSiNegatif(a - c))}=${d - b + miseEnEvidence('\\div' + ecritureParentheseSiNegatif(a - c))}$<br>`;
-				texteCorr += `$x=${texFraction(d - b, a - c)}$`;
-				if (pgcd(abs(d - b), abs(a - c)) > 1 || (a - c) < 0) {
-					texteCorr += `<br>$x=${texFractionReduite(d - b, a - c)}$`;
-				}
-				texteCorr += `<br> La solution est $${texFractionReduite(d - b, a - c)}$.`;
-			}
+        if (this.correctionDetaillee) { texteCorr += `On divise les deux membres par $${a - c}$.<br>` }
+        texteCorr += `$${rienSi1(a - c)}x${miseEnEvidence('\\div' + ecritureParentheseSiNegatif(a - c))}=${d - b + miseEnEvidence('\\div' + ecritureParentheseSiNegatif(a - c))}$<br>`
+        texteCorr += `$x=${texFraction(d - b, a - c)}$`
+        if (pgcd(abs(d - b), abs(a - c)) > 1 || (a - c) < 0) {
+          texteCorr += `<br>$x=${texFractionReduite(d - b, a - c)}$`
+        }
+        texteCorr += `<br> La solution est $${texFractionReduite(d - b, a - c)}$.`
+      }
 
-			if (listeTypeDeQuestions[i] == 'k(ax+b)=cx+d') {
-				if (c == k * a) { c = randint(1, 9, [a]); } // sinon on arrive à une division par 0
-				texte = `$${k}(${rienSi1(a)}x${ecritureAlgebrique(b)})=${rienSi1(c)}x${ecritureAlgebrique(d)}$`;
-				texteCorr = texte + '<br>';
-				if (this.correctionDetaillee) {
-					texteCorr += 'On développe le membre de gauche.<br>';
-				}
-				texteCorr += `$${k * a}x${ecritureAlgebrique(k * b)}=${rienSi1(c)}x${ecritureAlgebrique(d)}$<br>`;
-				if (this.correctionDetaillee) {
-					if (c > 0) {
-						texteCorr += `On soustrait $${rienSi1(c)}x$ aux deux membres.<br>`;
-					} else {
-						texteCorr += `On ajoute $${rienSi1(-1 * c)}x$ aux deux membres.<br>`;
-					}
-				}
-				texteCorr += `$${k * a}x${ecritureAlgebrique(k * b)}${miseEnEvidence(signe(-1 * c) + rienSi1(abs(c)) + 'x')}=${c}x${ecritureAlgebrique(d)}${miseEnEvidence(signe(-1 * c) + rienSi1(abs(c)) + 'x')}$<br>`;
-				texteCorr += `$${rienSi1(k * a - c)}x${ecritureAlgebrique(k * b)}=${d}$<br>`;
-				if (this.correctionDetaillee) {
-					if (k * b > 0) {
-						texteCorr += `On soustrait $${k * b}$ aux deux membres.<br>`;
-					} else {
-						texteCorr += `On ajoute $${-k * b}$ aux deux membres.<br>`;
-					}
-				}
-				texteCorr += `$${rienSi1(k * a - c)}x${ecritureAlgebrique(k * b)}${miseEnEvidence(ecritureAlgebrique(-k * b))}=${d}${miseEnEvidence(ecritureAlgebrique(-k * b))}$<br>`;
-				texteCorr += `$${rienSi1(k * a - c)}x=${d - k * b}$<br>`;
+      if (listeTypeDeQuestions[i] === 'k(ax+b)=cx+d') {
+        if (c === k * a) { c = randint(1, 9, [a]) } // sinon on arrive à une division par 0
+        texte = `$${k}(${rienSi1(a)}x${ecritureAlgebrique(b)})=${rienSi1(c)}x${ecritureAlgebrique(d)}$<br>`
+        texteCorr = texte
+        texte += '$x =$' + ajouteChampTexteMathLive(this, i, 'inline largeur25') + '<br><br>'
+        setReponse(this, i, new Fraction(d - k * b, a * k - c), { formatInteractif: 'fractionEgale' })
+        if (this.correctionDetaillee) {
+          texteCorr += 'On développe le membre de gauche.<br>'
+        }
+        texteCorr += `$${k * a}x${ecritureAlgebrique(k * b)}=${rienSi1(c)}x${ecritureAlgebrique(d)}$<br>`
+        if (this.correctionDetaillee) {
+          if (c > 0) {
+            texteCorr += `On soustrait $${rienSi1(c)}x$ aux deux membres.<br>`
+          } else {
+            texteCorr += `On ajoute $${rienSi1(-1 * c)}x$ aux deux membres.<br>`
+          }
+        }
+        texteCorr += `$${k * a}x${ecritureAlgebrique(k * b)}${miseEnEvidence(signe(-1 * c) + rienSi1(abs(c)) + 'x')}=${c}x${ecritureAlgebrique(d)}${miseEnEvidence(signe(-1 * c) + rienSi1(abs(c)) + 'x')}$<br>`
+        texteCorr += `$${rienSi1(k * a - c)}x${ecritureAlgebrique(k * b)}=${d}$<br>`
+        if (this.correctionDetaillee) {
+          if (k * b > 0) {
+            texteCorr += `On soustrait $${k * b}$ aux deux membres.<br>`
+          } else {
+            texteCorr += `On ajoute $${-k * b}$ aux deux membres.<br>`
+          }
+        }
+        texteCorr += `$${rienSi1(k * a - c)}x${ecritureAlgebrique(k * b)}${miseEnEvidence(ecritureAlgebrique(-k * b))}=${d}${miseEnEvidence(ecritureAlgebrique(-k * b))}$<br>`
+        texteCorr += `$${rienSi1(k * a - c)}x=${d - k * b}$<br>`
 
-				if (this.correctionDetaillee) { texteCorr += `On divise les deux membres par $${k * a - c}$.<br>`; }
-				texteCorr += `$${rienSi1(k * a - c)}x${miseEnEvidence('\\div' + ecritureParentheseSiNegatif(k * a - c))}=${d - k * b + miseEnEvidence('\\div' + ecritureParentheseSiNegatif(k * a - c))}$<br>`;
-				texteCorr += `$x=${texFraction(d - k * b, k * a - c)}$`;
-				if (pgcd(abs(d - k * b), abs(k * a - c)) > 1 || (k * a - c) < 0) {
-					texteCorr += `<br>$x=${texFractionReduite(d - k * b, k * a - c)}$`;
-				}
-				texteCorr += `<br> La solution est $${texFractionReduite(d - k * b, k * a - c)}$.`;
-			}
+        if (this.correctionDetaillee) { texteCorr += `On divise les deux membres par $${k * a - c}$.<br>` }
+        texteCorr += `$${rienSi1(k * a - c)}x${miseEnEvidence('\\div' + ecritureParentheseSiNegatif(k * a - c))}=${d - k * b + miseEnEvidence('\\div' + ecritureParentheseSiNegatif(k * a - c))}$<br>`
+        texteCorr += `$x=${texFraction(d - k * b, k * a - c)}$`
+        if (pgcd(abs(d - k * b), abs(k * a - c)) > 1 || (k * a - c) < 0) {
+          texteCorr += `<br>$x=${texFractionReduite(d - k * b, k * a - c)}$`
+        }
+        texteCorr += `<br> La solution est $${texFractionReduite(d - k * b, k * a - c)}$.`
+      }
 
-			if (listeTypeDeQuestions[i] == 'k-(ax+b)=cx+d') {
-				if (c == -a) { c = randint(-9, 9, [0, a]); } // sinon on arrive à une division par 0
-				texte = `$${k}-(${rienSi1(a)}x${ecritureAlgebrique(b)})=${rienSi1(c)}x${ecritureAlgebrique(d)}$`;
-				texteCorr = texte + '<br>';
-				if (this.correctionDetaillee) {
-					texteCorr += 'On développe le membre de gauche.<br>';
-				}
-				texteCorr += `$${k}${ecritureAlgebrique(-a)}x${ecritureAlgebrique(-b)}=${rienSi1(c)}x${ecritureAlgebrique(d)}$<br>`;
-				texteCorr += `$${rienSi1(-a)}x${ecritureAlgebrique(k - b)}=${rienSi1(c)}x${ecritureAlgebrique(d)}$<br>`;
+      if (listeTypeDeQuestions[i] === 'k-(ax+b)=cx+d') {
+        if (c === -a) { c = randint(-9, 9, [0, a]) } // sinon on arrive à une division par 0
+        texte = `$${k}-(${rienSi1(a)}x${ecritureAlgebrique(b)})=${rienSi1(c)}x${ecritureAlgebrique(d)}$<br>`
+        texteCorr = texte
+        texte += '$x =$' + ajouteChampTexteMathLive(this, i, 'inline largeur25') + '<br><br>'
+        setReponse(this, i, new Fraction(k - b - d, a + c), { formatInteractif: 'fractionEgale' })
+        if (this.correctionDetaillee) {
+          texteCorr += 'On développe le membre de gauche.<br>'
+        }
+        texteCorr += `$${k}${ecritureAlgebrique(-a)}x${ecritureAlgebrique(-b)}=${rienSi1(c)}x${ecritureAlgebrique(d)}$<br>`
+        texteCorr += `$${rienSi1(-a)}x${ecritureAlgebrique(k - b)}=${rienSi1(c)}x${ecritureAlgebrique(d)}$<br>`
 
-				//On reprend le cas ax+b=cx+d en changeant les valeurs de a et b
-				a = -a;
-				b = k - b;
+        // On reprend le cas ax+b=cx+d en changeant les valeurs de a et b
+        a = -a
+        b = k - b
 
-				if (this.correctionDetaillee) {
-					if (c > 0) {
-						texteCorr += `On soustrait $${rienSi1(c)}x$ aux deux membres.<br>`;
-					} else {
-						texteCorr += `On ajoute $${rienSi1(-1 * c)}x$ aux deux membres.<br>`;
-					}
-				}
-				texteCorr += `$${rienSi1(a)}x${ecritureAlgebrique(b)}${miseEnEvidence(signe(-1 * c) + rienSi1(abs(c)) + 'x')}=${c}x+${d}${miseEnEvidence(signe(-1 * c) + rienSi1(abs(c)) + 'x')}$<br>`;
-				texteCorr += `$${rienSi1(a - c)}x${ecritureAlgebrique(b)}=${d}$<br>`;
-				if (this.correctionDetaillee) {
-					if (b > 0) {
-						texteCorr += `On soustrait $${b}$ aux deux membres.<br>`;
-					} else {
-						texteCorr += `On ajoute $${-1 * b}$ aux deux membres.<br>`;
-					}
-				}
-				texteCorr += `$${rienSi1(a - c)}x${ecritureAlgebrique(b)}${miseEnEvidence(ecritureAlgebrique(-1 * b))}=${d}${miseEnEvidence(ecritureAlgebrique(-1 * b))}$<br>`;
-				texteCorr += `$${rienSi1(a - c)}x=${d - b}$<br>`;
+        if (this.correctionDetaillee) {
+          if (c > 0) {
+            texteCorr += `On soustrait $${rienSi1(c)}x$ aux deux membres.<br>`
+          } else {
+            texteCorr += `On ajoute $${rienSi1(-1 * c)}x$ aux deux membres.<br>`
+          }
+        }
+        texteCorr += `$${rienSi1(a)}x${ecritureAlgebrique(b)}${miseEnEvidence(signe(-1 * c) + rienSi1(abs(c)) + 'x')}=${c}x+${d}${miseEnEvidence(signe(-1 * c) + rienSi1(abs(c)) + 'x')}$<br>`
+        texteCorr += `$${rienSi1(a - c)}x${ecritureAlgebrique(b)}=${d}$<br>`
+        if (this.correctionDetaillee) {
+          if (b > 0) {
+            texteCorr += `On soustrait $${b}$ aux deux membres.<br>`
+          } else {
+            texteCorr += `On ajoute $${-1 * b}$ aux deux membres.<br>`
+          }
+        }
+        texteCorr += `$${rienSi1(a - c)}x${ecritureAlgebrique(b)}${miseEnEvidence(ecritureAlgebrique(-1 * b))}=${d}${miseEnEvidence(ecritureAlgebrique(-1 * b))}$<br>`
+        texteCorr += `$${rienSi1(a - c)}x=${d - b}$<br>`
 
-				if (this.correctionDetaillee) { texteCorr += `On divise les deux membres par $${a - c}$.<br>`; }
-				texteCorr += `$${rienSi1(a - c)}x${miseEnEvidence('\\div' + ecritureParentheseSiNegatif(a - c))}=${d - b + miseEnEvidence('\\div' + ecritureParentheseSiNegatif(a - c))}$<br>`;
-				texteCorr += `$x=${texFraction(d - b, a - c)}$`;
-				if (pgcd(abs(d - b), abs(a - c)) > 1 || (a - c) < 0) {
-					texteCorr += `<br>$x=${texFractionReduite(d - b, a - c)}$`;
-				}
-				texteCorr += `<br> La solution est $${texFractionReduite(d - b, a - c)}$.`;
-			}
+        if (this.correctionDetaillee) { texteCorr += `On divise les deux membres par $${a - c}$.<br>` }
+        texteCorr += `$${rienSi1(a - c)}x${miseEnEvidence('\\div' + ecritureParentheseSiNegatif(a - c))}=${d - b + miseEnEvidence('\\div' + ecritureParentheseSiNegatif(a - c))}$<br>`
+        texteCorr += `$x=${texFraction(d - b, a - c)}$`
+        if (pgcd(abs(d - b), abs(a - c)) > 1 || (a - c) < 0) {
+          texteCorr += `<br>$x=${texFractionReduite(d - b, a - c)}$`
+        }
+        texteCorr += `<br> La solution est $${texFractionReduite(d - b, a - c)}$.`
+      }
 
-			if (this.listeQuestions.indexOf(texte) === -1) { // Si la question n'a jamais été posée, on en créé une autre
-				this.listeQuestions.push(texte); //replace(/1x/g,'x')); //remplace 1x par x
-				this.listeCorrections.push(texteCorr); //.replace(/1x/g,'x')); //remplace 1x par x
-				i++;
-			}
-			cpt++;
-		}
-		listeQuestionsToContenu(this);
-	};
+      if (this.listeQuestions.indexOf(texte) === -1) { // Si la question n'a jamais été posée, on en créé une autre
+        this.listeQuestions.push(texte) // replace(/1x/g,'x')); //remplace 1x par x
+        this.listeCorrections.push(texteCorr) // .replace(/1x/g,'x')); //remplace 1x par x
+        i++
+      }
+      cpt++
+    }
+    listeQuestionsToContenu(this)
+  }
 }
