382db088c8ed900a2e2b9b675b4c157be98fdbf2
moragi <gilles.mora@ac-rouen.fr>
2021-09-04 16:58:01 +0200

    CAN2Q16 cassée en 8 questions simples

---
+++ src/js/exercices/beta/betaCAN2Q16-05.js
@@ -0,0 +1,30 @@
+import Exercice from '../Exercice.js'
+import { randint, calcul, texNombrec, rienSi1 } from '../../modules/outils.js'
+export const titre = 'Calcul avec puissance de 10'
+export const interactifReady = true
+export const interactifType = 'mathLive'
+export const amcReady = true
+export const amcType = 'AMCNum'
+/**
+ * Modèle d'exercice très simple pour la course aux nombres
+ * @author Gille Mora
+ * Référence
+ * Date de publication
+*/
+export default function CalculPuissance10 () {
+  Exercice.call(this)
+  this.typeExercice = 'simple'
+  this.nbQuestions = 1
+  this.interactif = true
+  this.formatChampTexte = 'largeur15 inline'
+  this.nouvelleVersion = function () {
+    const a = randint(1, 6)
+    const n = calcul(2 * randint(1, 6) + 1) / 2
+    const N = calcul(2 * randint(1, 6, a) + 1) / 2
+    const c = randint(1, 3)
+    const d = randint(1, 3)
+    this.question = `Calculer sous forme décimale : $B=${texNombrec(n)}\\times 10^{${rienSi1(c)}}+${texNombrec(N)}\\times 10^{${rienSi1(d)}}$.`
+    this.correction = `$B=${texNombrec(n * 10 ** c)}+${texNombrec(N * 10 ** d)}=${n * 10 ** c + N * 10 ** d}$.`
+    this.reponse = calcul(n * 10 ** c + N * 10 ** d)
+  }
+}
---
+++ src/js/exercices/beta/betaCAN2Q16-06.js
@@ -0,0 +1,66 @@
+import Exercice from '../Exercice.js'
+import { randint, calcul, choice, texNombrec, abs, ecritureParentheseSiNegatif } from '../../modules/outils.js'
+export const titre = 'Calcul avec puissance'
+export const interactifReady = true
+export const interactifType = 'mathLive'
+export const amcReady = true
+export const amcType = 'AMCNum'
+/**
+ * Modèle d'exercice très simple pour la course aux nombres
+ * @author Gille Mora
+ * Référence
+ * Date de publication
+*/
+export default function CalculPuissance1 () {
+  Exercice.call(this)
+  this.typeExercice = 'simple'
+  this.nbQuestions = 1
+  this.interactif = true
+  this.formatChampTexte = 'largeur15 inline'
+  this.nouvelleVersion = function () {
+    let a, b, c, d
+    switch (choice(['a', 'b', 'c', 'd', 'e', 'f'])) { //
+      case 'a':
+        a = choice([0.25, 0.5])
+        b = randint(2, 5)
+        this.question = `$4^{${b}} \\times ${texNombrec(a)}^{${b}}=$`
+        this.correction = `$4^{${b}}\\times ${texNombrec(a)}^{${b}}=(4\\times ${a})^{${b}}=${texNombrec((4 ** b) * (a ** b))} $`
+        this.reponse = ((4 ** b) * (a ** b))
+        break
+      case 'b':
+        a = choice([0.2, 0.4])
+        b = randint(2, 5)
+        this.question = `$5^{${b}} \\times ${texNombrec(a)}^{${b}}= $`
+        this.correction = `$5^{${b}} \\times ${texNombrec(a)}^{${b}}=(5\\times ${texNombrec(a)})^{${b}}=${texNombrec((5 ** b) * (a ** b))} $`
+        this.reponse = calcul((5 ** b) * (a ** b))
+        break
+
+      case 'c':
+        a = randint(-3, -1)
+        this.question = `$2^{${a}} \\times 8=$`
+        this.correction = `$2^{${a}}\\times 8=\\dfrac{1}{2^{${abs(a)}}}\\times 8=${texNombrec(8 * 1 / 2 ** (-a))} $`
+        this.reponse = calcul((2 ** a) * 8)
+        break
+      case 'd':
+        a = randint(-4, -1)
+        this.question = `$2^{${a}} \\times 16=$`
+        this.correction = `$2^{${a}} \\times 16=\\dfrac{1}{2^{${abs(a)}}}\\times 16=${texNombrec(16 * 1 / 2 ** (-a))} $`
+        this.reponse = calcul((2 ** a) * 16)
+        break
+      case 'e':
+        a = randint(-5, -1)
+        this.question = `$2^{${a}} \\times 32=$`
+        this.correction = `$2^{${a}} \\times 32=\\dfrac{1}{2^{${abs(a)}}}\\times 32=${texNombrec(32 * 1 / 2 ** (-a))} $`
+        this.reponse = calcul((2 ** a) * 32)
+        break
+      case 'f':
+        a = randint(2, 10)
+        c = randint(-5, 10, [0, 1])
+        d = randint(2, 10)
+        this.question = `Compléter : $${a}^{...}\\times ${a}^{${c}}=${a}^{${-d}}$`
+        this.correction = `$\\ldots +${ecritureParentheseSiNegatif(c)}=-${d}$, soit $....=-${d}-${ecritureParentheseSiNegatif(c)}=${-d - c}$.`
+        this.reponse = calcul(-d - c)
+        break
+    }
+  }
+}
