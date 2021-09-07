a8bc1334782c239ead4ce6a07fb3944cebc45ce2
J-C Lhote <lhotejc@gmail.com>
2021-09-07 23:15:43 +0200

    4 petites can de CM1-CM2...

---
+++ src/js/exercices/can/c3/canc3N01.js
@@ -0,0 +1,71 @@
+import { context } from '../../../modules/context'
+import { propositionsQcm } from '../../../modules/gestionInteractif'
+import { choice, listeQuestionsToContenu, nombreEnLettres, randint, texNombre } from '../../../modules/outils'
+import Exercice from '../../Exercice'
+export const titre = 'Écriture de nombre entier'
+export const interactifReady = true
+export const interactifType = 'qcm'
+export const amcReady = true
+export const amcType = 'qcmMono'
+
+/*!
+ * @author Jean-Claude Lhote
+ * Date de publication septembre 2021
+ * Référence canc3N01
+ */
+export default function EcritureDeNombreEntier () {
+  Exercice.call(this)
+  this.nbQuestions = 1
+  this.interactif = true
+  this.nouvelleVersion = function () {
+    this.listeQuestions = []
+    this.listeCorrections = []
+    const a = randint(1, 9)
+    const b = randint(1, 9, a)
+    const c = randint(1, 9, [a, b])
+    let N1, N2, N3
+    switch (choice([1, 2, 3])) {
+      case 1:
+        N1 = a * 1000 + b * 100 + c
+        N2 = a * 1000 + b * 10 + c
+        N3 = a * 1000 + b * 100 + c * 10
+        break
+      case 2:
+        N1 = a * 1000 + b * 10 + c
+        N2 = a * 1000 + b * 100 + c
+        N3 = a * 1000 + b * 100 + c * 10
+        break
+      case 3:
+        N1 = a * 1000 + b * 100 + c * 10
+        N2 = a * 1000 + b * 100 + c
+        N3 = a * 1000 + b * 10 + c
+        break
+    }
+    let texte = `Le nombre $${texNombre(N1)}$ s'écrit (coche la bonne réponse) :<br>`
+    this.autoCorrection[0] = {
+      enonce: texte,
+      options: { vertical: true },
+      propositions: [
+        {
+          texte: nombreEnLettres(N1),
+          statut: true
+        },
+        {
+          texte: nombreEnLettres(N2),
+          statut: false
+        },
+        {
+          texte: nombreEnLettres(N3),
+          statut: false
+        }
+      ]
+    }
+    if (!context.isAmc) {
+      texte += propositionsQcm(this, 0).texte
+    }
+    const texteCorr = `$${texNombre(N1)}$ s'écrit ${nombreEnLettres(N1)}.`
+    this.listeQuestions.push(texte)
+    this.listeCorrections.push(texteCorr)
+    listeQuestionsToContenu(this)
+  }
+}
