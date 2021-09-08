fefc913ccf14a6426bc8112f48561d1226c39259
J-C Lhote <lhotejc@gmail.com>
2021-09-08 20:16:51 +0200

    DEbug 2N14-1 interactif.

--- src/js/exercices/2e/2N14-1.js
+++ src/js/exercices/2e/2N14-1.js
@@ -1,6 +1,6 @@
 import Exercice from '../Exercice.js'
 import { listeQuestionsToContenu, randint, choice, combinaisonListes, texNombrec, texNombre } from '../../modules/outils.js'
-import { setReponse, ajouteChampTexteMathLive } from '../../modules/gestionInteractif.js'
+import { setReponse, ajouteChampTexte } from '../../modules/gestionInteractif.js'
 export const interactifReady = true
 export const interactifType = 'mathLive'
 export const titre = 'Déterminer le plus petit ensemble de nombres dans lequel le nombre proposé appartient'
@@ -13,12 +13,13 @@ export default function EnsembleDeNombres () {
   Exercice.call(this) // Héritage de la classe Exercice()
   this.titre = titre
   this.consigne = 'Déterminer le plus petit ensemble de nombres dans lequel le nombre proposé appartient. :'
-  this.nbQuestions = 5
+  this.nbQuestions = 1
   this.nbCols = 2
   this.nbColsCorr = 2
   this.sup = 1 //
 
   this.nouvelleVersion = function () {
+    this.autoCorrection = []
     this.listeQuestions = [] // Liste de questions
     this.listeCorrections = [] // Liste de questions corrigées
     const typesDeQuestionsDisponibles = [1, 2, 3, 4, 5, 6, 7, 8, 9]; let typesDeQuestions
@@ -35,7 +36,7 @@ export default function EnsembleDeNombres () {
           texte = `$${a} \\in \\dots$`
           texteCorr = `$${a}$ est un entier naturel, on a donc $${a}\\in \\mathbb{N}$
                   `
-          setReponse(this, i, 'N')
+          setReponse(this, i, 'N', { formatInteractif: 'texte' })
           break
         case 2:
 
@@ -44,7 +45,7 @@ export default function EnsembleDeNombres () {
           texte = `$${a} \\in \\dots$`
           texteCorr = `$${a}$ est un entier relatif, on a donc $${a}\\in \\mathbb{Z}$
                     `
-          setReponse(this, i, 'Z')
+          setReponse(this, i, 'Z', { formatInteractif: 'texte' })
           break
         case 3:
 
@@ -57,7 +58,7 @@ export default function EnsembleDeNombres () {
           texte = `$${texNombrec(b + c / 10 + d / 100)}\\in \\dots$`
           texteCorr = `$${texNombrec(b + c / 10 + d / 100)}$ est un nombre décimal, on a donc $${texNombrec(b + c / 10 + d / 100)}\\in \\mathbb{D}$
                     `
-          setReponse(this, i, 'D')
+          setReponse(this, i, 'D', { formatInteractif: 'texte' })
           break
         case 4:
 
@@ -68,7 +69,7 @@ export default function EnsembleDeNombres () {
           texte = `$\\sqrt{${texNombrec(a * a)}}\\in \\dots$`
           texteCorr = `$\\sqrt{${a * a}}=${a}$  est un entier naturel, on a donc $\\sqrt{${texNombrec(a * a)}}\\in \\mathbb{N}$
                     `
-          setReponse(this, i, 'N')
+          setReponse(this, i, 'N', { formatInteractif: 'texte' })
           break
         case 5:
 
@@ -79,7 +80,7 @@ export default function EnsembleDeNombres () {
           texte = `$\\dfrac{${texNombrec(b * a)}}{${a}}\\in \\dots$`
           texteCorr = `$\\dfrac{${texNombrec(b * a)}}{${a}}=\\dfrac{${b}\\times ${a}}{${a}}=${b}$  est un entier naturel, on a donc $\\dfrac{${texNombrec(b * a)}}{${a}}\\in \\mathbb{N}$
                     `
-          setReponse(this, i, 'N')
+          setReponse(this, i, 'N', { formatInteractif: 'texte' })
           break
         case 6:
 
@@ -89,7 +90,7 @@ export default function EnsembleDeNombres () {
           texte = `$\\dfrac{${a}}{${b}}\\in \\dots$`
           texteCorr = `$\\dfrac{${a}}{${b}}$ n'est pas un nombre décimal. On a donc $\\dfrac{${a}}{${b}}\\in \\mathbb{Q}$
                     `
-          setReponse(this, i, 'Q')
+          setReponse(this, i, 'Q', { formatInteractif: 'texte' })
           break
         case 7:
 
@@ -100,7 +101,7 @@ export default function EnsembleDeNombres () {
           texte = `$\\dfrac{${a}}{${b}}\\in \\dots$`
           texteCorr = `$\\dfrac{${a}}{${b}}=${texNombre(a / b)}$  est un nombre décimal. On a donc $\\dfrac{${a}}{${b}}\\in \\mathbb{D}$
                     `
-          setReponse(this, i, 'D')
+          setReponse(this, i, 'D', { formatInteractif: 'texte' })
           break
         case 8:
 
@@ -108,17 +109,17 @@ export default function EnsembleDeNombres () {
           texte = `$\\sqrt{${a}} \\in \\dots$`
           texteCorr = `$\\sqrt{${a}}$  est un nombre irrationnel. On a donc $\\sqrt{${a}}\\in \\mathbb{R}$
                     `
-          setReponse(this, i, 'R')
+          setReponse(this, i, 'R', { formatInteractif: 'texte' })
           break
         case 9:
           a = randint(2, 9)
           texte = `$${a}\\pi \\in \\dots$`
           texteCorr = `$${a}\\pi$   est un nombre irrationnel. On a donc $${a}\\pi \\in \\mathbb{R}$
                     `
-          setReponse(this, i, 'R')
+          setReponse(this, i, 'R', { formatInteractif: 'texte' })
           break
       }
-      texte += ajouteChampTexteMathLive(this, i)
+      texte += ajouteChampTexte(this, i)
       if (this.listeQuestions.indexOf(texte) === -1) { // Si la question n'a jamais été posée, on en créé une autre
         this.listeQuestions.push(texte)
         this.listeCorrections.push(texteCorr)
