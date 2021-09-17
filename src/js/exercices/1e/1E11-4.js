ec76e597728f1bf1d52344a3268726f45a4c4b44
Stéphane <mathsguyon@gmail.com>
2021-09-17 15:56:14 +0200

    amélioration avec classe fraction

--- src/js/exercices/1e/1E11-4.js
+++ src/js/exercices/1e/1E11-4.js
@@ -1,6 +1,7 @@
 import Exercice from '../Exercice.js'
 import { listeQuestionsToContenu, abs, randint, combinaisonListes, rienSi1, ecritureAlgebrique, ecritureAlgebriqueSauf1, pgcd, texFractionSigne, texFractionReduite } from '../../modules/outils.js'
 import { ajouteChampTexteMathLive } from '../../modules/gestionInteractif.js'
+import { fraction } from '../../modules/fractions.js'
 export const interactifReady = true
 export const interactifType = 'mathLive'
 export const titre = 'Résoudre une équation du second degré à partir de la forme canonique'
@@ -31,25 +32,22 @@ export default function Resolutionavecformecanonique () {
       listeTypeDeQuestions = combinaisonListes(['solutionsEntieres', 'solutionsEntieres'], this.nbQuestions)
     }
 
-    for (let i = 0, texte, texteCorr, a, b, b1, a1, c, x1, x2, k, beta, delta, cpt = 0; i < this.nbQuestions && cpt < 50;) {
+    for (let i = 0, texte, texteCorr, a, b, b1, a1, c, x1, x2, k, beta, delta, alpha, cpt = 0; i < this.nbQuestions && cpt < 50;) {
       if (listeTypeDeQuestions[i] === 'solutionsEntieres') {
         // On définit les paramètres utiles à l'exo :
         x1 = randint(-5, 5, [0])
         x2 = randint(-5, 5, [0, x1]) // les deux racines non-nulles et distinctes du polynôme
         a = randint(-4, 4, [0]) // Coefficient a
         b = 0
-        while (b === 0) {
+        c = 0
+        while (b === 0 || c === 0) {
           a = randint(-4, 4, [0])
           b = -a * x1 - a * x2 // b non-nul définit algébriquement
-        }
-        c = 0
-        while (c === 0) {
-          a = randint(-4, 4, [0])
           c = a * x1 * x2 // c non-nul définit algébriquement
         }
 
-        // alpha = -b / (2 * a)
-        beta = -(b * b - 4 * a * c) / (4 * a)
+        alpha = fraction(b, 2 * a)
+        beta = fraction(-(b * b - 4 * a * c), 4 * a)
         delta = b * b - 4 * a * c
         texte = `Résoudre dans $\\mathbb{R}$ l'équation $${rienSi1(a)}x^2${ecritureAlgebriqueSauf1(b)}x${ecritureAlgebrique(c)}=0$ sans utiliser le discriminant.`
         texte += 'en utilisant la forme canoique du polynôme.'
@@ -58,40 +56,21 @@ export default function Resolutionavecformecanonique () {
         texteCorr += '<br>La consigne nous amène à commencer par écrire le polynôme du second degré sous forme canonique, <br>c\'est à dire sous la forme :  $a(x-\\alpha)^2+\\beta$,'
         texteCorr += `<br>Dans le polynôme de l'énoncé : $${rienSi1(a)}x^2${ecritureAlgebriqueSauf1(b)}x${ecritureAlgebrique(c)}$`
         if (a !== 1) { // On simplifie par a si a<>1
-          texteCorr = `<br>On commence par diviser les deux membres de l'égalité par $${a}$.`
-          texteCorr += '<br>$x^ '
-          if (a * b > 0) { // On teste le signe de la fraction b/a
-            texteCorr += '+' // pour ajouter ou non un signe + si besoin
-          }
-          texteCorr += `${texFractionReduite(b, a)}x`
-          if (c * b > 0) { // On teste le signe de la fraction c/a
-            texteCorr += '+' // pour ajouter ou non un signe + si besoin
-          }
-          texteCorr += `${texFractionReduite(c, a)}=0$`
+          texteCorr += `<br>On commence par diviser les deux membres de l'égalité par $${a}$.`
+          texteCorr += '<br>$x^2 '
+
           // fin du test si a<>1
         }
         // ******************************************************************************************************************
         // ******************      Reconnaissance de l'identité remarquable :    ********************************************
         // ******************************************************************************************************************
         texteCorr += ', on reconnaît le début d\'une identité remarquable :'
-        texteCorr += '<br>$\\left(x'
-        if (a * b > 0) { // On teste le signe de la fraction b/a pour ajouter ou non un signe +
-          texteCorr += '+'
-        }
-        texteCorr += ` ${texFractionSigne(b, 2 * a)}\\right)^2=x^2`
-        if (a * b > 0) { // On teste le signe de la fraction b/a pour ajouter ou non un signe +
-          texteCorr += '+'
-        } else texteCorr += '-'
-        texteCorr += `2\\times ${texFractionSigne(abs(b), 2 * abs(a))}\\times x +\\left(${texFractionSigne(b * b, 2 * a * a)}\\right)^2$`
-        if (pgcd(b, a) !== 1) { // On teste pour savoir si b/a est irreductible
-          texteCorr += `<br>$\\big(x+${texFractionReduite(b, 2 * a)}\\big)^2=x^2+${texFractionReduite(b, a)}\\times x +${texFractionReduite(b * b, 4 * a * a)}$`
-        } else {
-          texteCorr += `<br>$\\left(x${texFractionSigne(b, 2 * a)}\\right)^2=x^2${texFractionSigne(b, a)}\\times x +\\left(${texFractionSigne(b1, 2 * a1)}\\right)^2$`
-        }
-
-        if (pgcd(b, 2 * a) !== 1) { // On teste pour savoir si b/2a est irreductible
-          texteCorr += `<br>$\\big(x${texFractionReduite(b, 2 * a)}\\big)^2=x^2${texFractionReduite(b, a)}\\times x +${texFractionReduite(b * b, 4 * a * a)}$`
-        }
+        texteCorr += `<br>$\\left(x ${alpha.simplifie().ecritureAlgebriqueFraction}\\right)^2=x^2`
+        texteCorr += `${alpha.signe === 1 ? '+' : '-'}2\\times ${alpha.valeurAbsolue().simplifie().texFraction}\\times x +${alpha.simplifie().den === 1 ? alpha.simplifie().valeurAbsolue().texFraction : '\\left(' + alpha.simplifie().valeurAbsolue().texFraction + '\\right)'}^2$`
+        // 2èmeligne correction
+        texteCorr += `<br>$\\left(x ${alpha.simplifie().ecritureAlgebriqueFraction}\\right)^2=x^2`
+        texteCorr += `${alpha.signe === 1 ? '+' : '-'}${Math.abs(alpha.num * 2) === Math.abs(alpha.den) ? '' : alpha.multiplieEntier(2).valeurAbsolue().simplifie().texFraction}x+${alpha.produitFraction(alpha).simplifie().texFraction} $`
+        // 3èmeligne correction
         texteCorr += '<br>On en déduit que :'
         texteCorr += `<br>$x^2 ${texFractionSigne(b, a)} \\times x =\\big(x+${texFractionReduite(b, 2 * a)}\\big)^2-${texFractionReduite(b * b, 4 * a * a)}$`
         texteCorr += '<br>On peut donc dire que l\'équation de départ est équivalente à :'
@@ -108,6 +87,8 @@ export default function Resolutionavecformecanonique () {
           texteCorr += `et $b =\\sqrt{${texFractionReduite(delta, 4 * a * a)}}$`
           texteCorr += '<br>L\'équation à résoudre est équivalente à :'
           texteCorr += `<br> $\\left(x+${texFractionReduite(b, 2 * a)} -\\sqrt{${texFractionReduite(delta, 4 * a * a)}}\\right)\\left(x+${texFractionReduite(b, 2 * a)} +\\sqrt{${texFractionReduite(delta, 4 * a * a)}}\\right)=0$`
+          texteCorr += '<br> On applique lepropriété du produit nul.'
+          texteCorr += `<br> Soit $x+${texFractionReduite(b, 2 * a)} -\\sqrt{${texFractionReduite(delta, 4 * a * a)}}=0$ , soit $x+${texFractionReduite(b, 2 * a)} +\\sqrt{${texFractionReduite(delta, 4 * a * a)}}=0$`
         }
       }
 
