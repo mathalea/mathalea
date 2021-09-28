5466eb8d8551ec3c7f87ee0d8b32a62e2d1222c2
EricElter <eric.elter@ac-rouen.fr>
2021-09-28 17:43:48 +0200

    6G10-3 AMC QCMmono
    1er jet : encore en phase d'optimisation

--- src/js/exercices/6e/6G10-3.js
+++ src/js/exercices/6e/6G10-3.js
@@ -1,14 +1,17 @@
 import Exercice from '../Exercice.js'
-import { combinaisonListes, lettreDepuisChiffre, listeQuestionsToContenu, randint, shuffle } from '../../modules/outils.js'
+import { combinaisonListes, enleveElement, lettreDepuisChiffre, listeQuestionsToContenu, randint, shuffle } from '../../modules/outils.js'
 import { demiDroite, droite, labelPoint, mathalea2d, point, segmentAvecExtremites } from '../../modules/2d.js'
 import { context } from '../../modules/context.js'
+import { propositionsQcm } from '../../modules/gestionInteractif.js'
 export const titre = 'Choisir la bonne figure'
+export const amcReady = true
+export const amcType = 'qcmMono' // type de question AMC
 export const interactifReady = true
-export const interactifType = 'cliqueFigure'
+export const interactifType = ['qcm', 'cliqueFigure']
 
 /**
- * Plusieurs éléments sont proposés, il faut cliquer sur le bon
- * @author ANGOT Rémi
+ * Plusieurs éléments sont proposés, il faut choisir le bon (par clic si interactif, par case à cocher par AMC)
+ * @author ANGOT Rémi (Ajout AMC par Eric Elter)
  * Référence
 */
 export default function cliqueFigure () {
@@ -18,7 +21,9 @@ export default function cliqueFigure () {
   this.nbColsCorr = 1
 
   this.nouvelleVersion = function () {
-    this.consigne = (this.interactif) ? 'Cliquer sur la bonne figure.' : 'Entourer la bonne figure.'
+    this.interactifType = parseInt(this.sup2) === 2 ? 'cliqueFigure' : 'qcm'
+    this.autoCorrection = []
+    this.consigne = (this.interactif) ? 'Cliquer sur la bonne figure.' : 'Entourer la bonne figure.' /// Penser ici à AMC aussi.
     this.listeQuestions = [] // Liste de questions
     this.listeCorrections = [] // Liste de questions corrigées
     this.figures = [] // Liste des objets de toutes les figures sur lesquelles on pourra cliquer avec leur id et un booléen de réponse
@@ -40,35 +45,72 @@ export default function cliqueFigure () {
       const figDemiDroite = mathalea2d({ xmin: -2, xmax: 6, ymin: -2, style: '', scale: 0.4, id: `figure2Ex${this.numeroExercice}Q${i}` }, labels, segmentAvecExtremites(A, B), demiDroite(A, B))
       const figDemiDroite2 = mathalea2d({ xmin: -2, xmax: 6, ymin: -2, style: '', scale: 0.4, id: `figure3Ex${this.numeroExercice}Q${i}` }, labels, segmentAvecExtremites(A, B), demiDroite(B, A))
       let figCorr
+      let figCorrecteQCM
+      const figIncorrect = [figSegment, figDroite, figDemiDroite, figDemiDroite2]
       switch (typesDeQuestions[i]) {
         case 'segment':
           texte = `Le segment d'extrémités $${A.nom}$ et $${B.nom}$.`
           this.figures[i][0].solution = true
           figCorr = {}
+          figCorrecteQCM = figSegment
           break
         case 'droite':
           texte = `La droite passant par les points $${A.nom}$ et $${B.nom}$.`
           this.figures[i][1].solution = true
           figCorr = droite(A, B)
+          figCorrecteQCM = figDroite
           break
         case 'demidroite':
           texte = `La demi-droite d'origine $${A.nom}$ passant par $${B.nom}$.`
           this.figures[i][2].solution = true
           figCorr = demiDroite(A, B)
+          figCorrecteQCM = figDemiDroite
           break
         case 'demidroite2':
           texte = `La demi-droite d'origine $${B.nom}$ passant par $${A.nom}$.`
           this.figures[i][3].solution = true
           figCorr = demiDroite(B, A)
+          figCorrecteQCM = figDemiDroite2
           break
       }
-
-      texte += '<br>'
-      texteCorr = texte + mathalea2d({ xmin: -4, xmax: 6, ymin: -2, style: '', scale: 0.4, id: `figure3Ex${this.numeroExercice}Q${i}` }, labels, segmentAvecExtremites(A, B), figCorr)
-      const figures = shuffle([figSegment, figDroite, figDemiDroite, figDemiDroite2])
-      texte += figures.join('')
-      if (this.interactif && context.isHtml) {
-        texte += `<span id="resultatCheckEx${this.numeroExercice}Q${i}"></span>`
+      enleveElement(figIncorrect, figCorrecteQCM)
+      if (this.interactif && this.interactifType === 'qcm') {
+        this.autoCorrection[i] = {}
+        this.autoCorrection[i].propositions = [
+          {
+            texte: figCorrecteQCM,
+            statut: true,
+            feedback: 'Correct !'
+          },
+          {
+            texte: figIncorrect[0],
+            statut: false,
+            feedback: 'xxxxxxxxx.'
+          },
+          {
+            texte: figIncorrect[1],
+            statut: false,
+            feedback: 'xxxxxxxxx.'
+          },
+          {
+            texte: figIncorrect[2],
+            statut: false,
+            feedback: 'xxxxxxxxx.'
+          }
+        ]
+        this.autoCorrection[i].options = {
+          ordered: false,
+          lastChoice: 4
+        }
+        texte += propositionsQcm(this, i).texte
+      } else {
+        texte += '<br>'
+        texteCorr = texte + mathalea2d({ xmin: -4, xmax: 6, ymin: -2, style: '', scale: 0.4, id: `figure3Ex${this.numeroExercice}Q${i}` }, labels, segmentAvecExtremites(A, B), figCorr)
+        const figures = shuffle([figSegment, figDroite, figDemiDroite, figDemiDroite2])
+        texte += figures.join('')
+        if (this.interactif && context.isHtml) {
+          texte += `<span id="resultatCheckEx${this.numeroExercice}Q${i}"></span>`
+        }
       }
 
       if (this.listeQuestions.indexOf(texte) === -1) {
@@ -81,5 +123,6 @@ export default function cliqueFigure () {
     }
     listeQuestionsToContenu(this)
   }
+  if (context.isHtml && !context.isDiaporama) this.besoinFormulaire2Numerique = ['Exercice interactif', 2, '1 : QCM\n2 : Cliquer sur figure'] // Texte, tooltip
   // this.besoinFormulaireNumerique = ['Niveau de difficulté', 2,'1 : Facile\n2 : Difficile'];
 }
