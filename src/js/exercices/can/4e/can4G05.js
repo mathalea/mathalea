1e341b209872d4b483fe332cc92ae264f422a19c
moragi <gilles.mora@ac-rouen.fr>
2021-10-06 19:05:45 +0200

    modif can4ième (7 questions)

--- src/js/exercices/can/4e/can4G05.js
+++ src/js/exercices/can/4e/can4G05.js
@@ -1,5 +1,5 @@
 import Exercice from '../../Exercice.js'
-import { randint, choice } from '../../../modules/outils.js'
+import { randint, choice, texteEnCouleur } from '../../../modules/outils.js'
 export const titre = 'Calcul de la diagonale d’un carré'
 export const interactifReady = true
 export const interactifType = 'mathLive'
@@ -25,22 +25,60 @@ export default function DiagonaleCarre () {
       case 'a':
         a = randint(1, 10)//
         this.question = `La longueur de la diagonale d'un carré de côté $${a}$ est :<br>
-    (donner le résultat sous la forme $\\sqrt{a}$)`
+    (donner la valeur exacte)`
 
-        this.correction = ` En utilisant le théorème de Pythagore dans un carré de côté $${a}$ et de diagonale $d$, on a <br>
-    $${a}^2+${a}^2=d^2$ soit $d^2=2\\times ${a}^2$, d'où $d=\\sqrt{2\\times ${a}^2}=\\sqrt{${2 * a ** 2}}$
-   <br>`
+        this.correction = ` En utilisant le théorème de Pythagore dans un carré de côté $${a}$ et de diagonale $d$, on a :<br>
+    $\\begin{aligned}
+    ${a}^2+${a}^2&=d^2\\\\
+    d^2&=2\\times ${a}^2\\\\
+    d&=\\sqrt{2\\times ${a}^2}\\\\
+    d&=\\sqrt{${2 * a ** 2}}
+       \\end{aligned}$
+   `
+        this.correction += texteEnCouleur(`<br> Mentalement : <br>
+   On calcule le double du carré du côté du carré, 
+   soit $2\\times ${a}^2=2\\times ${a ** 2}=${2 * a ** 2}$, puis on en prend la racine carrée.    `)
         this.reponse = `\\sqrt{${2 * a ** 2}}`
         break
       case 'b':
-        a = randint(2, 48, [4, 9, 16, 18, 8, 25, 36])//
+        a = randint(2, 48, [4, 9, 16, 25, 36])//
         this.question = `La longueur de la diagonale d'un carré de côté $\\sqrt{${a}}$ est :<br>
-        (donner le résultat sous la forme $\\sqrt{a}$)`
+        (donner la valeur exacte)`
 
-        this.correction = ` En utilisant le théorème de Pythagore dans un carré de côté $\\sqrt{${a}}$ 
-        et de diagonale $d$, on a <br>
-        $\\sqrt{${a}}^2+\\sqrt{${a}}^2=d^2$ soit $d^2=${a}+${a}=${2 * a}$, d'où $d=\\sqrt{${2 * a}}$
-       <br>`
+        if (2 * a === 36 || 2 * a === 16 || 2 * a === 64) {
+          this.correction = ` En utilisant le théorème de Pythagore dans un carré de côté $c=\\sqrt{${a}}$ 
+       et de diagonale $d$, on a :<br>
+
+       $\\begin{aligned}
+       c^2+c^2&=d^2\\\\
+       \\sqrt{${a}}^2+\\sqrt{${a}}^2&=d^2\\\\
+       d^2&=${a}+${a}\\\\
+       d^2&=${2 * a}\\\\
+       d&=\\sqrt{${2 * a}}\\\\
+       d&=${Math.sqrt(2 * a)}
+       \\end{aligned}$
+      
+      `
+          this.correction += texteEnCouleur(`<br> Mentalement : <br>
+       On calcule le double du carré du côté du carré, soit 
+       $2\\times (\\sqrt{${a}})^2=2\\times ${a}=${2 * a}$, puis on en prend la racine carrée, soit $${Math.sqrt(2 * a)}$.    `)
+        } else {
+          this.correction = ` En utilisant le théorème de Pythagore dans un carré de côté $c=\\sqrt{${a}}$ 
+       et de diagonale $d$, on a :<br>
+
+       $\\begin{aligned}
+       c^2+c^2&=d^2\\\\
+       \\sqrt{${a}}^2+\\sqrt{${a}}^2&=d^2\\\\
+       d^2&=${a}+${a}\\\\
+       d^2&=${2 * a}\\\\
+       d&=\\sqrt{${2 * a}}
+       \\end{aligned}$
+            `
+          this.correction += texteEnCouleur(`<br> Mentalement : <br>
+       On calcule le double du carré du côté du carré, 
+       soit $2\\times (\\sqrt{${a}})^2=2\\times ${a}=${2 * a}$, puis on en prend la racine carrée.    `)
+        }
+
         this.reponse = [`\\sqrt{${2 * a}}`, `${Math.sqrt(2 * a)}`]
         break
     }
--- src/js/exercices/can/4e/can4P01.js
+++ src/js/exercices/can/4e/can4P01.js
@@ -1,4 +1,4 @@
-import { calcul, choice, randint } from '../../../modules/outils'
+import { calcul, choice, randint, texteEnCouleur } from '../../../modules/outils'
 import Exercice from '../../Exercice'
 export const titre = 'Problèmes de robinets'
 export const interactifReady = true
@@ -14,14 +14,22 @@ export const amcType = 'AMCNum'
 export default function ProblemesDeRobinets () {
   Exercice.call(this)
   this.typeExercice = 'simple'
+  this.formatChampTexte = 'largeur25 inline'
   this.nbQuestions = 1
-    this.nouvelleVersion = function () {
+  this.nouvelleVersion = function () {
     const a = choice([2, 3, 4, 5, 6, 10]) // nombre de secondes pour remplir un litre
     const b = calcul(60 / a) // nombres de litres/min
     const c = randint(2, b - 1) % 10 // volume du seau à remplir
     this.reponse = calcul(c * a)
     this.question = `Le débit d'eau d'un robinet est de $${b}$ L/min. Combien de secondes faut-il pour remplir un seau de $${c}$ L ?`
-    this.correction = `Comme le débit est de $${b}$ L/min, il faut $\\dfrac{60}{${b}}=${a}$ s pour remplir un litre.<br>`
-    this.correction += `Il faut donc $${a}\\times ${c}=${a * c}$ s pour remplir le seau de ${c} L.`
+    this.correction = `Il faut $${a * c}$ s pour remplir le seau de $${c}$ L.`
+    this.correction += texteEnCouleur(`<br> Mentalement : <br>
+    On commence par déterminer le temps en seconde (puisque dans la question,
+       il est demandé un temps en seconde) qu'il faut pour remplir $1$ L.<br>
+    Comme le débit est de  $${b}$ L 
+    pour une minute soit $60$ secondes, on divise $60$ par $${b}$ pour obtenir 
+    ce temps :  $\\dfrac{60}{${b}}=${a}$ s.<br>
+    Puisqu'il faut $${a}$ s pour remplir un litre, il en faut $${c}$ fois plus pour remplir un seau de 
+    $${c}$ L, soit $${a}\\times ${c}=${a * c}$ s.`)
   }
 }
