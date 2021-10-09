9133bd491b4af4de52e70407f1f61f3cfbc25c19
EricElter <eric.elter@ac-rouen.fr>
2021-10-09 08:49:39 +0200

    6C14 : Correction des espaces. Exercice non encore finalisé.

--- src/js/exercices/6e/6C14.js
+++ src/js/exercices/6e/6C14.js
@@ -103,20 +103,21 @@ export default function Questions6G14 () {
       const NbArticles5 = choice(rangeMinMax(3, 9), [NbArticles, NbArticles2, NbArticles3, NbArticles4]) + NbArticles4
       const DixOuCent = choice([10, 100])
       const Nbpartage = choice(rangeMinMax(2, 8))
-      texte = `${quidame} repère un article dans un magazine de publicité à ${texNombre3(PrixUnitaire)}&nbsp;€. <br><br>`
+      texte = `${quidame} repère un article dans un magazine de publicité à ${texNombre3(PrixUnitaire)}${sp()}€. <br><br>`
+      texteCorr = ''
       // listeQuestions[0] = 1 // A SUPPRIMER
       for (let kk = 0; kk < QuestionsDisponibles.length; kk++) {
         switch (QuestionsDisponibles[kk]) {
           case 1:
             if (this.interactif && !context.isAmc) {
-              texte = `${quidame} repère un article dans un magazine de publicité à $${texNombre(PrixUnitaire)}$ toto&nbsp;€. <br>`
+              texte = `${quidame} repère un article dans un magazine de publicité à $${texNombre(PrixUnitaire)}$ toto${sp()}€. <br>`
               texte += 'Il veut, en utilisant toutes ses fleurs, réaliser un maximum de bouquets '
               texte += 'contenant tous le même nombre d’iris et le même nombre de roses. <br>'
               texte += 'Donner le nombre maximal de bouquets que le fleuriste peut réaliser '
               texte += 'et la composition du bouquet.<br><br>'
               texte += numAlpha(kk + 1) + `Nombre maximal de bouquets :${sp(20)}`
               texte += ajouteChampTexteMathLive(this, 3 * i, 'inline largeur25') + '<br><br>'
-              texteCorr = numAlpha(0)
+              texteCorr += numAlpha(0)
               texteCorr += `- Les diviseurs de ${var1 * objet} sont : ${listeDesDiviseurs(var1 * objet).join(', ')}.<br>`
               texteCorr += `${sp(2)}- Les diviseurs de ${var2 * objet} sont : ${listeDesDiviseurs(var2 * objet).join(', ')}.<br>`
               texteCorr += `${objet} est le plus grand nombre qui divise à la fois ${var1 * objet} et ${var2 * objet}.<br>`
@@ -135,72 +136,72 @@ export default function Questions6G14 () {
               texteCorr += 'Le nombre de roses dans chaque bouquet est :' + texteEnCouleurEtGras(` ${var2}`) + '.<br><br>'
               setReponse(this, 3 * i + 2, var2)
             } else {
-              texte += numAlpha(kk) + `Quel serait le prix de ${DixOuCent} articles à ${texNombre3(PrixUnitaire)}&nbsp;€ l'unité&nbsp;?<br><br>`
-              texteCorr = numAlpha(kk)
+              texte += numAlpha(kk) + `Quel serait le prix de ${DixOuCent} articles à ${texNombre3(PrixUnitaire)}${sp()}€ l'unité${sp()}?<br><br>`
+              texteCorr += numAlpha(kk)
               texteCorr += ` $${DixOuCent} \\times ${texNombre3(PrixUnitaire)} = ${texNombre3(DixOuCent * PrixUnitaire)}$<br>`
-              texteCorr += `Le prix de ${DixOuCent} articles serait de ${texNombre3(DixOuCent * PrixUnitaire)}&nbsp;€.<br><br>`
+              texteCorr += `Le prix de ${DixOuCent} articles serait de ${texNombre3(DixOuCent * PrixUnitaire)}${sp()}€.<br><br>`
             }
             break
           case 2:
-            texte += numAlpha(kk) + `Quel serait le prix de ${NbArticles} articles à ${texNombre3(PrixUnitaire)}&nbsp;€ l'unité&nbsp;?<br><br>`
+            texte += numAlpha(kk) + `Quel serait le prix de ${NbArticles} articles à ${texNombre3(PrixUnitaire)}${sp()}€ l'unité${sp()}?<br><br>`
             texteCorr += numAlpha(kk)
             texteCorr += ` $${NbArticles} \\times ${texNombre3(PrixUnitaire)} = ${texNombre3(arrondi(NbArticles * PrixUnitaire))}$<br>`
-            texteCorr += `Le prix de ${NbArticles} articles serait de ${texNombre3(arrondi(NbArticles * PrixUnitaire))}&nbsp;€.<br><br>`
+            texteCorr += `Le prix de ${NbArticles} articles serait de ${texNombre3(arrondi(NbArticles * PrixUnitaire))}${sp()}€.<br><br>`
             break
           case 3:
-            texte += numAlpha(kk) + `Si ${quidame} achetait un seul article à ${texNombre3(PrixUnitaire)}&nbsp;€ ainsi que d'autres articles pour ${texNombre(AutrePrix)}&nbsp;€, quel serait le prix final&nbsp;?<br><br>`
+            texte += numAlpha(kk) + `Si ${quidame} achetait un seul article à ${texNombre3(PrixUnitaire)}${sp()}€ ainsi que d'autres articles pour ${texNombre(AutrePrix)}${sp()}€, quel serait le prix final${sp()}?<br><br>`
             texteCorr += numAlpha(kk)
             texteCorr += ` $${PrixUnitaire} + ${texNombre3(AutrePrix)} = ${texNombre3(PrixUnitaire + AutrePrix)}$<br>`
-            texteCorr += `Si ${quidame} achetait un seul article à ${texNombre3(PrixUnitaire)}&nbsp;€ ainsi que d'autres articles pour ${texNombre(AutrePrix)}&nbsp;€, `
-            texteCorr += `le prix final serait de ${texNombre3(PrixUnitaire + AutrePrix)}&nbsp;€.<br><br>`
+            texteCorr += `Si ${quidame} achetait un seul article à ${texNombre3(PrixUnitaire)}${sp()}€ ainsi que d'autres articles pour ${texNombre(AutrePrix)}${sp()}€, `
+            texteCorr += `le prix final serait de ${texNombre3(PrixUnitaire + AutrePrix)}${sp()}€.<br><br>`
             break
           case 4:
-            texte += numAlpha(kk) + `${quidame} dispose d'un bon de réduction de ${texNombre3(PrixReduction)}&nbsp;€. Si ${quidame} achetait un seul article à ${texNombre(PrixUnitaire)}&nbsp;€, quel somme d'argent paierait ${quidame} au final&nbsp;?<br><br>`
+            texte += numAlpha(kk) + `${quidame} dispose d'un bon de réduction de ${texNombre3(PrixReduction)}${sp()}€. Si ${quidame} achetait un seul article à ${texNombre(PrixUnitaire)}${sp()}€, quel somme d'argent paierait ${quidame} au final${sp()}?<br><br>`
             texteCorr += numAlpha(kk)
             texteCorr += ` $${PrixUnitaire} - ${texNombre3(PrixReduction)} = ${texNombre3(PrixUnitaire - PrixReduction)}$<br>`
-            texteCorr += `Grâce à son bon de réduction, ${quidame} ne paierait que ${texNombre3(PrixUnitaire - PrixReduction)}&nbsp;€.<br><br>`
+            texteCorr += `Grâce à son bon de réduction, ${quidame} ne paierait que ${texNombre3(PrixUnitaire - PrixReduction)}${sp()}€.<br><br>`
             break
           case 5:
-            texte += numAlpha(kk) + `Si ${quidame} achetait ${NbArticles2} articles à ${PrixUnitaire}&nbsp;€ l'unité et son ${Personnage1} en achetait ${NbArticles3} de plus, quelle somme paierait-ils à eux deux&nbsp;?<br><br>`
+            texte += numAlpha(kk) + `Si ${quidame} achetait ${NbArticles2} articles à ${PrixUnitaire}${sp()}€ l'unité et son ${Personnage1} en achetait ${NbArticles3} de plus, quelle somme paierait-ils à eux deux${sp()}?<br><br>`
             texteCorr += numAlpha(kk + 1)
             texteCorr += ` $${NbArticles2} + ${NbArticles3} = ${NbArticles2 + NbArticles3}$<br>`
             texteCorr += `${quidame} et son ${Personnage1} achèteraient ${NbArticles2 + NbArticles3} articles.<br>`
             texteCorr += `$${NbArticles2 + NbArticles3} \\times ${texNombre3(PrixUnitaire)} = ${texNombre3(arrondi((NbArticles2 + NbArticles3) * PrixUnitaire))}$<br>`
-            texteCorr += `Si ${quidame} et son ${Personnage1} achetaient ${NbArticles2 + NbArticles3} articles à ${texNombre3(PrixUnitaire)}&nbsp;€ l'unité, `
-            texteCorr += `le prix final serait de ${texNombre3(arrondi((NbArticles2 + NbArticles3) * PrixUnitaire))}&nbsp;€.<br><br>`
+            texteCorr += `Si ${quidame} et son ${Personnage1} achetaient ${NbArticles2 + NbArticles3} articles à ${texNombre3(PrixUnitaire)}${sp()}€ l'unité, `
+            texteCorr += `le prix final serait de ${texNombre3(arrondi((NbArticles2 + NbArticles3) * PrixUnitaire))}${sp()}€.<br><br>`
             break
           case 6:
-            texte += numAlpha(kk) + `Si ${quidame} achetait ${NbArticles5} articles à ${PrixUnitaire}&nbsp;€ l'unité mais que sa ${Personnage2} lui propose de lui en rembourser ${NbArticles4}, quelle somme ${quidame} dépenserait-elle&nbsp;?<br><br>`
+            texte += numAlpha(kk) + `Si ${quidame} achetait ${NbArticles5} articles à ${PrixUnitaire}${sp()}€ l'unité mais que sa ${Personnage2} lui propose de lui en rembourser ${NbArticles4}, quelle somme ${quidame} dépenserait-elle${sp()}?<br><br>`
             texteCorr += numAlpha(kk)
             texteCorr += `$${NbArticles5} - ${NbArticles4} = ${NbArticles5 - NbArticles4}$<br>`
             texteCorr += `${quidame} ne payerait que ${NbArticles5 - NbArticles4} articles.<br>`
             texteCorr += `$${NbArticles5 - NbArticles4} \\times ${texNombre3(PrixUnitaire)} = ${texNombre3(arrondi((NbArticles5 - NbArticles4) * PrixUnitaire))}$<br>`
-            texteCorr += `Si ${quidame} achetait ${NbArticles5} articles à ${PrixUnitaire}&nbsp;€ l'unité mais que sa ${Personnage2} lui propose de lui en rembourser ${NbArticles4}, `
-            texteCorr += `${quidame} dépenserait ${texNombre3(arrondi((NbArticles5 - NbArticles4) * PrixUnitaire))}&nbsp;€.<br><br>`
+            texteCorr += `Si ${quidame} achetait ${NbArticles5} articles à ${PrixUnitaire}${sp()}€ l'unité mais que sa ${Personnage2} lui propose de lui en rembourser ${NbArticles4}, `
+            texteCorr += `${quidame} dépenserait ${texNombre3(arrondi((NbArticles5 - NbArticles4) * PrixUnitaire))}${sp()}€.<br><br>`
             break
           case 7 :
-            texte += numAlpha(kk) + `Si ${quidame} décidait d'acheter cet article à ${PrixUnitaire}&nbsp;€ avec 9 amis, quelle somme équitable minimale devraient-ils, chacun, payer&nbsp;?<br><br>`
+            texte += numAlpha(kk) + `Si ${quidame} décidait d'acheter cet article à ${PrixUnitaire}${sp()}€ avec 9 amis, quelle somme équitable minimale devraient-ils, chacun, payer${sp()}?<br><br>`
             texteCorr += numAlpha(kk)
             texteCorr += '$1 + 9 = 10$<br>'
             texteCorr += 'Le partage se ferait entre 10 personnes.<br>'
             if (this.sup3) {
               texteCorr += `$${PrixUnitaire} \\div 10 = ${texNombre3(arrondi(PrixUnitaire / 10, 3))}$<br>`
-              texteCorr += `Si ${quidame} partageait cet article avec 9 amis, chacun paierait au minimum ${texNombre3(arrondi(PrixUnitaire / 10, 3))}&nbsp;€.<br><br>`
+              texteCorr += `Si ${quidame} partageait cet article avec 9 amis, chacun paierait au minimum ${texNombre3(arrondi(PrixUnitaire / 10, 3))}${sp()}€.<br><br>`
             } else {
               texteCorr += `$${PrixUnitaire} \\div ${Nbpartage + 1} \\approx ${texNombre3(arrondi(PrixUnitaire / (Nbpartage + 1), 3))}$ et $${texNombre3(troncature(arrondi(PrixUnitaire / (Nbpartage + 1), 3), 2))} < ${texNombre(arrondi(PrixUnitaire / (Nbpartage + 1), 3))} < ${texNombre(troncature(arrondi(PrixUnitaire / (Nbpartage + 1), 3) + 0.01, 2))}$<br>`
-              texteCorr += `Si ${quidame} partageait cet article avec ${Nbpartage} camarades, chacun paierait au minimum ${texNombre3(troncature(arrondi(PrixUnitaire / (Nbpartage + 1), 3) + 0.01, 2))}&nbsp;€.<br><br>`
+              texteCorr += `Si ${quidame} partageait cet article avec ${Nbpartage} camarades, chacun paierait au minimum ${texNombre3(troncature(arrondi(PrixUnitaire / (Nbpartage + 1), 3) + 0.01, 2))}${sp()}€.<br><br>`
             } break
           case 8 :
-            texte += numAlpha(kk) + `Si ${quidame} décidait d'acheter cet article à ${PrixUnitaire}&nbsp;€ avec ${Nbpartage} camarades, quelle somme équitable minimale devraient-ils, chacun, payer&nbsp;?<br><br>`
+            texte += numAlpha(kk) + `Si ${quidame} décidait d'acheter cet article à ${PrixUnitaire}${sp()}€ avec ${Nbpartage} camarades, quelle somme équitable minimale devraient-ils, chacun, payer${sp()}?<br><br>`
             texteCorr += numAlpha(kk)
             texteCorr += `$1 + ${Nbpartage} = ${Nbpartage + 1}$<br>`
             texteCorr += `Le partage se ferait entre ${Nbpartage + 1} personnes.<br>`
             if (estentier(arrondi(PrixUnitaire * 100, 0) / (Nbpartage + 1))) {
               texteCorr += `$${PrixUnitaire} \\div ${Nbpartage + 1} = ${texNombre3(arrondi(PrixUnitaire / (Nbpartage + 1), 3))}$<br>`
-              texteCorr += `Si ${quidame} partageait cet article avec ${Nbpartage} camarades, chacun paierait au minimum ${texNombre3(arrondi(PrixUnitaire / (Nbpartage + 1), 3))}&nbsp;€.<br><br>`
+              texteCorr += `Si ${quidame} partageait cet article avec ${Nbpartage} camarades, chacun paierait au minimum ${texNombre3(arrondi(PrixUnitaire / (Nbpartage + 1), 3))}${sp()}€.<br><br>`
             } else {
               texteCorr += `$${PrixUnitaire} \\div ${Nbpartage + 1} \\approx ${texNombre3(arrondi(PrixUnitaire / (Nbpartage + 1), 3))}$ et $${texNombre3(troncature(arrondi(PrixUnitaire / (Nbpartage + 1), 3), 2))} < ${texNombre(arrondi(PrixUnitaire / (Nbpartage + 1), 3))} < ${texNombre(troncature(arrondi(PrixUnitaire / (Nbpartage + 1), 3) + 0.01, 2))}$<br>`
-              texteCorr += `Si ${quidame} partageait cet article avec ${Nbpartage} camarades, chacun paierait au minimum ${texNombre3(troncature(arrondi(PrixUnitaire / (Nbpartage + 1), 3) + 0.01, 2))}&nbsp;€.<br><br>`
+              texteCorr += `Si ${quidame} partageait cet article avec ${Nbpartage} camarades, chacun paierait au minimum ${texNombre3(troncature(arrondi(PrixUnitaire / (Nbpartage + 1), 3) + 0.01, 2))}${sp()}€.<br><br>`
             }
             break
         } // fin du switch
