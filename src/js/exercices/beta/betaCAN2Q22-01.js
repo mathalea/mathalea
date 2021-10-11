ca098dd55f9907840c2ee4f75a13aae53b1b9c28
moragi <gilles.mora@ac-rouen.fr>
2021-10-11 17:18:51 +0200

    Question CAN seconde betaCAN2Q22-01

---
+++ src/js/exercices/beta/betaCAN2Q22-01.js
@@ -0,0 +1,40 @@
+import Exercice from '../Exercice.js'
+import { randint, ecritureParentheseSiNegatif, texNombrec, reduireAxPlusB } from '../../modules/outils.js'
+export const titre = 'Coordonnées d’un point sur une droite'
+export const interactifReady = true
+export const interactifType = 'mathLive'
+
+/**
+ * Modèle d'exercice très simple pour la course aux nombres
+ * @author Gilles Mora
+ * Référence
+ * Date de publication
+*/
+export default function CoordonneesPointDroite () {
+  Exercice.call(this) // Héritage de la classe Exercice()
+  this.typeExercice = 'simple' // Cette ligne est très importante pour faire faire un exercice simple !
+  this.nbQuestions = 1
+  this.formatChampTexte = 'largeur15 inline'
+  // Dans un exercice simple, ne pas mettre de this.listeQuestions = [] ni de this.consigne
+
+  this.nouvelleVersion = function () {
+    const a = randint(-10, 10, 0)
+    const b = randint(-10, 10, 0)
+    const c = randint(-10, 10, 0)
+
+    this.question = ` Donner les coordonnées du point de la droite 
+    d'équation $y=${reduireAxPlusB(a, b)}$ qui a pour abscisse $${c}$.`
+    if (a === 1) {
+      this.correction = `Puisque $${c}$ est l'abscisse de ce point, son ordonnée est donnée par :<br>
+        $y= ${c}+${ecritureParentheseSiNegatif(b)}=${a * c + b}$.<br>
+  Les coordonnées du  point sont donc : $(${c};${texNombrec(a * c + b)})$.`
+    } else {
+      this.correction = `Puisque $${c}$ est l'abscisse de ce point, son ordonnée est donnée par :<br>
+
+  $y=${a}\\times ${ecritureParentheseSiNegatif(c)}+${ecritureParentheseSiNegatif(b)}=${a * c} + ${ecritureParentheseSiNegatif(b)}=${a * c + b}$.<br>
+  Les coordonnées du  point sont donc : $(${c};${texNombrec(a * c + b)})$.`
+    }
+
+    this.reponse = `(${texNombrec(c)};${texNombrec(a * c + b)})`
+  }
+}
