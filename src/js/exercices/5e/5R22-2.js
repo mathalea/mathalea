6e95c7d05c49c45a66775b5ef9962d528503be20
Guillaume Valmont <guillaume.valmont@pm.me>
2021-10-16 16:06:12 +0400

    ESLint 5R22-2

--- src/js/exercices/5e/5R22-2.js
+++ src/js/exercices/5e/5R22-2.js
@@ -1,7 +1,5 @@
 import Exercice from '../Exercice.js'
-import { context } from '../../modules/context.js'
-import {listeQuestionsToContenu,randint,choice,ecritureNombreRelatif,ecritureAlgebrique} from '../../modules/outils.js'
-
+import { listeQuestionsToContenu, randint, choice, ecritureNombreRelatif, ecritureAlgebrique } from '../../modules/outils.js'
 
 export const titre = 'Simplifier l’écriture d’une somme de 2 relatifs et calculer'
 
@@ -12,37 +10,37 @@ export const titre = 'Simplifier l’écriture d’une somme de 2 relatifs et ca
 * @author Rémi Angot
 * 5R22-2
 */
-export default function Exercice_simplification_somme_algebrique(max = 20) {
-	Exercice.call(this); // Héritage de la classe Exercice()
-	this.sup = max;
-	this.titre = titre;
-	this.consigne = 'Simplifier puis calculer.';
-	this.spacing = 2;
+export default function ExerciceSimplificationSommeAlgebrique (max = 20) {
+  Exercice.call(this) // Héritage de la classe Exercice()
+  this.sup = max
+  this.titre = titre
+  this.consigne = 'Simplifier puis calculer.'
+  this.spacing = 2
 
-	this.nouvelleVersion = function () {
-		this.listeQuestions = []; // Liste de questions
-		this.listeCorrections = []; // Liste de questions corrigées
-		for (let i = 0, a, b, s, texte, texteCorr, cpt = 0; i < this.nbQuestions && cpt < 50;) { // On limite le nombre d'essais pour chercher des valeurs nouvelles
-			a = randint(1, this.sup) * choice([-1, 1]);
-			b = randint(1, this.sup) * choice([-1, 1]);
-			s = choice([-1, 1]); // + ou -
+  this.nouvelleVersion = function () {
+    this.listeQuestions = [] // Liste de questions
+    this.listeCorrections = [] // Liste de questions corrigées
+    for (let i = 0, a, b, s, texte, texteCorr, cpt = 0; i < this.nbQuestions && cpt < 50;) { // On limite le nombre d'essais pour chercher des valeurs nouvelles
+      a = randint(1, this.sup) * choice([-1, 1])
+      b = randint(1, this.sup) * choice([-1, 1])
+      s = choice([-1, 1]) // + ou -
 
-			if (s == 1) {
-				texte = '$ ' + ecritureNombreRelatif(a) + ' + ' + ecritureNombreRelatif(b) + ' = \\dotfill $';
-				texteCorr = '$ ' + ecritureNombreRelatif(a) + ' + ' + ecritureNombreRelatif(b) + ' = ' + a + ecritureAlgebrique(s * b) + ' = ' + (a + b) + ' $';
-			} else {
-				texte = '$ ' + ecritureNombreRelatif(a) + ' - ' + ecritureNombreRelatif(b) + ' = \\dotfill $';
-				texteCorr = '$ ' + ecritureNombreRelatif(a) + ' - ' + ecritureNombreRelatif(b) + ' = ' + a + ecritureAlgebrique(s * b) + ' = ' + (a - b) + ' $';
-			}
+      if (s === 1) {
+        texte = '$ ' + ecritureNombreRelatif(a) + ' + ' + ecritureNombreRelatif(b) + ' = \\dotfill $'
+        texteCorr = '$ ' + ecritureNombreRelatif(a) + ' + ' + ecritureNombreRelatif(b) + ' = ' + a + ecritureAlgebrique(s * b) + ' = ' + (a + b) + ' $'
+      } else {
+        texte = '$ ' + ecritureNombreRelatif(a) + ' - ' + ecritureNombreRelatif(b) + ' = \\dotfill $'
+        texteCorr = '$ ' + ecritureNombreRelatif(a) + ' - ' + ecritureNombreRelatif(b) + ' = ' + a + ecritureAlgebrique(s * b) + ' = ' + (a - b) + ' $'
+      }
 
-			if (this.listeQuestions.indexOf(texte) === -1) { // Si la question n'a jamais été posée, on en créé une autre
-				this.listeQuestions.push(texte);
-				this.listeCorrections.push(texteCorr);
-				i++;
-			}
-			cpt++;
-		}
-		listeQuestionsToContenu(this);
-	};
-	this.besoinFormulaireNumerique = ['Valeur maximale', 99999];
+      if (this.listeQuestions.indexOf(texte) === -1) { // Si la question n'a jamais été posée, on en créé une autre
+        this.listeQuestions.push(texte)
+        this.listeCorrections.push(texteCorr)
+        i++
+      }
+      cpt++
+    }
+    listeQuestionsToContenu(this)
+  }
+  this.besoinFormulaireNumerique = ['Valeur maximale', 99999]
 }
