d9ebfce6b6feb56ad893ea3dad2bde47a7942145
Jean-Claude Lhote <lhotejc@gmail.com>
2021-10-28 17:00:43 +0200

    ex: Ajout 5G11-4 & 4G10-1

---
+++ src/js/exercices/4e/4G10-1.js
@@ -0,0 +1,14 @@
+import TransformationsDuPlanEtCoordonnees from '../3e/3G10-1.js'
+export const titre = 'Trouver les coordonnée de l’image d’un point par une translation'
+export { interactifReady, interactifType, amcReady, amcType } from '../3e/3G10-1.js'
+export const dateDePublication = '28/10/2021'
+/**
+ * Transformations : symétrie centrale et repérage
+ * @author Jean-Claude Lhote
+ * référence 4G10-1 réglage de 3G10-1
+ */
+export default function TransformationsDuPlanEtCoordonnees4e () {
+  TransformationsDuPlanEtCoordonnees.call(this)
+  this.sup = 3
+  this.sup2 = true
+}
---
+++ src/js/exercices/5e/5G11-4.js
@@ -0,0 +1,14 @@
+import TransformationsDuPlanEtCoordonnees from '../3e/3G10-1.js'
+export const titre = 'Trouver les coordonnée de l’image d’un point par une symétrie centrale'
+export { interactifReady, interactifType, amcReady, amcType } from '../3e/3G10-1.js'
+export const dateDePublication = '28/10/2021'
+/**
+ * Transformations : symétrie centrale et repérage
+ * @author Jean-Claude Lhote
+ * référence 5G11-4 réglage de 3G10-1
+ */
+export default function TransformationsDuPlanEtCoordonnees5e () {
+  TransformationsDuPlanEtCoordonnees.call(this)
+  this.sup = 2
+  this.sup2 = true
+}
