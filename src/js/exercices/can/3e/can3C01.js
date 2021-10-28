db106ccb08f38d65e180ca67993f224ffbaa0a6a
Jean-Claude Lhote <lhotejc@gmail.com>
2021-10-28 21:41:29 +0200

    fix: can3C01 est corrigé et exportable AMC
    Il manquait le quintuple et le cinquième prévu dans le code.

--- src/js/exercices/can/3e/can3C01.js
+++ src/js/exercices/can/3e/can3C01.js
@@ -1,4 +1,6 @@
-import { choice, miseEnEvidence, randint } from '../../../modules/outils'
+import { context } from '../../../modules/context'
+import { ajouteChampTexteMathLive, setReponse } from '../../../modules/gestionInteractif'
+import { choice, listeQuestionsToContenu, miseEnEvidence, randint } from '../../../modules/outils'
 import Exercice from '../../Exercice'
 export const titre = 'Puissances de nombre entier'
 export const interactifReady = true
@@ -13,43 +15,79 @@ export const amcType = 'AMCNum'
  */
 export default function CalculPuissanceSimple () {
   Exercice.call(this)
-  this.typeExercice = 'simple'
   this.nbQuestions = 1
   this.formatChampTexte = 'largeur15 inline'
   this.nouvelleVersion = function () {
-    const a = choice([2, 3, 4, 5])
-    const b = randint(20, 50)
-    const c = [['Le double', 'La moitié'], ['Le triple', 'Le tiers'], ['Le quadruple', 'Le quart']]
-    switch (choice(['a', 'b', 'c', 'd'])) { // 'b', 'c'
-      case 'a':
-        this.question = `${c[a - 2][0]} de  $${a}^{${b}}$ est égal à : `
-        this.reponse = [`${a}^{${b + 1}}`]
-        this.correction = `${c[a - 2][0]} de $${a}^{${b}}$ se calcule  par 
+    this.listeQuestions = []
+    this.listeCorrections = []
+    this.autoCorrection = []
+    for (let i = 0, a, b, c, texte, texteCorr; i < this.nbQuestions; i++) {
+      this.autoCorrection[i] = {}
+      a = choice([2, 3, 4, 5])
+      b = randint(20, 50)
+      c = [['Le double', 'La moitié'], ['Le triple', 'Le tiers'], ['Le quadruple', 'Le quart'], ['Le quintuple', 'Le cinquième']]
+      switch (choice(['a', 'b', 'c', 'd'])) { // 'b', 'c'
+        case 'a':
+          texte = `${c[a - 2][0]} de  $${a}^{${b}}$ est égal à : ` + ajouteChampTexteMathLive(this, i, 'largeur15 inline')
+          setReponse(this, i, [`${a}^{${b + 1}}`], { formatInteractif: 'texte' })
+          texteCorr = `${c[a - 2][0]} de $${a}^{${b}}$ se calcule  par 
        : <br>
        $${a}\\times ${a}^{${b}}=${a}^{${b} + 1}=${a}^{${miseEnEvidence(b + 1)}}$`
-        break
-      case 'b':
-        this.question = `${c[a - 2][1]} de $${a}^{${b}}$ est égal à : `
-        this.reponse = [`${a}^{${b - 1}}`]
-        this.correction = `${c[a - 2][1]} de $${a}^{${b}}$ se calcule  par 
+          if (context.isAmc) {
+            setReponse(this, i, a ** (b + 1), { formatInteractif: 'calcul' })
+            this.autoCorrection[i].reponse.param.basePuissance = a
+            this.autoCorrection[i].reponse.param.exposantPuissance = b + 1
+            this.autoCorrection[i].reponse.param.baseNbChiffres = 1
+            this.autoCorrection[i].reponse.param.exposantNbChiffres = 2
+          }
+          break
+        case 'b':
+          texte = `${c[a - 2][1]} de $${a}^{${b}}$ est égal à : ` + ajouteChampTexteMathLive(this, i, 'largeur15 inline')
+          setReponse(this, i, [`${a}^{${b - 1}}`], { formatInteractif: 'texte' })
+          texteCorr = `${c[a - 2][1]} de $${a}^{${b}}$ se calcule  par 
       : <br> 
       
       $ ${a}^{${b}}\\div ${a}=\\dfrac{${a}^{${b}}}{${a}}=${a}^{${b} - 1}=${a}^{${miseEnEvidence(b - 1)}}$`
-        break
-      case 'c' :
-        this.question = ` $${a ** 2}\\times ${a}^{${b}}=$ <br>
-      Donner le résultat sous la forme d'une puissance de $${a}$.`
-        this.reponse = [`${a}^{${b + 2}}`]
-        this.correction = ` Comme $${a ** 2}=${a}^2$, alors $${a ** 2}\\times ${a}^{${b}}=${a}^2\\times ${a}^{${b}}=${a}^{${b}+2}=${a}^{${miseEnEvidence(2 + b)}}$`
-        break
-      case 'd' :
-        this.question = ` $${a}^{${b}}\\div ${a ** 2}=$ <br>
-        Donner le résultat sous la forme d'une puissance de $${a}$.`
-        this.reponse = [`${a}^{${b - 2}}`]
-        this.correction = `Comme $${a ** 2}=${a}^2$, alors $${a}^{${b}}\\div ${a ** 2}=
+          if (context.isAmc) {
+            setReponse(this, i, a ** (b - 1), { formatInteractif: 'calcul' })
+            this.autoCorrection[i].reponse.param.basePuissance = a
+            this.autoCorrection[i].reponse.param.exposantPuissance = b - 1
+            this.autoCorrection[i].reponse.param.baseNbChiffres = 1
+            this.autoCorrection[i].reponse.param.exposantNbChiffres = 2
+          }
+          break
+        case 'c' :
+          texte = ` $${a ** 2}\\times ${a}^{${b}}=$ <br>
+      Donner le résultat sous la forme d'une puissance de $${a}$.` + ajouteChampTexteMathLive(this, i, 'largeur15 inline')
+          setReponse(this, i, [`${a}^{${b + 2}}`], { formatInteractif: 'texte' })
+          texteCorr = ` Comme $${a ** 2}=${a}^2$, alors $${a ** 2}\\times ${a}^{${b}}=${a}^2\\times ${a}^{${b}}=${a}^{${b}+2}=${a}^{${miseEnEvidence(2 + b)}}$`
+          if (context.isAmc) {
+            setReponse(this, i, a ** (b + 2), { formatInteractif: 'calcul' })
+            this.autoCorrection[i].reponse.param.basePuissance = a
+            this.autoCorrection[i].reponse.param.exposantPuissance = b + 2
+            this.autoCorrection[i].reponse.param.baseNbChiffres = 1
+            this.autoCorrection[i].reponse.param.exposantNbChiffres = 2
+          }
+          break
+        case 'd' :
+          texte = ` $${a}^{${b}}\\div ${a ** 2}=$ <br>
+        Donner le résultat sous la forme d'une puissance de $${a}$.` + ajouteChampTexteMathLive(this, i, 'largeur15 inline')
+          setReponse(this, i, [`${a}^{${b - 2}}`], { formatInteractif: 'texte' })
+          texteCorr = `Comme $${a ** 2}=${a}^2$, alors $${a}^{${b}}\\div ${a ** 2}=
         \\dfrac{${a}^{${b}}}{${a}^2}=${a}^{${b}-2}=${a}^{${miseEnEvidence(b - 2)}}$`
-        break
+          if (context.isAmc) {
+            setReponse(this, i, a ** (b - 2), { formatInteractif: 'calcul' })
+            this.autoCorrection[i].reponse.param.basePuissance = a
+            this.autoCorrection[i].reponse.param.exposantPuissance = b - 2
+            this.autoCorrection[i].reponse.param.baseNbChiffres = 1
+            this.autoCorrection[i].reponse.param.exposantNbChiffres = 2
+          }
+          break
     // this.optionsChampTexte = { texteApres: "(juste l'exposant)" }
+      }
+      this.listeQuestions.push(texte)
+      this.listeCorrections.push(texteCorr)
     }
+    listeQuestionsToContenu(this)
   }
 }
