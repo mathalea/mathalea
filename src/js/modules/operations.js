f7ad181282147705df38b10504a4883e85378e95
Sébastien LOZANO <sebastien.lozano@ac-nancy-metz.fr>
2021-10-31 04:40:13 +0100

    fix: operations.js, dans la sortie LaTeX,  ajout des retenues pour la soustraction et des points pour la multiplication

--- src/js/modules/operations.js
+++ src/js/modules/operations.js
@@ -364,10 +364,10 @@ export default function Operation ({ operande1 = 1, operande2 = 2, type = 'addit
 
       break
     case 'soustraction':
-      if (context.isHtml) { Code = SoustractionPosee3d(operande1, operande2) } else { Code = `$\\opsub[decimalsepsymbol={,}]{${operande1}}{${operande2}}$` }
+      if (context.isHtml) { Code = SoustractionPosee3d(operande1, operande2) } else { Code = `$\\opsub[carrysub,lastcarry,decimalsepsymbol={,}]{${operande1}}{${operande2}}$` }
       break
     case 'multiplication':
-      if (context.isHtml) { Code = MultiplicationPosee3d(operande1, operande2) } else { Code = `$\\opmul[decimalsepsymbol={,}]{${operande1}}{${operande2}}$` }
+      if (context.isHtml) { Code = MultiplicationPosee3d(operande1, operande2) } else { Code = `$\\opmul[displayshiftintermediary=all,decimalsepsymbol={,}]{${operande1}}{${operande2}}$` }
       break
     case 'division':
       if (context.isHtml) { Code = DivisionPosee3d(operande1, operande2, precision) } else { Code = `$\\opdiv[displayintermediary=all,voperation=top,period,decimalsepsymbol={,},shiftdecimalsep=none]{${operande1}}{${operande2}}$` }
