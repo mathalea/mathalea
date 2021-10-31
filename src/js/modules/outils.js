d1d364fb337bf7ba8727bacfed22b45f72b80f3d
Jean-Claude Lhote <lhotejc@gmail.com>
2021-10-31 20:24:31 +0100

    feat: Ajout de fonction de conversion de base dans outils.js
    baseNVersBase10 (stringNombre, b) retourne un nombre entier à partir d'une chaine en base b (<=16)
    base10VersBaseN (nombre,b) retourne une chaine correspondant au nombre en base b (<=16).

--- src/js/modules/outils.js
+++ src/js/modules/outils.js
@@ -1042,6 +1042,54 @@ export function valeurBase (n) {
   }
 }
 
+export function baseValeur (n) {
+  switch (n) {
+    case 10: return 'A'
+    case 11: return 'B'
+    case 12: return 'C'
+    case 13: return 'D'
+    case 14: return 'E'
+    case 15: return 'F'
+    default: return Number(n).toString()
+  }
+}
+/**
+ * Convertit une chaine correspondant à un nombre écrit en base b en un nombre entier en base 10.
+ * @param {} nombre
+ * @param {number} b la base de départ
+ */
+export function baseNVersBase10 (stringNombre, b) {
+  let result = 0
+  if (typeof stringNombre === 'number') {
+    stringNombre = Number(stringNombre).toString()
+  }
+  for (let i = 0; i < stringNombre.length; i++) {
+    result += b ** i * valeurBase(stringNombre.charAt(stringNombre.length - 1 - i))
+  }
+  return result
+}
+
+export function base10VersBaseN (nombre, b) {
+  let puissanceMax = 0
+  let chiffre
+  let valeur
+  let code = ''
+  while (b ** (puissanceMax + 1) < nombre) {
+    puissanceMax++
+  }
+  for (let i = puissanceMax; i >= 0; i--) {
+    chiffre = 0
+    do {
+      valeur = chiffre * b ** i
+      chiffre++
+    } while (valeur + b ** i <= nombre)
+    chiffre--
+    code += baseValeur(chiffre)
+    nombre -= chiffre * b ** i
+  }
+  return code
+}
+
 /**
  *
  * @param {array} matrice M tableau 3x3 nombres
