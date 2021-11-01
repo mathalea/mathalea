de741eeb2c03f6d2c9e8478f5961754d06f6b8ee
moragi <gilles.mora@ac-rouen.fr>
2021-11-01 18:22:51 +0100

    correction parenthèse CAN1L01 et titre can2G02

--- src/js/exercices/can/1e/can1L01.js
+++ src/js/exercices/can/1e/can1L01.js
@@ -1,6 +1,6 @@
 
 import Exercice from '../../Exercice'
-import { choice, randint, reduirePolynomeDegre3 } from '../../../modules/outils'
+import { choice, randint, reduirePolynomeDegre3, ecritureParentheseSiNegatif } from '../../../modules/outils'
 export const titre = 'Discriminant'
 export const interactifReady = true
 export const interactifType = 'mathLive'
@@ -15,13 +15,15 @@ export default function Discriminant () {
   Exercice.call(this)
   this.typeExercice = 'simple'
   this.nbQuestions = 1
+  this.formatChampTexte = 'largeur15 inline'
   this.nouvelleVersion = function () {
     const a = randint(1, 5) * choice([-1, 1, 1, 1])
     const b = randint(-5, 5)
     const c = randint(-5, 5)
     const d = b * b - 4 * a * c
-    this.question = `Calculer le discriminant de cette expression : $${reduirePolynomeDegre3(0, a, b, c)}$`
-    this.correction = `$\\Delta =b^2-4ac=${b}^2 - 4 \\times ${a} \\times ${c}=${d}$`
+    this.question = `Calculer le discriminant de cette expression : $${reduirePolynomeDegre3(0, a, b, c)}$.<br>
+    $\\Delta=$`
+    this.correction = `$\\Delta =b^2-4ac=${ecritureParentheseSiNegatif(b)}^2 - 4 \\times ${ecritureParentheseSiNegatif(a)} \\times ${ecritureParentheseSiNegatif(c)}=${d}$`
     this.reponse = d
   }
 }
