21d10cd8b03c0b2c741efe8516433308028e7e46
Jean-Claude Lhote <lhotejc@gmail.com>
2021-11-03 10:54:58 +0100

    ex: 2 nouvelles can en beta...
    betacan6G04 et betacan6G05

--- src/js/exercices/can/6e/betacan6G05.js
+++ src/js/exercices/can/6e/betacan6G05.js
@@ -1,5 +1,5 @@
 import { codeSegments, droite, labelPoint, mathalea2d, point, segment, segmentAvecExtremites, texteSurSegment, tracePointSurDroite } from '../../../modules/2d.js'
-import { randint } from '../../../modules/outils.js'
+import { calcul, randint } from '../../../modules/outils.js'
 import Exercice from '../../Exercice.js'
 export const titre = 'Problèmes de longueurs'
 export const dateDePublication = '2/11/2021'
@@ -22,8 +22,8 @@ export default function ProblemesDeLongueurs () {
     const objets = []
     const pointsSurDE = []
     const pointsSurAB = []
-    const a = randint(2, 5)
-    const b = randint(2, 3)
+    const b = randint(2, 5)
+    const a = randint(2, 8 - b)
     const c = randint(3, 8)
     const A = point(0, 0, 'A', 'below')
     const B = point(16, 0, 'B', 'below')
@@ -34,6 +34,8 @@ export default function ProblemesDeLongueurs () {
       pointsSurAB.push(point(i * 16 / b, 0), point(i * 16 / b, 0))
       objets.push(tracePointSurDroite(pointsSurAB[2 * (i - 1)], dd))
     }
+    pointsSurAB[2 * (b - 2)].nom = 'C'
+    pointsSurAB[2 * (b - 2)].positionLabel = 'below'
     objets.push(codeSegments('//', 'red', A, ...pointsSurAB, B))
     const D = point((b - 1) * 16 / b, 2, 'D', 'above')
     const x = D.x
@@ -42,7 +44,7 @@ export default function ProblemesDeLongueurs () {
     const F = point(x + (a - 1) * l / a, 2, 'F', 'above')
     const DE = segmentAvecExtremites(D, E)
     const d = droite(D, E)
-    objets.push(DE, labelPoint(D, E))
+    objets.push(DE, labelPoint(D, E, pointsSurAB[2 * (b - 2)]))
     for (let i = 1; i < a; i++) {
       pointsSurDE.push(point(x + i * l / a, 2), point(x + i * l / a, 2))
       objets.push(tracePointSurDroite(pointsSurDE[2 * (i - 1)], d))
@@ -54,8 +56,8 @@ export default function ProblemesDeLongueurs () {
     s2.pointilles = 2
     s2.color = 'green'
     objets.push(texteSurSegment(c, F, E), labelPoint(F), codeSegments('O', 'blue', D, ...pointsSurDE, E), s1, s2)
-    this.question = `Sachant que $FE=${c}$ cm, combien mesure $AB$ ?<br>` + mathalea2d({ xmin: -0.5, ymin: -1, xmax: 16.5, ymax: 3.5, scale: 0.5 }, objets)
-    this.reponse = a * b * c
-    this.correction = `Commme $AB=${b}\\times DE$ et comme $DE=${a}\\times FE$ alors $AB=${b}\\times${a}\\times ${c} \\text{ cm} =${a * b * c}$ cm`
+    this.question = `Sachant que $AB=${calcul(a * b * c)}$ cm et que $CB=DE$, détermine $FE$.<br>` + mathalea2d({ xmin: -0.5, ymin: -1, xmax: 16.5, ymax: 3.5, scale: 0.5 }, objets)
+    this.reponse = c
+    this.correction = `Commme $CB=\\dfrac{AB}{${b}}$ alors $CB=\\dfrac{${calcul(a * b * c)}\\text{ cm}}{${b}}=${calcul(a * c)}$ cm.<br><br>Comme $DE=CB=${calcul(a * c)}$ cm et $FE=\\dfrac{DE}{${a}}$ alors $FE=\\dfrac{${calcul(a * c)}\\text{ cm}}{${a}}=${c}$ cm.`
   }
 }
