bd26d8c1806a669183319be7ce3903ed229640b7
Sébastien LOZANO <sebastien.lozano@ac-nancy-metz.fr>
2021-11-12 10:00:34 +0100

    temp: Tous les 2G1 sortent du dico lycée

---
+++ src/js/exercices/2e/2G11-2.js
@@ -0,0 +1,13 @@
+import ProblemesThales from '../3e/3G20-1.js'
+export const titre = 'Problèmes utilisant le théorème de Thalès'
+// export { interactifReady, interactifType } from '../3e/3G20-1.js'
+// export { amcReady, amcType } from '../3e/3G20-1.js'
+
+/**
+ * Clone de 3G20-1 pour les 2nde
+ *
+ * @author Sébastien LOZANO
+ */
+export default function ProblemesThales2nde () {
+  ProblemesThales.call(this)
+}
