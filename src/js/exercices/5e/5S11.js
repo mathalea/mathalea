0eaeac5afc87f9d7ebf4879fadd4c3a74017dc66
Sébastien LOZANO <sebastien.lozano@ac-nancy-metz.fr>
2021-11-14 08:33:49 +0100

    fix: 5S11 récupérationdes constantes pour AMC et interactif. 6S10 ajout des champs interactifs pout this.sup2 = 2

--- src/js/exercices/5e/5S11.js
+++ src/js/exercices/5e/5S11.js
@@ -1,15 +1,19 @@
 import LectureDiagrammeBarre from '../6e/6S10.js'
 export const titre = 'Lire un diagramme en barre'
 
+export { interactifReady, interactifType } from '../6e/6S10.js'
+export { amcReady, amcType } from '../6e/6S10.js'
+
 /**
  * Lire un diagramme en barre
  * @author Guillaume Valmont
  * reference 5S11
  * Publié le 08/08/2021
+ * Fix export interactif et AMC Sébastien LOZANO
  */
 export default function LectureDiagrammeBarre5e () {
   LectureDiagrammeBarre.call(this)
-  this.titre = titre
+  // this.titre = titre
   this.sup = 3
   this.sup2 = 2
 }
