94194c95c832f47f3024bdcd9a61ee0110d66dd6
Gain <mireillegain.prof@gmail.com>
2021-11-18 13:02:15 +0100

    export date

--- src/js/exercices/6e/6N23-6.js
+++ src/js/exercices/6e/6N23-6.js
@@ -8,6 +8,7 @@ export const interactifReady = true
 export const interactifType = 'numerique'
 export const amcType = 'AMCNum' // Question numérique
 export const titre = 'Valeur décimale d\'une fraction'
+export const dateDePublication = '18/11/2021' // La date de publication initiale au format 'jj/mm/aaaa' pour affichage temporaire d'un tag
 
 /**
  * Calculer la valeur décimale des fractions suivantes.
