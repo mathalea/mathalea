252e10a3257fdc1d92bdee727390e4a8f6ed3de4
EricElter <eric.elter@ac-rouen.fr>
2021-11-22 14:16:32 +0100

    temp : sauvegarde provisoire d'un nouvel exo

---
+++ src/js/exercices/6e/6C12-3.js
@@ -0,0 +1,137 @@
+import Exercice from '../Exercice.js'
+import { context } from '../../modules/context.js'
+import { listeQuestionsToContenu, creerCouples, randint, choice, texNombre, texNombre2, calcul, texteEnCouleurEtGras, texteGras } from '../../modules/outils.js'
+import { ajouteChampTexteMathLive, propositionsQcm, setReponse } from '../../modules/gestionInteractif.js'
+export const amcReady = true
+export const amcType = 'qcmMono' // type de question AMC
+export const interactifReady = true
+export const interactifType = ['qcm', 'mathLive']
+
+export const titre = 'Tables de multiplications et multiples de 10'
+
+/**
+ * Les 2 facteurs peuvent terminer par aucun, 1, 2 ou 3 zéros
+ * @author Rémi Angot
+* Référence 6C10-2
+ */
+export default function ExerciceTablesMultiplicationsEtMultiplesDe10 (
+  tablesParDefaut = '2-3-4-5-6-7-8-9'
+) {
+  // Multiplier deux nombres
+  Exercice.call(this) // Héritage de la classe Exercice()
+  this.sup = tablesParDefaut
+  this.sup2 = 1
+  this.titre = titre
+  this.consigne = 'Dans chaque problème, coche les informations qui servent à sa résolution.'
+  this.spacing = 2
+  this.tailleDiaporama = 3
+
+  this.nouvelleVersion = function () {
+    this.interactifType = parseInt(this.sup2) === 2 ? 'mathLive' : 'qcm'
+    this.autoCorrection = []
+    let tables = []
+    this.listeQuestions = [] // Liste de questions
+    this.listeCorrections = [] // Liste de questions corrigées
+    this.nbQuestions = 1
+    this.sup = parseInt(this.sup)
+    const cocheIndique = ['coche', 'indique']
+    const chaqueCe = ['chaque', 'ce']
+    const affirmatifNegatif = ['servent', 'ne servent pas']
+    this.consigne = 'Dans '
+    this.nbQuestions === 1 ? this.consigne += chaqueCe[1] : this.consigne += chaqueCe[0]
+    this.consigne += ' problème, '
+    context.isDiaporama ? this.consigne += cocheIndique[1] : this.consigne += cocheIndique[0]
+    this.consigne += ' les informations qui '
+    this.sup !== 1 ? this.consigne += affirmatifNegatif[1] : this.consigne += affirmatifNegatif[0]
+    this.consigne += ' à sa résolution.'
+    if (typeof this.sup === 'number') {
+      // Si c'est un nombre c'est qu'il y a qu'une seule table
+      tables[0] = this.sup
+    } else {
+      tables = this.sup.split('-') // Sinon on crée un tableau à partir des valeurs séparées par des ;
+    }
+    const couples = creerCouples(
+      tables,
+      [2, 3, 4, 5, 6, 7, 8, 9, 10],
+      this.nbQuestions
+    ) // Liste tous les couples possibles (2,3)≠(3,2)
+    for (
+      let i = 0, nbEleves, ageMin, nbLivres, masseLivre, a, b, k1, k2, texte, texteCorr, melange;
+      i < this.nbQuestions;
+      i++
+    ) {
+      this.autoCorrection[i] = {}
+      a = couples[i][0]
+      b = couples[i][1]
+      if (a > 1) {
+        k1 = choice([1, 10, 100, 1000])
+      } else {
+        k1 = choice([10, 100, 1000])
+      }
+      k2 = choice([1, 10, 100, 1000])
+      a = k1 * a
+      b = k2 * b
+      melange = randint(0, 1)
+      if (melange === 1) {
+        // échange a et b pour que les multiplications ne soient pas toujours dans le même ordre (surtout lorsque tables n'a qu'un seul élément)
+        const c = a
+        a = b
+        b = c
+      }
+      nbEleves = randint(17, 35)
+      ageMin = randint(7, 15)
+      nbLivres = randint(3, 10)
+      masseLivre = 10 * randint(20, 60)
+      texte = `Dans une classe de $${nbEleves}$ élèves âgés de $${ageMin}$  à $${ageMin + 2}$  ans,`
+      texte += ` un professeur distribue à chaque enfant $${nbLivres}$ livres pesant en moyenne $${masseLivre}$ g chacun.<br>`
+      texte += 'Calculer le nombre total de livres distribués.'
+      texteCorr = ''
+      if (this.sup === 1) {
+        texteCorr += texteEnCouleurEtGras(nbEleves + ' élèves')
+        texteCorr += ' et ' + texteEnCouleurEtGras(nbLivres + ' livres') + ' sont utiles pour la résolution du problème.<br>'
+        texteCorr += 'La solution du problème est donnée par : '
+        texteCorr += texteEnCouleurEtGras(nbEleves + ' élèves ') + '$\\times$' + texteEnCouleurEtGras(nbLivres + ' livres') + '.'
+      } else {
+        texteCorr += texteEnCouleurEtGras(ageMin + ' ans') + ', ' + texteEnCouleurEtGras(ageMin + 2 + ' ans')
+        texteCorr += ' et ' + texteEnCouleurEtGras(masseLivre + ' g') + ' ne sont pas utiles pour la résolution du problème.<br>'
+        texteCorr += 'La solution du problème est donnée par : '
+        texteCorr += texteGras(nbEleves + ' élèves ') + '$\\times$' + texteGras(nbLivres + ' livres') + '.'
+      }
+      this.autoCorrection[i].enonce = `${texte}\n`
+      this.autoCorrection[i].propositions = [
+        {
+          texte: `$${nbEleves}$ élèves`,
+          statut: this.sup === 1
+        },
+        {
+          texte: `$${ageMin}$ ans`,
+          statut: this.sup !== 1
+        },
+        {
+          texte: `$${ageMin + 2}$ ans`,
+          statut: this.sup !== 1
+        },
+        {
+          texte: `$${nbLivres}$ livres`,
+          statut: this.sup === 1
+        },
+        {
+          texte: `$${masseLivre}$ g`,
+          statut: this.sup !== 1
+        }
+      ]
+      this.autoCorrection[i].options = {
+        ordered: false,
+        lastChoice: 5
+      }
+      if (!context.isAmc) {
+        texte += propositionsQcm(this, i).texte
+      }
+      this.listeQuestions.push(texte)
+      this.listeCorrections.push(texteCorr)
+    }
+    listeQuestionsToContenu(this)
+  }
+  this.besoinFormulaireNumerique = ['Choix de la formulation', 2, '1 : Coche les informations qui servent à sa résolution\n2 : Coche les informations qui NE servent PAS à sa résolution']
+  if (context.isHtml && !context.isDiaporama) this.besoinFormulaire2Numerique = ['Exercice interactif', 2, '1 : QCM\n2 : Numérique'] // Texte, tooltip
+}
