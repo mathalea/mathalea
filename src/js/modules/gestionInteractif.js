8e5d30557c7425927d3d7aadd79fd7f98e8bf03e
Angot <remi.angot@gmail.com>
2021-11-22 19:01:02 +0100

    style: suppression des énoncés en double pour 5R11-2 et 5R12-2

--- src/js/modules/gestionInteractif.js
+++ src/js/modules/gestionInteractif.js
@@ -154,7 +154,6 @@ function verifQuestionMathLive (exercice, i) {
         saisie = saisie.toString().replace(',', '.')
         reponse = reponse.replace(',', '.')
       }
-      console.log(saisie, engine.canonical(parse(saisie)), reponse, engine.canonical(parse(reponse)))
       if (engine.same(engine.canonical(parse(saisie)), engine.canonical(parse(reponse)))) {
         saisie = saisie.split('\\times')
         if (number(saisie[0]) >= 1 & number(saisie[0]) < 10) { resultat = 'OK' }
