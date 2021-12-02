da6483a5e9ccf5a73c38377f5c689845666ed9c4
Sébastien LOZANO <sebastien.lozano@ac-nancy-metz.fr>
2021-12-02 09:53:35 +0100

    temp: les 2N3 sortent du dico lycee

---
+++ src/js/exercices/2e/2N30-1.js
@@ -0,0 +1,16 @@
+export const titre = 'Décomposer une fraction (partie entière +fraction inférieure à 1) puis donner l\'écriture décimale'
+
+// eslint-disable-next-line import/first
+import ExerciceFractionsDifferentesEcritures from '../6e/6N20-2.js'
+
+export { interactifReady, interactifType } from '../6e/6N20-2.js'
+// export { amcReady, amcType } from '../6e/6N20-2.js'
+
+/**
+ * Clone de 6N20-2 pour les 2nde
+ *
+ * @author Sébastien LOZANO
+ */
+export default function ExerciceFractionsDifferentesEcritures2nde () {
+  ExerciceFractionsDifferentesEcritures.call(this)
+}
---
+++ src/js/exercices/2e/2N30-5.js
@@ -0,0 +1,16 @@
+export const titre = 'Fractions et priorités opératoires'
+
+// eslint-disable-next-line import/first
+import SommeOuProduitFractions from '../4e/4C23.js'
+
+export { interactifReady, interactifType } from '../4e/4C23.js'
+// export { amcReady, amcType } from '../4e/4C23.js'
+
+/**
+ * Clone de 4C23 pour les 2nde
+ *
+ * @author Sébastien LOZANO
+ */
+export default function SommeOuProduitFractions2nde () {
+  SommeOuProduitFractions.call(this)
+}
