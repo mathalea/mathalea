db4a65dd3107060d5a7d16de5c11752c1ffcfed4
ftapiero <florence.tapiero@gmail.com>
2021-12-06 22:09:03 +0100

    ex: Corrections can2G13 et ajout lettre aléatoire

--- src/js/exercices/can/2e/can2G13.js
+++ src/js/exercices/can/2e/can2G13.js
@@ -1,5 +1,5 @@
 import Exercice from '../../Exercice.js'
-import { randint, choice, ecritureParentheseSiNegatif, texteEnCouleur, extraireRacineCarree } from '../../../modules/outils.js'
+import { randint, choice, ecritureParentheseSiNegatif, texteEnCouleur, extraireRacineCarree, lettreMinusculeDepuisChiffre } from '../../../modules/outils.js'
 export const titre = 'Calcul de la norme d’un vecteur'
 export const interactifReady = true
 export const interactifType = 'mathLive'
@@ -17,19 +17,20 @@ export default function NormeVecteur () {
   this.nbQuestions = 1
   this.formatChampTexte = 'largeur15 inline'
   this.nouvelleVersion = function () {
-    let a, b, reduction
+    let a, b, nom, reduction
     switch (choice(['a'])) {
       case 'a':
         a = randint(-9, 9)
         b = randint(-6, 6, a)
+        nom = lettreMinusculeDepuisChiffre(randint(11, 26, [15, 24, 25]))
         reduction = extraireRacineCarree(a ** 2 + b ** 2)
         if (a ** 2 + b ** 2 === 1 || a ** 2 + b ** 2 === 4 || a ** 2 + b ** 2 === 9 || a ** 2 + b ** 2 === 16 || a ** 2 + b ** 2 === 25 || a ** 2 + b ** 2 === 36 || a ** 2 + b ** 2 === 49 || a ** 2 + b ** 2 === 64 || a ** 2 + b ** 2 === 81 || a ** 2 + b ** 2 === 100) {
-          this.question = `Dans un repère orthonormé du plan, on donne $\\overrightarrow{u}(${a};${b})$ .<br>
-        Déterminer la norme du vecteur $\\overrightarrow{u}$.<br><br>
+          this.question = `Dans un repère orthonormé du plan, on donne $\\overrightarrow{${nom}}(${a};${b})$ .<br>
+        Déterminer la norme du vecteur $\\overrightarrow{${nom}}$.<br><br>
         Donner le résultat sous la forme $\\sqrt{a}$ ou d'un nombre entier le cas échéant.`
           this.optionsChampTexte = { texteApres: '' }
           this.correction = `D'après le cours, si $\\overrightarrow{u}(x;y)$ alors $\\| \\overrightarrow{u} \\|=\\sqrt{x^2+y^2}$. On a donc : <br><br>
-          $ \\| \\overrightarrow{u} \\|=
+          $ \\| \\overrightarrow{${nom}} \\|=
           \\sqrt{${ecritureParentheseSiNegatif(a)}^2+${ecritureParentheseSiNegatif(b)}^2}=
           \\sqrt{${a ** 2}+ ${b ** 2}}=
           \\sqrt{${a ** 2 + b ** 2}}=
@@ -38,12 +39,12 @@ export default function NormeVecteur () {
            `)
           this.reponse = Math.sqrt(a ** 2 + b ** 2)
         } else {
-          this.question = `Dans un repère orthonormé du plan, on donne $\\overrightarrow{u}(${a};${b})$ .<br>
-          Déterminer la norme du vecteur $\\overrightarrow{u}$.<br><br>
+          this.question = `Dans un repère orthonormé du plan, on donne $\\overrightarrow{${nom}}(${a};${b})$ .<br>
+          Déterminer la norme du vecteur $\\overrightarrow{${nom}}$.<br><br>
           Donner le résultat sous la forme $\\sqrt{a}$ ou d'un nombre entier le cas échéant.`
           this.optionsChampTexte = { texteApres: '' }
           this.correction = `D'après le cours, si $\\overrightarrow{u}(x;y)$ alors $\\| \\overrightarrow{u} \\|=\\sqrt{x^2+y^2}$. On a donc : <br><br>
-          $\\| \\overrightarrow{u} \\| =
+          $\\| \\overrightarrow{${nom}} \\| =
           \\sqrt{${ecritureParentheseSiNegatif(a)}^2+${ecritureParentheseSiNegatif(b)}^2}=
           \\sqrt{${a ** 2}+ ${b ** 2}}=
           \\sqrt{${a ** 2 + b ** 2}}$<br><br>`
@@ -51,7 +52,7 @@ export default function NormeVecteur () {
           `)
           this.reponse = [`\\sqrt{${a ** 2 + b ** 2}}`, `${Math.sqrt(a ** 2 + b ** 2)}`]
           if (reduction[0] !== 1) {
-            this.correction += texteEnCouleur(`Remarque : sous forme simplifiée, la réponse est $${reduction[0]}\\sqrt{${reduction[1]}}$ car $${a ** 2 + b ** 2}=${reduction[0]}^2 \\times ${reduction[1]}$ `)
+            this.correction += texteEnCouleur(`Remarque : sous forme simplifiée, la réponse est $${reduction[0]}\\sqrt{${reduction[1]}}$ car $${a ** 2 + b ** 2}=${reduction[0]}^2 \\times ${reduction[1]}$.`)
           }
         }
         break
