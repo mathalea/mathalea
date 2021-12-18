1757f9de980eb08e1e8e7400f006b85b9cfc84b4
Stéphane <mathsguyon@gmail.com>
2021-12-18 13:11:13 +0100

    1A11 OK niv 1

--- src/js/exercices/1e/1AN11.js
+++ src/js/exercices/1e/1AN11.js
@@ -1,5 +1,5 @@
 import Exercice from '../Exercice.js'
-import { listeQuestionsToContenu, combinaisonListes, ecritureAlgebrique, randint, ecritureParentheseSiNegatif, ecritureAlgebriquec, ecritureAlgebriqueSauf1 } from '../../modules/outils.js'
+import { listeQuestionsToContenu, combinaisonListes, ecritureAlgebrique, randint, ecritureParentheseSiNegatif, ecritureAlgebriqueSauf1 } from '../../modules/outils.js'
 export const titre = 'Equation de tangente'
 
 // Les exports suivants sont optionnels mais au moins la date de publication semble essentielle
@@ -33,34 +33,37 @@ export default function equationdetangente () {
     }
     const listeTypeQuestions = combinaisonListes(typesDeQuestionsDisponibles, this.nbQuestions) // Tous les types de questions sont posés mais l'ordre diffère à chaque "cycle"
 
-    for (let i = 0, a, texte, texteCorr, cpt = 0; i < this.nbQuestions && cpt < 50;) { // Boucle principale où i+1 correspond au numéro de la question
-      switch (listeTypeQuestions[i]) { 
-       
+    for (let i = 0, a, b, c, texte, texteCorr, cpt = 0; i < this.nbQuestions && cpt < 50;) { // Boucle principale où i+1 correspond au numéro de la question
+      switch (listeTypeQuestions[i]) {
         case 1 :// Sans formule
           a = randint(-5, 5)
-          b=randint(-5, 5)// f(a)
-          c=randint(-5, 5)//f'(a)
-          texte = 'Soit $f$ une fonction dérivable sur $[-5;5]$ et $\\mathcal{}.<br>'
-          texte += `Déterminer la valeur de  $f'(${a})$, en utilisant la définition de cours.`
-          texteCorr = `Pour déterminer $f'(${a})$, `
-          texteCorr += `on commence par calculer le taux de variation de $f$, <br> entre $${a}$ et $${a}+h$ , `
-          texteCorr += 'noté $\\tau(h)$, où $h$ est un réel non-nul.<br>'
-          texteCorr += `$\\begin{aligned}\\tau(h) &= \\dfrac{f(${a}+h)-f(${a})}{h}&\\text{Définition du taux de variation}\\\\`
-          texteCorr += `&= \\dfrac{(${a}+h)^2-(${a})^2}{h}&\\text{Application à la fonction carré.}\\\\`
-          texteCorr += `&= \\dfrac{${ecritureParentheseSiNegatif(a)}^2+2\\times${ecritureParentheseSiNegatif(a)}\\times h+h^2-${ecritureParentheseSiNegatif(a)}^2}{h}&\\text{Développement de l'identité remarquable.}\\\\`
-          texteCorr += `&= \\dfrac{${a * a}${ecritureAlgebrique(2 * a)} h+h^2-${a * a}}{h}&\\text{Simplification au numérateur.}\\\\`
+          b = randint(-5, 5)// f(a)
+          c = randint(-5, 5, [1])// f'(a)
+          texte = 'Soit $f$ une fonction dérivable sur $[-5;5]$ et $\\mathcal{C}_f$ sa courbe représentative.<br>'
+          texte += `On sait que  $f(${a})=${b}~~$ et que $~~f'(${a})=${c}$.`
+          texte += `<br>Déterminer une équation de la tangente $(T)$ à la courbe $\\mathcal{C}_f$ au point d'abscisse $${a}$,`
+          texte += '<br>sans utiliser la formule de cours de l\'équation de tangente.'
 
-          texteCorr += `&= \\dfrac{${2 * a} h+h^2}{h}&\\text{Réduction au numérateur.}\\\\`
-          texteCorr += `&= \\dfrac{h(${2 * a}+h)}{h}&\\text{Factorisation  par } h \\text{ au numérateur.}\\\\`
-          texteCorr += `&=${2 * a} +h&\\text{Simplification par} h\\\\`
+          texteCorr = 'On sait que la tangente n\'est pas une droite verticale, puisque la fonction est dérivable sur l\'intervale.'
+          texteCorr += `<br>On en déduit que la tangente $(T)$ au point d'abscisse $${a}$, admet une équation réduite de la forme :  `
+          texteCorr += '$(T) : y=m x + p$. <br>'
+          texteCorr += '<br>Il nous faut trouver $m$  et  $p$.'
+          texteCorr += `<br>On sait que le nombre dérivé en $${a}$ est par définition, le coefficient directeur de la tangente au point d'abscisse $${a}$.`
+          texteCorr += `<br>Par conséquent, on a déjà : $m=f'(${a})=${c}$`
+          texteCorr += `<br> On en déduit que : $(T) : y= ${c} x + p$. `
+          texteCorr += '<br> Il nous faut maintenant déterminer la valeur de $p$.'
+          texteCorr += `<br> Pour cela, on utilise que si $f(${a})=${b}~~$, alors le point $A$ de coordonnées $(${a};${b})$ appartient à $\\mathcal{C}_f$ mais aussi à $(T)$.`
+          texteCorr += `<br> On peut écrire $A(${a};${b}) = \\mathcal{C}_f \\cap (T)$.`
+          texteCorr += `<br> On remplace alors les coordonnées de $A(${a};${b})$ dans l'équation  $(T) : y= ${c} x + p$`
+          texteCorr += `<br> $\\begin{aligned} \\phantom{\\iff}&A(${a};${b})\\in (T)\\\\`
+          texteCorr += ` \\iff& ${b}= ${c} \\times ${ecritureParentheseSiNegatif(a)}  + p\\\\`
+          texteCorr += ` \\iff& p=${b} ${ecritureAlgebriqueSauf1(-c)} \\times ${ecritureParentheseSiNegatif(a)}  \\\\`
+          texteCorr += ` \\iff& p=${b} ${ecritureAlgebriqueSauf1(-c * a)}   \\\\`
+          texteCorr += ` \\iff& p=${b - c * a}\\\\`
           texteCorr += '\\end{aligned}$'
-          texteCorr += '<br>On cherche maintenant la limite du taux de variations quand $h$ tend vers $0$.'
-          texteCorr += `<br>$\\lim\\limits_{h \\rightarrow 0} ${2 * a} +h=${2 * a} $`
-          texteCorr += `<br>Comme la limite existe, on peut en déduire que $f$ est dérivable en $${a}$ <br>et`
-          texteCorr += ` on peut conclure que $f'(${a})=${2 * a} $`
-
+          texteCorr += `<br>On peut conclure que : $(T) : y=${c}x ${ecritureAlgebrique(b - c * a)}$`
           break
-        case 3 :// 'inverse':
+        case 2 :// 'inverse':
           a = randint(-5, 5, [0])
           texte = 'Soit $f$ la fonction définie pour tout $x$ de $\\mathbb{R}^{*}$ par $f(x)=\\dfrac{1}{x}$.<br>'
           texte += `Déterminer la valeur de  $f'(${a})$, en utilisant la définition de cours.`
@@ -77,28 +80,6 @@ export default function equationdetangente () {
           texteCorr += '<br>On cherche maintenant la limite du taux de variations quand $h$ tend vers $0$.'
           texteCorr += `<br>$\\lim\\limits_{h \\rightarrow 0} \\dfrac{-1}{(${a}+h)\\times ${ecritureParentheseSiNegatif(a)}}= \\dfrac{-1}{${a * a}} $`
           if (a !== 1 && a !== -1) { texteCorr += `<br>On peut donc conclure que $f'(${a})=\\dfrac{-1}{${a * a}} $` } else { texteCorr += `<br>On peut donc conclure que $f'(${a})=-1 $` }
-          break
-        case 4 :// 'racine_carree':
-          a = randint(1, 8)
-          texte = 'Soit $f$ la fonction définie pour tout $x$ de $\\mathbb{R}_{+}$ par $f(x)=\\sqrt{x}$.<br>'
-          texte += `Déterminer la valeur de  $f'(${a})$, en utilisant la définition de cours.`
-          texteCorr = `Pour déterminer $f'(${a})$, `
-          texteCorr += `on commence par calculer le taux de variation de $f$, <br> entre $${a}$ et $${a}+h$ , `
-          texteCorr += 'noté $\\tau(h)$, où $h$ est un réel non-nul.<br>'
-          texteCorr += `$\\begin{aligned}\\tau(h) &= \\dfrac{f(${a}+h)-f(${a})}{h}&\\text{Définition du taux de variation}\\\\`
-          texteCorr += `&= \\dfrac{\\sqrt{${a}+h}-\\sqrt{${a}}}{h}&\\text{Application à la fonction racine carrée.}\\\\`
-          texteCorr += `&=\\dfrac{(\\sqrt{${a}+h}-\\sqrt{${a}})(\\sqrt{${a}+h}+\\sqrt{${a}})}{h(\\sqrt{${a}+h}+\\sqrt{${a}})}&\\text{Multiplication par la "quantité conjuguée".}\\\\`
-          texteCorr += `&=\\dfrac{${a}+h${ecritureAlgebrique(-a)}}{h(\\sqrt{${a}+h}+\\sqrt{${a}})}&\\text{Identité remarquable : } (a-b)(a+b)=a^2-b^2\\\\`
-
-          texteCorr += `&=\\dfrac{h}{h(\\sqrt{${a}+h}+\\sqrt{${a}})}&\\text{Réduction au numérateur }.\\\\`
-          texteCorr += `&=\\dfrac{1}{\\sqrt{${a}+h}+\\sqrt{${a}}}&\\text{Simplification de la fraction par } h.\\\\`
-
-          texteCorr += '\\end{aligned}$'
-          texteCorr += '<br>On cherche maintenant la limite du taux de variations quand $h$ tend vers $0$.'
-          texteCorr += `<br>$\\lim\\limits_{h \\rightarrow 0} \\dfrac{1}{\\sqrt{${a}+h}+\\sqrt{${a}}}=\\dfrac{1}{2 \\sqrt{${a}}}$`
-          if (a !== 1 && a !== 4) { texteCorr += `<br>On peut donc conclure que $f'(${a})=\\dfrac{1}{2 \\sqrt{${a}}}$` }
-          if (a === 1) { texteCorr += `<br>On peut donc conclure que $f'(${a})=\\dfrac{1}{2} $` }
-          if (a === 4) { texteCorr += `<br>On peut donc conclure que $f'(${a})=\\dfrac{1}{4} $` }
           break
       }
 
