7e404456aa14b7660f61bba010c1aaec8ff410f6
Stéphane <mathsguyon@gmail.com>
2021-12-18 17:03:49 +0100

    1AN11 qui tourne

--- src/js/exercices/1e/1AN11.js
+++ src/js/exercices/1e/1AN11.js
@@ -35,7 +35,7 @@ export default function equationdetangente () {
 
     for (let i = 0, a, b, c, texte, texteCorr, cpt = 0; i < this.nbQuestions && cpt < 50;) { // Boucle principale où i+1 correspond au numéro de la question
       switch (listeTypeQuestions[i]) {
-        case 1 :// Sans formule
+        case 2 :// Sans formule
           a = randint(-5, 5)
           b = randint(-5, 5)// f(a)
           c = randint(-5, 5, [1])// f'(a)
@@ -44,42 +44,44 @@ export default function equationdetangente () {
           texte += `<br>Déterminer une équation de la tangente $(T)$ à la courbe $\\mathcal{C}_f$ au point d'abscisse $${a}$,`
           texte += '<br>sans utiliser la formule de cours de l\'équation de tangente.'
 
-          texteCorr = 'On sait que la tangente n\'est pas une droite verticale, puisque la fonction est dérivable sur l\'intervale.'
+          texteCorr = 'On sait que la tangente n\'est pas une droite verticale, puisque la fonction est dérivable sur l\'intervalle.'
           texteCorr += `<br>On en déduit que la tangente $(T)$ au point d'abscisse $${a}$, admet une équation réduite de la forme :  `
           texteCorr += '$(T) : y=m x + p$. <br>'
-          texteCorr += '<br>Il nous faut trouver $m$  et  $p$.'
+          texteCorr += '<br>$\\bullet$ Détermination de $m$ :'
           texteCorr += `<br>On sait que le nombre dérivé en $${a}$ est par définition, le coefficient directeur de la tangente au point d'abscisse $${a}$.`
-          texteCorr += `<br>Par conséquent, on a déjà : $m=f'(${a})=${c}$`
-          texteCorr += `<br> On en déduit que : $(T) : y= ${c} x + p$. `
-          texteCorr += '<br> Il nous faut maintenant déterminer la valeur de $p$.'
+          texteCorr += `<br>Par conséquent, on a déjà : $m=f'(${a})=${c}$.`
+          texteCorr += `<br> On en déduit que  $(T) : y= ${c} x + p$. `
+          texteCorr += '<br>$\\bullet$ Détermination de $p$:'
           texteCorr += `<br> Pour cela, on utilise que si $f(${a})=${b}~~$, alors le point $A$ de coordonnées $(${a};${b})$ appartient à $\\mathcal{C}_f$ mais aussi à $(T)$.`
           texteCorr += `<br> On peut écrire $A(${a};${b}) = \\mathcal{C}_f \\cap (T)$.`
-          texteCorr += `<br> On remplace alors les coordonnées de $A(${a};${b})$ dans l'équation  $(T) : y= ${c} x + p$`
+          texteCorr += `<br> On remplace alors les coordonnées de $A(${a};${b})$ dans l'équation  $(T) : y= ${c} x + p$.`
           texteCorr += `<br> $\\begin{aligned} \\phantom{\\iff}&A(${a};${b})\\in (T)\\\\`
           texteCorr += ` \\iff& ${b}= ${c} \\times ${ecritureParentheseSiNegatif(a)}  + p\\\\`
           texteCorr += ` \\iff& p=${b} ${ecritureAlgebriqueSauf1(-c)} \\times ${ecritureParentheseSiNegatif(a)}  \\\\`
           texteCorr += ` \\iff& p=${b} ${ecritureAlgebriqueSauf1(-c * a)}   \\\\`
           texteCorr += ` \\iff& p=${b - c * a}\\\\`
           texteCorr += '\\end{aligned}$'
-          texteCorr += `<br>On peut conclure que : $(T) : y=${c}x ${ecritureAlgebrique(b - c * a)}$`
+          texteCorr += `<br>On peut conclure que : $(T) : y=${c}x ${ecritureAlgebrique(b - c * a)}$.`
           break
-        case 2 :// 'inverse':
-          a = randint(-5, 5, [0])
-          texte = 'Soit $f$ la fonction définie pour tout $x$ de $\\mathbb{R}^{*}$ par $f(x)=\\dfrac{1}{x}$.<br>'
-          texte += `Déterminer la valeur de  $f'(${a})$, en utilisant la définition de cours.`
-          texteCorr = `Pour déterminer $f'(${a})$, `
-          texteCorr += `on commence par calculer le taux de variation de $f$, <br> entre $${a}$ et $${a}+h$ , `
-          texteCorr += 'noté $\\tau(h)$, où $h$ est un réel non-nul.<br>'
-          texteCorr += `$\\begin{aligned}\\tau(h) &= \\dfrac{f(${a}+h)-f(${a})}{h}&\\text{Définition du taux de variation}\\\\`
-          texteCorr += `&= \\dfrac{\\dfrac{1}{${a}+h}-\\dfrac{1}{${a}}}{h}&\\text{Application à la fonction inverse.}\\\\`
-          texteCorr += `&= \\dfrac{\\dfrac{${a}}{(${a}+h)\\times ${ecritureParentheseSiNegatif(a)}}-\\dfrac{${a}+h}{${a}\\times (${a}+h)}}{h}&\\text{Mise au même dénominateur.}\\\\`
-          texteCorr += `&= \\dfrac{\\dfrac{${a}${ecritureAlgebriqueSauf1(-a)}-h}{(${a}+h)\\times ${ecritureParentheseSiNegatif(a)}}}{h}&\\text{Réduction au numérateur.}\\\\`
-          texteCorr += `&= \\dfrac{-h}{(${a}+h)\\times ${ecritureParentheseSiNegatif(a)}}\\times \\dfrac{1}{h}&\\text{Diviser par } h, \\text{c'est multiplier par }\\dfrac{1}{h}.\\\\`
-          texteCorr += `&= \\dfrac{-1}{(${a}+h)\\times ${ecritureParentheseSiNegatif(a)}}&\\text{Simplification par }h \\\\`
+        case 1 :// 'formule':
+          a = randint(-5, 5)
+          b = randint(-5, 5)// f(a)
+          c = randint(-5, 5, [1])// f'(a)
+          texte = 'Soit $f$ une fonction dérivable sur $[-5;5]$ et $\\mathcal{C}_f$ sa courbe représentative.<br>'
+          texte += `On sait que  $f(${a})=${b}~~$ et que $~~f'(${a})=${c}$.`
+          texte += `<br>Déterminer une équation de la tangente $(T)$ à la courbe $\\mathcal{C}_f$ au point d'abscisse $${a}$,`
+          texte += '<br>en utilisant la formule de cours de l\'équation de tangente.'
+          texteCorr = ` $${a}\\in[-5;5]$ donc la fonction est dérivable en $${a}$.`
+          texteCorr += ` <br> On peut donc appliquer la formule de cours qui donne une équation de la tangente $(T)$ au point d'abscisse $${a}$ : `
+          texteCorr += '<br> $\\begin{aligned} '
+          texteCorr += '(T) : y&=f\'(a)(x-a)+f(a)&\\text{On cite la relation de cours.}\\\\ '
+          texteCorr += `(T) : y&=f'(${a})(x-${ecritureParentheseSiNegatif(a)})+f(${a})&\\text{On applique à l'énoncé.}\\\\ `
+          texteCorr += `(T) : y&=${c}(x-${ecritureParentheseSiNegatif(a)})${ecritureAlgebrique(b)}&\\text{On remplace les valeurs connues.}\\\\ `
+          if (a < 0) { texteCorr += `(T) : y&=${c}(x${ecritureAlgebrique(-a)})${ecritureAlgebrique(b)}&\\text{On simplifie l'expression.}\\\\ ` }
+          texteCorr += `(T) : y&=${c}x${ecritureAlgebrique(-a * c)}${ecritureAlgebrique(b)}&\\text{On développe.}\\\\ `
+
           texteCorr += '\\end{aligned}$'
-          texteCorr += '<br>On cherche maintenant la limite du taux de variations quand $h$ tend vers $0$.'
-          texteCorr += `<br>$\\lim\\limits_{h \\rightarrow 0} \\dfrac{-1}{(${a}+h)\\times ${ecritureParentheseSiNegatif(a)}}= \\dfrac{-1}{${a * a}} $`
-          if (a !== 1 && a !== -1) { texteCorr += `<br>On peut donc conclure que $f'(${a})=\\dfrac{-1}{${a * a}} $` } else { texteCorr += `<br>On peut donc conclure que $f'(${a})=-1 $` }
+          texteCorr += `<br>On peut conclure que : $(T) : y=${c}x ${ecritureAlgebrique(b - c * a)}$.`
           break
       }
 
@@ -93,5 +95,5 @@ export default function equationdetangente () {
     }
     listeQuestionsToContenu(this) // On envoie l'exercice à la fonction de mise en page
   }
-  this.besoinFormulaireNumerique = ['Type de fonctions :', 5, '1 : Fonction affine 2 : Fonction carré 3: Fonction inverse 4: Fonction racine carrée 5: Méli-mélo']
+  this.besoinFormulaireNumerique = ['Consigne :', 2, '1 : avec formule. 2 : avec démonstration.']
 }
