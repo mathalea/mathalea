c7706156fd313528de4ec59c15d0991ac0080349
Stéphane <mathsguyon@gmail.com>
2021-12-20 00:55:00 +0100

    version avancée 1P10
    arbre à terminer
    pb arrondis, float

--- src/js/exercices/1e/1P10.js
+++ src/js/exercices/1e/1P10.js
@@ -1,5 +1,6 @@
 import Exercice from '../Exercice.js'
-import { listeQuestionsToContenu, combinaisonListes, randint } from '../../modules/outils.js'
+import { listeQuestionsToContenu, combinaisonListes, randint, texFraction, texNombre, arrondi } from '../../modules/outils.js'
+import { labelPoint, mathalea2d, point, segment } from '../../modules/2d.js'
 export const titre = 'Nom de l\'exercice'
 
 // Les exports suivants sont optionnels mais au moins la date de publication semble essentielle
@@ -28,12 +29,21 @@ export default function ProbabilitésConditionnelles () {
     const typeQuestionsDisponibles = ['type1'] // On créé 3 types de questions
 
     const listeTypeQuestions = combinaisonListes(typeQuestionsDisponibles, this.nbQuestions) // Tous les types de questions sont posés mais l'ordre diffère à chaque "cycle"
-    for (let i = 0, a, v, av, texte, texteCorr, cpt = 0; i < this.nbQuestions && cpt < 50;) { // Boucle principale où i+1 correspond au numéro de la question
+    for (let i = 0, a, v, av, A, B, O, pA, pA1, s, s1, k1, k2, texte, texteCorr, cpt = 0; i < this.nbQuestions && cpt < 50;) { // Boucle principale où i+1 correspond au numéro de la question
       switch (listeTypeQuestions[i]) { // Suivant le type de question, le contenu sera différent
         case 'type1':
           a = randint(30, 70)// p(A)
           v = randint(30, 70)// P_T(V)
           av = randint(30, 70)// P(A \cap V)
+          A = point(4, 3, 'A') // 1er noeud Avion
+          B = point(4, 1, '$\\bar A$')// 1er noeud événement contraire \bar A
+          O = point(2, 2, '$\\Omega$')// Univers, point de départ de l'arbre
+          pA = point(3, 2, '$0,4$')// proba de A, ici ${a}
+          pA1 = point(3, 1, '$0,6$')// proba de \\bar A 1-${a}
+          // Il manque le 2ème noeud avec  V et \\bar V
+          labelPoint(pA, pA1, O, A, B)
+          s = segment(O, A, 'blue')
+          s1 = segment(O, B, 'red')
           texte = 'Une agence de voyage propose deux formules week-end pour se rendre à Londres depuis Paris.'
           texte += '<br> Les clients choisissent leur moyen de transport : train ou avion.'
           texte += '<br> De plus, s\'ils le souhaitent, ils peuvent compléter leur formule par l\'option "visites guidées".'
@@ -45,13 +55,31 @@ export default function ProbabilitésConditionnelles () {
           texte += '<br> On considère les évènements suivants:'
           texte += '<br> $\\bullet~~$ $A$ :  le client a choisi l\'avion.'
           texte += '<br> $\\bullet~~$ $V$ : le client a choisi l\'option "visites guidées".<br>'
-
           texte += '<br> 1. Déterminer $P_A(V)$.'
-          texte += '<br> 2.  Démontrer que la probabilité pour que le client interrogé ait choisi l\'option "visites guidées" est égale à $0,42$.'
+          texte += `<br> 2.  Démontrer que la probabilité pour que le client interrogé ait choisi l'option "visites guidées" est égale à $${texNombre(av / 100 + (1 - a / 100) * v / 100)}$.`
           texte += '<br> 3. Calculer la probabilité pour que le client interrogé ait pris l\'avion sachant qu\'il n\'a pas choisi l\'option "visites guidées". Arrondir le résultat au centième.'
           texte += '<br> 4. On interroge au hasard deux clients de manière aléatoire et indépendante.'
           texte += '<br> Quelle est la probabilité qu\'aucun des deux ne prennent l\'option "visites guidées" ?'
-          texteCorr = `Correction ${i + 1} de type 1`
+          texteCorr = '1. De l\'énoncé on déduit que :'
+          texteCorr += `<br> $P(A)=${a / 100}$`
+          texteCorr += `<br> $P_{\\bar{A}}(V)=${v / 100}$`
+          texteCorr += `<br> $P(A \\cap V)=${av / 100}$`
+          texteCorr += `<br>On a donc $P_{A}(V)=\\dfrac{P(A \\cap V)}{P(A)}=\\dfrac{${av / 100}}{${a / 100}}=${texFraction(av, a)} $.`
+          texteCorr += '<br><br>2. Comme $A$ et $\\bar A$ forment une partition de l\'univers, d\'après la loi des probabilités totales :'
+          texteCorr += ' <br>$P(V)=P(A \\cap V)+P(\\bar{A} \\cap V) . $'
+          texteCorr += mathalea2d(-10, -10, 20, 20, A, B, O, pA1, pA, s, s1)
+          texteCorr += `<br>Or $P(\\bar{A} \\cap V)=P(\\bar{A}) \\times P_{\\bar{A}}(V)=(1-${a / 100}) \\times ${v / 100}=${texNombre((1 - a / 100) * v / 100)}$.`
+          texteCorr += `<br>Donc $P(V)=${av / 100}+${(1 - a / 100) * v / 100}=${texNombre(av / 100 + (1 - a / 100) * v / 100)}$.`
+          texteCorr += '<br><br>3. On a $P_{\\bar{V}}(A)=\\dfrac{P(\\bar{V} \\cap A)}{P(\\bar{V})}=\\dfrac{P(A \\cap \\bar{V})}{P(\\bar{V})}=\\dfrac{P(A) \\times P_A(\\bar{V})}{P(\\bar{V})}$.'
+          texteCorr += `<br>Or d'après la question précédente: $P(\\bar{V})=1-P(V)=1-${texNombre(av / 100 + (1 - a / 100) * v / 100)}=${texNombre(1 - (av / 100 + (1 - a / 100) * v / 100))}$`
+          texteCorr += `<br>et d'après la question $1: P_{A}(\\bar{V})=1-P_{A}(V)=1-${texFraction(av, a)}=${texFraction(a - av, a)}$.`
+          k1 = (a - av) / a
+          k2 = 1 - (av / 100 + (1 - a / 100) * v / 100)
+          texteCorr += `<br>Donc $P_{\\bar{V}}(A)=\\dfrac{${a / 100} \\times ${texFraction(a - av, a)}}{${texNombre(k2)}} \\approx ${arrondi(((a / 100) * k1) / k2)}$.`
+          texteCorr += `<br><br>4. On a vu que $P(\\bar{V})=1-${k2}=${1 - k2}$.`
+          texteCorr += 'Comme les deux événements sont indépendants, $P(\\bar{V_1}\\cap\\bar{V_2})=P(\\bar{V_1}\\times P(\\bar{V_2})$'
+          texteCorr += `<br>La probabilité cherchée est donc égale à $${1 - k2} \\times ${1 - k2}\\approx ${arrondi((1 - k2) ** 2)}$.`
+
           break
       }
       // Si la question n'a jamais été posée, on l'enregistre
@@ -65,3 +93,5 @@ export default function ProbabilitésConditionnelles () {
     listeQuestionsToContenu(this) // On envoie l'exercice à la fonction de mise en page
   }
 }
+
+// permet de repérer les points A et C sur la droite (AC)
