1d7a93e5176fa118bb772a44ab59b014b3e68250
Jean-Claude Lhote <lhotejc@gmail.com>
2021-12-21 20:53:49 +0100

    temp: ça pousse

--- src/js/modules/arbres.js
+++ src/js/modules/arbres.js
@@ -7,7 +7,8 @@ export class Arbre {
     this.proba = proba
   }
 
-  ajouteFils (nom, proba) {
+  // questionnement : est-ce qu'on vérifie à chaque ajout que la somme des probabilités ne dépasse pas 1 ?
+  setFils (nom, proba) {
     this.enfants.push(new Arbre(this, nom, proba))
   }
 
@@ -15,10 +16,36 @@ export class Arbre {
     for (const arbre of this.enfants) {
       if (arbre.nom === nom) return arbre
     }
-    return null
+    return false
   }
 
   getFilsProba (nom) {
-    return this.getFils(nom).proba
+    const arbre = this.getFils(nom)
+    return arbre ? arbre.proba : 0
   }
+
+  // est-ce qu'on vérifie si la somme des probabilités ne dépasse pas 1 ?
+  setFilsProba (nom, proba) { // si le fils nommé nom existe, on fixe sa proba (en gros, on la modifie)
+    const arbre = this.getFils(nom)
+    if (arbre) {
+      arbre.proba = proba
+      return true
+    } else { // sinon on ajoute ce fils.
+      this.enfants.push(new Arbre(this, nom, proba))
+    }
+  }
+
+  // Essai de fonction récursive pour calculer la probabilité d'un événement.
+  /* getProba (nom, proba) {
+    let p
+    if (this.enfants.length !== 0) {
+      for (const arbre of this.enfants) {
+        if (arbre.nom === nom) p = proba * arbre.proba
+        }
+        return arbre.getProba(nom, proba)
+      }
+    } else {
+      return 0
+    }
+  } */
 }
