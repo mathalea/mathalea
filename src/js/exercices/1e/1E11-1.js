3ad821303477e35451da30a5eaed2dd094b3c211
fpiou <frederic.piou@free.fr>
2022-01-14 00:44:23 +0100

    fix: 1E11-1
    Correction de 1E11-1
    Modification de toTex dans outilsMathjs :
    - récusrivité
    - correction des 1*n et -1*n en n et -n
    - ne pas rendre implicit les multiplications par un (-n)

--- src/js/exercices/1e/1E11-1.js
+++ src/js/exercices/1e/1E11-1.js
@@ -1,6 +1,7 @@
 
 import Exercice from '../Exercice'
-import { choice, ecritureAlgebrique, ecritureAlgebriqueSauf1, randint } from '../../modules/outils'
+import { toTex, aleaVariables } from '../../modules/outilsMathjs'
+import { simplify } from 'mathjs'
 export const titre = 'calculer le discriminant d\'un polynôme du second degré'
 export const interactifReady = true
 export const interactifType = 'mathLive'
@@ -10,20 +11,26 @@ export const amcType = 'AMCNum'
 /*!
  * @author Jean-Claude Lhote
  */
-export default function NombrePairFois5 () {
+export default function calculDiscrimant () {
   Exercice.call(this)
   this.typeExercice = 'simple'
   this.nbQuestions = 1
   this.nouvelleVersion = function () {
     this.autoCorrection = []
-    const a = randint(1, 5) * choice([-1, 1])
-    const b = randint(-5, 5)
-    const c = randint(-5, 5)
-    const d = b * b - 4 * a * c
-    if (b === 0) { this.question = `Le discriminant de  $${ecritureAlgebriqueSauf1(a)}x^2$` } else { this.question = `Le discriminant de  $${ecritureAlgebriqueSauf1(a)}x^2${ecritureAlgebriqueSauf1(b)}x$` }
-    if (c !== 0) { this.question += `$${ecritureAlgebrique(c)}$` }
-    this.question += '  est '
-    this.correction = `$\\Delta =b^2-4ac=${b}^2 - 4 \\times ${a} \\times ${c}=${b * b - 4 * a * c}$`
-    this.reponse = d
+
+    const variables = aleaVariables(
+      {
+        a: 'randomInt(1,5) * pickRandom([-1, 1])',
+        b: 'randomInt(-5, 5)',
+        c: 'randomInt(-5, 5)',
+        disc: 'b^2-4*a*c'
+      }
+    )
+    const polynomeTex = toTex(simplify('a*x^2+b*x+c', [], variables))
+    const discriminantTex = toTex(simplify('b^2-4*a*c', [], variables))
+
+    this.correction = `$\\Delta = b^2-4ac=${discriminantTex}=${variables.disc}$`
+    this.question = `Le discriminant de $${polynomeTex}$ est : `
+    this.reponse = variables.disc
   }
 }
