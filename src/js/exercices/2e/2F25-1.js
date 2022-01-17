8a913bda078080a6e335fd10b0be36a7f0506859
Jean-Claude Lhote <lhotejc@gmail.com>
2022-01-17 21:13:16 +0100

    fix: amélioration visuelle + nettoyage du code.

--- src/js/exercices/2e/2F25-1.js
+++ src/js/exercices/2e/2F25-1.js
@@ -1,6 +1,6 @@
 import Exercice from '../Exercice.js'
-import { listeQuestionsToContenu, randint, combinaisonListes, reduireAxPlusB } from '../../modules/outils.js'
-import { courbe2, labelPoint, mathalea2d, point, repere2, segment, tracePoint } from '../../modules/2d.js'
+import { listeQuestionsToContenu, randint, combinaisonListes } from '../../modules/outils.js'
+import { courbe2, latexParCoordonnees, mathalea2d, point, repere2, segment, tracePoint } from '../../modules/2d.js'
 
 export const titre = 'Propriétés graphiques de la parité d\'une fonction.'
 
@@ -25,36 +25,35 @@ export default function EtudierGraphiqueParite () {
     this.listeQuestions = [] // Liste de questions
     this.listeCorrections = [] // Liste de questions corrigées
     let typesDeQuestionsDisponibles = []
-    typesDeQuestionsDisponibles = [2]
+    typesDeQuestionsDisponibles = [1, 2]
 
     const listeTypeDeQuestions = combinaisonListes(typesDeQuestionsDisponibles, this.nbQuestions)
-    for (let i = 0, texte, texteCorr, cpt = 0, A, B, A0, B0, A1, B1, s1, s2, s3, s4, a, b, c, d, e, f, k, r, x, C, tA, tB, tA0, tB0, tA1, tB1, lA1, lB1, lA0, lB0, lA, lB = [], typesDeQuestions; i < this.nbQuestions && cpt < 50;) {
+    for (let i = 0, texte, texteCorr, cpt = 0, A, B, s1, s2, s3, s4, a, b, f, r, x, C, traceAetB, labA1, labB1, labA0, labB0, lA, lB = [], typesDeQuestions; i < this.nbQuestions && cpt < 50;) {
       typesDeQuestions = listeTypeDeQuestions[i]
 
       switch (typesDeQuestions) {
         case 1:// Cas f(x)=ax+b
           a = randint(-2, 2, [0])
           b = randint(-2, 2, [0])
-          c = randint(-8, 8, [0])
           r = repere2({ xMin: -7, xMax: 9, yMin: -7, yMax: 7 })
-          x = randint(-2, 2, [0])
+          x = randint(-1, 1, [0]) * 2
           f = x => a * x + b
           C = courbe2(f, { repere: r, step: 0.25 })
           // C.color = 'red'
           // C.epaisseur = 2
-          B = point(x, a * x + b, 'M')
-          A = point(-x, -a * x + b, 'M\'')
+          B = point(x, a * x + b)
+          A = point(-x, -a * x + b)
 
-          tA = tracePoint(A, 'red') // Variable qui trace les points avec une croix
-          tB = tracePoint(B, 'red') // Variable qui trace les points avec une croix
-          lA = labelPoint(A, 'red')// Variable qui trace les nom s A et B
-          lB = labelPoint(B, 'red')// Variable qui trace les nom s A et B
+          labA0 = latexParCoordonnees('-x', -x, 0.8 * (a > 0 ? 1 : -1), 'red', 20, 10, 'white', 8)
+          labB0 = latexParCoordonnees('x', x, 0.8 * (a > 0 ? -1 : 1), 'red', 20, 10, 'white', 8)
+          lA = latexParCoordonnees('M\'', -x - 1.1, -a * x + b, 'red', 15, 10, 'white', 6)
+          lB = latexParCoordonnees('M', x - 1.1, a * x + b, 'red', 15, 10, 'white', 6)
+          labA1 = latexParCoordonnees('f(-x)', 0.5, -a * x + b, 'red', 30, 10, 'white', 8)
+          labB1 = latexParCoordonnees('f(x)', -1.5, a * x + b, 'red', 25, 10, 'white', 8)
+          traceAetB = tracePoint(A, B, 'red') // Variable qui trace les points avec une croix
+          traceAetB.taille = 4
+          traceAetB.epaisseur = 2
 
-          tA.taille = 5
-          tA.epaisseur = 2
-
-          tB.taille = 5
-          tB.epaisseur = 2
           texte = mathalea2d({ xmin: -7.5, xmax: 9.5, ymin: -7.5, ymax: 7.5, scale: 0.6 }, r, C)
           texteCorr = 'On observe que la représentation graphique n\'admet pas l\'axe des ordonnées comme axe de symétries,'
           texteCorr += ' ni l\'origine comme centre de symétrie.<br> '
@@ -64,54 +63,41 @@ export default function EtudierGraphiqueParite () {
           texteCorr += 'On observe bien que ces deux points ont des ordonnées ni égales, ni opposées.<br>'
           texteCorr += 'La fonction représentée est donc ni paire, ni impaire.<br>'
 
-          texteCorr += mathalea2d({ xmin: -7.5, xmax: 9.5, ymin: -7.5, ymax: 7.5, scale: 0.6 }, lA, lB, tA, tB, r, C)
+          texteCorr += mathalea2d({ xmin: -7.5, xmax: 9.5, ymin: -7.5, ymax: 7.5, scale: 0.6 }, r, C, lA, lB, traceAetB)
 
           break
         case 2:// Cas f(x)=ax
           a = randint(-2, 2, [0])
-          b = 0
-          c = randint(-8, 8, [0])
           r = repere2({ xMin: -7, xMax: 9, yMin: -7, yMax: 7 })
           x = randint(2, 3, [0])
-          f = x => a * x + b
+          f = x => a * x
           C = courbe2(f, { repere: r, step: 0.25 })
           // C.color = 'red'
           // C.epaisseur = 2
-          B = point(x, a * x + b, 'M','right')
-          A = point(-x, -a * x + b, 'M\'','left')
-          A0 = point(-x, 0, '-x')
-          B0 = point(x, 0, 'x','left')
-          A1 = point(0, -a * x, '$f(-x)$', 'right')
-          B1 = point(0, a * x, '$f(x)$','left')
-          tA0 = tracePoint(A0, 'red')
-          tA1 = tracePoint(A1, 'red')
-          tB1 = tracePoint(B1, 'red')
-          tB0 = tracePoint(B0, 'red')
-          tA = tracePoint(A, 'red') // Variable qui trace les points avec une croix
-          tB = tracePoint(B, 'red') // Variable qui trace les points avec une croix
-          lA = labelPoint(A, 'red')// Variable qui trace les nom s A et B
-          lB = labelPoint(B, 'red')// Variable qui trace les nom s A et B
-          lB0 = labelPoint(B0, 'red')// Variable qui trace les nom s A et B
-          lA0 = labelPoint(A0, 'red')// Variable qui trace les nom s A et B
-          lA1 = labelPoint(A1, 'red')// Variable qui trace les nom s A et B
-          lB1 = labelPoint(B1, 'red')// Variable qui trace les nom s A et B
-          s1 = segment(B, B0, 'red')
-          s2 = segment(A, A0, 'red')
-          s3 = segment(A, A1, 'red')
-          s4 = segment(B, B1, 'red')
+          B = point(x, a * x)
+          A = point(-x, -a * x)
+          labA0 = latexParCoordonnees('-x', -x - 0.2, 0.8 * (a > 0 ? 1 : -1), 'red', 20, 10, 'white', 8)
+          labB0 = latexParCoordonnees('x', x, 0.8 * (a > 0 ? -1 : 1), 'red', 20, 10, 'white', 8)
+          lA = latexParCoordonnees('M\'', -x - 1, -a * x, 'red', 15, 10, 'white', 7)
+          lB = latexParCoordonnees('M', x + 1, a * x, 'red', 15, 10, 'white', 7)
+          labA1 = latexParCoordonnees('f(-x)', 1.2, -a * x + 0.2, 'red', 30, 10, '', 8)
+          labB1 = latexParCoordonnees('f(x)', -2, a * x + 0.2, 'red', 25, 10, '', 8)
+          traceAetB = tracePoint(A, B, 'red') // objet qui contient les croix des points
+          s1 = segment(x, a * x, x, 0, 'red')
+          s2 = segment(-x, -a * x, -x, 0, 'red')
+          s3 = segment(-x, -a * x, 0, -a * x, 'red')
+          s4 = segment(x, a * x, 0, a * x, 'red')
           s1.pointilles = true
           s2.pointilles = true
           s3.pointilles = true
           s4.pointilles = true
-          s1.epaisseur = 3
-          s2.epaisseur = 3
-          s3.epaisseur = 3
-          s4.epaisseur = 3
-          tA.taille = 5
-          tA.epaisseur = 2
+          s1.epaisseur = 2
+          s2.epaisseur = 2
+          s3.epaisseur = 2
+          s4.epaisseur = 2
+          traceAetB.taille = 4
+          traceAetB.epaisseur = 2
 
-          tB.taille = 5
-          tB.epaisseur = 2
           texte = mathalea2d({ xmin: -7.5, xmax: 9.5, ymin: -7.5, ymax: 7.5, scale: 0.6 }, r, C)
           texteCorr = 'On observe que la représentation graphique admet l\'origine comme centre de symétrie.<br> '
           texteCorr += 'Prenons un point $M$ de la courbe, d\'abscisse $x$, et '
@@ -120,12 +106,12 @@ export default function EtudierGraphiqueParite () {
           texteCorr += 'On observe bien que ces deux points qui ont des abscisses opposées, ont aussi des ordonnées opposées.<br>'
           texteCorr += 'La fonction représentée est impaire.<br>'
 
-          texteCorr += mathalea2d({ xmin: -7.5, xmax: 9.5, ymin: -7.5, ymax: 7.5, scale: 0.6 }, lA, lB, tA, tB, lA0, lB0, tA0, tB0, tB1, tA1, lB1, lA1, r, C, s1, s2, s3, s4)
+          texteCorr += mathalea2d({ xmin: -7.5, xmax: 9.5, ymin: -7.5, ymax: 7.5, scale: 0.6 }, r, C, lA, lB, traceAetB, labB0, labB1, labA1, s1, s2, s3, s4, labA0)
 
           break
       }
 
-      if (this.questionJamaisPosee(i, k, a, b, c, d, e)) {
+      if (this.questionJamaisPosee(i, a, b)) {
         // Si la question n'a jamais été posée, on en créé une autre
         this.listeQuestions.push(texte)
         this.listeCorrections.push(texteCorr)
