3dd541e02f139ed62a665efe9eae77e1ccb53240
Stéphane <mathsguyon@gmail.com>
2022-02-05 17:58:18 +0100

    rajout package tikz tab

--- src/js/exercices/2e/2F31-3.js
+++ src/js/exercices/2e/2F31-3.js
@@ -6,7 +6,7 @@ export const titre = 'Comparaison d\'images dans un tableau de variations'
 // Les exports suivants sont optionnels mais au moins la date de publication semble essentielle
 export const dateDePublication = '25/10/2021' // La date de publication initiale au format 'jj/mm/aaaa' pour affichage temporaire d'un tag
 export const dateDeModifImportante = '24/10/2021' // Une date de modification importante au format 'jj/mm/aaaa' pour affichage temporaire d'un tag
-
+this.listePackages = ['tkz-tab'] 
 /**
  * Description didactique de l'exercice
  * @author
