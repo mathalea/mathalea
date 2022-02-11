af87ff991a030e399ec869e2711b2eec2203686d
DegrangeM <53106394+DegrangeM@users.noreply.github.com>
2022-02-11 22:55:46 +0100

    Prototype fonctionnel de <mathalea-moodle>

--- src/assets/externalJs/moodle.js
+++ src/assets/externalJs/moodle.js
@@ -1,4 +1,8 @@
 if (typeof window.iMathAlea === 'undefined') {
+  // Normalement ce script ne devrait être chargé qu'une unique fois car appelé en module
+  // On vérifie tout de même au cas où que le fichier ne soit pas appelé en module
+  // Ou que l'on appelle des fichiers depuis des serveurs différents
+  // Remarque dans ce dernier cas : tout les exos seront chargés depuis le premier serveur
   window.iMathAlea = []
 
   window.addEventListener('message', (event) => {
@@ -26,59 +30,56 @@ if (typeof window.iMathAlea === 'undefined') {
       // Always call super first in constructor
       super()
 
-      const SERVEUR_URL = new URL('../..', document.currentScript.src).href // ou origin + pathname
-      // Create a shadow root
-      const shadow = this.attachShadow({ mode: 'open' })
+      const SERVEUR_URL = new URL('../..', document.currentScript?.src || import.meta.url).href // ou origin + pathname
 
-      // Take attribute content and put it inside the info span
-      const text = this.getAttribute('data-text')
-      info.textContent = text
+      const shadow = this.attachShadow({ mode: 'open' }) // this.shadowRoot
 
-      shadow.appendChild()
+      const iMoodle = window.iMathAlea.length
+      let questionSeed = ''
+
+      let questionDiv = this.parentNode
+      // On remonte de parent en parent depuis la balise script jusqu'à trouver le div avec le numero de la question en id
+      while (questionDiv !== null) { // s'arrêtera lorsqu'il n'y aura plus de parents
+        if (typeof questionDiv.id === 'string' && questionDiv.id.startsWith('question-')) {
+          questionSeed = questionDiv.id
+          break // la seed a été trouvée
+        }
+        questionDiv = questionDiv.parentNode
+      }
+
+      let answer
+      const addIframe = () => {
+        iframe.setAttribute('width', '100%')
+        iframe.setAttribute('height', '400')
+        iframe.setAttribute('src', SERVEUR_URL + 'mathalea.html?ex=' + this.getAttribute('ex') + '&v=' + (this.getAttribute('correction') === null ? 'exMoodle' : 'correctionMoodle') + '&z=1&iMoodle=' + iMoodle + '&serie=' + questionSeed + (typeof answer !== 'undefined' ? '&moodleJson=' + answer : ''))
+        iframe.setAttribute('frameBorder', '0')
+        iframe.setAttribute('allow', 'fullscreen')
+        shadow.appendChild(iframe)
+      }
+
+      const iframe = document.createElement('iframe')
+      window.iMathAlea.push(this)
+
+      if (questionDiv.classList.contains('notyetanswered')) {
+        // L'élève n'a pas encore répondu à la question, on affiche immédiatement l'iframe
+        addIframe()
+      } else {
+        // L'élève a répondu, on attend que la page charge pour récupérer ses réponses
+        document.addEventListener('DOMContentLoaded', () => {
+          answer = questionDiv.querySelector('[name$="_answer"]').value
+          answer = answer.substring(answer.indexOf('|') + 1)
+          addIframe()
+        })
+      }
+
+      shadow.appendChild(iframe)
+    }
+
+    connectedCallback () {
+      // Différent de constructor si l'élément est créé avec createElement
     }
   }
 
   // Define the new element
   customElements.define('mathalea-moodle', MathALeaMoodle)
 }
-
-/*
-const currentScript = document.currentScript
-const iMoodle = window.iMathAlea.length
-let questionSeed = ''
-
-let questionDiv = currentScript
-// On remonte de parent en parent depuis la balise script jusqu'à trouver le div avec le numero de la question en id
-while (questionDiv !== null) { // s'arrêtera lorsqu'il n'y aura plus de parents
-  if (typeof questionDiv.id === 'string' && questionDiv.id.startsWith('question-')) {
-    questionSeed = questionDiv.id
-    break // la seed a été trouvée
-  }
-  questionDiv = questionDiv.parentNode
-}
-
-let answer
-const addIframe = () => {
-  iframe.setAttribute('width', '100%')
-  iframe.setAttribute('height', '400')
-  iframe.setAttribute('src', url + '&iMoodle=' + iMoodle + '&serie=' + questionSeed + (typeof answer !== 'undefined' ? '&moodleJson=' + answer : ''))
-  iframe.setAttribute('frameBorder', '0')
-  iframe.setAttribute('allow', 'fullscreen')
-  currentScript.parentNode.insertBefore(iframe, currentScript)
-}
-
-const iframe = document.createElement('iframe')
-window.iMathAlea.push(iframe)
-
-if (questionDiv.classList.contains('notyetanswered')) {
-  // L'élève n'a pas encore répondu à la question, on affiche immédiatement l'iframe
-  addIframe()
-} else {
-  // L'élève a répondu, on attend que la page charge pour récupérer ses réponses
-  document.addEventListener('DOMContentLoaded', () => {
-    answer = questionDiv.querySelector('[name$="_answer"]').value
-    answer = answer.substring(answer.indexOf('|') + 1)
-    addIframe()
-  })
-}
-*/
