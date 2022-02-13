aa1263938ac43ce65d6365f68a3f1e30ed0e43b8
Jean-Claude Lhote <lhotejc@gmail.com>
2022-02-13 21:39:40 +0100

    temp: beta3L13-3 amélioré

--- src/js/exercices/beta/beta3L13-3.js
+++ src/js/exercices/beta/beta3L13-3.js
@@ -1,5 +1,5 @@
-import { combinaisonListes, listeQuestionsToContenu, randint } from '../../modules/outils'
-import { resoudreEquation } from '../../modules/outilsMathjs'
+import { arrondi, choice, combinaisonListes, listeQuestionsToContenu, prenom, randint, texNombre, texPrix } from '../../modules/outils'
+import { resoudre, resoudreEquation } from '../../modules/outilsMathjs'
 import Exercice from '../Exercice'
 export const titre = 'Problèmes à mettre en équation et à résoudre'
 export default class ProblemesEnEquation extends Exercice {
@@ -10,9 +10,11 @@ export default class ProblemesEnEquation extends Exercice {
   }
 
   nouvelleVersion () {
-    const listeTypeDeProblemes = ['basket']
+    const listeTypeDeProblemes = ['basket', 'achats']
     const listeDeProblemes = combinaisonListes(listeTypeDeProblemes, this.nbQuestions)
     for (let i = 0, cpt = 0, texte, x, a, b, d, equation, resolution, texteCorr; i < this.nbQuestions && cpt < 50;) {
+      const quidam = prenom()
+      const produit = choice(['fraises', 'pêches', 'poires', 'pommes', 'mangues', 'prunes', 'citrons'])
       switch (listeDeProblemes[i]) {
         case 'basket':
           x = randint(5, 15) // nombre de paniers à trois points
@@ -20,7 +22,7 @@ export default class ProblemesEnEquation extends Exercice {
           b = randint(15, 30) // nombre de points marqués au lancer franc
           d = b + (a + x) * 2 + x * 3 // nombre de points de la partie
           equation = `3*x+2*(${a}+x)+${b}=${d}`
-          resolution = resoudreEquation(equation)
+          resolution = resoudre(equation, { reduceSteps: false, substeps: true, comment: true })
           texte = `Une équipe de basket a marqué ${d} points lors d'un match. Au cours de ce match, elle a marqué ${b} points sur lancers francs.<br>`
           texte += `L'équipe a marqué ${a} paniers à deux points de plus que de paniers à trois points. Combien a-t-elle marqué de panier à trois points ?`
           texteCorr = `Posons $x$ le nombre de paniers à trois points. Le nombre de paniers à deux points est donc $${a}+x$. Le score de l'équipe fournit donc l'équation :<br>`
@@ -30,6 +32,19 @@ export default class ProblemesEnEquation extends Exercice {
           texteCorr += `<br>L'équipe a donc marqué ${x} paniers à trois points.`
           break
         case 'achats':
+          x = arrondi(randint(2, 4) + randint(0, 19) / 2, 2) // prix de 1kg de produit
+          a = arrondi(randint(2, 5) + randint(0, 45) / 5, 1) // nombre de kg de produit
+          b = arrondi(a * x, 2) // prix total du produit
+          d = b > 50 ? 100 : b > 20 ? 50 : b > 10 ? 20 : 10 // valeur du billet donné
+          equation = `${a}*x+${arrondi(d - b, 2)}=${d}`
+          resolution = resoudre(equation, { reduceSteps: false, substeps: true, comment: true })
+          texte = `${quidam} a acheté $${texNombre(a)}$ kg de ${produit} avec un billet de ${d} €. Le marchand lui a rendu ${texPrix(d - b)} €.<br>`
+          texte += `Quel est le prix d'un kilogramme de ${produit} ?`
+          texteCorr = `Posons $x$ le prix d'un kilogramme de ${produit}.<br>L'énoncé se traduit par l'équation suivante :<br>`
+          texteCorr += `$${resolution.equation}$`
+          texteCorr += '<br>Résolvons l\'équation :<br>'
+          texteCorr += resolution.texteCorr
+          texteCorr += `<br>Le prix d'un kilogramme de ${produit} est donc de ${x} €.`
 
           break
         case 'polygone':
