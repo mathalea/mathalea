7b416693c1e455afe676890e3fb2a8836906f23f
Jean-Claude Lhote <lhotejc@gmail.com>
2022-02-14 22:46:30 +0100

    fix: Problème d'arguments invalide dans un tracePoint()

--- src/js/exercices/4e/4G51.js
+++ src/js/exercices/4e/4G51.js
@@ -253,7 +253,7 @@ export default function RepresenterUnSolide4e () {
       }
 
       if (listeTypeDeQuestions[i] === 6) {
-        objetsEnonce.push(AB, BF, tracePoint(I, 0.5, 'red'), labelPoint(I),
+        objetsEnonce.push(AB, BF, tracePoint(I, 'red'), labelPoint(I),
           g,
           carreaux
         )
