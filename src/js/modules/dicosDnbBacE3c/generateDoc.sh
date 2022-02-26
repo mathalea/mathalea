44a2ad4d67140ece1140422064151a8a1066295e
Sébastien LOZANO <sebastien.lozano@ac-nancy-metz.fr>
2022-02-26 18:05:12 +0100

    feat: Un script python pour mettre à jour les dictionnaires DNB, BAC et E3C

---
+++ src/js/modules/dicosDnbBacE3c/generateDoc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# @author : Sébastien LOZANO
+
+########################################################################################################################################
+#==========> https://pdoc3.github.io/pdoc/
+#
+#  Script pour générer automatiquement la documentation du package du projet
+#==========>  documentation est le dossier créé qui contiendra la doc HTML dans un sous-dossier du nom du module, ici dicosDnbBacE3c
+#==========>  src/js/modules/dicosDnbBacE3c est le chemin vers le dossier contenant les modules du projet
+#
+########################################################################################################################################
+
+pdoc --force --html -o documentation src/js/modules/dicosDnbBacE3c
