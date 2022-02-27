adde6297b3cec159667449729b1962e8f18db92e
Sébastien LOZANO <sebastien.lozano@ac-nancy-metz.fr>
2022-02-27 12:38:01 +0100

    feat: menuDesExercicesDisponibles.js OK pour ajout E3C

--- src/js/modules/menuDesExercicesDisponibles.js
+++ src/js/modules/menuDesExercicesDisponibles.js
@@ -4,6 +4,7 @@ import dictionnaireDesExercicesAleatoires from './dictionnaireDesExercicesAleato
 import { dictionnaireC3 } from './dictionnaireC3.js'
 import { dictionnaireDNB } from './dictionnaireDNB.js'
 import { dictionnaireBAC } from './dictionnaireBAC.js'
+import { dictionnaireE3C } from './dictionnaireE3C.js'
 import { dictionnaireLycee } from './dictionnaireLycee.js'
 import { dictionnaireCrpe } from './dictionnaireCrpe.js'
 import $ from 'jquery'
@@ -32,8 +33,10 @@ const tableauTagsCrpe = dictionnaireToTableauTags(dictionnaireCrpe)
 
 const tableauTagsBAC = dictionnaireToTableauTags(dictionnaireBAC)
 
+const tableauTagsE3C = dictionnaireToTableauTags(dictionnaireE3C)
+
 // On concatène les différentes listes d'exercices
-export const dictionnaireDesExercices = { ...dictionnaireDesExercicesAleatoires, ...dictionnaireDNB, ...dictionnaireC3, ...dictionnaireLycee, ...dictionnaireCrpe, ...dictionnaireBAC }
+export const dictionnaireDesExercices = { ...dictionnaireDesExercicesAleatoires, ...dictionnaireDNB, ...dictionnaireC3, ...dictionnaireLycee, ...dictionnaireCrpe, ...dictionnaireBAC, ...dictionnaireE3C }
 let listeDesExercicesDisponibles
 if (getVueFromUrl() === 'amc') {
   const dictionnaireDesExercicesAMC = {}
@@ -55,7 +58,7 @@ function coupeChaine (titre, maxLength) {
 }
 
 function spanExercice (id, titre) {
-  // Construit le span de la ligne d'exercice (sauf pour les exercices dnb,bac ou crpe qui sont particuliers.
+  // Construit le span de la ligne d'exercice (sauf pour les exercices dnb,bac,e3c ou crpe qui sont particuliers.
   let maxLength
   // Selon la taille de la fenêtre (de l'écran) on tronque les titres trop longs pour qu'ils restent sur une ligne.
   if (document.getElementById('exercices_disponibles') && document.getElementById('exercices_disponibles').clientWidth > 600) {
@@ -127,6 +130,17 @@ function aBac (id, dictionnaire, mode) {
     return `<a style="line-height:2.5" class="lien_id_exercice" data-id_exercice="${id}">${dictionnaire[id].annee} - ${id.substr(9, 2)} - ${dictionnaire[id].lieu} - Ex ${dictionnaire[id].numeroInitial}</a> ${listeHtmlDesTags(dictionnaire[id])} <i id="${id}" class="eye icon icone_preview"></i></br>\n`
   }
 }
+
+function aE3c (id, dictionnaire, mode) {
+  // donne la ligne pour un exercice e3c lorsqu'on les regarde par année.
+  if (mode === 'annee') {
+    return `<a style="line-height:2.5" class="lien_id_exercice" data-id_exercice="${id}">${dictionnaire[id].lieu} - ${dictionnaire[id].mois} -  Ex ${dictionnaire[id].numeroInitial}</a> ${listeHtmlDesTags(dictionnaire[id])} <i id="${id}" class="eye icon icone_preview"></i></br>\n`
+  } else {
+    return `<a style="line-height:2.5" class="lien_id_exercice" data-id_exercice="${id}">${dictionnaire[id].annee} - ${id.substr(9, 2)} - ${dictionnaire[id].lieu} - Ex ${dictionnaire[id].numeroInitial}</a> ${listeHtmlDesTags(dictionnaire[id])} <i id="${id}" class="eye icon icone_preview"></i></br>\n`
+  }
+}
+
+
 function listeHtmlDesExercicesCrpeAnnee (annee) {
   let liste = ''
   const dictionnaire = filtreDictionnaireValeurCle(dictionnaireCrpe, 'annee', annee)
@@ -190,6 +204,28 @@ function listeHtmlDesExercicesBACTheme (theme) {
   tableauDesExercices = tableauDesExercices.sort().reverse()
   for (const id of tableauDesExercices) {
     liste += aBac(id, dictionnaire, 'theme')
+  }
+  return liste
+}
+function listeHtmlDesExercicesE3CAnnee (annee) {
+  let liste = ''
+  const dictionnaire = filtreDictionnaireValeurCle(dictionnaireE3C, 'annee', annee)
+  for (const id in dictionnaire) {
+    liste += aE3c(id, dictionnaire, 'annee')
+  }
+  return liste
+}
+function listeHtmlDesExercicesE3CTheme (theme) {
+  let liste = ''
+  const dictionnaire = filtreDictionnaireValeurTableauCle(dictionnaireE3C, 'tags', theme)
+  let tableauDesExercices = []
+  for (const id in dictionnaire) {
+    tableauDesExercices.push(id)
+  }
+  // On créé un tableau "copie" du dictionnaire pour pouvoir le trier dans l'inverse de l'ordre alphabétique et faire ainsi apparaitre les exercices les plus récents
+  tableauDesExercices = tableauDesExercices.sort().reverse()
+  for (const id of tableauDesExercices) {
+    liste += aE3c(id, dictionnaire, 'theme')
   }
   return liste
 }
@@ -310,6 +346,28 @@ function getListeHtmlDesExercicesBACTheme () {
   return liste
 }
 
+function getListeHtmlDesExercicesE3C () {
+  let liste = '<div class="accordion">'
+  for (const annee of ['2021']) {
+    liste += `<div class="title"><i class="dropdown icon"></i> ${annee}</div><div class="content">`
+    liste += listeHtmlDesExercicesE3CAnnee(annee)
+    liste += '</div>'
+  }
+  liste += '</div>'
+  return liste
+}
+
+function getListeHtmlDesExercicesE3CTheme () {
+  let liste = '<div class="accordion">'
+  for (const theme of tableauTagsE3C) {
+    liste += `<div class="title"><i class="dropdown icon"></i> ${theme}</div><div class="content">`
+    liste += listeHtmlDesExercicesE3CTheme(theme)
+    liste += '</div>'
+  }
+  liste += '</div>'
+  return liste
+}
+
 function getListeHtmlDesExercicesCrpeTheme () {
   let liste = '<div class="accordion">'
   for (const theme of tableauTagsCrpe) {
@@ -341,7 +399,7 @@ function listeHtmlDesTags (objet) {
 function divNiveau (obj, active, id) {
   // construction de la div contenant l'ensemble d'un niveau.
   let nombreExo = ''
-  if (id !== 'DNB' && id !== 'DNBtheme' && id !== 'CRPE' && id !== 'CrpeTheme' && id !== 'BAC' && id !== 'BACtheme') {
+  if (id !== 'DNB' && id !== 'DNBtheme' && id !== 'CRPE' && id !== 'CrpeTheme' && id !== 'BAC' && id !== 'BACtheme' && id !== 'E3C' && id !== 'E3Ctheme') {
     nombreExo = '(' + obj.nombre_exercices_dispo + ')'
   }
   return `<div id=${id} class="${active ? 'active title fermer_niveau' : 'title ouvrir_niveau'}"><i class="dropdown icon"></i>${obj.label} ${nombreExo}</div><div id="content${id}" class="${active} content">${active ? obj.liste_html_des_exercices : ''}</div>`
@@ -664,6 +722,18 @@ export function menuDesExercicesDisponibles () {
       liste_html_des_exercices: getListeHtmlDesExercicesBACTheme(),
       lignes_tableau: ''
     },
+    E3C: {
+      label: 'Exercices E3C (classés par année)',
+      nombre_exercices_dispo: 0,
+      liste_html_des_exercices: getListeHtmlDesExercicesE3C(),
+      lignes_tableau: ''
+    },
+    E3Ctheme: {
+      label: 'Exercices E3C (classés par thème)',
+      nombre_exercices_dispo: 0,
+      liste_html_des_exercices: getListeHtmlDesExercicesE3CTheme(),
+      lignes_tableau: ''
+    },
     C: {
       label: 'Calcul mental',
       nombre_exercices_dispo: 0,
@@ -791,6 +861,11 @@ export function menuDesExercicesDisponibles () {
         objExercicesDisponibles.BAC.lignes_tableau += ligneTableau(id)
       }
     }
+    if (id[0] === 'e' && id[1] === '3' && id[2] === 'c') {
+      if (filtre !== 'interactif') {
+        objExercicesDisponibles.E3C.lignes_tableau += ligneTableau(id)
+      }
+    }
   }
 
   listeHtmlDesExercices = '<div class="ui accordion">'
@@ -826,6 +901,11 @@ export function menuDesExercicesDisponibles () {
     listeHtmlDesExercices += divNiveau(objExercicesDisponibles.BACtheme, 'active', 'BACtheme')
     listeHtmlDesExercices += '</div>'
     listeHtmlDesExercicesTab += objExercicesDisponibles.BAC.lignes_tableau
+  } else if (filtre === 'e3c') {
+    listeHtmlDesExercices += divNiveau(objExercicesDisponibles.E3C, 'active', 'E3C')
+    listeHtmlDesExercices += divNiveau(objExercicesDisponibles.E3Ctheme, 'active', 'E3Ctheme')
+    listeHtmlDesExercices += '</div>'
+    listeHtmlDesExercicesTab += objExercicesDisponibles.E3C.lignes_tableau
   } else if (filtre === 'primaire') {
     listeHtmlDesExercices += divNiveau(objExercicesDisponibles.c3, 'active', 'c3')
     listeHtmlDesExercices += '</div>'
@@ -864,7 +944,7 @@ export function menuDesExercicesDisponibles () {
     listeHtmlDesExercicesTab += htmlAffichage.lignes
   } else {
     htmlAffichage = htmlListes({
-      liste_affichage: ['ca', 'c3', 6, 5, 4, 3, 'DNB', 'DNBtheme', 2, 1, 'T', 'Ex', 'HP', 'BAC', 'BACtheme', 'PE', 'CrpeTheme', 'CRPE', 'C'],
+      liste_affichage: ['ca', 'c3', 6, 5, 4, 3, 'DNB', 'DNBtheme', 2, 1, 'T', 'Ex', 'HP', 'E3C', 'E3Ctheme', 'BAC', 'BACtheme', 'PE', 'CrpeTheme', 'CRPE', 'C'],
       active: '',
       obj_ex: objExercicesDisponibles
     })
