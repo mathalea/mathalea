ac308dc6acac278f3fef71f2b1853a35085cf71c
Guillaume Valmont <guillaume.valmont@pm.me>
2022-03-02 20:28:32 +0400

    refactor: 3G32, 3G32-1, 3G32-2, 3G32-3, 3G32-4 et 3G32-5 sont maintenant des variantes de 3G32-0

--- src/js/exercices/3e/3G32-1.js
+++ src/js/exercices/3e/3G32-1.js
@@ -1,92 +1,15 @@
-import Exercice from '../Exercice.js'
-import { context } from '../../modules/context.js'
-import { listeQuestionsToContenu, randint, texNombre, texNombrec } from '../../modules/outils.js'
-import { tracePoint, labelPoint, afficheMesureAngle, codageAngleDroit, mathalea2d } from '../../modules/2d.js'
-import { point3d, vecteur3d, sphere3d, arete3d, rotationV3d, demicercle3d } from '../../modules/3d.js'
-import { setReponse } from '../../modules/gestionInteractif.js'
-import { ajouteChampTexteMathLive } from '../../modules/interactif/questionMathLive.js'
-export const amcReady = true // tant qu'il n'a pas été adapté à la version 2.6
-export const amcType = 'AMCOpenNum' // type de question AMC
-export const titre = 'Calcul d’un parallèle terrestre'
-export const interactifReady = true
-export const interactifType = 'mathLive'
+import problemesTrigoLongueur from '../3e/3G32-0.js'
+export const titre = 'Calcul d\'un parallèle terrestre'
+export { interactifReady, interactifType, amcReady, amcType } from '../3e/3G32-0.js'
 
 /**
- * propose de calculer la longueur d'un parallèle terrestre à partir de la latitde
- * @author Jean-Claude Lhote
- * Référence 3G32-1
-*/
-export default function CalculsTrigonometriques1 () {
-  Exercice.call(this) // Héritage de la classe Exercice()
-  this.consigne = ''
-  this.nbQuestions = 1
-  this.nbCols = 1 // Uniquement pour la sortie LaTeX
-  this.nbColsCorr = 1 // Uniquement pour la sortie LaTeX
-  this.tailleDiaporama = 3 // Pour les exercices chronométrés. 50 par défaut pour les exercices avec du texte
-  this.video = '' // Id YouTube ou url
-  this.spacingCorr = 2
-  this.spacing = 2
-
-  this.nouvelleVersion = function () {
-    this.autoCorrection = []
-    this.listeQuestions = [] // Liste de questions
-    this.listeCorrections = [] // Liste de questions corrigées
-    let alpha; let O; let H; let M; let R; let R2; let Axe; let normalV; let normalH; let P; let HP; let Sph; let OP; let PoleNord; let PoleSud; let objets = []
-
-    for (let i = 0, texte, texteCorr, reponse, cpt = 0; i < this.nbQuestions && cpt < 50;) {
-      // Boucle principale où i+1 correspond au numéro de la question
-
-      objets = []
-      alpha = randint(30, 60)
-      O = point3d(0, 0, 0, false, 'O')
-      M = point3d(5, 0, 0, true, 'M')
-      PoleNord = point3d(0, 0, 5, false, 'N')
-      PoleSud = point3d(0, 0, -5, false, 'S')
-      R = vecteur3d(O, M)
-      Axe = arete3d(PoleSud, PoleNord)
-      normalV = vecteur3d(0, 0, 1)
-      M = rotationV3d(M, normalV, context.anglePerspective)
-      M.c2d.nom = 'M'
-      normalH = rotationV3d(R, normalV, 90)
-      P = rotationV3d(M, normalH, -alpha)
-      P.c2d.nom = 'P'
-      H = point3d(0, 0, P.z, false)
-      R2 = vecteur3d(H, P)
-      H.c2d.nom = 'H'
-      Sph = sphere3d(O, 5, 1, 3)
-      HP = arete3d(H, P)
-      OP = arete3d(O, P)
-      objets.push(...Sph.c2d, Axe.c2d, HP.c2d, OP.c2d, codageAngleDroit(P.c2d, H.c2d, O.c2d), tracePoint(H.c2d, P.c2d, O.c2d, M.c2d), labelPoint(H.c2d, P.c2d, O.c2d, M.c2d))
-      objets.push(demicercle3d(H, normalV, R2, 'caché', 'red', 0), demicercle3d(H, normalV, R2, 'visible', 'red', 0))
-      objets.push(arete3d(O, M).c2d)
-      objets.push(afficheMesureAngle(M.c2d, O.c2d, P.c2d, 'black', 1.5, `${alpha}`))
-      texte = mathalea2d({ xmin: -8, ymin: -6, xmax: 8, ymax: 6, pixelsParCm: 20, scale: 0.5 }, objets) + '<br>'
-      texte += `Quelle est la longueur du $${alpha}$e parallèle Nord au kilomètre près ?`
-      texteCorr = mathalea2d({ xmin: -8, ymin: -6, xmax: 8, ymax: 6, pixelsParCm: 20, scale: 0.5 }, objets) + '<br>'
-      texteCorr += `Considérons que le $${alpha}$e parallèle Nord est un cercle. Soit $H$ le centre de ce cercle situé sur l'axe de rotation de la terre.<br>`
-      texteCorr += 'Les segments $[HP]$ et $[OM]$ sont parallèles, donc les angles alternes-internes $\\widehat{MOP}$ et $\\widehat{OPH}$ sont égaux.<br>'
-      texteCorr += 'Dans le triangle $OPH$ rectangle en $H$, $\\cos(\\widehat{OPH})=\\dfrac{HP}{OP}$ d\'où $HP=OP\\times \\cos(\\widehat{OPH})$.<br>'
-      texteCorr += 'Le rayon de la terre étant approximativement de $6400$ km, nous pouvons calculer $HP$ :<br>'
-      texteCorr += `$HP=6400\\times \\cos(${alpha})\\approx ${texNombrec(6400 * Math.cos(alpha * Math.PI / 180))}$ km.<br>`
-      reponse = Math.round(2 * Math.PI * 6400 * Math.cos(alpha * Math.PI / 180))
-      texteCorr += `Calculons maintenant la longueur $L$ du $${alpha}$e parallèle : $L\\approx 2\\times \\pi\\times ${texNombrec(6400 * Math.cos(alpha * Math.PI / 180))}\\approx ${texNombre(reponse)}$ km.<br>`
-
-      if (this.questionJamaisPosee(i, alpha)) {
-        // Si la question n'a jamais été posée, on en crée une autre
-        if (context.isAmc) {
-          this.autoCorrection[i] = { enonce: texte, propositions: [{ texte: texteCorr, statut: 3, feedback: '' }], reponse: { texte: 'Longueur arrondie au km près', valeur: [reponse], options: { digits: 0, decimals: 0 } } }
-        } else {
-          texte += ajouteChampTexteMathLive(this, i, 'largeur25 inline', { texte: ' $L=$', texteApres: ' km' })
-          setReponse(this, i, reponse)
-        }
-        this.listeQuestions.push(texte)
-        this.listeCorrections.push(texteCorr)
-        i++
-      }
-      cpt++
-    }
-    listeQuestionsToContenu(this)
-  }
+ * @author Guillaume Valmont
+ * reference 3G32-1
+ */
+export default function calculParalleleTerrestre () {
+  problemesTrigoLongueur.call(this)
+  this.titre = titre
+  this.sup2 = 2
+  this.besoinFormulaireCaseACocher = false
+  this.besoinFormulaire2Numerique = false
 }
-
-// python3 list-to-js.py pour faire apparaitre l'exercice dans le menu
--- src/js/exercices/3e/3G32-5.js
+++ src/js/exercices/3e/3G32-5.js
@@ -1,81 +1,15 @@
-import Exercice from '../Exercice.js'
-import { texFractionReduite, choice, texNombre, listeQuestionsToContenu, randint, arrondi, creerNomDePolygone } from '../../modules/outils.js'
-import { mathalea2d, angle, point, droite, droiteVerticaleParPoint, cercle, pointIntersectionLC, polygone, projectionOrtho, segment, codageAngleDroit, labelPoint, longueur } from '../../modules/2d.js'
-
-export const titre = 'Problème trigonométrique - Triangle rectangle inscrit dans un triangle rectangle'
+import problemesTrigoLongueur from '../3e/3G32-0.js'
+export const titre = 'Triangle rectangle inscrit dans un triangle rectangle'
+export { interactifReady, interactifType, amcReady, amcType } from '../3e/3G32-0.js'
 
 /**
- * Calculer un angle pour pouvoir ensuite calculer une longueur avec 2 triangles rectangles imbriqués
- * @author Rémi Angot
- * Référence 3G32-5
- * 2021-02-14
-*/
-export default function CalculsTrigo () {
-  Exercice.call(this)
+ * @author Guillaume Valmont
+ * reference 3G32-5
+ */
+export default function calculHauteurMontagne () {
+  problemesTrigoLongueur.call(this)
   this.titre = titre
-  this.nbQuestions = 1
-  this.nbQuestionsModifiable = false
-  this.nbCols = 1
-  this.nbColsCorr = 1
-  this.spacingCorr = 3
-
-  this.nouvelleVersion = function () {
-    this.listeQuestions = [] // tableau contenant la liste des questions
-    this.listeCorrections = []
-
-    let objetsEnonce = []; let paramsEnonce = {}; let texte = ''; let texteCorr = ''
-    const AD = randint(5, 9)
-    const AE = randint(AD + 1, AD + 4)
-    const AC = randint(3, AD - 1)
-    const A = point(0, 0, 'A', 'below left')
-    const C = point(AC, 0, 'C', 'below')
-    const D = point(AD, 0, 'D', 'below right')
-    const dDE = droiteVerticaleParPoint(D)
-    const cAE = cercle(A, AE)
-    const E = pointIntersectionLC(dDE, cAE, 'E')
-    E.positionLabel = 'right'
-    const p = polygone(A, D, E)
-    const dAE = droite(A, E)
-    const B = projectionOrtho(C, dAE, 'B', 'above left')
-    const codage1 = codageAngleDroit(A, B, C)
-    const codage2 = codageAngleDroit(A, D, E)
-    const labels = labelPoint(A, B, C, D, E)
-    const nomDesSommets = creerNomDePolygone(5)
-    A.nom = nomDesSommets[0]
-    B.nom = nomDesSommets[1]
-    C.nom = nomDesSommets[2]
-    D.nom = nomDesSommets[3]
-    E.nom = nomDesSommets[4]
-    const mirroir = choice([true, false])
-    if (mirroir) {
-      B.x *= -1
-      C.x *= -1
-      D.x *= -1
-      E.x *= -1
-      A.positionLabel = 'below'
-      B.positionLabel = 'above'
-      C.positionLabel = 'below'
-      D.positionLabel = 'below'
-      E.positionLabel = 'above'
-    }
-    const sBC = segment(B, C)
-
-    objetsEnonce = [p, sBC, codage1, codage2, labels]
-    paramsEnonce = { xmin: -10, ymin: -1, xmax: 10, ymax: E.y + 1.5, pixelsParCm: 20, scale: 1, mainlevee: false }
-    texte += mathalea2d(paramsEnonce, objetsEnonce)
-    texte += `<br><br> $${A.nom + E.nom} = ${AE}~\\text{cm}$, $${A.nom + D.nom} = ${AD}~\\text{cm}$ et $${A.nom + C.nom} = ${AC}~\\text{cm}$.`
-    texte += `<br> Calculer la longueur $${A.nom + B.nom}$ et donner une valeur approchée au millimètre près.`
-    texteCorr += `Dans le triangle $${A.nom + D.nom + E.nom}$ rectangle en $${D.nom}$ : `
-    texteCorr += `<br>$\\cos(\\widehat{${D.nom + A.nom + E.nom}})=\\dfrac{${A.nom + D.nom}}{${A.nom + E.nom}}\\quad$ soit $\\quad\\cos(\\widehat{${D.nom + A.nom + E.nom}})=\\dfrac{${AD}}{${AE}}$,`
-    texteCorr += `<br> d'où $\\widehat{${D.nom + A.nom + E.nom}}=\\text{arccos}\\left(\\dfrac{${AD}}{${AE}}\\right)\\approx${texNombre(arrondi(angle(D, A, E), 1))}\\degree$.`
-
-    texteCorr += `<br><br>Dans le triangle $${A.nom + B.nom + C.nom}$ rectangle en $${B.nom}$ : `
-    texteCorr += `<br>$\\cos(\\widehat{${B.nom + A.nom + C.nom}})=\\dfrac{${A.nom + B.nom}}{${A.nom + C.nom}}\\quad$ soit $\\quad\\cos(${texNombre(arrondi(angle(D, A, E), 1))}\\degree)\\approx\\dfrac{${A.nom + B.nom}}{${AC}}$,`
-    texteCorr += `<br> d'où $${A.nom + B.nom} \\approx ${AC} \\times \\cos(${texNombre(arrondi(angle(D, A, E), 1))}\\degree)\\approx${texNombre(arrondi(longueur(A, B), 1))}~\\text{cm}$.`
-
-    texteCorr += `<br><br>On pouvait aussi écrire : $${A.nom + B.nom} = ${AC} \\times \\cos\\left(\\text{arccos}\\left(\\dfrac{${AD}}{${AE}}\\right)\\right)=${AC}\\times\\dfrac{${AD}}{${AE}}=${texFractionReduite(AC * AD, AE)}$.`
-    this.listeQuestions.push(texte)
-    this.listeCorrections.push(texteCorr)
-    listeQuestionsToContenu(this) // On envoie l'exercice à la fonction de mise en page
-  }
+  this.sup2 = 6
+  this.besoinFormulaireCaseACocher = false
+  this.besoinFormulaire2Numerique = false
 }
