ffc8988d745c1a3e7a97d1d5d5f05c0c932b8b8d
moragi <gilles.mora@ac-rouen.fr>
2022-03-03 20:30:55 +0100

    fix: coquille can6C18 bonbon singulier et pas fix can6C26 avec les arrondis

--- src/js/exercices/can/6e/can6C26.js
+++ src/js/exercices/can/6e/can6C26.js
@@ -1,5 +1,5 @@
 import Exercice from '../../Exercice.js'
-import { randint, choice, texNombrec, calcul, texNombre, texFraction } from '../../../modules/outils.js'
+import { randint, choice, texNombrec, texNombre, calcul, texFraction } from '../../../modules/outils.js'
 export const titre = 'Multiplier ou diviser par 10, 100,  1000 ou 0,1 ou 0,01'
 export const interactifReady = true
 export const interactifType = 'mathLive'
@@ -21,22 +21,23 @@ export default function MultiplierDiviserPar10Par100Par1000 () {
   this.formatChampTexte = 'largeur15 inline'
   this.nouvelleVersion = function () {
     let a, b, den, resultat
-    switch (choice([1, 2, 3])) {
+    switch (choice([1])) { //, 2, 3
       case 1:// multiplier par 10, 100 ou 1000
         a = choice([randint(11, 99), randint(100, 999)])
-        a = calcul(a / choice([10, 100, 1000, 10000]))
+        a /= choice([10, 100, 1000, 10000])
         b = choice([10, 100, 1000])
-        resultat = calcul(a * b)
-        this.question = `$${texNombrec(a)}\\times${texNombre(b)}=$`
-        this.correction = `$${texNombrec(a)}\\times ${b} = ${texNombre(resultat)}$`
+        resultat = a * b
+        this.question = `$${texNombre(a, 4)}\\times ${texNombre(b, 0)}=$`
+        this.correction = `$${texNombre(a, 4)}\\times ${texNombre(b, 0)} = ${texNombre(resultat, 3)}$`
         this.reponse = resultat
         break
+
       case 2:// multiplier par 0,1....
         a = randint(10, 1000)
         b = choice([0.1, 0.01, 0.001])
-        resultat = texNombrec(a * b)
-        this.question = `$${texNombrec(a)}\\times${texNombre(b)}=$`
-        this.correction = `$${texNombrec(a)}\\times ${texNombre(b)} = ${texNombre(resultat)}$`
+        resultat = calcul(a * b)
+        this.question = `$${texNombrec(a)}\\times${texNombrec(b)}=$`
+        this.correction = `$${texNombrec(a)}\\times ${texNombrec(b)} = ${texNombrec(resultat)}$`
         this.reponse = resultat
         break
       case 3:// multiplier par 10, 100 et fractions /10, /100....
@@ -44,8 +45,8 @@ export default function MultiplierDiviserPar10Par100Par1000 () {
         den = choice([10, 100, 1000])
         b = choice([10, 100, 1000])
         resultat = calcul(a * b / den)
-        this.question = `$${texFraction(a, den)}\\times${texNombre(b)}=$`
-        this.correction = `$${texFraction(a, den)} \\times ${texNombre(
+        this.question = `$${texFraction(a, den)}\\times${texNombrec(b)}=$`
+        this.correction = `$${texFraction(a, den)} \\times ${texNombrec(
                 b)} = ${texFraction(a * b, den)} = ${texNombrec((a / den) * b)}$`
         this.reponse = resultat
         break
