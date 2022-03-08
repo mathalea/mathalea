fd3a9d29230769cb861328d481682dc21299443a
Gain <mireillegain.prof@gmail.com>
2022-03-08 20:05:03 +0100

    Aires et périmètres en expression littérale

--- src/exosNonAmcNonInteractifs.md
+++ src/exosNonAmcNonInteractifs.md
@@ -13,27 +13,27 @@
 |6G21-1|5G11-2|4G15|3F21|2F31-2|ref : 1E12||PEA13|
 |6G21|5G11-3|4G20-5|3G10-3|2F31-3|ref : 1E13||PEG20|
 |6G23-2|5G12|4G22|3G11|2F32-3|ref : 1E14||PEG21|
-|6G24-3|5G13|4G31|3G12|2G10-1|ref : 1E15||PEG22|
-|6G25-3|5G20-0|4G40-1|3G13|2G10-2|ref : 1E16||PEG23|
-|6G42|5G20-1|4G40-2|3G20-1|2G11-1|ref : 1F10||PEG24|
-|6I11|5G20-2|4G51|3G22-1|2G11-2|ref : 1N10||P003|
-|6M22-2|5G20|4G52|3G22|2G11-3|ref : 1N11||P004|
-|6M25|5G21-1|4I1|3G23|2G12-1|ref : 1P10||P005|
-|6M31-2|5G22-1|4L10-1|3G30-1|2G12-2|||P006|
-|6N10-4|5G22|4L12|3G31-1|2G12-3|||P007|
-|6N11-3|5G24-1|4L13-0|3G32-2|2G12-4|||P008|
-|6N11-4|5G24-2|4L13-1|3G32-3|2G21-1|||P009|
-|6N12|5G30-1|4L14-0|3G32-4|2G22-1|||P010|
-|6N22-1|5G30-2|4L14-1|3G32-5|2G23-2|||P011|
-|6N24-1|5G30|4L14-2|3G32|2G24-1|||P012|
-|6N31-1|5G31-1|4L15-0|3G40|2G30-2|||P013|
-|6N31-2|5G31|4L15-1|3I1-1|2G30-3|||P014|
-|6N32|5G40-1|4L16|3I1|2G30-4|||beta2F31|
-|6N33-0|5G40-2|4P10-1|3L13-2|2G30-5|||beta3F23|
-|6N41-1|5G40|4P10-2|3L14-1|2G33-1|||beta3G15|
-|6P13-1|5G41|4P10|3L14|2G33-2|||beta3G41|
-|6S11|5G42|4S10|3L15|2N11-1|||beta3S20-1|
-||5G51||3S20|2N11-2|||beta3s21|
+|6G23-3|5G13|4G31|3G12|2G10-1|ref : 1E15||PEG22|
+|6G24-3|5G20-0|4G40-1|3G13|2G10-2|ref : 1E16||PEG23|
+|6G25-3|5G20-1|4G40-2|3G20-1|2G11-1|ref : 1F10||PEG24|
+|6G42|5G20-2|4G51|3G22-1|2G11-2|ref : 1N10||P003|
+|6I11|5G20|4G52|3G22|2G11-3|ref : 1N11||P004|
+|6M22-2|5G21-1|4I1|3G23|2G12-1|ref : 1P10||P005|
+|6M25|5G22-1|4L10-1|3G30-1|2G12-2|||P006|
+|6M31-2|5G22|4L10-2|3G31-1|2G12-3|||P007|
+|6N10-4|5G24-1|4L12|3G40|2G12-4|||P008|
+|6N11-3|5G24-2|4L13-0|3I1-1|2G21-1|||P009|
+|6N11-4|5G30-1|4L13-1|3I1|2G22-1|||P010|
+|6N12|5G30-2|4L14-0|3L13-2|2G23-2|||P011|
+|6N22-1|5G30|4L14-1|3L13-4|2G24-1|||P012|
+|6N24-1|5G31-1|4L14-2|3L14-1|2G30-2|||P013|
+|6N31-1|5G31|4L15-0|3L14|2G30-3|||P014|
+|6N31-2|5G40-1|4L15-1|3L15|2G30-4|||beta2F31|
+|6N32|5G40-2|4L16|3S20|2G30-5|||beta3F23|
+|6N33-0|5G40|4P10-1||2G33-1|||beta3G15|
+|6N41-1|5G41|4P10-2||2G33-2|||beta3G41|
+|6P13-1|5G42|4P10||2N11-1|||beta3S20-1|
+|6S11|5G51|4S10||2N11-2|||beta3s21|
 ||5I11|||2N15-1|||beta4C31|
 ||5L10-1|||2N15-2|||beta4G20-3|
 ||5L10-3|||2N20-3|||beta4G20-4|
@@ -41,30 +41,30 @@
 ||5L12-1|||2N20-7|||beta6C33-1|
 ||5L12|||2N20-8|||beta6test2|
 ||5L14-1|||2N30-6|||beta6test2021|
-||5L14-3|||2N30-7|||betaAsymptotesObliques|
-||5L14-4|||2N31-3|||betaEqCarreDansC|
-||5L14-5|||2N32-1|||betaEqValAbs|
-||5L14-6|||2N32-2|||betaEquations|
-||5L15|||2N32-3|||betaEquationsLog|
-||5M10|||2N32-5|||betaExo3d|
-||5N11-1|||2N32-6|||betaExoLimite|
-||5N11-2|||2N32-7|||betaExoSimpleMatthieu|
-||5N11-4|||2N41-7|||betaModele10_simple_question-reponse|
-||5N14-2|||2N41-7v2|||betaModele11_parametrable|
-||5N15|||2N50-1|||betaModele20_plusieurs_types_de_questions|
-||5N20-0|||2N50-2|||betaModele21_parametrables|
-||5P10|||2N50-3|||betaModele30_constructions_géométriques|
-||5P12|||2N50-4|||betaModele31_parametrables|
-||5R10-0|||2N51-1|||betaModele40_tableau_proportionnalite|
-||5R10-1|||2N51-2|||betaModele41_tableau_signes_variations|
-||5R12-2|||2N51-3|||betaModele50_Mathsteps|
-||5R12|||2N51-3v2|||betaProbaAouB|
-||5R20-4|||2N52-1|||betaProbabilites|
-||5R20-5|||2N52-2|||betaProbabilitesJC|
-||5R22-2|||2N52-4|||betaPuissances|
-||5S10|||2N60-4|||betaSpline|
-||5S12|||2N61-2|||betaSys2x2CombLin|
-||5S13-1|||2N61-4|||betaThales|
+||5L14-3|||2N30-7|||betaAleaFigure|
+||5L14-4|||2N31-3|||betaAsymptotesObliques|
+||5L14-5|||2N32-1|||betaEqCarreDansC|
+||5L14-6|||2N32-2|||betaEqValAbs|
+||5L15|||2N32-3|||betaEquations|
+||5M10|||2N32-5|||betaEquationsLog|
+||5N11-1|||2N32-6|||betaExo3d|
+||5N11-2|||2N32-7|||betaExoLimite|
+||5N11-4|||2N41-7|||betaExoSimpleMatthieu|
+||5N14-2|||2N41-7v2|||betaModele10_simple_question-reponse|
+||5N15|||2N50-1|||betaModele11_parametrable|
+||5N20-0|||2N50-2|||betaModele20_plusieurs_types_de_questions|
+||5P10|||2N50-3|||betaModele21_parametrables|
+||5P12|||2N50-4|||betaModele30_constructions_géométriques|
+||5R10-0|||2N51-1|||betaModele31_parametrables|
+||5R10-1|||2N51-2|||betaModele40_tableau_proportionnalite|
+||5R12-2|||2N51-3|||betaModele41_tableau_signes_variations|
+||5R12|||2N51-3v2|||betaModele50_Mathsteps|
+||5R20-4|||2N52-1|||betaProbaAouB|
+||5R20-5|||2N52-2|||betaProbabilites|
+||5R22-2|||2N52-4|||betaProbabilitesJC|
+||5S10|||2N60-4|||betaPuissances|
+||5S12|||2N61-2|||betaSpline|
+||5S13-1|||2N61-4|||betaSys2x2CombLin|
 ||5S20|||2S30-2|||betaTracerParabole|
 ||5S21-1|||2S30-3|||betarotation3d|
 ||5S21|||2S30-4|||moule_a_exo_mathalea|
---
+++ src/js/exercices/4e/4L10-2.js
@@ -0,0 +1,104 @@
+import Exercice from '../Exercice.js'
+import { listeQuestionsToContenu, combinaisonListes, randint, creerNomDePolygone } from '../../modules/outils.js'
+import { afficheCoteSegment, codageAngleDroit, codeSegments, mathalea2d, point, pointAdistance, polygone, polygoneAvecNom, rotation, segment, similitude, translation, vecteur } from '../../modules/2d.js'
+export const titre = 'Donner l\'expression littérale d\'un périmètre et d\'une aire de quadrilatère'
+
+// Les exports suivants sont optionnels mais au moins la date de publication semble essentielle
+export const dateDePublication = '08/03/2022'
+
+/**
+ * Description didactique de l'exercice : Faire le lien entre le calcul littéral et son expression visuelle
+ * @author Mireille Gain
+ * Référence 4L10-2
+*/
+export default function AirePerimetrePolygone () {
+  Exercice.call(this) // Héritage de la classe Exercice()
+  this.consigne = 'Exprimer le périmètre et l\'aire des rectangles et carrés suivants par une expression littérale réduite.'
+  this.nbQuestions = 4 // Nombre de questions par défaut
+  this.nbCols = 2 // Uniquement pour la sortie LaTeX
+  this.nbColsCorr = 2 // Uniquement pour la sortie LaTeX
+  this.video = '' // Id YouTube ou url
+
+  this.nouvelleVersion = function () {
+    const nom = creerNomDePolygone(16, 'Q')
+    this.listeQuestions = [] // Liste de questions
+    this.listeCorrections = [] // Liste de questions corrigées
+    this.autoCorrection = []
+
+    const c = randint(2, 6)
+    const Lo = randint(5, 7)
+    // on choisit un nom pour les variables 1 et 2
+    const variables1 = ['t', 'u', 'v', 'w', 'y', 'z']
+    const inc1 = variables1[randint(0, variables1.length - 1)]
+    const variables2 = ['a', 'b', 'c', 'd', 'e', 'f']
+    const inc2 = variables2[randint(0, variables2.length - 1)]
+    const l = randint(2, 4)
+
+    const M = point(0, 0, nom[12])
+    const N = pointAdistance(M, Lo, 0, nom[13])
+    const O = similitude(M, N, -90, l / Lo, nom[14])
+    const P = translation(O, vecteur(N, M), nom[15])
+    const rectangle0 = polygone(M, N, O, P)
+    const L0 = segment(M, P)
+    const l0 = segment(P, O)
+
+    const A = point(8, 0, nom[0])
+    const B = pointAdistance(A, c, 0, nom[1])
+    const C = rotation(A, B, -90, nom[2])
+    const D = rotation(B, A, 90, nom[3])
+    const carre = polygone(A, B, C, D)
+    const ca = segment(D, C)
+
+    const E = point(15, 0, nom[4])
+    const F = pointAdistance(E, Lo, 0, nom[5])
+    const G = similitude(E, F, -90, l / Lo, nom[6])
+    const H = translation(G, vecteur(F, E), nom[7])
+    const rectangle1 = polygoneAvecNom(E, F, G, H)
+    const L1 = segment(E, H)
+    const l1 = segment(H, G)
+
+    const I = point(24, 0, nom[8])
+    const J = pointAdistance(I, Lo, 0, nom[9])
+    const K = similitude(I, J, -90, l / Lo, nom[10])
+    const L = translation(K, vecteur(J, I), nom[11])
+    const rectangle2 = polygone(I, J, K, L)
+    const L2 = segment(I, L)
+    const l2 = segment(L, K)
+
+    this.introduction = mathalea2d({ xmin: -4, xmax: 32, ymin: -3, ymax: 7, pixelsParCm: 20, scale: 0.75, mainlevee: false },
+      rectangle0, codageAngleDroit(M, N, O), codageAngleDroit(N, O, P), codageAngleDroit(O, P, M), codageAngleDroit(P, M, N), codeSegments('/', 'red', M, N, O, P), codeSegments('||', 'blue', N, O, P, M), afficheCoteSegment(L0, `${inc1}`, 1, 'red', 2, 0.5, 'black'), afficheCoteSegment(l0, `${inc2}`, 1, 'red', 2, 0.5, 'black'),
+      carre, codageAngleDroit(A, B, C), codageAngleDroit(A, D, C), codageAngleDroit(D, C, B), codageAngleDroit(B, A, D), codeSegments('//', 'blue', [A, B, C, D, A]), afficheCoteSegment(ca, `${inc1}`, 1, 'red', 2, 0.5, 'black'),
+      rectangle1, codageAngleDroit(E, F, G), codageAngleDroit(F, G, H), codageAngleDroit(G, H, E), codageAngleDroit(H, E, F), codeSegments('/', 'red', E, F, G, H), codeSegments('||', 'blue', F, G, H, E), afficheCoteSegment(L1, Lo, 1, 'red', 2, 0.5, 'black'), afficheCoteSegment(l1, `${inc2}`, 1, 'red', 2, 0.5, 'black'),
+      rectangle2, codageAngleDroit(I, J, K), codageAngleDroit(J, K, L), codageAngleDroit(K, L, I), codageAngleDroit(L, I, J), codeSegments('/', 'orange', I, J, K, L), codeSegments('||', 'blue', J, K, L, I), afficheCoteSegment(L2, l, 1, 'red', 2, 0.5, 'black'), afficheCoteSegment(l2, `${inc1} + ${inc2}`, 1, 'red', 2, 0.5, 'black')
+    )
+
+    const typeQuestionsDisponibles = ['r1', 'r2', 'r3', 'c'] // On crée 4 types de questions
+
+    const listeTypeQuestions = combinaisonListes(typeQuestionsDisponibles, this.nbQuestions) // Tous les types de questions sont posés mais l'ordre diffère à chaque "cycle"
+    for (let i = 0, texte, texteCorr, cpt = 0; i < this.nbQuestions && cpt < 50;) { // Boucle principale où i+1 correspond au numéro de la question
+      texte = ''
+      switch (listeTypeQuestions[i]) { // Suivant le type de question, le contenu sera différent
+        case 'r1': // Rectangle ayant une lettre pour Longueur et une autre lettre pour largeur
+          texteCorr = `Correction ${i + 1} de type 1`
+          break
+        case 'r2': // Rectangle ayant une lettre pour Longueur et un nombre pour largeur, ou inversement
+          texteCorr = `Correction ${i + 1} de type 2`
+          break
+        case 'r3': // Rectangle ayant un nombre pour largeur et une somme de lettres pour Longueur
+          texteCorr = `Correction ${i + 1} de type 3`
+          break
+        case 'c':// Carré de côté une lettre
+          texteCorr = `Correction ${i + 1} de type 3`
+          break
+      }
+      // Si la question n'a jamais été posée, on l'enregistre
+      if (this.questionJamaisPosee(i, texte)) { // <- laisser le i et ajouter toutes les variables qui rendent les exercices différents (par exemple a, b, c et d)
+        this.listeQuestions.push(texte)
+        this.listeCorrections.push(texteCorr)
+        i++
+      }
+      cpt++
+    }
+    listeQuestionsToContenu(this) // On envoie l'exercice à la fonction de mise en page
+  }
+}
