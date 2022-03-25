c88f1777ef6882a02f30d3706595257f8f06fff0
Jean-Claude Lhote <lhotejc@gmail.com>
2022-03-25 20:49:57 +0100

    betaTestFractionInteractif pour tester les formulaires mathLive

--- src/js/exercices/beta/betaTestFraction.js
+++ src/js/exercices/beta/betaTestFraction.js
@@ -1,7 +1,12 @@
 import Exercice from '../Exercice.js'
-import { texNombre, listeQuestionsToContenu } from '../../modules/outils.js'
+import { listeQuestionsToContenu, randint } from '../../modules/outils.js'
 import FractionX from '../../modules/FractionEtendue'
-import { Fraction } from 'mathjs'
+import { Fraction, evaluate } from 'mathjs'
+import { ajouteChampTexteMathLive } from '../../modules/interactif/questionMathLive'
+import { setReponse } from '../../modules/gestionInteractif.js'
+
+export const interactifReady = true
+export const interactifType = 'mathLive'
 export const titre = 'Tester la classe FractionX'
 export const dateDePublication = '20/03/2022' // La date de publication initiale au format 'jj/mm/aaaa' pour affichage temporaire d'un tag
 
@@ -13,22 +18,24 @@ export const dateDePublication = '20/03/2022' // La date de publication initiale
 export default function testFractions () {
   Exercice.call(this) // Héritage de la classe Exercice()
   this.nbQuestions = 1
-  this.sup = 0
-  this.sup2 = 1
+  this.sup = randint(1, 1000)
+  this.sup2 = randint(2, 1000)
   // Dans un exercice simple, ne pas mettre de this.listeQuestions = [] ni de this.consigne
-  this.besoinFormulaireTexte = ['numérateur : ', '']
-  this.besoinFormulaire2Texte = ['dénominateur : ', '']
+  this.besoinFormulaireTexte = ['numérateur ', '']
+  this.besoinFormulaire2Texte = ['dénominateur ', '']
   this.nouvelleVersion = function () {
     this.listeCorrections = []
     this.listeQuestions = []
-
-    const a = Number(this.sup) // randint(101, 999) * randint(101, 999) * randint(101, 999) * randint(101, 999)
-    const b = Number(this.sup2) // randint(101, 999) * randint(101, 999) * randint(101, 999) * randint(101, 999) / 10 ** 12
+    console.log(evaluate(this.sup), evaluate(this.sup2))
+    const a = Number(evaluate(this.sup)) // randint(101, 999) * randint(101, 999) * randint(101, 999) * randint(101, 999)
+    const b = Number(evaluate(this.sup2)) // randint(101, 999) * randint(101, 999) * randint(101, 999) * randint(101, 999) / 10 ** 12
     const f1 = new FractionX(a, b)
     const f2 = new Fraction(a, b)
+    setReponse(this, 0, f1, { formatInteractif: 'fractionEgale' })
     console.log('Fraction selon FractionX : ', f1.num, ' / ', f1.den, ' fraction selon Fraction : ', f2.n, ' / ', f2.d)
-    this.listeQuestions.push(`FractionX : $\\dfrac{${f1.num}}{${f1.den}}$<br><br>Fraction : $\\dfrac{${f2.n}}{${f2.d}}$`)
-    this.listeCorrections.push('rien')
+    this.listeQuestions.push('Saisir une fraction ou ce que vous voulez : ' + ajouteChampTexteMathLive(this, 0, 'largeur25 inline'))
+    this.listeCorrections.push(`FractionX : $\\dfrac{${f1.num}}{${f1.den}}$<br><br>Fraction : $\\dfrac{${f2.n}}{${f2.d}}$`)
+
     listeQuestionsToContenu(this)
   }
 }
