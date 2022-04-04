2d067ecaa120e349d581cef2d2ebd9edc76a3127
Guillaume Valmont <guillaume.valmont@pm.me>
2022-04-04 19:19:59 +0400

    L'exercice original est importé avec la nouvelle méthode

--- src/js/exercices/4e/4L13-2.js
+++ src/js/exercices/4e/4L13-2.js
@@ -1,14 +1,17 @@
 import ProblemesEnEquation from '../3e/3L13-3.js'
 export const titre = 'Mettre un problème en équation et le résoudre'
 export { interactifReady, interactifType, amcReady, amcType } from '../3e/3L13-3.js'
+
 export const dateDePublication = '04/04/2022'
 
 /**
  * @author Guillaume Valmont
  * reference 4L13-2
  */
-export default function ProblemesEnEquation4e () {
-  ProblemesEnEquation.call(this)
-  this.titre = titre
-  this.sup = 1
+export default class ProblemesEnEquation4e extends ProblemesEnEquation {
+  constructor () {
+    super()
+    this.titre = titre
+    this.sup = 1
+  }
 }
