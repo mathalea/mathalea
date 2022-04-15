96be3a25e4a70563f49f83764d59a67f2098a617
Jean-Claude Lhote <lhotejc@gmail.com>
2022-04-15 18:30:53 +0200

    temp: modifications et ajouts

--- src/js/exercices/1e/1G10_1.js
+++ src/js/exercices/1e/1G10_1.js
@@ -8,7 +8,6 @@ export const interactifType = 'mathLive'
 // Les exports suivants sont optionnels mais au moins la date de publication semble essentielle
 export const dateDePublication = '14/04/2022' // La date de publication initiale au format 'jj/mm/aaaa' pour affichage temporaire d'un tag
 export const dateDeModifImportante = '' // Une date de modification importante au format 'jj/mm/aaaa' pour affichage temporaire d'un tag
-this.interactif = true
 /**
  * Description didactique de l'exercice
  * @author Stéphane Guyon
@@ -17,11 +16,48 @@ this.interactif = true
 export default function CosetSin () {
   Exercice.call(this) // Héritage de la classe Exercice()
   this.consigne = 'Donner la valeur exacte de :'
-  this.nbQuestions = 5 // Nombre de questions par défaut
+  this.nbQuestions = 1 // Nombre de questions par défaut
   this.nbCols = 2 // Uniquement pour la sortie LaTeX
   this.nbColsCorr = 2 // Uniquement pour la sortie LaTeX
   this.video = '' // Id YouTube ou url
 
+  function angleOppose (angle) {
+    return { degres: '-' + angle.degres, cos: angle.cos, sin: '-' + angle.sin, radian: '-' + angle.radian }
+  }
+  function complementaire (angleEnRadian) {
+    switch (angleEnRadian) {
+      case '\\dfrac{\\pi}{4}':
+        return angleEnRadian
+      case '\\dfrac{\\pi}{6}':
+        return '\\dfrac{\\pi}{3}'
+      case '\\dfrac{\\pi}{3}':
+        return '\\dfrac{\\pi}{6}'
+      case '90' :
+        return '0'
+      case '0' :
+        return '90'
+    }
+  }
+  function supplementaire (angleEnRadian) {
+    switch (angleEnRadian) {
+      case '\\dfrac{\\pi}{4}':
+        return '\\dfrac{3\\pi}{4}'
+      case '\\dfrac{\\pi}{6}':
+        return '\\dfrac{5\\pi}{6}'
+      case '\\dfrac{\\pi}{3}':
+        return '\\dfrac{2\\pi}{3}'
+      case '90' :
+        return '90'
+      case '0' :
+        return '180'
+    }
+  }
+  function angleComplementaire (angle) {
+    return { degres: (90 - parseInt(angle.degres)).toString(), cos: angle.sin, sin: angle.cos, radian: complementaire(angle.radian) }
+  }
+  function angleSupplementaire (angle) {
+    return { degres: (180 - parseInt(angle.degres)).toString(), cos: '-' + angle.cos, sin: angle.sin, radian: supplementaire(angle.radian) }
+  }
   this.nouvelleVersion = function (numeroExercice) {
     this.listeQuestions = [] // Liste de questions
     this.listeCorrections = [] // Liste de questions corrigées
@@ -32,33 +68,29 @@ export default function CosetSin () {
       { degres: '90', cos: '0', sin: '1', radian: '\\dfrac{\\pi}{2}' },
       { degres: '45', cos: '\\dfrac{\\sqrt{2}}{2}', sin: '\\dfrac{\\sqrt{2}}{2}', radian: '\\dfrac{\\pi}{4}' }
     ]
+    
     const mesAnglesAleatoires = combinaisonListes(mesAngles, this.nbQuestions)
     // On mélange
 
     const listeTypeQuestions = combinaisonListes(typeQuestionsDisponibles, this.nbQuestions) // Tous les types de questions sont posés mais l'ordre diffère à chaque "cycle"
     for (let i = 0, texte, texteCorr, cpt = 0; i < this.nbQuestions && cpt < 50;) { // Boucle principale où i+1 correspond au numéro de la question
       const monAngle = mesAnglesAleatoires[i]
-      const degres = monAngle.degres
+      /* const degres = monAngle.degres
       const radian = monAngle.radian
       const cos = monAngle.cos
+      */
       switch (listeTypeQuestions[i]) { // Suivant le type de question, le contenu sera différent
         case 'cos':
           texte = `Déterminer la valeur exacte de $\\cos\\big(${monAngle.radian}\\big)$`
-          if (this.interactif) { // Si l'exercice est interactif
-            texte += ajouteChampTexteMathLive(this, i)
-            texte += ' = '
-          }
+          texte += ajouteChampTexteMathLive(this, i, 'largeur15 inline', { texte: ' = ' })
           texteCorr = `$\\cos\\big(${monAngle.radian}\\big)=${monAngle.cos}$`
-          if (this.interactif) { setReponse(this, i, `$${monAngle.cos}$`, { formatInteractif: 'calcul' }) }
+          setReponse(this, i, `${monAngle.cos}`, { formatInteractif: 'calcul' })
           break
         case 'sin':
           texte = `Déterminer la valeur exacte de $\\sin\\big(${monAngle.radian}\\big)$`
-          if (this.interactif) { // Si l'exercice est interactif
-            texte += ajouteChampTexteMathLive(this, i)
-            texte += ' = '
-          }
+          texte += ajouteChampTexteMathLive(this, i, 'largeur15 inline', { texte: ' = ' })
           texteCorr = `$\\sin\\big(${monAngle.radian}\\big)=${monAngle.sin}$`
-          if (this.interactif) { setReponse(this, i, `$${monAngle.sin}$`, { formatInteractif: 'calcul' }) }
+          setReponse(this, i, `${monAngle.sin}`, { formatInteractif: 'calcul' })
           break
       }
       // Si la question n'a jamais été posée, on l'enregistre
