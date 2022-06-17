531e4d7f6ab3313a4e13ff5891271d352e9868fb
Jean-Claude Lhote <lhotejc@gmail.com>
2022-06-17 23:10:22 +0200

    fix: suppressions de calcul() inutiles.

--- src/js/modules/2d.js
+++ src/js/modules/2d.js
@@ -2671,7 +2671,7 @@ function PolygoneATrous ({ data = [], holes = [], noms = '', color = 'black', co
   for (let i = 0; i < 2 * holes[0]; i += 2) {
     sommetsContour.push(point(data[i], data[i + 1]))
     if (noms.length >= data.length << 1) {
-      sommetsContour[i << 1].nom = noms[i << 1]
+      sommetsContour[i >> 1].nom = noms[i << 1]
     }
   }
   // On cherche les bordures
@@ -2695,8 +2695,8 @@ function PolygoneATrous ({ data = [], holes = [], noms = '', color = 'black', co
     trous[i] = []
     for (let j = holes[i] * 2; j < (i !== holes.length - 1 ? holes[i + 1] * 2 : data.length); j += 2) {
       trou = point(data[j], data[j + 1])
-      if (noms.length >= data.length << 1) {
-        trou.nom = noms[j << 1]
+      if (noms.length >= data.length >> 1) {
+        trou.nom = noms[j >> 1]
       }
       trous[i].push(trou)
     }
