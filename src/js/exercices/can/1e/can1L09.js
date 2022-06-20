51335f7ed3e72f4d2c3c840e1f6020a521ab98ec
moragi <gilles.mora@ac-rouen.fr>
2022-06-20 14:50:17 +0200

    ex: can1L09 fin

--- src/js/exercices/can/1e/can1L09.js
+++ src/js/exercices/can/1e/can1L09.js
@@ -1,5 +1,5 @@
 import Exercice from '../../Exercice.js'
-import { randint, listeQuestionsToContenu, reduirePolynomeDegre3, ecritureAlgebrique, rienSi1 } from '../../../modules/outils.js'
+import { randint, listeQuestionsToContenu, reduirePolynomeDegre3, ecritureAlgebrique, rienSi1, texteCentre } from '../../../modules/outils.js'
 import FractionX from '../../../modules/FractionEtendue.js'
 import { ajouteChampTexteMathLive } from '../../../modules/interactif/questionMathLive.js'
 import { setReponse } from '../../../modules/gestionInteractif.js'
@@ -29,12 +29,13 @@ export default function EquationSecondDegreParticuliere () {
 
     let texte, texteCorr, a, b, c, f
     for (let i = 0, cpt = 0; i < this.nbQuestions && cpt < 50;) {
-      a = randint(-5, 5, 0)
-      b = randint(-5, 5, 0)
+      a = randint(-10, 10, 0)
+      b = randint(-10, 10, 0)
       c = randint(-10, 10, 0)
       f = new FractionX(-b, a)
-      texte = `Donner l'ensemble des solutions $\\mathscr{S}$ de l'équation : $${reduirePolynomeDegre3(0, a, b, c)}=${c}$.<br>
-        `
+      texte = `Donner l'ensemble des solutions $\\mathscr{S}$ de l'équation :
+     ${texteCentre(`$${reduirePolynomeDegre3(0, a, b, c)}=${c}$`)}`
+
       if (this.interactif) {
         texte += 'Ecrire les solutions dans l\'ordre croissant :<br> $\\mathscr{S}=\\bigg\\{$'
         texte += ajouteChampTexteMathLive(this, 2 * i, 'largeur15 inline')
