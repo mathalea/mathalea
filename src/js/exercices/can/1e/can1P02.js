2d3c5712e3fef71a8863b5ed74f55973f841b29b
moragi <gilles.mora@ac-rouen.fr>
2022-06-30 18:41:34 +0200

    ex: can1P02

--- src/js/exercices/can/1e/can1P02.js
+++ src/js/exercices/can/1e/can1P02.js
@@ -1,8 +1,7 @@
 import Exercice from '../../Exercice.js'
 import { listeQuestionsToContenu, randint, choice, texNombre, choisitLettresDifferentes } from '../../../modules/outils.js'
 import Decimal from 'decimal.js'
-import { number } from 'mathjs'
-import { Arbre, texProba } from '../../../modules/arbres.js'
+import { Arbre } from '../../../modules/arbres.js'
 import { mathalea2d } from '../../../modules/2d.js'
 import { ajouteChampTexteMathLive } from '../../../modules/interactif/questionMathLive.js'
 import { setReponse } from '../../../modules/gestionInteractif.js'
@@ -34,19 +33,21 @@ export default function LectureProbabilite () {
     this.listeQuestions = [] // Liste de questions
     this.listeCorrections = [] // Liste de questions corrigées
     this.autoCorrection = []
-    for (let i = 0, cpt = 0, pA, pB, pAC, pBC, omega, texte, texteCorr, choix, nom1, nom2, objets, pC; i < this.nbQuestions && cpt < 50;) {
+    for (let i = 0, cpt = 0, pA, pB, pAC, pBC, omega, texte, texteCorr, choix, nom1, nom2, objets; i < this.nbQuestions && cpt < 50;) {
       objets = []
       // On choisit les probas de l'arbre
       nom1 = choisitLettresDifferentes(1)
       nom2 = choisitLettresDifferentes(1, nom1)
-      pA = (new Decimal(randint(1, 9) )).div(10)
+      pA = (new Decimal(randint(1, 9))).div(10)
 
       pB = new Decimal(1 - pA)
-      pAC = (new Decimal(randint(1, 9)*10+randint(1, 9))).div(100)
-      pBC = (new Decimal(randint(1, 9)*10+randint(1, 9))).div(100)
-      while(pAC===pBC){ pAC = (new Decimal(randint(1, 9)*10+randint(1, 9))).div(100)
-        pBC = (new Decimal(randint(1, 9)*10+randint(1, 9))).div(100)}
-        choix=choice([pA, pB, pAC, 1-pAC, pBC, 1-pBC])
+      pAC = (new Decimal(randint(1, 9) * 10 + randint(1, 9))).div(100)
+      pBC = (new Decimal(randint(1, 9) * 10 + randint(1, 9))).div(100)
+      while (pAC === pBC) {
+        pAC = (new Decimal(randint(1, 9) * 10 + randint(1, 9))).div(100)
+        pBC = (new Decimal(randint(1, 9) * 10 + randint(1, 9))).div(100)
+      }
+      choix = choice([pA, pB, pAC, 1 - pAC, pBC, 1 - pBC])
       // On définit l'arbre complet
       omega = new Arbre({
         racine: true,
@@ -82,12 +83,12 @@ export default function LectureProbabilite () {
             enfants: [new Arbre({
               rationnel: false,
               nom: `${nom2}`,
-              proba: pBC,
+              proba: pBC
             }),
             new Arbre({
               rationnel: false,
               nom: `\\bar{${nom2}}`,
-              proba: new Decimal(1 - pBC),
+              proba: new Decimal(1 - pBC)
             })
             ]
           })
@@ -96,14 +97,38 @@ export default function LectureProbabilite () {
 
       omega.setTailles() // On calcule les tailles des arbres.
       objets = omega.represente(0, 7, 0, 1.5, true, 1) // On crée l'arbre complet echelle 1.4 feuilles verticales sens gauche-droite
-      texte = `On donne l'arbre de probabilités ci dessous :<br>`
+      texte = 'On donne l\'arbre de probabilités ci dessous :<br>'
       texte += mathalea2d({ xmin: -0.1, xmax: 14, ymin: 0, ymax: 7, style: 'inline' }, ...objets)
       texte += `<br>Compléter avec la notation qui convient :  ${(this.interactif || !context.isHtml) ? ajouteChampTexteMathLive(this, i, 'largeur10 inline') : '\\ldots'}`
-      texte += `$=${texNombre(choix,2)}$`
-      texteCorr = `On peut lire à l'aide de l'arbre :<br>
+      texte += `$=${texNombre(choix, 2)}$`
+      texteCorr = `On peut lire à l'aide de l'arbre (les probabilités conditionnelles se lisent sur la deuxième partie de l'arbre):<br>
+      $p(${nom1})=${texNombre(pA, 2)}$, 
+      $p(\\overline{${nom1}})=${texNombre(pB, 2)}$, 
+      $p_{${nom1}}(${nom2})=${texNombre(pAC, 2)}$, 
+      $p_{${nom1}}(\\overline{${nom2}})=${texNombre(1 - pAC, 2)}$, 
+      $p_{\\overline{${nom1}}}(${nom2})=${texNombre(pBC, 2)}$, 
+      $p_{\\overline{${nom1}}}(\\overline{${nom2}})=${texNombre(1 - pBC, 2)}$.
       `
+      if (choix === pA) {
+        setReponse(this, i, pA)
+      }
+      if (choix === pB) {
+        setReponse(this, i, pB)
+      }
 
-      setReponse(this, i, 'p_A')
+      if (choix === pAC) {
+        setReponse(this, i, pAC)
+      }
+      if (choix === 1 - pAC) {
+        setReponse(this, i, 1 - pAC)
+      }
+      if (choix === pBC) {
+        setReponse(this, i, pBC)
+      }
+      if (choix === 1 - pBC) {
+        setReponse(this, i, 1 - pBC)
+      }
+
       if (this.questionJamaisPosee(i, pA, pAC, pBC)) {
         this.listeQuestions.push(texte)
         this.listeCorrections.push(texteCorr)
