1b3018cc603479af3dcd548ef934df873519bf35
DegrangeM <53106394+DegrangeM@users.noreply.github.com>
2022-07-12 03:22:12 +0200

    Update buildMoodleGift.js

--- tasks/buildMoodleGift.js
+++ tasks/buildMoodleGift.js
@@ -8,7 +8,7 @@ const mathAleaURL = 'https://mathalea.com/' // ne pas oublier le / final
 const now = new Date()
 
 const date = {
-    US: now.getFullYear() + '-' + (now.getMonth() + 1).toString().padStart(2, '0') + '-' + now.getDate().toString().padStart(2, '0'),
+    // US: now.getFullYear() + '-' + (now.getMonth() + 1).toString().padStart(2, '0') + '-' + now.getDate().toString().padStart(2, '0'),
     FR: now.getDate().toString().padStart(2, '0') + '/' + (now.getMonth() + 1).toString().padStart(2, '0') + '/' + now.getFullYear()
 }
 
@@ -24,25 +24,12 @@ for (const [id, exercice] of Object.entries(exercices)) {
 }
 
 
-const exportPath = path.resolve(__dirname, '..', 'assets', 'gift', `mathalea-${date.US}.gift.txt`) 
+const exportPath = path.resolve(__dirname, '..', 'assets', 'gift', `mathalea.gift.txt`) 
 
 fs.writeFileSync(exportPath, gift)
 
 console.log(`Export de Mathalea pour Moodle au format GIFT terminé.`)
-console.log(`Le fichier est disponible dans le dossier assets/gift/mathalea-${date.US}.gift.txt`)
-
-const redirectHtml = `<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <title>Mathalea - Téléchargement de l'export Gift des exercices</title>
-    <!-- Redirect to the file -->
-    <meta http-equiv="refresh" content="0; url=${mathAleaURL}assets/gift/" />
-</head>
-<body>
-    <p>Redirection vers le fichier...</p>
-</body>
-</html>`
+console.log(`Le fichier est disponible dans le dossier assets/gift/mathalea.gift.txt`)
 
 function exportExerciceAsGift(id, exercice) {
     let c = s => s.replace(/[~=#{}:]/g, '\\$&'); // échappement des caratères spéciaux pour les GIFT
