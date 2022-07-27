7eb5e5232d8eff8bf275b9bd92b213e24e08c467
moragi <gilles.mora@ac-rouen.fr>
2022-07-27 15:45:00 +0200

    can5L01 restauré

--- src/js/exercices/can/5e/can5L01.js
+++ src/js/exercices/can/5e/can5L01.js
@@ -1,5 +1,6 @@
 import Exercice from '../../Exercice.js'
-import { randint, calcul, texNombre, texNombrec, choice } from '../../../modules/outils.js'
+import Decimal from 'decimal.js/decimal.mjs'
+import { randint, texNombre, texNombrec, choice } from '../../../modules/outils.js'
 export const titre = 'Trouver $a+1$ ou $a-1$ connaissant $2a$'
 export const interactifReady = true
 export const interactifType = 'mathLive'
@@ -18,15 +19,19 @@ export default function MoitiePlusOuMoinsUn () {
   // Dans un exercice simple, ne pas mettre de this.listeQuestions = [] ni de this.consigne
   this.formatChampTexte = 'largeur15 inline'
   this.nouvelleVersion = function () {
-    const a = randint(21, 35, 30) / 10
+    const a = new Decimal(randint(21, 35, 30)).div(10)
+    const b = new Decimal(a).div(2)
+
     if (choice([true, false])) {
-      this.reponse = calcul(a / 2 + 1)
-      this.question = `On a  $2\\times a=${texNombre(a)}$, combien vaut $a+1$ ?`
-      this.correction = `$2\\times a=${texNombre(a)}$, donc le nombre $a$ est égal à $\\dfrac{${texNombre(a)}}{2}=${texNombrec(a / 2)}$.<br>Donc $a+1=${texNombrec(a / 2)}+1=${texNombrec(a / 2 + 1)}$.`
+      this.reponse = new Decimal(a).div(2).add(1)
+      this.question = `On a  $2\\times a=${texNombre(a, 1)}$.<br>
+      Combien vaut $a+1$ ?`
+      this.correction = `$2\\times a=${texNombre(a, 1)}$, donc le nombre $a$ est égal à $\\dfrac{${texNombre(a, 1)}}{2}=${texNombre(b, 2)}$.<br>Donc $a+1=${texNombrec(b, 2)}+1=${texNombre(this.reponse, 3)}$.`
     } else {
-      this.reponse = calcul(a / 2 - 1)
-      this.question = `On a  $2\\times a=${texNombre(a)}$, combien vaut $a-1$ ?`
-      this.correction = `$2\\times a=${texNombre(a)}$, donc le nombre $a$ est égal à $\\dfrac{${texNombre(a)}}{2}=${texNombrec(a / 2)}$.<br>Donc $a-1=${texNombrec(a / 2)}-1=${texNombrec(a / 2 - 1)}$.`
+      this.reponse = new Decimal(a).div(2).sub(1)
+      this.question = `On a  $2\\times a=${texNombre(a, 1)}$.<br>
+      Combien vaut $a-1$ ?`
+      this.correction = `$2\\times a=${texNombre(a, 1)}$, donc le nombre $a$ est égal à $\\dfrac{${texNombre(a, 1)}}{2}=${texNombre(b, 2)}$.<br>Donc $a-1=${texNombrec(b, 2)}-1=${texNombre(this.reponse, 3)}$.`
     }
   }
 }
