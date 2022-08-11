4a1ff3431a391f23fb5bfcfca33814cf59a6da10
Jean-Claude Lhote <lhotejc@gmail.com>
2022-08-11 22:06:05 +0200

    feat: modification fonction rose en class

--- src/js/exercices/6e/6C10-6.js
+++ src/js/exercices/6e/6C10-6.js
@@ -1,73 +1,183 @@
 /* eslint-disable camelcase */
 import Exercice from '../Exercice.js'
-import { contraindreValeur, listeQuestionsToContenu, randint, rangeMinMax, shuffle } from '../../modules/outils.js'
-import { fixeBordures, mathalea2d, point, rotation, similitude, texteParPoint, longueur, milieu, segment, homothetie, polygoneRegulierParCentreEtRayon, colorToLatexOrHTML } from '../../modules/2d.js'
+import { choice, listeQuestionsToContenu, randint } from '../../modules/outils.js'
+import { fixeBordures, mathalea2d, point, rotation, similitude, texteParPoint, longueur, segment, homothetie, polygoneRegulierParCentreEtRayon, colorToLatexOrHTML, vide2d } from '../../modules/2d.js'
+import { create, all } from 'mathjs'
+import { calculer } from '../../modules/outilsMathjs.js'
 export const titre = 'Rose multiplicative'
-
+const math = create(all)
 /**
  * Travailler les tables de multiplication autrement
  * @author Jean-Claude Lhote
  * Référence 6C10-6
  */
-export function rose ({ values = [3, 4, 5], type = 'produit' }) {
-  const objets = []
-  const n = values.length
-  let rayon
-  switch (n) {
-    case 3:
-    case 4:
-      rayon = 2
-      break
-    case 5:
-    case 6:
-      rayon = 3
-      break
-    default:
-      rayon = 4
-  }
-  const O = point(0, 0)
-  const A = rotation(point(rayon, 0), O, 180 / n - 90)
-  let indexInconnue = 999 // une valeur ne pouvant pas être atteinte par i sauf pour les can
-  if (type === 'can1' || type === 'can2') {
-    indexInconnue = randint(0, n - 1)
-  }
-  for (let i = 0, bulle1, bulle2; i < n; i++) {
-    const M = rotation(A, O, 360 * i / n)
-    const N = rotation(M, O, 360 / n)
-    const P = similitude(M, O, 180 / n, 1.6)
-    const s = homothetie(segment(M, N), milieu(M, N), (longueur(M, N) - 1.6) / longueur(M, N))
-    const s1 = homothetie(segment(M, P), milieu(M, P), (longueur(M, P) - 1.6) / longueur(M, P))
-    s1.styleExtremites = '->'
-    s1.tailleExtremites = 2
-    s1.pointilles = 2
-    const s2 = homothetie(segment(N, P), milieu(N, P), (longueur(N, P) - 1.6) / longueur(N, P))
-    s2.styleExtremites = '->'
-    s2.tailleExtremites = 2
-    s2.pointilles = 2
-    bulle1 = rotation(polygoneRegulierParCentreEtRayon(M, 0.8, n), M, 180 / n - 90)
-    objets.push(bulle1)
-    objets.push(s, s1, s2)
-    bulle2 = rotation(polygoneRegulierParCentreEtRayon(P, 0.8, n), P, 360 / n - 90)
-    if (type === 'produit' || type === 'solution' || type === 'can1' || type === 'can2') {
-      if (!(type === 'can1' && (indexInconnue === i || i === (indexInconnue - 1) % n || i === (indexInconnue + 1) % n))) {
-        if (!(type === 'can2' && (indexInconnue === i || i === (indexInconnue + 1) % n))) {
-          objets.push(texteParPoint(values[i].toString(), M))
+export class Rose {
+  // operation = 'addition' | 'multiplication
+  // type = 'résultats' | 'valeurs' | 'can1' | 'can2' | 'solutions'
+  constructor ({ values = [], nombreDeValeurs, rayon = 2, operation = 'addition', type = 'résultats', typeDonnees = 'entiers', cellulesPreremplies = Array.from('abcdefghi'), valeurMax = 10 }) {
+    this.type = type
+    this.operation = operation
+    this.typeDonnees = typeDonnees
+    this.nombreDeValeurs = nombreDeValeurs
+    this.cellulesPreremplies = cellulesPreremplies
+    this.rayon = rayon
+    this.resultats = []
+    this.valeurMax = valeurMax
+    if (values === undefined || values.length === 0) {
+      const den = randint(2, this.valeurMax)
+      for (let i = 0; i < this.nombreDeValeurs ** 2; i++) {
+        switch (this.typeDonnees) {
+          case 'entiers' :
+            values.push(randint(1, this.valeurMax))
+            break
+          case 'entiers relatifs' :
+            values.push(randint(-this.valeurMax, this.valeurMax, 0))
+            break
+          case 'littéraux' :
+            values.push(calculer(`${randint(1, this.valeurMax)}x + ${randint(1, this.valeurMax)}`).printResult)
+            break
+          case 'fractions dénominateurs multiples':
+            values.push(math.fraction(randint(1, this.valeurMax), den))
+            break
+          case 'fractions positives dénominateurs premiers':
+            values.push(math.fraction(randint(1, this.valeurMax), choice([2, 3, 5, 7])))
+            break
+
+          case 'fractions positives' :
+            values.push(math.fraction(randint(1, this.valeurMax), randint(2, this.valeurMax)))
+            break
+          case 'fractions relatives' :
+            values.push(math.fraction(randint(-this.valeurMax, this.valeurMax, 0), randint(2, this.valeurMax)))
+            break
         }
       }
-      if (type === 'can1' && indexInconnue === i) bulle1.color = colorToLatexOrHTML('red')
-      else bulle1.color = colorToLatexOrHTML('black')
-    }
-    if (type === 'solution' || type === 'facteur' || type === 'can1' || type === 'can2') { // on ajoute les produits
-      if (!(type === 'can2' && indexInconnue === i)) {
-        objets.push(texteParPoint((values[i] * values[(i + 1) % n]).toString(), P))
+    } else { // si elles sont définies, on complète éventuellement la grille aléatoirement.
+      for (let i = this.values.length; i < this.nombreDeValeurs ** 2; i++) {
+        switch (this.typeDonnees) {
+          case 'entiers' :
+            values.push(randint(1, this.valeurMax))
+            break
+          case 'entiers relatifs' :
+            values.push(randint(-this.valeurMax, this.valeurMax, 0))
+            break
+          case 'littéraux' :
+            values.push(calculer(`${randint(1, this.valeurMax)}x + ${randint(1, this.valeurMax)}`).printResult)
+            break
+          case 'fractions dénominateurs multiples':
+            values.push(math.fraction(randint(1, this.valeurMax), values[i - 1].d))
+            break
+          case 'fractions positives dénominateurs premiers':
+            values.push(math.fraction(randint(1, this.valeurMax), choice([2, 3, 5, 7])))
+            break
+          case 'fractions positives' :
+            values.push(math.fraction(randint(1, this.valeurMax), randint(2, this.valeurMax)))
+            break
+          case 'fractions relatives' :
+            values.push(math.fraction(randint(-this.valeurMax, this.valeurMax, 0), randint(2, this.valeurMax)))
+            break
+        }
       }
-      if (type === 'can2' && indexInconnue === i) bulle2.color = colorToLatexOrHTML('red')
-      else bulle2.color = colorToLatexOrHTML('black')
     }
-    objets.push(bulle2)
+    this.values = values
+    console.log(this.values)
+    this.calculeResultats()
+    if (this.type === 'can1' || this.type === 'can2') {
+      this.indexInconnue = randint(0, this.nombreDeValeurs - 1)
+    } else {
+      this.indexInconnue = 999
+    }
   }
-  return objets
+
+  // méthode qui calcule les résultats si on le veut (sinon on peut les renseigner dans this.resultats manuellement)
+  calculeResultats () {
+    for (let i = 0; i < this.nombreDeValeurs; i++) {
+      this.resultats[i] = this.operate(this.values[i], this.values[(i + 1) % this.nombreDeValeurs])
+    }
+  }
+
+  // fonction utilisée par calculeResultats
+  operate (a, b) {
+    switch (this.operation) {
+      case 'addition':
+        if (this.typeDonnees !== 'littéraux') {
+          if (this.typeDonnees.substring(0, 4) === 'frac') {
+            return math.fraction(math.add(a, b))
+          } else {
+            return math.add(a, b)
+          }
+        } else {
+          return calculer(`${a.toString()}+${b.toString()}`).printResult
+        }
+      case 'multiplication':
+        if (this.typeDonnees !== 'littéraux') {
+          if (this.typeDonnees.substring(0, 4) === 'frac') {
+            return math.fraction(math.multiply(a, b))
+          } else {
+            return math.multiply(a, b)
+          }
+        } else {
+          return calculer(`(${a.toString()})*(${b.toString()})`).printResult
+        }
+    }
+  }
+
+  representation () {
+    const objets = []
+    const O = point(0, 0)
+    const A = rotation(point(this.rayon, 0), O, 180 / this.nombreDeValeurs - 90)
+    for (let i = 0, bulle1, bulle2; i < this.nombreDeValeurs; i++) {
+      const M = rotation(A, O, 360 * i / this.nombreDeValeurs)
+      const B = similitude(M, O, 180 / this.nombreDeValeurs, 1.3)
+      const D = similitude(M, O, -180 / this.nombreDeValeurs, 1.3)
+      const C = homothetie(M, O, 1.6)
+      const N = rotation(C, O, 360 / this.nombreDeValeurs)
+      const P = similitude(M, O, 180 / this.nombreDeValeurs, 2.5)
+      const s = segment(O, B, 'black')
+      const t = segment(B, C, 'black')
+      const u = segment(C, D, 'black')
+      const s1 = homothetie(segment(C, P), C, (longueur(C, P) - 0.8) / longueur(C, P))
+      s1.styleExtremites = '->'
+      s1.tailleExtremites = 2
+      s1.pointilles = 2
+      const s2 = homothetie(segment(N, P), N, (longueur(N, P) - 0.8) / longueur(N, P))
+      s2.styleExtremites = '->'
+      s2.tailleExtremites = 2
+      s2.pointilles = 2
+      if (this.type === 'can1') {
+        bulle1 = vide2d() // rotation(boite({??????}), M, 180 / this.nombreDeValeurs - 90)
+      } else {
+        bulle1 = vide2d()
+      }
+      objets.push(bulle1)
+      objets.push(s, t, u, s1, s2)
+      bulle2 = rotation(polygoneRegulierParCentreEtRayon(P, 0.8, this.nombreDeValeurs), P, 360 / this.nombreDeValeurs - 90)
+      if (this.type === 'résultats' || this.type === 'solutions' || this.type === 'can1' || this.type === 'can2') {
+        if (!(this.type === 'can1' && (this.indexInconnue === i || i === (this.indexInconnue - 1) % this.nombreDeValeurs || i === (this.indexInconnue + 1) % this.nombreDeValeurs))) {
+          if (!(this.type === 'can2' && (this.indexInconnue === i || i === (this.indexInconnue + 1) % this.nombreDeValeurs))) {
+            objets.push(texteParPoint(this.values[i].toString(), M))
+          }
+        }
+        if (this.type === 'can1' && this.indexInconnue === i) bulle1.color = colorToLatexOrHTML('red')
+        else {
+          bulle1.color = colorToLatexOrHTML('black')
+        }
+      }
+      if (this.type === 'solutions' || this.type === 'facteur' || this.type === 'can1' || this.type === 'can2') { // on ajoute les produits
+        if (!(this.type === 'can2' && this.indexInconnue === i)) {
+          objets.push(texteParPoint((this.values[i] * this.values[(i + 1) % this.nombreDeValeurs]).toString(), P))
+        }
+        if (this.type === 'can2' && this.indexInconnue === i) bulle2.color = colorToLatexOrHTML('red')
+        else bulle2.color = colorToLatexOrHTML('black')
+      } else {
+        objets.push(texteParPoint(this.cellulesPreremplies[i], P))
+      }
+
+      objets.push(bulle2)
+    }
+    return objets
+  }
 }
+
 export default function RoseMultiplicative () {
   Exercice.call(this) // Héritage de la classe Exercice()
   this.spacing = 2
@@ -76,44 +186,60 @@ export default function RoseMultiplicative () {
   this.sup = 10
   this.sup2 = 5
   this.sup3 = 1
+  this.operation = 'multiplication'
+  this.type = 'résultats'
+  this.typeDonnees = 'entiers'
+  this.nombreDeValeurs = 5
+  this.valeurMax = 10
+  this.values = []
   this.introduction = 'Les nombres situés à l\'extrémité des flèches sont les produits des nombres dont les flèches sont issues.'
 
   this.nouvelleVersion = function () {
     this.listeQuestions = [] // Liste de questions
     this.listeCorrections = [] // Liste de questions corrigées
-    let type
-    switch (contraindreValeur(1, 4, this.sup3, 1)) {
-      case 1:
-        type = 'produit'
-        this.consigne = 'Calculer les produits à l\'extrémité des flèches'
+    switch (this.type) {
+      case 'résultats':
+        if (this.operation === 'multiplication') {
+          this.consigne = 'Calculer les produits à l\'extrémité des flèches'
+        } else {
+          this.consigne = 'Calculer les sommes à l\'extrémité des flèches'
+        }
         break
-      case 2:
-        type = 'facteur'
-        this.consigne = 'Retrouver les facteurs à l\'origine des flèches'
+      case 'valeurs':
+        if (this.operation === 'multiplication') {
+          this.consigne = 'Retrouver les facteurs à l\'origine des flèches'
+        } else {
+          this.consigne = 'Retrouver les termes à l\'origine des flèches'
+        }
         break
-      case 3:
-        type = 'can1'
-        this.consigne = 'Trouver le nombre de la case rouge'
+      case 'can1':
+        if (this.typeDonnees === 'nombres') {
+          this.consigne = 'Trouver le nombre de la case rouge'
+        } else {
+          this.consigne = 'Trouver l\'expression de la case rouge'
+        }
         break
-      case 4:
-        type = 'can2'
-        this.consigne = 'Trouver le nombre de la case rouge'
+      case 'can2':
+        if (this.typeDonnees === 'nombres') {
+          this.consigne = 'Trouver le nombre de la case rouge'
+        } else {
+          this.consigne = 'Trouver l\'expression de la case rouge'
+        }
         break
     }
-    const nombreDeFacteurs = contraindreValeur(3, 9, this.sup2, 5)
-    const facteurMax = contraindreValeur(10, 30, this.sup, 10)
+
     for (
-      let i = 0, values, objets, objetsCorr, texte, texteCorr, cpt = 0;
+      let i = 0, objets, objetsCorr, texte, texteCorr, cpt = 0;
       i < this.nbQuestions && cpt < 50;
 
     ) {
-      values = shuffle(rangeMinMax(2, facteurMax)).slice(0, nombreDeFacteurs)
-      console.log(type)
-      objets = rose({ values, type })
-      objetsCorr = rose({ values, type: 'solution' })
+      const rose = new Rose({ values: this.values, nombreDeValeurs: this.nombreDeValeurs, type: this.type, operation: this.operation, valeurMax: this.valeurMax, typeDonnees: this.typeDonnees })
+      objets = rose.representation()
+      rose.type = 'solutions'
+      objetsCorr = rose.representation()
       texte = mathalea2d(Object.assign({}, fixeBordures(objets)), objets)
       texteCorr = mathalea2d(Object.assign({}, fixeBordures(objetsCorr)), objetsCorr)
-      if (this.questionJamaisPosee(i, ...values)) {
+      if (this.questionJamaisPosee(i, ...rose.values)) {
         // Si la question n'a jamais été posée, on en crée une autre
         this.listeQuestions.push(texte)
         this.listeCorrections.push(texteCorr)
@@ -125,5 +251,5 @@ export default function RoseMultiplicative () {
   }
   this.besoinFormulaireNumerique = ['Valeur maximale (entre 10 et 30) des facteurs', 30]
   this.besoinFormulaire2Numerique = ['Nombre de facteur entre 3 et 9']
-  this.besoinFormulaire3Numerique = ['Type de question', 4, '1 : Calculer les produits\n2 : Calculer les facteurs\n3 : Type course aux nombres 1\n4 : Type course aux nombres 2']
+  this.besoinFormulaire3Numerique = ['Type de question', 4, '1 : Calculer les produits\n2 : Calculer les facteurs\n3 : this.Type course aux nombres 1\n4 : this.Type course aux nombres 2']
 }
