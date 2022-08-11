395d9ce5a1fb8d879c1b75f72af828ba368f13f2
Jean-Claude Lhote <lhotejc@gmail.com>
2022-08-11 19:53:25 +0200

    feat: 2 nouvelles Can : can6C38 et can6C39

--- src/js/exercices/can/6e/can6C38.js
+++ src/js/exercices/can/6e/can6C38.js
@@ -1,14 +1,14 @@
-import { randint } from '../../modules/outils.js'
-import { Yohaku } from '../beta/betaYohaku.js'
-import Exercice from '../Exercice.js'
-export const titre = 'Multiplication à trou Yohaku'
+import { randint } from '../../../modules/outils.js'
+import { Yohaku } from '../../beta/betaYohaku.js'
+import Exercice from '../../Exercice.js'
+export const titre = 'Addition à trou Yohaku'
 export const interactifReady = true
 export const interactifType = 'mathLive'
 export const amcReady = true
 export const amcType = 'AMCNum'
 export const dateDePublication = '10/08/2022'
 
-export default function YohakuCan6b () {
+export default function YohakuCan6a () {
   Exercice.call(this)
   this.typeExercice = 'simple'
   this.nbQuestions = 1
@@ -17,9 +17,9 @@ export default function YohakuCan6b () {
   this.nouvelleVersion = function () {
     const laCase = randint(0, 3)
     const indexReponse = 3 - laCase
-    const yohaku1 = new Yohaku({ type: 'entiers', largeur: 2, hauteur: 2, taille: 2, operation: 'multiplication', valeurMax: 10, solution: false, cellulesPreremplies: Array.from('abcd'), Case: laCase })
+    const yohaku1 = new Yohaku({ type: 'entiers', largeur: 2, hauteur: 2, taille: 2, operation: 'addition', valeurMax: 10, solution: false, cellulesPreremplies: Array.from('abcd'), Case: laCase })
     yohaku1.calculeResultats()
-    this.question = 'Les nombres en bout de ligne ou de colonne sont les produits des nombres contenus dans la ligne ou la colonne.<br>'
+    this.question = 'Les nombres en bout de ligne ou de colonne sont les sommes des nombres contenus dans la ligne ou la colonne.<br>'
     this.question += `Donner la valeur de la case ${yohaku1.cellulesPreremplies[indexReponse]}<br>${yohaku1.representation()}`
     this.reponse = yohaku1.cellules[indexReponse]
     console.log(indexReponse, yohaku1)
