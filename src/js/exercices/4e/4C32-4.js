74898ec7e418aaae637ef3601daf37716f696f95
EricElter <eric.elter@ac-rouen.fr>
2022-08-20 16:54:38 +0200

    fix : 4C32-4 : Pb de couleurs Latex réglé

--- src/js/exercices/4e/4C32-4.js
+++ src/js/exercices/4e/4C32-4.js
@@ -73,10 +73,10 @@ export default function CalculsPuissancesDe10 () {
       scientifiquestring = `${texNombre(mantisse)} \\times 10^{${exp}}`
 
       texte = `$\\dfrac{${texNombre(a)}\\times 10^{${aexp}}}{${texNombre(c)}\\times 10^{${cexp}}} \\times \\dfrac{${texNombre(b)}\\times 10^{${bexp}}}{${texNombre(d)}\\times 10^{${dexp}}} =$`
-      texteCorr = `$\\dfrac{ ${miseEnEvidence(`${texNombre(a)}\\times 10^{${aexp}}`, 'blue')}  }{ ${miseEnEvidence(`${texNombre(c)}\\times 10^{${cexp}}`, 'blueviolet')} } \\times \\dfrac{ ${miseEnEvidence(`${texNombre(b)}\\times 10^{${bexp}}`, 'deeppink')} }{ ${miseEnEvidence(`${texNombre(d)}\\times 10^{${dexp}}`, 'seagreen')} } =$`
+      texteCorr = `$\\dfrac{ ${miseEnEvidence(`${texNombre(a)}\\times 10^{${aexp}}`, 'blue')}  }{ ${miseEnEvidence(`${texNombre(c)}\\times 10^{${cexp}}`, '#8A2BE2')} } \\times \\dfrac{ ${miseEnEvidence(`${texNombre(b)}\\times 10^{${bexp}}`, '#FF1493')} }{ ${miseEnEvidence(`${texNombre(d)}\\times 10^{${dexp}}`, '#2E8B57')} } =$`
       if (this.correctionDetaillee) {
-        texteCorr += `$ \\dfrac{${miseEnEvidence(texFactorisation(a), 'blue')} \\times ${miseEnEvidence(texFactorisation(b), 'deeppink')}}{${miseEnEvidence(texFactorisation(c), 'blueviolet')} \\times ${miseEnEvidence(texFactorisation(d), 'seagreen')}} $`
-        texteCorr += `$ \\times \\dfrac{${miseEnEvidence(`10^{${aexp}}`, 'blue')} \\times ${miseEnEvidence(`10^{${bexp}}`, 'deeppink')}}{${miseEnEvidence(`10^{${cexp}}`, 'blueviolet')} \\times ${miseEnEvidence(`10^{${dexp}}`, 'seagreen')}} =$`
+        texteCorr += `$ \\dfrac{${miseEnEvidence(texFactorisation(a), 'blue')} \\times ${miseEnEvidence(texFactorisation(b), '#FF1493')}}{${miseEnEvidence(texFactorisation(c), '#8A2BE2')} \\times ${miseEnEvidence(texFactorisation(d), '#2E8B57')}} $`
+        texteCorr += `$ \\times \\dfrac{${miseEnEvidence(`10^{${aexp}}`, 'blue')} \\times ${miseEnEvidence(`10^{${bexp}}`, '#FF1493')}}{${miseEnEvidence(`10^{${cexp}}`, '#8A2BE2')} \\times ${miseEnEvidence(`10^{${dexp}}`, '#2E8B57')}} =$`
       }
       texteCorr += ` $ ${scientifiquestring}$`
       this.autoCorrection[i] = {}
