5f0f4d748cf0ec348f670b3cf09bbb6b485653eb
EricElter <eric.elter@ac-rouen.fr>
2022-09-06 16:03:35 +0200

    new : Nouvel exercice 3A10-6 (Corrections non terminées)

--- src/js/exercices/3e/3A10-6.js
+++ src/js/exercices/3e/3A10-6.js
@@ -1,6 +1,5 @@
 import Exercice from '../Exercice.js'
-import { listeQuestionsToContenu, randint, contraindreValeur, compteOccurences, texNombre, combinaisonListes, rangeMinMax, choice, texteGras, sp, texteEnCouleur, texteEnCouleurEtGras, sommeDesChiffres } from '../../modules/outils.js'
-import { max } from 'mathjs'
+import { listeQuestionsToContenu, randint, contraindreValeur, compteOccurences, combinaisonListes, rangeMinMax, choice, texteGras, sp, texteEnCouleur, texteEnCouleurEtGras, sommeDesChiffres } from '../../modules/outils.js'
 // import { setReponse } from '../../modules/gestionInteractif.js'
 // import { ajouteChampTexteMathLive } from '../../modules/interactif/questionMathLive.js'
 export const titre = 'Trouver un chiffre pour qu\'un nombre soit divisible par un autre'
@@ -17,8 +16,8 @@ export const dateDePublication = '29/08/2022'
  * Référence 3A10-6
  */
 
-export const ref = '3A10-5'
-export default function recourirDecompositionFacteursPremiers () {
+export const ref = '3A10-6'
+export default function trouverChiffre () {
   Exercice.call(this) // Héritage de la classe Exercice()
   this.nbQuestions = 4
   this.besoinFormulaireTexte = ['Nombres premiers utilisés ', ' Choix séparés par des tirets\n1 : 2, 3 et 5\n2 : 2, 3 et 7\n3 : 2, 5 et 7\n4 : 3, 5 et 7\n5 : Mélange']
@@ -69,56 +68,36 @@ export default function recourirDecompositionFacteursPremiers () {
         }
       }
     }
-    casChoixDiviseurs = [0, 1]
+    casChoixDiviseurs = [2, 3]
     if (compteOccurences(casChoixDiviseurs, 8) > 0) casChoixDiviseurs = rangeMinMax(0, 7) // Teste si l'utilisateur a choisi tout
     casChoixDiviseurs = combinaisonListes(casChoixDiviseurs, this.nbQuestions)
 
-    for (let i = 0, texte, texteCorr, cpt = 0, nbAvant, nbApres, positionX, a, nbAvecChiffreCache,
+    for (let i = 0, texte, texteCorr, cpt = 0, nb, positionX, a, tabChiffresX, nbAvecChiffreCache,
       sommePourTroisouNeuf, ajoutPourTroisouNeuf; i < this.nbQuestions && cpt < 50;) {
       texte = 'Dans le nombre suivant à ' + nombreDeChiffres[i] + ' chiffres '
       positionX = this.sup3 === 1 ? nombreDeChiffres[i] - 1 : this.sup3 === 2 ? randint(1, nombreDeChiffres[i] - 2) : choice([nombreDeChiffres[i] - 1, randint(1, nombreDeChiffres[i] - 2)])
-      a = [randint(1, 9)]
-      for (let ee = 1; ee < nombreDeChiffres[i] - 1; ee++) a.push(randint(0, 9))
 
-      if (nombreDeChiffres[i] > 3) {
-        nbAvecChiffreCache = texNombre(a[0])
-        if (nombreDeChiffres[i] === 6) {
-          if (positionX === 1) nbAvecChiffreCache += '...' + texNombre(a[1])
-          else if (positionX === 2) nbAvecChiffreCache += texNombre(a[1]) + '...'
-          else nbAvecChiffreCache += texNombre(a[1]) + texNombre(a[2])
-        } else if (nombreDeChiffres[i] === 5) {
-          if (positionX === 1) nbAvecChiffreCache += '...' + texNombre(a[1])
-          else nbAvecChiffreCache += texNombre(a[1])
-        }
-        nbAvecChiffreCache += sp(2)
-        nbAvant = 0
-        for (let ee = (nombreDeChiffres[i] - positionX > 3 ? nombreDeChiffres[i] - 4 : nombreDeChiffres[i] - 3); ee < positionX; ee++) {
-          nbAvant = nbAvant * 10 + a[ee]
-        }
-        nbApres = 0
-        for (let ee = max((nombreDeChiffres[i] - positionX > 3 ? nombreDeChiffres[i] - 4 : nombreDeChiffres[i] - 3), positionX); ee < nombreDeChiffres[i] - 1; ee++) {
-          nbApres = nbApres * 10 + a[ee]
-        }
-        nbAvecChiffreCache += (nbAvant !== 0 ? texNombre(nbAvant) : '') + (nombreDeChiffres[i] - 3 > positionX ? '' : '...') + (nbApres !== 0 ? texNombre(nbApres) : '')
-      } else {
-        nbAvant = 0
-        for (let ee = 0; ee < positionX; ee++) {
-          nbAvant = nbAvant * 10 + a[ee]
-        }
-        nbApres = 0
-        for (let ee = positionX; ee < nombreDeChiffres[i] - 1; ee++) {
-          nbApres = nbApres * 10 + a[ee]
-        }
-        nbAvecChiffreCache = texNombre(nbAvant) + '...' + (nbApres === 0 ? '' : texNombre(nbApres))
+      nb = randint(0, 9)
+      a = [nb]
+      tabChiffresX = [nb]
+      for (let ee = 1; ee < nombreDeChiffres[i]; ee++) {
+        if (positionX !== ee) {
+          nb = randint(0, 9)
+          a.push(nb)
+          tabChiffresX.push((ee === nombreDeChiffres[i] - 3 ? sp(2) : '') + nb)
+        } else tabChiffresX.push((ee === nombreDeChiffres[i] - 3 ? sp(2) : '') + '...')
       }
+      nbAvecChiffreCache = tabChiffresX.join('').toString()
+
       texte += sp(2) + texteGras(nbAvecChiffreCache) + sp(2)
       texte += ', il manque un chiffre (à la place de ...). <br>'
       texteCorr = `Quel chiffre peut-on mettre dans ${sp(2)} ${texteGras(nbAvecChiffreCache)} ${sp(2)} pour qu'il soit divisible ${choixDiviseurs[casChoixDiviseurs[i]]} ?`
       texte += texteCorr
       texte += texteEnCouleur(' Une ou plusieurs réponses sont possibles tout comme qu\'aucune.', 'gray')
+
       switch (casChoixDiviseurs[i]) {
         case 0 : // Divisible par 2
-          texteCorr += '<br>Un nombre divisible par 2 est pair donc ici, '
+          texteCorr += '<br>Un entier divisible par 2 est pair donc ici, '
           switch (positionX) {
             case nombreDeChiffres[i] - 1 :
               texteCorr += texteEnCouleurEtGras('tout chiffre pair suffit.<br>')
@@ -151,7 +130,7 @@ export default function recourirDecompositionFacteursPremiers () {
           texteCorr += '<br>Il suffit de savoir quel nombre à un chiffre il faut ajouter à '
           texteCorr += (nombreDeChiffres[i] !== 2 ? sommeDesChiffres(a)[1] + '=' : '')
           texteCorr += sommePourTroisouNeuf
-          texteCorr += ' pour obtenir un multiple de 3.<br>Or '
+          texteCorr += ' pour obtenir un multiple de 3.<br>Or seuls '
           for (let ee = 0; ee < ajoutPourTroisouNeuf.length - 2; ee++) {
             texteCorr += sommePourTroisouNeuf + '+' + ajoutPourTroisouNeuf[ee] + '=' + (sommePourTroisouNeuf + ajoutPourTroisouNeuf[ee]) + ', '
           }
@@ -168,7 +147,51 @@ export default function recourirDecompositionFacteursPremiers () {
           }
           texteCorr += `${nbAvecChiffreCache.replace('...', ajoutPourTroisouNeuf[ajoutPourTroisouNeuf.length - 2])} et ${nbAvecChiffreCache.replace('...', ajoutPourTroisouNeuf[ajoutPourTroisouNeuf.length - 1])} sont divisibles par 3.`
           break
+        case 2 : // Divisible par 5
+          texteCorr += '<br>Un entier divisible par 5 a son chiffre des unités égal à 0 ou 5 donc ici, '
+          switch (positionX) {
+            case nombreDeChiffres[i] - 1 :
+              texteCorr += texteEnCouleurEtGras('les chiffres 0 et 5 suffisent.<br>')
+              texteCorr += `${nbAvecChiffreCache.replace('...', 0)} et ${nbAvecChiffreCache.replace('...', '5')} sont divisibles par 5.`
+              break
+            default :
+              if (a[nombreDeChiffres[i] - 2] % 5 === 0) {
+                texteCorr += texteEnCouleurEtGras('tout chiffre convient') + ' car le chiffre des unités est déjà égal à 0 ou 5.<br>'
+                for (let ee = 0; ee < 8; ee++) {
+                  texteCorr += `${nbAvecChiffreCache.replace('...', ee)}, `
+                }
+                texteCorr += `${nbAvecChiffreCache.replace('...', 8)} et ${nbAvecChiffreCache.replace('...', 9)} sont divisibles par 5.`
+              } else {
+                texteCorr += texteEnCouleurEtGras('aucun chiffre convient') + ' car le chiffre des unités n\'est déjà pas égal à 0 ou 5.<br>'
+                for (let ee = 0; ee < 8; ee++) {
+                  texteCorr += `${nbAvecChiffreCache.replace('...', ee)}, `
+                }
+                texteCorr += `${nbAvecChiffreCache.replace('...', 8)} et ${nbAvecChiffreCache.replace('...', 9)} ne sont pas divisibles par 5.`
+              }
+              break
+          }
+          break
+        case 3 : // Divisible par 9
+          sommePourTroisouNeuf = sommeDesChiffres(a)[0]
+          ajoutPourTroisouNeuf = sommePourTroisouNeuf % 9 === 0 ? [0, 9] : [9 - sommePourTroisouNeuf % 9]
+
+          texteCorr += '<br>Il suffit de savoir quel nombre à un chiffre il faut ajouter à '
+          texteCorr += (nombreDeChiffres[i] !== 2 ? sommeDesChiffres(a)[1] + '=' : '')
+          texteCorr += sommePourTroisouNeuf
+          texteCorr += ' pour obtenir un multiple de 9.<br>Or '
+          texteCorr += ajoutPourTroisouNeuf.length === 2 ? 'seuls ' : 'seul '
+          texteCorr += sommePourTroisouNeuf + '+' + ajoutPourTroisouNeuf[0] + '=' + (sommePourTroisouNeuf + ajoutPourTroisouNeuf[0])
+          texteCorr += ajoutPourTroisouNeuf.length === 2
+            ? ' et ' + sommePourTroisouNeuf + '+' + ajoutPourTroisouNeuf[1] + '=' + (sommePourTroisouNeuf + ajoutPourTroisouNeuf[1]) + ' sont divisibles '
+            : ' est divisible '
+          texteCorr += 'par 9 donc ici, '
+          texteCorr += ajoutPourTroisouNeuf.length === 2
+            ? `les chiffres qui conviennent sont : 0 et 9.<br>${nbAvecChiffreCache.replace('...', ajoutPourTroisouNeuf[0])} et ${nbAvecChiffreCache.replace('...', ajoutPourTroisouNeuf[1])} sont divisibles par 9.`
+
+            : `le chiffre qui convient est : ${texteEnCouleurEtGras(ajoutPourTroisouNeuf[0])}.<br>${nbAvecChiffreCache.replace('...', ajoutPourTroisouNeuf[0])}  est divisible par 9.`
+          break
       }
+
       if (this.listeQuestions.indexOf(texte) === -1) {
         // Si la question n'a jamais été posée, on en crée une autre
         this.listeQuestions.push(texte)
