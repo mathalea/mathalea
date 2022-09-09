594bd230296a61a33a44b2ae16cad1b91149ab58
EricElter <eric.elter@ac-rouen.fr>
2022-09-09 18:00:59 +0200

    new : 6C11-3 + amélioration pose de la division euclidienne

--- src/js/modules/outils.js
+++ src/js/modules/outils.js
@@ -3108,11 +3108,15 @@ export function premiereLettreEnMajuscule (text) { return (text + '').charAt(0).
 
 /**
 * Renvoie le nombre de chiffres de la partie décimale
+* @param nb : nombre décimal
+* @param except : chiffre à ne pas compter (0 par exemple) [Ajout EE]
 * @author Rémi Angot
 */
-export function nombreDeChiffresDansLaPartieDecimale (nb) {
+export function nombreDeChiffresDansLaPartieDecimale (nb, except = 'aucune') {
+  let sauf = 0
   if (String(nb).indexOf('.') > 0) {
-    return String(nb).split('.')[1].length
+    if (!isNaN(except)) sauf = (String(nb).split('.')[1].split(String(except)).length - 1)
+    return String(nb).split('.')[1].length - sauf
   } else {
     return 0
   }
@@ -3121,25 +3125,29 @@ export function nombreDeChiffresDansLaPartieDecimale (nb) {
  * Renvoie le nombre de chiffres dans la partie entière
  * @author ?
  */
-export function nombreDeChiffresDansLaPartieEntiere (nb) {
-  let nombre
+export function nombreDeChiffresDansLaPartieEntiere (nb, except = 'aucune') {
+  let nombre; let sauf = 0
   if (nb < 0) {
     nombre = -nb
   } else {
     nombre = nb
   }
   if (String(nombre).indexOf('.') > 0) {
-    return String(nombre).split('.')[0].length
+    if (!isNaN(except)) sauf = (String(nombre).split('.')[0].split(String(except)).length - 1)
+    return String(nombre).split('.')[0].length - sauf
   } else {
+    if (!isNaN(except)) sauf = (String(nombre).split(String(except)).length - 1)
     return String(nombre).length
   }
 }
 /**
  * Renvoie le nombre de chiffres d'un nombre décimal
+ * @param nb : nombre décimal
+ * @param except : chiffre à ne pas compter (0 par exemple) [Ajout EE]
  * @author Jean-Claude Lhote
  */
-export function nombreDeChiffresDe (nb) {
-  return nombreDeChiffresDansLaPartieDecimale(nb) + nombreDeChiffresDansLaPartieEntiere(nb)
+export function nombreDeChiffresDe (nb, except) {
+  return nombreDeChiffresDansLaPartieDecimale(nb, except) + nombreDeChiffresDansLaPartieEntiere(nb, except)
 }
 /**
  * Retourne la string LaTeX de la fraction
