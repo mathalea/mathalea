c37d0fb5bb65242400567050ada2cca32fbf8719
EricElter <eric.elter@ac-rouen.fr>
2022-09-14 18:53:43 +0200

    new : 4 nouvels exos Arithmétique 3eme

---
+++ src/js/exercices/3e/3A13-2.js
@@ -0,0 +1,16 @@
+import DivisionsEuclidiennesEgalite from '../6e/6C11-1.js'
+export const titre = 'Déterminer reste et quotient d\'une division euclidienne à partir d\'une égalité'
+
+export const dateDePublication = '14/09/2022'
+
+/**
+ * Détermination du reste et quotient à partir de l'égalité découlant de la division euclidienne
+ * Ref 3A13-2
+ *
+ * @author Eric Elter
+ */
+
+export default function DivisionsEuclidiennesEgalite3e () {
+  DivisionsEuclidiennesEgalite.call(this)
+  this.sup = 1
+}
