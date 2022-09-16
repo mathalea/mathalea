ce01e5722d4339278b6358ae894b822e81f44e25
moragi <gilles.mora@ac-rouen.fr>
2022-09-16 16:50:41 +0200

    ex : can2C13 sur les opérations puissances

--- src/js/exercices/can/2e/can2C13.js
+++ src/js/exercices/can/2e/can2C13.js
@@ -1,5 +1,5 @@
 import Exercice from '../../Exercice.js'
-import { randint, calcul, choice, texNombrec, abs, stringNombre, ecritureParentheseSiNegatif, sp } from '../../../modules/outils.js'
+import { randint, choice, ecritureParentheseSiNegatif } from '../../../modules/outils.js'
 export const titre = 'Calculer avec  des puissances'
 export const interactifReady = true
 export const interactifType = 'mathLive'
@@ -19,70 +19,74 @@ export default function CalculPuissancesOperation () {
   this.nbQuestions = 1
   this.formatChampTexte = 'largeur15 inline'
   this.nouvelleVersion = function () {
-    let a, b, c, d, n, p, s
-    switch (choice(['a'])) { //, 'b', 'c', 'd', 'e', 'f'
+    let a, b, n, p, s
+    switch (choice(['a', 'b', 'c', 'd', 'e'])) { //, 'b', 'c', 'd', 'e', 'f'
       case 'a':
-        a = randint(-9, 9)
+        a = randint(-9, 9, [0, 1, -1])
         n = randint(-9, 9, [0, 1, -1])
         p = randint(-9, 9, [0, 1, -1])
-       s=n+p
+        s = n + p
         this.question = `Écrire sous la forme $a^n$ où $a$ est un nombre relatif et $n$ est un entier relatif. <br>
         $${ecritureParentheseSiNegatif(a)}^{${n}}\\times ${ecritureParentheseSiNegatif(a)}^{${p}}=$`
-        this.correction = `(${stringNombre(a)})^{${stringNombre(s)}} et  $${a}^{${n + p}}$ `
-         this.reponse = `${ecritureParentheseSiNegatif(a)}^{${n + p}}` 
-         
-        console.log(this.reponse)
+        this.correction = `On utilise la formule $a^n\\times a^m=a^{n+m}$ avec $a=${a}$, $n=${n}$ et $p=${p}$.<br>
+        $${ecritureParentheseSiNegatif(a)}^{${n}}\\times ${ecritureParentheseSiNegatif(a)}^{${p}}=${ecritureParentheseSiNegatif(a)}^{${n}+${p}}=${ecritureParentheseSiNegatif(a)}^{${n + p}}$
+        `
+        this.reponse = `${ecritureParentheseSiNegatif(a)}^{${n + p}}`
+
         break
       case 'b':
-        a = choice([0.2, 0.4])
-        b = randint(2, 5)
-        this.question = `Calculer sous la forme d'un nombre entier : <br>
-        $5^{${b}} \\times ${texNombrec(a)}^{${b}}= $`
+        a = randint(-9, 9, [0, 1, -1])
+        b = randint(-9, 9, [0, 1, -1])
+        n = randint(-9, 9, [0, 1, -1])
+        p = a * b
+        this.question = `Écrire sous la forme $a^n$ où $a$ est un nombre relatif et $n$ est un entier relatif. <br>
+        $${ecritureParentheseSiNegatif(a)}^{${n}}\\times ${ecritureParentheseSiNegatif(b)}^{${n}}=$`
         this.correction = `On utilise la formule $a^n\\times b^n=(a\\times b)^{n}$
-        avec $a=5$,  $b=${texNombrec(a)}$ et $n=${b}$.<br>
-       $5^{${b}}\\times ${texNombrec(a)}^{${b}}=(5\\times ${texNombrec(a)})^{${b}}=
-       ${5 * a}^${texNombrec(b)}=${texNombrec((5 ** b) * (a ** b))} $`
-        this.reponse = calcul((5 ** b) * (a ** b))
+        avec $a=${a}$,  $b=${b}$ et $n=${n}$.<br>
+        $${ecritureParentheseSiNegatif(a)}^{${n}}\\times ${ecritureParentheseSiNegatif(b)}^{${n}}=${ecritureParentheseSiNegatif(a)}\\times ${ecritureParentheseSiNegatif(b)}^{${n}}=${ecritureParentheseSiNegatif(p)}^{${n}}$
+        `
+        this.reponse = `${ecritureParentheseSiNegatif(p)}^{${n}}`
         break
 
       case 'c':
-        a = randint(-3, -1)
-        this.question = `Calculer sous la forme d'un nombre entier : <br>
-        $2^{${a}} \\times 8=$`
-        this.correction = `Comme $a^{-n}=\\dfrac{1}{a^n}$, ${sp(4)}  $2^{${a}}=\\dfrac{1}{2^{${-a}}}=\\dfrac{1}{${2 ** (-a)}}$. <br>
-
-        
-        $2^{${a}}\\times 8=\\dfrac{1}{${2 ** abs(a)}}\\times 8=${texNombrec(8 * 1 / 2 ** (-a))} $`
-        this.reponse = calcul((2 ** a) * 8)
+        a = randint(-9, 9, [0, 1, -1])
+        p = randint(-9, 9, [0, 1])
+        n = randint(-9, 9, [0, 1, -1])
+        s = n * p
+        this.question = `Écrire sous la forme $a^n$ où $a$ est un nombre relatif et $n$ est un entier relatif. <br>
+        $\\left(${ecritureParentheseSiNegatif(a)}^{${n}}\\right)^{${p}}=$`
+        this.correction = `On utilise la formule $\\left(a^n\\right)^p=a^{n\\times p}$
+        avec $a=${a}$,  $n=${n}$ et $p=${p}$.<br>
+        $\\left(${ecritureParentheseSiNegatif(a)}^{${n}}\\right)^{${p}}=${ecritureParentheseSiNegatif(a)}^{${n}\\times ${ecritureParentheseSiNegatif(p)}}=${ecritureParentheseSiNegatif(a)}^{${n * p}}$
+        `
+        this.reponse = `${ecritureParentheseSiNegatif(a)}^{${s}}`
         break
-      case 'd':
-        a = randint(-4, -1)
-        this.question = `Calculer sous la forme d'un nombre entier : <br> $2^{${a}} \\times 16=$`
-        this.correction = `Comme $a^{-n}=\\dfrac{1}{a^n}$, ${sp(4)}  $2^{${a}}=\\dfrac{1}{2^{${-a}}}=\\dfrac{1}{${2 ** (-a)}}$. <br>
 
-        
-        $2^{${a}}\\times 16=\\dfrac{1}{${2 ** abs(a)}}\\times 16=${texNombrec(16 * 1 / 2 ** (-a))} $`
-        this.reponse = calcul((2 ** a) * 16)
+      case 'd':
+        a = randint(-9, 9, [0, 1, -1])
+        p = randint(-9, 9, [0, 1])
+        n = randint(-9, 9, [0, 1, -1])
+        s = n - p
+        this.question = `Écrire sous la forme $a^n$ où $a$ est un nombre relatif et $n$ est un entier relatif. <br>
+        $\\dfrac{${ecritureParentheseSiNegatif(a)}^{${n}}}{${ecritureParentheseSiNegatif(a)}^{${p}}}=$`
+        this.correction = `On utilise la formule $\\dfrac{a^n}{a^p}=a^{n-p}$ avec $a=${a}$,  $n=${n}$ et $p=${p}$.<br>
+        $\\dfrac{${ecritureParentheseSiNegatif(a)}^{${n}}}{${ecritureParentheseSiNegatif(a)}^{${p}}}=${ecritureParentheseSiNegatif(a)}^{${n}- ${ecritureParentheseSiNegatif(p)}}=${ecritureParentheseSiNegatif(a)}^{${n - p}}$
+        `
+        this.reponse = `${ecritureParentheseSiNegatif(a)}^{${s}}`
         break
       case 'e':
-        a = randint(-5, -1)
-        this.question = `Calculer sous la forme d'un nombre entier : <br> $2^{${a}} \\times 32=$`
-        this.correction = `Comme $a^{-n}=\\dfrac{1}{a^n}$, ${sp(4)}  $2^{${a}}=\\dfrac{1}{2^{${-a}}}=\\dfrac{1}{${2 ** (-a)}}$. <br>
 
-        
-        $2^{${a}}\\times 32=\\dfrac{1}{${2 ** abs(a)}}\\times 32=${texNombrec(32 * 1 / 2 ** (-a))} $`
-        this.reponse = calcul((2 ** a) * 32)
-        break
-      case 'f':
-        a = randint(2, 10)
-        c = randint(-5, 10, [0, 1])
-        d = randint(2, 10)
-        this.question = `Compléter :<br> $${a}^{...}\\times ${a}^{${c}}=${a}^{${-d}}$`
-        this.correction = `On utilise la formule $a^n\\times a^m=a^{n+m}$.
-        La somme des exposants doit donner $${-d}$.<br>
-        On cherche donc le nombre qui, ajouté à $${c}$ donne $${-d}$.<br> Il s'agit de  : 
-$-${d}-${ecritureParentheseSiNegatif(c)}=${-d - c}$.`
-        this.reponse = calcul(-d - c)
+        b = randint(2, 6, [0, 1, -1])
+        a = choice([2, 3, 4, 5, 6, 7]) * b
+        n = randint(-9, 9, [0, 1, -1])
+        s = a / b
+        this.question = `Écrire sous la forme $a^n$ où $a$ est un nombre relatif et $n$ est un entier relatif. <br>
+        $\\dfrac{${ecritureParentheseSiNegatif(a)}^{${n}}}{${ecritureParentheseSiNegatif(b)}^{${n}}}=$`
+        this.correction = `On utilise la formule $\\dfrac{a^n}{b^n}=\\left(\\dfrac{a}{b}\\right)^{n}$ avec 
+        $a=${a}$,  $b=${b}$ et $n=${n}$.<br>
+        $\\dfrac{${ecritureParentheseSiNegatif(a)}^{${n}}}{${ecritureParentheseSiNegatif(b)}^{${n}}}=\\left(\\dfrac{${ecritureParentheseSiNegatif(a)}}{${ecritureParentheseSiNegatif(b)}}\\right)^{${n}}=${s}^${n}$
+        `
+        this.reponse = `${s}^{${n}}`
         break
     }
   }
