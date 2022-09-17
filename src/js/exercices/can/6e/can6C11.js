bd0dbce4ec23431435f18bdef37c17445e84b49b
EricElter <eric.elter@ac-rouen.fr>
2022-09-17 18:11:33 +0200

    doc : 2d : Amélioration de la doc labelY et suppression de labelX.

--- src/js/exercices/can/6e/can6C11.js
+++ src/js/exercices/can/6e/can6C11.js
@@ -1,4 +1,4 @@
-import { calcul, choice, randint, texNombre, texteEnCouleur } from '../../../modules/outils.js'
+import { choice, randint, texNombre, texteEnCouleur } from '../../../modules/outils.js'
 import Exercice from '../../Exercice.js'
 import Decimal from 'decimal.js/decimal.mjs'
 export const titre = 'Calculer le double ou le triple (décimal)'
