5974e8f0bcf9972fa72951728b5da39b021c0224
Jean-Claude Lhote <lhotejc@gmail.com>
2022-09-22 14:36:36 +0200

    feat: 6C10-8 permet de proposer aléatoirement des mots ou des phrases à décoder

--- src/js/exercices/Profs/P020.js
+++ src/js/exercices/Profs/P020.js
@@ -1,7 +1,7 @@
 import Exercice from '../Exercice.js'
-import { listeQuestionsToContenu, shuffle, tableauColonneLigne } from '../../modules/outils.js'
+import { choice, contraindreValeur, listeQuestionsToContenu, shuffle, tableauColonneLigne } from '../../modules/outils.js'
 export const titre = 'Encodeur de texte'
-export default function EncodeurTexte () {
+export default function EncodeurTexte (type = 'générateur') {
   Exercice.call(this)
   this.introduction = 'Générateur inspiré par la commande DefiTableTexte du package ProfCollege de Christophe Poulain.'
   this.consigne = 'Choisir un texte à encoder dans le formulaire en paramètre.'
@@ -10,6 +10,7 @@ export default function EncodeurTexte () {
   this.sup = 'mathématiques'
   this.sup2 = false
   this.nbQuestions = 1
+  this.besoinCorrection = false
 
   const tableauDesCaracteres = Array.from('-xçwjè,k~:aq«rlgdmftbéocsà.êeipzhu\'ynvî»â!')
   tableauDesCaracteres[2] = 'ç'
@@ -29,9 +30,30 @@ export default function EncodeurTexte () {
     return NaN
   }
   this.nouvelleVersion = function () {
+    const listeDeMots = ['mathématiques', 'diviseur', 'multiple', 'médiatrice', 'milieu', 'parallèle', 'perpendiculaire', 'multiplication', 'addition',
+      'soustraction', 'division', 'addition', 'cercle', 'histogramme', 'diagramme', 'numération', 'fraction', 'égalité', 'propriété', 'contre-exemple']
+    const listeDePhrases = ['Les mathématiques/c\'est fantastique', 'multiplier et diviser/se fait avant/additionner ou soustraire',
+      'être supérieur/à un nombre signifie/être plus grand que ce nombre', 'Il faut toujours/vérifier la cohérence/de ses résultats',
+      'Pour tracer des/droites ou des segments/on utilise une règle', 'Pour tracer des/droites perpendiculaires/utilise ton équerre']
+    this.sup3 = contraindreValeur(1, 3, this.sup3, 1)
     this.listeQuestions = []
     this.listeCorrections = []
-    const texteAEncoder = this.sup.replaceAll(' ', '~').split('/') // On récupère la saisie du formulaire
+    if (type === 'exo') {
+      switch (this.sup3) {
+        case 1:
+          this.sup = choice(listeDeMots)
+          break
+        case 2:
+          this.sup = choice(listeDePhrases).replaceAll('/', ' ')
+          this.sup2 = false
+          break
+        case 3:
+          this.sup = choice(listeDePhrases)
+          this.sup2 = true
+          break
+      }
+    }
+    const texteAEncoder = this.sup.replaceAll(' ', '~').split('/') // On récupère la saisie du formulaire ou du choix aléatoire si c'est un exo
     this.nbQuestions = texteAEncoder.length
     for (let j = 0; j < this.nbQuestions; j++) {
       texteAEncoder[j] = texteAEncoder[j].toLowerCase()
@@ -66,7 +88,7 @@ export default function EncodeurTexte () {
 
       if (this.questionJamaisPosee(i, texteAEncoder[i], table)) {
         this.listeQuestions.push(texte)
-        this.listeCorrections.push('')
+        this.listeCorrections.push(this.besoinCorrection ? texteAEncoder[i].replaceAll('~', ' ').replaceAll('/', ' ') : '')
         i++
       }
       cpt++
