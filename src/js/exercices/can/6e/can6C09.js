c227deb114366217a41d273acd87e9859d4949df
moragi <gilles.mora@ac-rouen.fr>
2022-10-04 19:27:17 +0200

    test sur 10 can 6ième pour pdf

--- src/js/exercices/can/6e/can6C09.js
+++ src/js/exercices/can/6e/can6C09.js
@@ -24,11 +24,13 @@ export default function SoustraireX9 () {
     const b = randint(1, 8)
     const c = randint(1, 4)
     this.reponse = calcul(a * 10 + b - c * 10 - 9)
-    this.question = `$${a * 10 + b} - ${c * 10 + 9}=$`
+    this.question = `Calculer $${a * 10 + b} - ${c * 10 + 9}$.`
     this.correction = `$${a * 10 + b} - ${c * 10 + 9}= ${this.reponse}$`
     this.correction += texteEnCouleur(`<br> Mentalement : <br>
     Soustraire $${c * 10 + 9}$ revient à soustraire $${c * 10 + 10}$, puis à ajouter $1$.<br>
     Ainsi, $${a * 10 + b} - ${c * 10 + 9}=${a * 10 + b} - ${c * 10 + 10}+1=${a * 10 + b - c * 10 - 10}+1=${this.reponse}$.
        `)
+    this.canEnonce = this.question
+    this.canReponseACompleter = ''
   }
 }
