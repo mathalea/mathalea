11d55404cf6f0326217a1ce30aa24f0c200f2674
Jean-Claude Lhote <lhotejc@gmail.com>
2022-10-04 20:17:36 +0200

    feat: Arbre >changer la position des probas + fonction texteIndice() dans outils.js
    fix: 3S21 tooltip

--- src/js/modules/outils.js
+++ src/js/modules/outils.js
@@ -918,6 +918,22 @@ export function texteExposant (texte) {
 }
 
 /**
+* Gère l'écriture de l'indice en mode text (ne doit pas s'utiliser entre $ $)
+* Pour le mode maths (entre $ $) on utilisera tout _3 pour mettre un indice 3 ou _{42} pour l'indice 42.
+* @param {string} texte
+* @Example
+* // `(d${texteIndice(3)})`
+* @author Jean-Claude Lhote
+*/
+export function texteIndice (texte) {
+  if (context.isHtml) {
+    return `<sub>${texte}</sub>`
+  } else {
+    return `\\textsubscript{${texte}}`
+  }
+}
+
+/**
 * Ajoute les parenthèses et le signe
 * @Example
 * //(+3) ou (-3)
