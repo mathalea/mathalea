d61bbd185dcc5202a616c683212ea00386f8216d
moragi <gilles.mora@ac-rouen.fr>
2022-10-11 19:10:30 +0200

    feat: can4 pdf (fin) + diap à 2 sur can5P07-08

--- src/js/exercices/can/5e/can5P08.js
+++ src/js/exercices/can/5e/can5P08.js
@@ -21,7 +21,7 @@ export default function Proportionnalite3 () {
   this.typeExercice = 'simple'
   this.nbQuestions = 1
   this.formatChampTexte = 'largeur15 inline'
-
+  this.tailleDiaporama = 2
   this.nouvelleVersion = function () {
     let prix1, prix2, fruits, fruits2, poids1, poids2, frac, choix
     const listefruits1 = [
