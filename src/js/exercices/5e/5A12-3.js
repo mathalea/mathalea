934dff7f692cfe69ed4bbbd57c73d226dd967b09
EricElter <eric.elter@ac-rouen.fr>
2022-10-12 15:17:55 +0200

    new : 3 nx exo d'arithmétique et labyrinthe : 3A10-7, 4A10-1 et 5A12-3.

---
+++ src/js/exercices/5e/5A12-3.js
@@ -0,0 +1,12 @@
+import ExerciceLabyrinthePremiers3e from '../3e/3A10-7.js'
+export const titre = 'Explorer un labyrinthe de nombres premiers'
+export const dateDePublication = '12/10/2022'
+
+/** Explorer un labyrinthe de nombres premiers
+ * @author Eric Elter
+*/
+
+export default function ExerciceLabyrinthePremiers5e () {
+  ExerciceLabyrinthePremiers3e.call(this)
+  this.sup = 1
+}
