d1e7ce7188e0996b8fb8da3d12314836c6ea29dd
Jean-Claude Lhote <lhotejc@gmail.com>
2022-10-12 18:09:01 +0200

    fix: 6G25-1 les couleurs (simples) tournaient au noir à cause du changement de type de this.couleurDeRemplissage

--- src/js/exercices/6e/6G25-1.js
+++ src/js/exercices/6e/6G25-1.js
@@ -1,5 +1,5 @@
 import Exercice from '../Exercice.js'
-import { mathalea2d } from '../../modules/2dGeneralites.js'
+import { colorToLatexOrHTML, mathalea2d } from '../../modules/2dGeneralites.js'
 import { context } from '../../modules/context.js'
 import { combinaisonListes } from '../../modules/outils/listes.js'
 import { randint } from '../../modules/outils/entiers.js'
@@ -354,7 +354,7 @@ export default function SymetrieAxialePavageTriangulaire () {
       typesDeQuestionsDisponibles = [0, 1, 2, 3, 4, 5]
     }
     const listeTypesDeQuestions = combinaisonListes(typesDeQuestionsDisponibles, 3)
-    const couleurs = ['blue', 'green', 'red', 'gray', 'magenta', 'purple']
+    const couleurs = [colorToLatexOrHTML('blue'), colorToLatexOrHTML('green'), colorToLatexOrHTML('red'), colorToLatexOrHTML('gray'), colorToLatexOrHTML('magenta'), colorToLatexOrHTML('purple')]
     let M
     let N
     const d = []
