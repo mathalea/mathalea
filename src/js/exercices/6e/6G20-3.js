9d712a1fa69f3d6470dea6a0e8a6d3909b7ca7c8
Guillaume Valmont <guillaume.valmont@pm.me>
2022-10-23 23:12:54 +0400

    Les sous-questions de 6G20-3 sont maintenant des questions et reformulation de la correction

--- src/js/exercices/6e/6G20-3.js
+++ src/js/exercices/6e/6G20-3.js
@@ -1,6 +1,6 @@
 import Exercice from '../Exercice.js'
 import { mathalea2d } from '../../modules/2dGeneralites.js'
-import { listeQuestionsToContenu, randint, lettreDepuisChiffre, numAlpha, shuffle } from '../../modules/outils.js'
+import { listeQuestionsToContenu, randint, lettreDepuisChiffre, combinaisonListes } from '../../modules/outils.js'
 import { point, pointSurCercle, cercle, polygoneAvecNom } from '../../modules/2d.js'
 import { propositionsQcm } from '../../modules/interactif/questionQcm.js'
 import { context } from '../../modules/context.js'
@@ -22,67 +22,70 @@ export default class NomExercice extends Exercice {
     super()
     this.titre = titre
     this.correctionDetailleeDisponible = true
+    this.nbQuestions = 4
+    this.nbQuestionsModifiable = false
   }
 
   nouvelleVersion (numeroExercice) {
     this.listeQuestions = []
     this.listeCorrections = []
     this.autoCorrection = []
+    const typeQuestionsDisponibles = ['nom', 'sommet', 'cote', 'diagonale']
+    const listeTypeQuestions = combinaisonListes(typeQuestionsDisponibles, this.nbQuestions)
+    const objets2d = []
+    const O = point(0, 0, 'O')
+    const points = []
+
+    const nbSommets = randint(4, 6)
+    // On commence par créer les angles à partir desquels les points seront créés
+    const anglesPoints = []
+    for (let i = 0; i < nbSommets; i++) {
+      anglesPoints.push(randint(1, 12, anglesPoints))
+    }
+    for (let i = 0; i < anglesPoints.length; i++) {
+      anglesPoints[i] = anglesPoints[i] * 30
+    }
+    anglesPoints.sort((a, b) => a - b)
+    // On crée leurs noms
+    const numerosNomsPoints = []
+    for (let i = 0; i < nbSommets; i++) {
+      numerosNomsPoints.push(randint(1, 26, [22, ...numerosNomsPoints])) // Il y a un espace bizarre après les V en LateX, alors on les zappe
+    }
+    // On crée les points autour d'un point O à partir des angles précédemment créés
+    for (let i = 0; i < nbSommets; i++) {
+      points.push(pointSurCercle(cercle(O, randint(20, 50) / 10), anglesPoints[i], lettreDepuisChiffre(numerosNomsPoints[i])))
+    }
+    const polygon = polygoneAvecNom(points)
+    objets2d.push(polygon)
+    // On affiche le cadre mathalea2d
+    const pointsX = []
+    const pointsY = []
+    for (const point of points) {
+      pointsX.push(point.x)
+      pointsY.push(point.y)
+    }
+    const xmin = Math.min(...pointsX) - 2
+    const xmax = Math.max(...pointsX) + 2
+    const ymin = Math.min(...pointsY) - 2
+    const ymax = Math.max(...pointsY) + 2
+    const parametres2d = { xmin: xmin, ymin: ymin, xmax: xmax, ymax: ymax, pixelsParCm: 20, scale: 1 }
+    this.introduction = '' + mathalea2d(parametres2d, objets2d)
 
     for (let i = 0, texte, texteCorr, cpt = 0; i < this.nbQuestions && cpt < 50;) {
       texte = ''
       texteCorr = ''
-      const objets2d = []
-      const O = point(0, 0, 'O')
-      const points = []
-
-      const nbPoints = randint(4, 6)
-      // On commence par créer les angles à partir desquels les points seront créés
-      const anglesPoints = []
-      for (let i = 0; i < nbPoints; i++) {
-        anglesPoints.push(randint(1, 12, anglesPoints))
-      }
-      for (let i = 0; i < anglesPoints.length; i++) {
-        anglesPoints[i] = anglesPoints[i] * 30
-      }
-      anglesPoints.sort((a, b) => a - b)
-      // On crée leurs noms
-      const numerosNomsPoints = []
-      for (let i = 0; i < nbPoints; i++) {
-        numerosNomsPoints.push(randint(1, 26, [22, ...numerosNomsPoints])) // Il y a un espace bizarre après les V en LateX, alors on les zappe
-      }
-      // On crée les points autour d'un point O à partir des angles précédemment créés
-      for (let i = 0; i < nbPoints; i++) {
-        points.push(pointSurCercle(cercle(O, randint(20, 50) / 10), anglesPoints[i], lettreDepuisChiffre(numerosNomsPoints[i])))
-      }
-      const polygon = polygoneAvecNom(points)
-      objets2d.push(polygon)
-      // On affiche le cadre mathalea2d
-      const pointsX = []
-      const pointsY = []
-      for (const point of points) {
-        pointsX.push(point.x)
-        pointsY.push(point.y)
-      }
-      const xmin = Math.min(...pointsX) - 2
-      const xmax = Math.max(...pointsX) + 2
-      const ymin = Math.min(...pointsY) - 2
-      const ymax = Math.max(...pointsY) + 2
-      const parametres2d = { xmin: xmin, ymin: ymin, xmax: xmax, ymax: ymax, pixelsParCm: 20, scale: 1 }
-      texte += mathalea2d(parametres2d, objets2d)
-
       // On construit les questions
-      const indiceA = randint(0, (nbPoints - 1))
-      const indiceB = (indiceA + 1) % nbPoints
-      const indiceC = (indiceA + 2) % nbPoints
+      const indiceA = randint(0, (nbSommets - 1))
+      const indiceB = (indiceA + 1) % nbSommets
+      const indiceC = (indiceA + 2) % nbSommets
       const A = points[indiceA].nom
       const B = points[indiceB].nom
       const C = points[indiceC].nom
 
       const indiceDepart = randint(0, points.length)
       let nomDirectCorrect = '$'
-      for (let i = 0; i < nbPoints; i++) {
-        nomDirectCorrect += points[(indiceDepart + i) % nbPoints].nom
+      for (let i = 0; i < nbSommets; i++) {
+        nomDirectCorrect += points[(indiceDepart + i) % nbSommets].nom
       }
       nomDirectCorrect += '$'
       const nomIndirectCorrect = nomDirectCorrect.split('').reverse().join('')
@@ -95,64 +98,73 @@ export default class NomExercice extends Exercice {
         arr[2] = temp
         return arr.join('')
       }
-      let questionsReponses = [
-        {
-          question: 'Quels sont les deux noms possibles de ce polygone ?',
-          propositions: [nomDirectCorrect, nomDirectIncorrect, nomIndirectCorrect, nomIndirrectIncorrect],
-          reponses: [nomDirectCorrect, nomIndirectCorrect],
-          explications: `On peut le nommer de plein de façons différentes.<br>
-          Il faut partir d'un point (n'importe lequel) et de nommer les points qu'on rencontre lorsqu'on fait le tour de la figure dans un sens ou dans l'autre.`
-        },
-        {
-          question: `$${A}$ est :`,
-          propositions: ['un sommet', 'un côté', 'une diagonale', 'je ne sais pas'],
-          reponses: ['un sommet'],
-          explications: 'Les sommets délimitent les côtés d\'un polygone'
-        },
-        {
-          question: `[$${B}${C}$] est :`,
-          propositions: ['un sommet', 'un côté', 'une diagonale', 'je ne sais pas'],
-          reponses: ['un côté'],
-          explications: 'Les segments d\'un polygone sont appelés ses côtés.'
-        },
-        {
-          question: `[$${C}${A}$] est :`,
-          propositions: ['un sommet', 'un côté', 'une diagonale', 'je ne sais pas'],
-          reponses: ['une diagonale'],
-          explications: 'Une diagonale est un segment qui a pour extrémités deux côtés non consécutifs (deux côtés qui ne se suivent pas).'
-        }
-      ]
-      questionsReponses = shuffle(questionsReponses)
-      let j = 0
-      for (const questionReponse of questionsReponses) {
-        const propositions = []
-        for (const proposition of questionReponse.propositions) {
-          let statut = false
-          for (const reponse of questionReponse.reponses) {
-            if (proposition === reponse) statut = true
+      let questionReponse
+      switch (listeTypeQuestions[i]) {
+        case 'nom':
+          questionReponse =
+          {
+            question: 'Quels sont les deux noms possibles de ce polygone ?',
+            propositions: [nomDirectCorrect, nomDirectIncorrect, nomIndirectCorrect, nomIndirrectIncorrect],
+            reponses: [nomDirectCorrect, nomIndirectCorrect],
+            explications: `On peut le nommer de plein de façons différentes.<br>
+              Il faut partir d'un point (n'importe lequel) et nommer les points qu'on rencontre lorsqu'on fait le tour de la figure dans un sens ou dans l'autre.`
           }
-          propositions.push({
-            texte: proposition,
-            statut,
-            feedback: questionReponse.explications
-          })
-        }
-        this.autoCorrection[i * questionsReponses.length + j] = {
-          enonce: questionReponse.question,
-          options: { ordered: false },
-          propositions: propositions
-        }
-        const monQcm = propositionsQcm(this, i * questionsReponses.length + j)
-        texte += numAlpha(j)
-        texte += context.isAmc ? '' : questionReponse.question
-        texte += monQcm.texte + '<br>'
-        texteCorr += numAlpha(j)
-        texteCorr += context.isAmc ? '' : questionReponse.question
-        texteCorr += monQcm.texteCorr
-        this.correctionDetaillee ? texteCorr += questionReponse.explications + '<br><br>' : texteCorr += '<br>'
-        j++
+          break
+        case 'sommet':
+          questionReponse =
+          {
+            question: `$${A}$ est :`,
+            propositions: ['un sommet', 'un côté', 'une diagonale', 'je ne sais pas'],
+            reponses: ['un sommet'],
+            explications: 'Les sommets sont les extrémités des côtés.'
+          }
+
+          break
+        case 'cote':
+          questionReponse =
+          {
+            question: `[$${B}${C}$] est :`,
+            propositions: ['un sommet', 'un côté', 'une diagonale', 'je ne sais pas'],
+            reponses: ['un côté'],
+            explications: 'Les côtés sont les segments qui forment le polygone.'
+          }
+
+          break
+        case 'diagonale':
+          questionReponse =
+          {
+            question: `[$${C}${A}$] est :`,
+            propositions: ['un sommet', 'un côté', 'une diagonale', 'je ne sais pas'],
+            reponses: ['une diagonale'],
+            explications: 'Une diagonale est un segment qui a pour extrémités deux sommets non consécutifs (deux côtés qui ne se suivent pas).'
+          }
+
+          break
       }
-      if (this.questionJamaisPosee(i, ...pointsX, ...pointsY)) {
+      const propositions = []
+      for (const proposition of questionReponse.propositions) {
+        let statut = false
+        for (const reponse of questionReponse.reponses) {
+          if (proposition === reponse) statut = true
+        }
+        propositions.push({
+          texte: proposition,
+          statut,
+          feedback: ''
+        })
+      }
+      this.autoCorrection[i] = {
+        enonce: questionReponse.question,
+        options: { ordered: false },
+        propositions: propositions
+      }
+      const monQcm = propositionsQcm(this, i)
+      texte += context.isAmc ? '' : questionReponse.question
+      texte += monQcm.texte + '<br>'
+      texteCorr += context.isAmc ? '' : questionReponse.question
+      texteCorr += monQcm.texteCorr
+      this.correctionDetaillee ? texteCorr += questionReponse.explications + '<br><br>' : texteCorr += '<br>'
+      if (this.questionJamaisPosee(i, ...pointsX, ...pointsY, listeTypeQuestions[i])) {
         this.listeQuestions.push(texte)
         this.listeCorrections.push(texteCorr)
         i++
