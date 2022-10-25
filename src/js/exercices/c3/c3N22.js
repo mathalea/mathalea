ae9ae75a47c47490ee36285b718359a53fc7fe1c
EricElter <eric.elter@ac-rouen.fr>
2022-10-25 21:33:18 +0200

    fix : miliers devient milliers

--- src/js/exercices/c3/c3N22.js
+++ src/js/exercices/c3/c3N22.js
@@ -3,7 +3,7 @@ export const titre = 'Lire abscisse décimale sous trois formes'
 export { interactifReady, interactifType, amcReady, amcType } from '../6e/6N23-2.js'
 /**
  * Lire un nombre / écrire un nombre : passer d'une écriture à une autre et inversement
- * On peut fixer la classe maximale : unités, miliers, millions, milliards
+ * On peut fixer la classe maximale : unités, milliers, millions, milliards
  * @author Jean-Claude Lhote
  * Référence 6N10
  */
