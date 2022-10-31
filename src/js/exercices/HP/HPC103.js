65e8325efb6836ad3354ee7719be7fdde70ec112
maxime NGUYEN <ngmaxime@gmail.com>
2022-10-31 15:10:49 +0100

    init HPC103 produit de matrices

---
+++ src/js/exercices/HP/HPC103.js
@@ -0,0 +1,46 @@
+import Exercice from '../Exercice.js'
+import { randint, listeQuestionsToContenu } from '../../modules/outils.js'
+export const titre = 'Produit de matrices'
+
+// Les exports suivants sont optionnels mais au moins la date de publication semble essentielle
+export const dateDePublication = '25/10/2021' // La date de publication initiale au format 'jj/mm/aaaa' pour affichage temporaire d'un tag
+export const dateDeModifImportante = '24/10/2021' // Une date de modification importante au format 'jj/mm/aaaa' pour affichage temporaire d'un tag
+
+/**
+ * Description didactique de l'exercice
+ * @author Maxime Nguyen
+ * Référence HPC103
+*/
+export default class nomExercice extends Exercice {
+  constructor () {
+    super()
+    this.titre = titre
+    this.consigne = 'Calcule'
+    this.nbQuestions = 10 // Nombre de questions par défaut
+    this.nbCols = 2 // Uniquement pour la sortie LaTeX
+    this.nbColsCorr = 2 // Uniquement pour la sortie LaTeX
+    this.tailleDiaporama = 3 // Pour les exercices chronométrés. 50 par défaut pour les exercices avec du texte
+    this.video = '' // Id YouTube ou url
+  }
+
+  nouvelleVersion (numeroExercice) {
+    this.listeQuestions = [] // Liste de questions
+    this.listeCorrections = [] // Liste de questions corrigées
+    this.autoCorrection = []
+
+    for (let i = 0, texte, texteCorr, cpt = 0; i < this.nbQuestions && cpt < 50;) { // Boucle principale où i+1 correspond au numéro de la question
+      const a = randint(1, 12) // Comme la valeur ne sera pas modifiée, on la déclare avec const
+      texte = `$${a} + 1 $` // Le LateX entre deux symboles $, les variables dans des ${ }
+      texteCorr = `$${a} + 1 = ${a + 1}$`
+
+      // Si la question n'a jamais été posée, on l'enregistre
+      if (this.questionJamaisPosee(i, a)) { // <- laisser le i et ajouter toutes les variables qui rendent les exercices différents (par exemple a, b, c, d...)
+        this.listeQuestions.push(texte)
+        this.listeCorrections.push(texteCorr)
+        i++
+      }
+      cpt++
+    }
+    listeQuestionsToContenu(this) // On envoie l'exercice à la fonction de mise en page
+  }
+}
