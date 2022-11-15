326e21fb046efb8742e5f87226c1397634e92b9e
moragi <gilles.mora@ac-rouen.fr>
2022-11-15 18:49:05 +0100

    ex: can6P06(clone de 6N33-2 modifié pour l'occase)

---
+++ src/js/exercices/can/6e/can6P06.js
@@ -0,0 +1,19 @@
+import CalculerUnPourcentage from '../../6e/6N33-2.js'
+export { interactifReady, interactifType, amcType, amcReady } from '../../6e/6N33-2.js'
+export const titre = 'Résoudre un problème de calcul de pourcentage par complément à 100%'
+
+export const dateDePublication = '15/11/2022'
+
+/*!
+ * @author Gilles Mora
+ * Créé le 20/01/2022
+ * Référence can6P06
+ */
+
+export default function CalculerUnPourcentageCAN () {
+  CalculerUnPourcentage.call(this)
+  this.nbQuestions = 1
+  this.can = true
+  this.consigne = ''
+  this.spacing = 1
+}
