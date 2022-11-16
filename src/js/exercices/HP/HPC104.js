2f08b8104492edd72768720dd3cd5e8a38c55a66
maxime NGUYEN <ngmaxime@gmail.com>
2022-11-16 16:02:42 +0100

    HPC104 calcul de déterminant 2x2 et 3x3

--- src/js/exercices/HP/HPC104.js
+++ src/js/exercices/HP/HPC104.js
@@ -1,5 +1,8 @@
 import Exercice from '../Exercice.js'
-import { listeQuestionsToContenu, combinaisonListes } from '../../modules/outils.js'
+import { listeQuestionsToContenu, combinaisonListes, MatriceCarree, lettreIndiceeDepuisChiffre, ecritureParentheseSiNegatif } from '../../modules/outils.js'
+import { create, all } from 'mathjs'
+const math = create(all)
+
 export const titre = 'Calcul de déterminant'
 
 // Les exports suivants sont optionnels mais au moins la date de publication semble essentielle
@@ -15,10 +18,10 @@ export default class nomExercice extends Exercice {
   constructor () {
     super()
     this.titre = titre
-    this.consigne = 'Consigne'
-    this.nbQuestions = 10
+    this.consigne = 'Calcul du déterminant d\'une matrice carrée'
+    this.nbQuestions = 3
 
-    this.besoinFormulaireNumerique = ['Difficulté', 3, '1 : Facile\n2 : Moyen\n3 : Difficile'] // le paramètre sera numérique de valeur max 3 (le 3 en vert)
+    this.besoinFormulaireNumerique = ['Difficulté', 2, '1 : Matrice 2x2\n2 : Matrice 3x3'] // le paramètre sera numérique de valeur max 3 (le 3 en vert)
     this.sup = 2 // Valeur du paramètre par défaut
     // Remarques : le paramètre peut aussi être un texte avec : this.besoinFormulaireTexte = [texte, tooltip]
     //              il peut aussi être une case à cocher avec : this.besoinFormulaireCaseACocher = [texte] (dans ce cas, this.sup = true ou this.sup = false)
@@ -33,29 +36,55 @@ export default class nomExercice extends Exercice {
     this.listeQuestions = []
     this.listeCorrections = []
     this.autoCorrection = []
-
-    const typeQuestionsDisponibles = ['type1', 'type2', 'type3']
-
-    const listeTypeQuestions = combinaisonListes(typeQuestionsDisponibles, this.nbQuestions)
-    for (let i = 0, texte, texteCorr, difficulte, cpt = 0; i < this.nbQuestions && cpt < 50;) {
-      if (this.sup === 1) { // On ajuste la difficulté selon le paramètre.
-        difficulte = 'facile'
-      } else if (this.sup === 2) {
-        difficulte = 'difficile'
-      }
+    let listeTypeDeQuestionsDisponibles
+    if (this.sup === 1) { // On ajuste la difficulté selon le paramètre.
+      listeTypeDeQuestionsDisponibles = ['type1']
+    } else if (this.sup === 2) {
+      listeTypeDeQuestionsDisponibles = ['type2']
+    }
+    const listeTypeQuestions = combinaisonListes(listeTypeDeQuestionsDisponibles, this.nbQuestions)
+    for (let i = 0, coefficients, texte, texteCorr, cpt = 0; i < this.nbQuestions && cpt < 50;) {
+      coefficients = [-9, -8, -7, -6, -5, -4, -3, -3, -2, -2, -2, -1, -1, -1, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 5, 6, 7, 8, 9]
       switch (listeTypeQuestions[i]) {
-        case 'type1':
-          texte = `Question ${difficulte} ${i + 1} de type 1` // Les questions sont modifiées en fonction de la difficulté
-          texteCorr = `Correction ${i + 1} de type 1`
-          break
-        case 'type2':
-          texte = `Question ${difficulte} ${i + 1} de type 2`
-          texteCorr = `Correction ${i + 1} de type 2`
-          break
-        case 'type3':
-          texte = `Question ${difficulte} ${i + 1} de type 3`
-          texteCorr = `Correction ${i + 1} de type 3`
-          break
+        case 'type1': {
+          const n = 2
+          const m = 2
+          const table = []
+          const nommatrice = lettreIndiceeDepuisChiffre(i + 1)
+          let coef = 0
+          for (let i = 0; i < n; i++) {
+            const ligne = []
+            for (let j = 0; j < m; j++) {
+              coef = math.pickRandom(coefficients)
+              ligne.push(coef)
+            }
+            table.push(ligne)
+          }
+          const matrice = new MatriceCarree(table)
+
+          texte = `Calculer le déterminant de la matrice $${nommatrice} = ${matrice.toTex()}$` // Les questions sont modifiées en fonction de la difficulté
+          texteCorr = ''
+          texteCorr += `On calcule $det(${nommatrice}) = ${ecritureParentheseSiNegatif(table[0][0])} \\times ${ecritureParentheseSiNegatif(table[1][1])} - ${ecritureParentheseSiNegatif(table[1][0])} \\times ${ecritureParentheseSiNegatif(table[0][1])}  = ${matrice.determinant()}$.`
+          break }
+        case 'type2': {
+          const n = 3
+          const m = 3
+          const table = []
+          const nommatrice = lettreIndiceeDepuisChiffre(i + 1)
+          let coef = 0
+          for (let i = 0; i < n; i++) {
+            const ligne = []
+            for (let j = 0; j < m; j++) {
+              coef = math.pickRandom(coefficients)
+              ligne.push(coef)
+            }
+            table.push(ligne)
+          }
+          const matrice = new MatriceCarree(table)
+          texte = `Calculer le déterminant de la matrice $${nommatrice} = ${matrice.toTex()}$` // Les questions sont modifiées en fonction de la difficulté
+          texteCorr = ''
+          texteCorr += `On calcule $det(${nommatrice}) = ${matrice.determinant()}$.`
+          break }
       }
       // Si la question n'a jamais été posée, on l'enregistre
       if (this.questionJamaisPosee(i, texte)) { // <- laisser le i et ajouter toutes les variables qui rendent les exercices différents (par exemple a, b, c et d)
