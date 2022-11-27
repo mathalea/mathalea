0262b7a21f42ee57384c53654d2f37e544eea5fa
Sébastien LOZANO <sebastien.lozano@ac-nancy-metz.fr>
2022-11-27 11:13:48 +0100

    3G11-1 : ajout des types de rapports et création des objets pour la correction

--- src/js/exercices/3e/3G11-1.js
+++ src/js/exercices/3e/3G11-1.js
@@ -1,5 +1,7 @@
 import Exercice from '../Exercice.js'
-import { listeQuestionsToContenu, combinaisonListes, texNombre, miseEnEvidence } from '../../modules/outils.js'
+import { listeQuestionsToContenu, combinaisonListes, texNombre, miseEnEvidence, randint, shuffle } from '../../modules/outils.js'
+import Decimal from 'decimal.js'
+import FractionX from '../../modules/FractionEtendue.js'
 
 export const titre = 'Homothétie - Rapport'
 
@@ -48,6 +50,61 @@ export default class HomothetieRapport extends Exercice {
       // => un décimal négatif de module inférieur à 1
       // => une fraction positive inférieure à 1
       // => une fraction négative de module supérieur à 1
+
+      // On crée les nombres
+      const decimalPositifInferieurUn = new Decimal(randint(1, 9)).div(10)
+      const entierNegatifModuleSuperieurUn = 0 - randint(2, 9)
+      const decimalPositifSuperieurUn = new Decimal(randint(11, 49)).div(10)
+      const decimalNegatifModuleInferieurUn = 0 - new Decimal(randint(1, 9)).div(10)
+      let numerateur = randint(2, 9)
+      let denominateur = randint(2, 9, [numerateur])
+      // Si le denominateur est inférieur au numérateur on inverse les deux
+      if (denominateur < numerateur) {
+        const temp = numerateur
+        numerateur = denominateur
+        denominateur = temp
+      }
+      const fraction = new FractionX(numerateur, denominateur)
+      const numerateurFractionInverseOpposee = -1 * denominateur
+      const denominateurFractionInverseOpposee = numerateur
+      const fractionInverseEtOpposee = new FractionX(numerateurFractionInverseOpposee, denominateurFractionInverseOpposee)
+
+      // On crée les objets
+      const cas1 = {
+        rapport: texNombre(decimalPositifInferieurUn, 1),
+        correctionReduction: miseEnEvidence('\\times'),
+        correctionAgrandissement: ''
+      }
+      const cas2 = {
+        rapport: texNombre(entierNegatifModuleSuperieurUn, 0) ,
+        correctionReduction: '',
+        correctionAgrandissement: miseEnEvidence('\\times')
+      }
+      const cas3 = {
+        rapport: texNombre(decimalPositifSuperieurUn, 1),
+        correctionReduction: '',
+        correctionAgrandissement: miseEnEvidence('\\times')
+      }
+      const cas4 = {
+        rapport: texNombre(decimalNegatifModuleInferieurUn, 1),
+        correctionReduction: miseEnEvidence('\\times'),
+        correctionAgrandissement: ''
+      }
+      const cas5 = {
+        rapport: fraction.texFraction,
+        correctionReduction: miseEnEvidence('\\times'),
+        correctionAgrandissement: ''
+      }
+      const cas6 = {
+        rapport: fractionInverseEtOpposee.texFraction,
+        correctionReduction: '',
+        correctionAgrandissement: miseEnEvidence('\\times')
+      }
+
+      // On va remplir un tableau avec des objets contenant nombre et réponse
+      const choixRapports = [cas1, cas2, cas3, cas4, cas5, cas6]
+      const choixRapportsMelanges = shuffle(choixRapports)
+
       switch (listeTypeDeQuestions[i]) { // Chaque question peut être d'un type différent, ici 1 cas sont prévus...
         case 1:
           texte += `$
