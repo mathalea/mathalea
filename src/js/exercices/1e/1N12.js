f53eaf215279ccb180321a8b052bd738f17c82a0
Rémi Angot <remi.angot@gmail.com>
2022-11-30 20:25:24 +0100

    style: Sum en displaystyle

--- src/js/exercices/1e/1N12.js
+++ src/js/exercices/1e/1N12.js
@@ -25,8 +25,8 @@ export default class SommeSuiteArithmetique extends Exercice {
     const r = randint(3, 10)
     const n = randint(2, 4) * 10
     const u = choice(['u', 'v', 'w'])
-    this.question = `Soit $${u}$ la suite arithmétique de premier terme $u_0 = ${u0}$ et de raison $${r}$.`
-    this.question += `<br>Calculer $S = ${u}_0 + ${u}_1 + ... + ${u}_{${n}} = \\displaystyle{\\Sigma_{k=0}^{k=${n}}${u}_k}$.`
+    this.question = `Soit $${u}$ la suite arithmétique de premier terme $${u}_0 = ${u0}$ et de raison $${r}$.`
+    this.question += `<br>Calculer $\\displaystyle S = ${u}_0 + ${u}_1 + ... + ${u}_{${n}} =\\sum_{k=0}^{k=${n}}${u}_k$.`
     this.reponse = (n + 1) * (u0 + u0 + n * r) / 2
     this.correction = `$S = ${u0} + (${u0} + ${r}) + (${u0} + 2\\times${r}) + ... + (${u0} + ${n} \\times ${r})$`
     this.correction += `<br>$\\phantom{S} = (\\underbrace{${u0} + ${u0} + ... + ${u0}}_{${n + 1}\\times ${u0}}) + ${r} \\times (1 + 2 + ... + ${n})$`
