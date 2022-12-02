3038643f1eb4c77ece6aac080cbbcf2d94bcb048
Guillaume Valmont <guillaume.valmont@pm.me>
2022-12-02 18:38:27 +0400

    fix: Ajout de "consécutifs" à la consigne de 4G20-6

--- src/js/exercices/4e/4G20-6.js
+++ src/js/exercices/4e/4G20-6.js
@@ -90,7 +90,7 @@ export default function CalculValeurApprocheeRacineCarree () {
         a = randint(2, 300, [4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169, 196, 225, 256, 289])
         reponseG = Math.floor(Math.sqrt(a))
         reponseD = Math.ceil(Math.sqrt(a))
-        texte = `Sans utiliser de calculatrice, encadrer $\\sqrt{${a}}$ entre deux nombres entiers.<br>`
+        texte = `Sans utiliser de calculatrice, encadrer $\\sqrt{${a}}$ entre deux nombres entiers consécutifs.<br>`
         if (this.interactif) {
           texte += ajouteChampTexteMathLive(this, indexRep, 'largeur10 inline')
           texte += ` $< \\sqrt{${a}} <$ `
