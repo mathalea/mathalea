863baabf5abbb2612950bf160ca4d99bb6b07620
moragi <gilles.mora@ac-rouen.fr>
2022-12-08 10:23:46 +0100

    ex: can5C23

---
+++ src/js/exercices/can/5e/can5C23.js
@@ -0,0 +1,35 @@
+import { randint, texNombre, texteEnCouleur, arrondi, choice } from '../../../modules/outils.js'
+import Exercice from '../../Exercice.js'
+export const titre = 'Multiplier par 1,5 ou 2,5 ou ...'
+export const interactifReady = true
+export const interactifType = 'mathLive'
+export const amcReady = true
+export const amcType = 'AMCNum'
+export const dateDePublication = '08/12/2022'
+/*!
+ * @author  Gilles Mora
+ *
+ *
+ */
+
+export default function MultiplierPar () {
+  Exercice.call(this)
+  this.nbQuestions = 1
+  this.formatChampTexte = 'largeur15 inline'
+  this.typeExercice = 'simple'
+  this.tailleDiaporama = 2
+  this.nouvelleVersion = function () {
+    const a = randint(3, 15)
+    const b = choice([15, 25, 35, 45, 55]) / 10
+
+    this.question = `Calculer $${a}\\times ${texNombre(b, 1)}$.`
+    this.correction = `$${a}\\times ${texNombre(b, 1)}=${texNombre(a * b, 1)}$`
+    this.reponse = arrondi(a * b, 1)
+    this.correction += texteEnCouleur(`
+    <br> Mentalement : <br>
+    $${a}\\times ${texNombre(b, 1)}=${a}\\times ${Math.floor(b)}+\\underbrace{${a}\\times 0,5}_{\\text{La moitié de }${a}}
+    =${a * Math.floor(b)}+${texNombre(a / 2, 1)}=${texNombre(this.reponse, 1)}$  `)
+    this.canEnonce = this.question// 'Compléter'
+    this.canReponseACompleter = ''
+  }
+}
