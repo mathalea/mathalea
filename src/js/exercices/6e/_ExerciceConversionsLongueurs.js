11a6a503d2008b33b8eae9492e49d15a4d442bd9
guironne <guironne@yahoo.fr>
2022-12-29 18:32:18 +0100

    feat: 6M12 ajout du tableau et responsive (#2452)

--- src/js/exercices/6e/_ExerciceConversionsLongueurs.js
+++ src/js/exercices/6e/_ExerciceConversionsLongueurs.js
@@ -1,6 +1,7 @@
+/* global jQuery */
 import Exercice from '../Exercice.js'
 import { context } from '../../modules/context.js'
-import { listeQuestionsToContenu, randint, choice, combinaisonListes, arrondi, texNombre, texTexte, calcul } from '../../modules/outils.js'
+import { listeQuestionsToContenu, randint, choice, combinaisonListes, arrondi, texNombre, texTexte, calcul, deuxColonnesResp } from '../../modules/outils.js'
 import { setReponse } from '../../modules/gestionInteractif.js'
 import { ajouteChampTexteMathLive } from '../../modules/interactif/questionMathLive.js'
 import Decimal from 'decimal.js'
@@ -23,11 +24,12 @@ export default function ExerciceConversionsLongueurs (niveau = 1) {
   Exercice.call(this) // Héritage de la classe Exercice()
   this.sup = niveau // Niveau de difficulté de l'exercice
   this.sup2 = false // Avec des nombres décimaux ou pas
+  this.sup3 = false // avec le tableau
   this.titre = 'Conversions de longueurs'
   this.consigne = 'Compléter : '
   this.spacing = 2
 
-  this.nouvelleVersion = function () {
+  this.nouvelleVersion = function (numeroExercice) {
     const reponses = []
     this.listeQuestions = [] // Liste de questions
     this.listeCorrections = [] // Liste de questions corrigées
@@ -120,6 +122,7 @@ export default function ExerciceConversionsLongueurs (niveau = 1) {
           texNombre(resultat) +
           texTexte(unite) +
           '$'
+        if (this.sup3 && context.vue === 'diap') texte += '<br>' + buildTab(0, '', 0, '', 2, true)
         texteCorr += '<br>' + buildTab(a, prefixeMulti[k][0] + 'm', resultat, unite)
       } else if (div && typesDeQuestions < 4) {
         resultat = calcul(a / prefixeDiv[k][1]).toString() // Attention aux notations scientifiques pour 10e-8
@@ -138,6 +141,7 @@ export default function ExerciceConversionsLongueurs (niveau = 1) {
           texNombre(resultat) +
           texTexte(unite) +
           '$'
+        if (this.sup3 && context.vue === 'diap') texte += '<br>' + buildTab(0, '', 0, '', 2, true)
         texteCorr += '<br>' + buildTab(a, prefixeDiv[k][0] + 'm', resultat, unite)
       } else {
         // pour type de question = 4
@@ -164,6 +168,7 @@ export default function ExerciceConversionsLongueurs (niveau = 1) {
             texNombre(resultat) +
             texTexte(listeUnite[unite1]) +
             '$'
+          if (this.sup3 && context.vue === 'diap') texte += '<br>' + buildTab(0, '', 0, '', 2, true)
           texteCorr += '<br>' + buildTab(a, listeUnite[unite2], resultat, listeUnite[unite1])
         } else {
           resultat = calcul(a / Math.pow(10, ecart))
@@ -182,6 +187,7 @@ export default function ExerciceConversionsLongueurs (niveau = 1) {
             texNombre(resultat) +
             texTexte(listeUnite[unite2]) +
             '$'
+          if (this.sup3 && context.vue === 'diap') texte += '<br>' + buildTab(0, '', 0, '', 2, true)
           texteCorr += '<br>' + buildTab(a, listeUnite[unite1], resultat, listeUnite[unite2])
         }
       }
@@ -205,132 +211,183 @@ export default function ExerciceConversionsLongueurs (niveau = 1) {
       }
       cpt++
     }
+    function insertInDom () {
+      const div = document.getElementById('exercice' + numeroExercice)
+      if (div) {
+        const button1 = document.createElement('button')
+        button1.setAttribute('data-tooltip', 'Moins d\'espace vertical')
+        button1.innerText = '- ⇕'
+        button1.onclick = function () { jQuery('#exercice' + numeroExercice + ' ol > li').css({ 'margin-top': '-=5px', 'margin-bottom': '-=5px' }) }
+        div.appendChild(button1)
+        const button2 = document.createElement('button')
+        button2.innerText = '+ ⇕'
+        button2.setAttribute('data-tooltip', 'Plus d\'espace vertical')
+        button2.onclick = function () { jQuery('#exercice' + numeroExercice + ' ol > li').css({ 'margin-top': '+=5px', 'margin-bottom': '+=5px' }) }
+        div.appendChild(button2)
+        button1.classList.add('btn', 'ui', 'icon', 'button')
+        button2.classList.add('btn', 'ui', 'icon', 'button')
+        document.removeEventListener('exercicesAffiches', insertInDom)
+      }
+    }
+    if (context.vue !== 'diap' && context.isHtml) {
+      document.addEventListener('exercicesAffiches', insertInDom)
+    }
     listeQuestionsToContenu(this)
+    const options = { eleId: numeroExercice, widthmincol1: '350px', widthmincol2: '200px' }
+    if (context.vue !== 'diap' && context.isHtml && this.sup3) {
+      this.contenu = deuxColonnesResp(this.contenu, buildTab(0, '', 0, '', Math.min(10, this.nbQuestions), true), options)
+
+      // listener
+      const reportWindowSize = function () {
+        const element = document.getElementById('cols-responsive1-' + options.eleId)
+        const element2 = document.getElementById('cols-responsive2-' + options.eleId)
+        if (element !== null &&
+          element !== undefined &&
+          element2 !== null &&
+          element2 !== undefined &&
+          element.clientWidth !== 0) {
+          // console.log('ele1:' + element.clientWidth + ': ' + element.offsetWidth)
+          // console.log('ele2:' + element2.clientWidth + ': ' + element2.offsetWidth)
+          // console.log('parent:' + element.parentElement.clientWidth + ': ' + element.parentElement.offsetWidth)
+          const col2 = element2.children[0].offsetWidth
+          const diff = element.parentElement.clientWidth - parseInt(options.widthmincol1.replaceAll('px', ''))
+          if (diff > col2) {
+            element.parentElement.style.gridTemplateColumns = 'repeat(2, 1fr)'
+          } else {
+            element.parentElement.style.gridTemplateColumns = 'auto'
+          }
+        }
+      }
+
+      const removelistener = function () {
+        document.removeEventListener('exercicesAffiches', reportWindowSize)
+        document.removeEventListener('zoominOrout', reportWindowSize)
+        document.removeEventListener('pleinEcran', reportWindowSize)
+        window.removeEventListener('resize', reportWindowSize)
+        document.removeEventListener('buildex', removelistener)
+      }
+
+      const createlistener = function () {
+        document.addEventListener('exercicesAffiches', reportWindowSize)
+        document.addEventListener('zoominOrout', reportWindowSize)
+        document.addEventListener('pleinEcran', reportWindowSize)
+        window.addEventListener('resize', reportWindowSize)
+        document.addEventListener('buildex', removelistener)
+      }
+      createlistener()
+    }
   }
   this.besoinFormulaireNumerique = ['Niveau de difficulté', 4, ' 1 : De dam, hm, km vers m\n 2 : De dm, cm, mm vers m\n 3 : Conversions en mètres\n4 : Mélange']
   this.besoinFormulaire2CaseACocher = ['Avec des nombres décimaux']
+  this.besoinFormulaire3CaseACocher = ['Avec tableau']
 }
-
-function getDigitFromNumbers (nb, pos) {
+/**
+ * Fonction utilitaire retournant le rang d'un nombre
+ * @param {*} nb Nombre entier ou décimal
+ * @param {*} pos Le rang cherché
+ * @returns retourne la valeur de la colonne, si zéro inutile alors on retourne ''
+ * Exemple :
+ * getDigitFromNumber(1302.56,1000) retourne '1'
+ * getDigitFromNumber(1302.56,10000) retourne ''
+ * getDigitFromNumber(1302.56,100) retourne '3'
+ * getDigitFromNumber(1302.56,10) retourne '0'
+ * getDigitFromNumber(1302.56,0.1) retourne '5'
+ * getDigitFromNumber(1302.56,0.001) retourne ''
+ */
+export function getDigitFromNumber (nb, pos) {
   const n = new Decimal(nb)
-  const exp = Decimal.ln(pos).div(Decimal.ln(10))
+  const po = new Decimal(pos)
+  const exp = Decimal.ln(po).div(Decimal.ln(10))
   let res = ''
-  if (pos >= 1) {
+  if (po.comparedTo(1) >= 0) {
     // partie entière : milliers, centaines, dizaines, unités
-    res = n.sub(n.div(pos * 10).trunc().mul(pos * 10))
-    res = res.div(pos).trunc()
-    res = (pos === 1 || n > pos) ? res.toString() : ''
+    res = n.sub(n.div(po.mul(10)).trunc().mul(po.mul(10)))
+    res = res.div(po).trunc()
+    res = (po.equals(1) || n.comparedTo(po) >= 0) ? res.toString() : ''
   } else {
     // partie décimale : dixième, centième, millième
-    res = n.sub(n.div(pos * 10).trunc().mul(pos * 10))
-    res = res.div(pos).trunc()
+    res = n.sub(n.div(po.mul(10)).trunc().mul(po.mul(10)))
+    res = res.div(po).trunc()
     res = Math.abs(exp) <= n.decimalPlaces() ? res.toString() : ''
   }
   return res
 }
 
-function buildTab (a, uniteA, r, uniteR) {
+function buildTab (a, uniteA, r, uniteR, ligne = 2, force = false) {
   const tabRep = function (nbre, uniteNbre) {
     const res = ['', '', '', '', '', '', '', '', '', '', '']
     switch (uniteNbre.replaceAll(' ', '')) {
       case 'km':
-        res[0] = getDigitFromNumbers(nbre, 100)
-        res[1] = getDigitFromNumbers(nbre, 10)
-        res[2] = '\\color{red}{' + getDigitFromNumbers(nbre, 1) + (new Decimal(nbre).decimalPlaces() === 0 ? '' : ',') + '}'// km
-        res[3] = getDigitFromNumbers(nbre, 0.1)
-        res[4] = getDigitFromNumbers(nbre, 0.01)
-        res[5] = getDigitFromNumbers(nbre, 0.001)
-        res[6] = getDigitFromNumbers(nbre, 0.0001)
-        res[7] = getDigitFromNumbers(nbre, 0.00001)
-        res[8] = getDigitFromNumbers(nbre, 0.000001)
+        for (let i = 0; i <= 10; i++) {
+          res[i] = (2 - i === 0 ? '\\color{red}{' : '') + getDigitFromNumber(nbre, Decimal.pow(10, 2 - i)) + (2 - i === 0 ? (new Decimal(nbre).decimalPlaces() === 0 ? '}' : ',}') : '')
+        }
         break
       case 'hm':
-        res[0] = getDigitFromNumbers(nbre, 1000)
-        res[1] = getDigitFromNumbers(nbre, 100)
-        res[2] = getDigitFromNumbers(nbre, 10) // km
-        res[3] = '\\color{red}{' + getDigitFromNumbers(nbre, 1) + (new Decimal(nbre).decimalPlaces() === 0 ? '' : ',') + '}'
-        res[4] = getDigitFromNumbers(nbre, 0.1)
-        res[5] = getDigitFromNumbers(nbre, 0.01)
-        res[6] = getDigitFromNumbers(nbre, 0.001)
-        res[7] = getDigitFromNumbers(nbre, 0.0001)
-        res[8] = getDigitFromNumbers(nbre, 0.00001)
+        for (let i = 0; i <= 10; i++) {
+          res[i] = (3 - i === 0 ? '\\color{red}{' : '') + getDigitFromNumber(nbre, Decimal.pow(10, 3 - i)) + (3 - i === 0 ? (new Decimal(nbre).decimalPlaces() === 0 ? '}' : ',}') : '')
+        }
         break
       case 'dam':
-        res[0] = getDigitFromNumbers(nbre, 10000)
-        res[1] = getDigitFromNumbers(nbre, 1000)
-        res[2] = getDigitFromNumbers(nbre, 100) // km
-        res[3] = getDigitFromNumbers(nbre, 10)
-        res[4] = '\\color{red}{' + getDigitFromNumbers(nbre, 1) + (new Decimal(nbre).decimalPlaces() === 0 ? '' : ',') + '}'
-        res[5] = getDigitFromNumbers(nbre, 0.1)
-        res[6] = getDigitFromNumbers(nbre, 0.01)
-        res[7] = getDigitFromNumbers(nbre, 0.001)
-        res[8] = getDigitFromNumbers(nbre, 0.0001)
+        for (let i = 0; i <= 10; i++) {
+          res[i] = (4 - i === 0 ? '\\color{red}{' : '') + getDigitFromNumber(nbre, Decimal.pow(10, 4 - i)) + (4 - i === 0 ? (new Decimal(nbre).decimalPlaces() === 0 ? '}' : ',}') : '')
+        }
         break
       case 'm':
-        res[0] = getDigitFromNumbers(nbre, 100000)
-        res[1] = getDigitFromNumbers(nbre, 10000)
-        res[2] = getDigitFromNumbers(nbre, 1000) // km
-        res[3] = getDigitFromNumbers(nbre, 100)
-        res[4] = getDigitFromNumbers(nbre, 10)
-        res[5] = '\\color{red}{' + getDigitFromNumbers(nbre, 1) + (new Decimal(nbre).decimalPlaces() === 0 ? '' : ',') + '}'
-        res[6] = getDigitFromNumbers(nbre, 0.1)
-        res[7] = getDigitFromNumbers(nbre, 0.01)
-        res[8] = getDigitFromNumbers(nbre, 0.001)
-        res[9] = getDigitFromNumbers(nbre, 0.0001)
-        res[10] = getDigitFromNumbers(nbre, 0.00001)
+        for (let i = 0; i <= 10; i++) {
+          res[i] = (5 - i === 0 ? '\\color{red}{' : '') + getDigitFromNumber(nbre, Decimal.pow(10, 5 - i)) + (5 - i === 0 ? (new Decimal(nbre).decimalPlaces() === 0 ? '}' : ',}') : '')
+        }
         break
       case 'dm':
-        res[0] = getDigitFromNumbers(nbre, 1000000)
-        res[1] = getDigitFromNumbers(nbre, 100000)
-        res[2] = getDigitFromNumbers(nbre, 10000) // km
-        res[3] = getDigitFromNumbers(nbre, 1000)
-        res[4] = getDigitFromNumbers(nbre, 100)
-        res[5] = getDigitFromNumbers(nbre, 10)
-        res[6] = '\\color{red}{' + getDigitFromNumbers(nbre, 1) + (new Decimal(nbre).decimalPlaces() === 0 ? '' : ',') + '}'
-        res[7] = getDigitFromNumbers(nbre, 0.1)
-        res[8] = getDigitFromNumbers(nbre, 0.01)
-        res[9] = getDigitFromNumbers(nbre, 0.001)
-        res[10] = getDigitFromNumbers(nbre, 0.0001)
+        for (let i = 0; i <= 10; i++) {
+          res[i] = (6 - i === 0 ? '\\color{red}{' : '') + getDigitFromNumber(nbre, Decimal.pow(10, 6 - i)) + (6 - i === 0 ? (new Decimal(nbre).decimalPlaces() === 0 ? '}' : ',}') : '')
+        }
         break
       case 'cm':
-        res[0] = getDigitFromNumbers(nbre, 10000000)
-        res[1] = getDigitFromNumbers(nbre, 1000000)
-        res[2] = getDigitFromNumbers(nbre, 100000) // km
-        res[3] = getDigitFromNumbers(nbre, 10000)
-        res[4] = getDigitFromNumbers(nbre, 1000)
-        res[5] = getDigitFromNumbers(nbre, 100)
-        res[6] = getDigitFromNumbers(nbre, 10)
-        res[7] = '\\color{red}{' + getDigitFromNumbers(nbre, 1) + (new Decimal(nbre).decimalPlaces() === 0 ? '' : ',') + '}'
-        res[8] = getDigitFromNumbers(nbre, 0.1)
-        res[9] = getDigitFromNumbers(nbre, 0.01)
-        res[10] = getDigitFromNumbers(nbre, 0.001)
+        for (let i = 0; i <= 10; i++) {
+          res[i] = (7 - i === 0 ? '\\color{red}{' : '') + getDigitFromNumber(nbre, Decimal.pow(10, 7 - i)) + (7 - i === 0 ? (new Decimal(nbre).decimalPlaces() === 0 ? '}' : ',}') : '')
+        }
         break
       case 'mm':
-        res[0] = getDigitFromNumbers(nbre, 100000000)
-        res[1] = getDigitFromNumbers(nbre, 10000000)
-        res[2] = getDigitFromNumbers(nbre, 1000000) // km
-        res[3] = getDigitFromNumbers(nbre, 100000)
-        res[4] = getDigitFromNumbers(nbre, 10000)
-        res[5] = getDigitFromNumbers(nbre, 1000)
-        res[6] = getDigitFromNumbers(nbre, 100)
-        res[7] = getDigitFromNumbers(nbre, 10)
-        res[8] = '\\color{red}{' + getDigitFromNumbers(nbre, 1) + (new Decimal(nbre).decimalPlaces() === 0 ? '' : ',') + '}'
-        res[9] = getDigitFromNumbers(nbre, 0.1)
-        res[10] = getDigitFromNumbers(nbre, 0.01)
+        for (let i = 0; i <= 10; i++) {
+          res[i] = (8 - i === 0 ? '\\color{red}{' : '') + getDigitFromNumber(nbre, Decimal.pow(10, 8 - i)) + (8 - i === 0 ? (new Decimal(nbre).decimalPlaces() === 0 ? '}' : ',}') : '')
+        }
         break
     }
     return res
   }
+  const createTab = function (aT, rT, first, end, ligne, force) {
+    let texte = '$\\def\\arraystretch{1.5}\\begin{array}{'
+    for (let i = first; i <= end; i++) {
+      texte += '|c' + (i === end ? '|}' : '')
+    }
+    const headers = ['\\hspace*{0.6cm}', '\\hspace*{0.6cm}', '\\; km \\;', '\\; hm \\;', 'dam', '\\;\\; m \\;\\;', '\\; dm \\;', '\\; cm \\;', '\\;mm\\;', '\\hspace*{0.6cm}', '\\hspace*{0.6cm}']
+    texte += '\\hline '
+    for (let i = first; i <= end; i++) {
+      texte += `${headers[i]} ${i < end ? '&' : '\\\\'}`
+    }
+    texte += '\\hline '
+    for (let i = first; i <= end; i++) {
+      texte += `${aT[i]} ${i < end ? '&' : '\\\\'}`
+    }
+    texte += '\\hline '
+    for (let i = first; i <= end; i++) {
+      texte += `${rT[i]} ${i < end ? '&' : '\\\\'}`
+    }
+    for (let k = 3; k <= ligne; k++) {
+      texte += '\\hline '
+      for (let i = first; i <= end; i++) {
+        texte += `  ${i < end ? '&' : '\\\\'}`
+      }
+    }
+    texte += '\\hline \\end{array}$'
+    return texte
+  }
   const aTab = tabRep(a, uniteA)
   const rTab = tabRep(r, uniteR)
-  const texte = `$\\def\\arraystretch{1.5}\\begin{array}{|c|c|c|c|c|c|c|c|c|c|c|}
-  \\hline
-  \\;\\;\\;\\; & \\;\\;\\;\\; & \\; km \\; & \\; hm  \\; & dam & \\;\\; m \\;\\; & \\; dm \\; & \\; cm \\; & \\; mm\\; & \\;\\;\\;\\; & \\;\\;\\;\\; \\\\
-  \\hline
-  ${aTab[0]} & ${aTab[1]}& ${aTab[2]} & ${aTab[3]}& ${aTab[4]} & ${aTab[5]}& ${aTab[6]} & ${aTab[7]} & ${aTab[8]} & ${aTab[9]} & ${aTab[10]}\\\\
-  \\hline
-  ${rTab[0]} & ${rTab[1]}& ${rTab[2]} & ${rTab[3]}& ${rTab[4]} & ${rTab[5]}& ${rTab[6]} & ${rTab[7]} & ${rTab[8]} & ${rTab[9]} & ${aTab[10]}\\\\
-  \\hline
-  \\end{array}$`
+  const minTab1 = aTab[0] !== '' || aTab[1] !== '' || force ? 0 : 2
+  const minTab2 = rTab[0] !== '' || rTab[1] !== '' || force ? 0 : 2
+  const maxTab1 = aTab[9] !== '' || aTab[10] !== '' || force ? 10 : 8
+  const maxTab2 = rTab[9] !== '' || rTab[10] !== '' || force ? 10 : 8
+  const texte = createTab(aTab, rTab, Math.min(minTab1, minTab2), Math.max(maxTab1, maxTab2), ligne, force)
   return texte
 }
