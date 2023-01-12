34b582af7aa4e1bccdf50796c0a7b0937d8ba21c
moragi <gilles.mora@ac-rouen.fr>
2023-01-12 09:33:48 +0100

    fix : techno1P4

--- src/js/exercices/techno1/techno1P4.js
+++ src/js/exercices/techno1/techno1P4.js
@@ -27,50 +27,48 @@ export default function ProportiondeProportion () {
         a = randint(20, 50)/* Valeur */
         c = randint(20, 50)/* Dénominateur fraction */
         d = randint(2, 15)/* Multiple */
-        this.question = `Dans une association,  $${b}\\%$ des adhérents ont plus de  de $${a}$ ans. <br>
-        parmi eux,   $${c}\\%$ à plus de $${d}$ années d'ancienneté.<br>
+        this.question = `Dans une association,  $${b}\\,\\%$ des adhérents ont plus de  de $${a}$ ans. <br>
+        Parmi eux,   $${c}\\,\\%$ ont plus de $${d}$ années d'ancienneté.<br>
         Quel est le pourcentage d'adhérents de plus de  $${a}$ ans ayant plus de $${d}$ années d'ancienneté ?`
         this.correction = `La population de référence est celle des membres du club.<br>
         La première sous-population est celle des plus de $${a}$ ans, 
-        qui représente $p_1=${b}\\%$ de la population de référence. <br>
-        Dans cette sous-population, on sait que la population de ceux qui ont plus de $${d}$ années d'ancienneté représente $p_2=${c}\\%$.<br>
-        D'après le cours, on calcule $p=p_1\\times p_2$, ce qui rerésente $${b}\\%$ de $${c}\\%$.<br>
+        qui représente $p_1=${b}\\,\\%$ de la population de référence. <br>
+        Dans cette sous-population, on sait que la population de ceux qui ont plus de $${d}$ années d'ancienneté représente $p_2=${c}\\,\\%$.<br>
+        D'après le cours, on calcule $p=p_1\\times p_2$, ce qui rerésente $${b}\\,\\%$ de $${c}\\,\\%$.<br>
       <br>Ainsi,  $p=${texNombrec(b / 100)}\\times ${texNombrec(c / 100)}=${texNombrec(b * c / 10000)}$.<br>
-      Il y a donc $${texNombrec(b * c / 100)}\\%$ d'adhérents de plus de  $${a}$ ans ayant plus de $${b}$ années d'ancienneté.`
+      Il y a donc $${texNombrec(b * c / 100)}\\%$ d'adhérents de plus de  $${a}$ ans ayant plus de $${a}$ années d'ancienneté.`
         this.reponse = calcul(b * c / 100)
         break
       case 'lycée':
         b = randint(20, 40)/* Pourcentage */
         a = randint(20, 50)/* Valeur */
         c = randint(10, 70)/* Dénominateur fraction */
-        this.question = `Dans une lycée,  $${b}\\%$ des lycéens sont en classe de 1ère. <br>
-            Parmi eux,   $${c}\\%$ sont en filière technologique.<br>
+        this.question = `Dans une lycée,  $${b}\\,\\%$ des lycéens sont en classe de 1ère. <br>
+            Parmi eux,   $${c}\\,\\%$ sont en filière technologique.<br>
             Quel est le pourcentage d'élèves en 1ère technologique de ce lycée ?`
         this.correction = `La population de référence est celle des élèves du lycée.<br>
-            La sous-population est celle des élèves de 1ères.<br>
-            d'après l'énoncé, $p_1=${b}\\%$.<br>
-             Les élèves de 1ère technologique sont une sous-population des élèves de 1ère, qui représente d'après l'énnoncé d'après l'énoncé, $p_2=${c}\\%$. <br>
+            La sous-population est celle des élèves de 1ères et d'après l'énoncé, $p_1=${b}\\,\\%$.<br>
+             Les élèves de 1ère technologique sont une sous-population des élèves de 1ère, qui représente d'après l'énnoncé d'après l'énoncé, $p_2=${c}\\,\\%$. <br>
              <br>Pour connaître la proportion $p$ des élèves de 1ère technologique par rapport à la population de référence (les élèves du lycée), <br>on sait que : $p=p_1\\times p_2$.<br>
-             ce qui revient à calculer $${b}\\%$ de $${c}\\%$.<br>
+             Ce qui revient à calculer $${b}\\,\\%$ de $${c}\\,\\%$.<br>
              <br>Ainsi, $p=${texNombrec(b / 100)}\\times ${texNombrec(c / 100)}=${texNombrec(b * c / 10000)}$.<br>
-             Il y a $${texNombrec(b * c / 100)}\\%$ d'élèves de 1ère technologique parmi les élèves du lycée.`
+             Il y a $${texNombrec(b * c / 100)}\\,\\%$ d'élèves de 1ère technologique parmi les élèves du lycée.`
         this.reponse = calcul(b * c / 100)
         break
       case 'election':
         b = randint(40, 80)/* Pourcentage */
         a = randint(20, 50)/* Valeur */
         c = randint(10, 70)/* Dénominateur fraction */
-        this.question = `Lors d'une élection,  la participation (suffrages exprimés) a été de $${b}\\%$ des inscrits.<br>
-               Un candidat a obtenu   $${c}\\%$ des suffrages exprimés.<br>
+        this.question = `Lors d'une élection,  la participation (suffrages exprimés) a été de $${b}\\,\\%$ des inscrits.<br>
+               Un candidat a obtenu   $${c}\\,\\%$ des suffrages exprimés.<br>
                 Quel est le pourcentage de voix obtenues par ce candidat par rapport au nombre d' inscrits ?`
         this.correction = `La population de référence est celle des inscrits sur les listes électorales.<br>
-                La sous-population est celle des suffrages exprimés.<br>
-                d'après l'énoncé, $p_1=${b}\\%$.<br>
-                 Les suffrages du candidat sont une sous-population des suffrages exprimés, qui représentent d'après l'énnoncé d'après l'énoncé, $p_2=${c}\\%$. <br>
+                La sous-population est celle des suffrages exprimés et d'après l'énoncé, $p_1=${b}\\,\\%$.<br>
+                 Les suffrages du candidat sont une sous-population des suffrages exprimés, qui représentent d'après l'énoncé, $p_2=${c}\\,\\%$. <br>
                  <br>Pour connaître le pourcentage de voix obtenues  par ce candidat par rapport aux nombre d'inscrits, on sait que : $p=p_1\\times p_2$.<br>
-                 ce qui revient à calculer $${b}\\%$ de $${c}\\%$.<br>
+                 ce qui revient à calculer $${b}\\,\\%$ de $${c}\\,\\%$.<br>
                  <br>Ainsi, $p=${texNombrec(b / 100)}\\times ${texNombrec(c / 100)}=${texNombrec(b * c / 10000)}$.<br>
-                Ce candidat a donc obtenu $${texNombrec(b * c / 100)}\\%$ des voix des inscrits.`
+                Ce candidat a donc obtenu $${texNombrec(b * c / 100)}\\,\\%$ des voix des inscrits.`
         this.reponse = calcul(b * c / 100)
         break
     }
