ebd2ad6d83381f6327807e0d1af9ed2b727c6673
moragi <gilles.mora@ac-rouen.fr>
2023-01-18 17:30:18 +0100

    fix : can3C07 prob diap label

--- src/js/exercices/can/3e/can3C07.js
+++ src/js/exercices/can/3e/can3C07.js
@@ -1,7 +1,7 @@
 import Exercice from '../../Exercice.js'
 import { mathalea2d } from '../../../modules/2dGeneralites.js'
-import { randint, texNombrec, calcul, texNum } from '../../../modules/outils.js'
-import { point, segmentAvecExtremites, codageSegments, labelPoint, texteParPosition } from '../../../modules/2d.js'
+import { randint, texNombrec, calcul, stringNombre } from '../../../modules/outils.js'
+import { point, segmentAvecExtremites, codageSegments, texteParPosition } from '../../../modules/2d.js'
 export const titre = 'Calculer l’abscisse d’un milieu'
 export const interactifReady = true
 export const interactifType = 'mathLive'
@@ -28,10 +28,12 @@ export default function MilieuEntre1EtFraction () {
     const B = point(4, 0, 'M', 'below')
     const C = point(8, 0)
     const objets = []
-    objets.push(segmentAvecExtremites(A, B), segmentAvecExtremites(B, C), labelPoint(B), codageSegments('||', 'blue', A, B, B, C))
-    objets.push(texteParPosition(`$${texNum(a)}$`, 0, -1, 'milieu', 'black', 1, 'middle', true)
+    objets.push(segmentAvecExtremites(A, B), segmentAvecExtremites(B, C), codageSegments('||', 'blue', A, B, B, C))
+    objets.push(texteParPosition(`${stringNombre(a)}`, 0, -0.7, 'milieu', 'black', 1.5)
     )
-    objets.push(texteParPosition(`$${texNum(b)}$`, 8, -1, 'milieu', 'black', 1, 'middle', true)
+    objets.push(texteParPosition('M', 4, -0.7, 'milieu', 'black', 1.5)
+    )
+    objets.push(texteParPosition(`${stringNombre(b, 1)}`, 8, -0.7, 'milieu', 'black', 1.5)
     )
     this.question = `Donner l'abscisse du point $M$ sous forme décimale.<br>
     
@@ -41,10 +43,10 @@ export default function MilieuEntre1EtFraction () {
       ymin: -2,
       xmax: 10,
       ymax: 1,
-      pixelsParCm: 20,
+      pixelsParCm: 30,
       mainlevee: false,
       amplitude: 0.5,
-      scale: 0.7,
+      scale: 0.6,
       style: 'margin: auto'
     }, objets)
     this.question += '<br>'
