e28c1ff6f2615e3764b5b19f0e703611f9bbd69e
moragi <gilles.mora@ac-rouen.fr>
2023-02-28 18:06:42 +0100

    fix can6C01 et canc3N06 plusieurs questions

--- src/js/exercices/can/6e/can6C01.js
+++ src/js/exercices/can/6e/can6C01.js
@@ -22,57 +22,64 @@ export default function OrdreDeGrandeur () {
   this.nouvelleVersion = function () {
     this.listeQuestions = []
     this.listeCorrections = []
-    const a = randint(3, 7)
-    const b = randint(2, 9)
-    const c = randint(1, 9)
-    const d = randint(5, 9)
-    const resultat = calcul((a * 100 + b * 10 + c) * d)
-    let texte = `$${texNombrec(a * 100 + b * 10 + c)}\\times ${d}$<br> 
+    for (let i = 0, cpt = 0; i < this.nbQuestions && cpt < 50;) {
+      const a = randint(3, 7)
+      const b = randint(2, 9)
+      const c = randint(1, 9)
+      const d = randint(5, 9)
+      const resultat = calcul((a * 100 + b * 10 + c) * d)
+      let texte = `$${texNombrec(a * 100 + b * 10 + c)}\\times ${d}$<br> 
     
     Choisir la bonne réponse sans effectuer précisément le calcul.<br>`
-    // Ajout avant l'ajout des propositions de réponse
-    // ça serait mieux en uniformisant avec this.question pour tous les exos can
-    this.canEnonce = texte
-    this.autoCorrection[0] = {
-      enonce: texte,
-      propositions: [
-        {
-          texte: `$${texNombre(resultat)}$`,
-          statut: true
-        },
-        {
-          texte: `$${texNombrec(d * 1000 + a * 100 + b * 10 + c)}$`,
-          statut: false
-        },
-        {
-          texte: `$${texNombrec((a * 1000 + b * 100 + c) * d)}$`,
-          statut: false
-        }
-      ]
-    }
-    const monQcm = propositionsQcm(this, 0)
-    if (!context.isAmc) {
-      texte += monQcm.texte
-    }
-    let texteCorr = `$${texNombrec(a * 100 + b * 10 + c)} \\times ${d} = ${texNombre(resultat)}$<br>
+      // Ajout avant l'ajout des propositions de réponse
+      // ça serait mieux en uniformisant avec this.question pour tous les exos can
+      this.canEnonce = texte
+      this.autoCorrection[0] = {
+        enonce: texte,
+        propositions: [
+          {
+            texte: `$${texNombre(resultat)}$`,
+            statut: true
+          },
+          {
+            texte: `$${texNombrec(d * 1000 + a * 100 + b * 10 + c)}$`,
+            statut: false
+          },
+          {
+            texte: `$${texNombrec((a * 1000 + b * 100 + c) * d)}$`,
+            statut: false
+          }
+        ]
+      }
+      const monQcm = propositionsQcm(this, 0)
+      if (!context.isAmc) {
+        texte += monQcm.texte
+      }
+      let texteCorr = `$${texNombrec(a * 100 + b * 10 + c)} \\times ${d} = ${texNombre(resultat)}$<br>
         `
-    if (a * 100 + b * 10 + c > a * 100 + 50) {
-      texteCorr += texteEnCouleur(`
+      if (a * 100 + b * 10 + c > a * 100 + 50) {
+        texteCorr += texteEnCouleur(`
     Mentalement : <br>
 On remplace le premier facteur $${a * 100 + b * 10 + c}$ par $${(a + 1) * 100}$, on calcule
 $${(a + 1) * 100}\\times ${d}=${((a + 1) * 100) * d}$ et on sélectionne le résultat qui s'en rapproche le plus. 
     `)
-    } else {
-      texteCorr += texteEnCouleur(`
+      } else {
+        texteCorr += texteEnCouleur(`
     Mentalement : <br>
     On remplace le premier facteur $${a * 100 + b * 10 + c}$ par $${a * 100}$, on calcule
     $${a * 100}\\times ${d}=${a * 100 * d}$ et on sélectionne le résultat qui s'en rapproche le plus. 
            `)
+      }
+      if (this.listeQuestions.indexOf(texte) === -1) {
+      // Si la question n'a jamais été posée, on en crée une autre
+        this.listeQuestions.push(texte)
+        this.listeCorrections.push(texteCorr)
+        if (i === 0) this.canReponseACompleter = monQcm.texte // FIXME Dans un exercice permettant plusieurs questions il n'y a qu'un this.canReponseACompleter ???
+        i++
+      }
+      cpt++
+      this.canReponseACompleter = monQcm.texte
     }
-    this.listeQuestions.push(texte)
-    this.listeCorrections.push(texteCorr)
     listeQuestionsToContenu(this)
-    // Ajout en dernier
-    this.canReponseACompleter = monQcm.texte
   }
 }
